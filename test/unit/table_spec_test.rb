e8b196e81716f1913e318e99c99ab17951174bce
Greg Lawson <GregLawson123@gmail.com>
2011-03-15 02:09:47 -0700

    Cleaned up association verification code.

--- test/unit/table_spec_test.rb
+++ test/unit/table_spec_test.rb
@@ -32,15 +32,22 @@ def assert_association(klass,ass)
 	assert_respond_to(klass,(ass.to_s+'=').to_sym)
 	assert(is_association?(klass,ass))
 end #def
+def assert_association_to_many(klass,ass)
+	assert_association(klass,ass)
+	assert(is_association_to_many?(klass,ass))
+end #def
+def assert_association_to_one(klass,ass)
+	assert_association(klass,ass)
+	assert(!is_association_to_many?(klass,ass))
+end #def
 def assert_include(element,list)
 	assert(list.include?(element),"#{element.inspect} is not in list #{list.inspect}")
 end #def
 def is_association_to_many?(klass,ass)
-	if klass.respond_to?(ass) and klass.respond_to?(ass+'=')  and klass.respond_to?(ass+'_ids') and klass.respond_to?(ass+'_ids=') then
-		#~ assert_public_instance_method(klass,ass)
-		#~ assert_public_instance_method(klass,ass+'=') 
-		#~ assert_public_instance_method(klass,ass+'_ids') 
-		#~ assert_public_instance_method(klass,ass+'_ids=')
+	if is_association?(klass,ass)  and klass.respond_to?((ass.to_s+'_ids').to_sym) and klass.respond_to?((ass.to_s+'_ids=').to_sym) then
+	assert_association(klass,ass)
+	assert_public_instance_method(klass,(ass.to_s+'_ids').to_sym) 
+		assert_public_instance_method(klass,(ass.to_s+'_ids=').to_sym)
 		return true
 	else
 		return false
@@ -54,7 +61,11 @@ def define_association_names
 	puts " @fixture_names.inspect=#{ @fixture_names.inspect}"
 	@my_fixture=eval(@table_name+'(:one)')
  	@possible_associations=@model_class.instance_methods(false).select { |m| m =~ /=$/ and !(m =~ /_ids=$/) and is_association?(@my_fixture,m[0..-2])}.collect {|m| m[0..-2] }
+	puts "@possible_associations.inspect=#{@possible_associations.inspect}"
+
 	puts "@model_class.column_names=#{@model_class.column_names.inspect}"
+ 	@possible_many_associations=@model_class.instance_methods(false).select { |m| (m =~ /_ids=$/) and is_association_to_many?(@my_fixture,m[0..-2])}.collect {|m| m[0..-2] }
+	puts "@possible_many_associations.inspect=#{@possible_many_associations.inspect}"
 	@content_column_names=@model_class.content_columns.collect {|m| m.name}
 	puts "@content_column_names.inspect=#{@content_column_names.inspect}"
 	@special_columns=@model_class.column_names-@content_column_names
@@ -65,6 +76,15 @@ end
 def setup
 	define_association_names
 end
+test "general associations" do
+	@possible_associations.each do |ass|
+		if is_association_to_many?(@my_fixture,ass) then
+			 assert_association_to_many(@my_fixture,ass)
+		else
+			assert_association_to_one(@my_fixture,ass)
+		end
+	end
+end #test
 test "specific, stable and working" do
 	assert_equal(@model_name,'TableSpec')
 	assert_equal(@model_class,TableSpec)
@@ -82,16 +102,21 @@ test "specific, stable and working" do
 	assert_equal(@possible_foreign_keys,['frequency_id'])
 	assert_raise(Test::Unit::AssertionFailedError) {assert_public_instance_method(table_specs(:one),:acquisition_interfaces) }
 	assert_raise(Test::Unit::AssertionFailedError) { assert_public_instance_method(table_specs(:one),:cabbage) }
+	assert_public_instance_method(table_specs(:one),:acquisition_stream_specs)
+	testCall(table_specs(:one),:acquisition_stream_specs)
 end #def
 test "association empty" do
-	assert(acquisition_stream_specs.empty?,"table_specs(:one)=#{table_specs(:one)} but acquisition_stream_specs not associated with #{acquisition_stream_specs(:one).inspect}")
+	message="table_specs(:one).inspect=#{table_specs(:one).inspect} but acquisition_stream_specs not associated with #{acquisition_stream_specs(:one).inspect}"
+	assert_not_nil(acquisition_stream_specs,message)
+#	assert_equal(@my_fixture.acquisition_stream_specs,[],message)
+	assert_operator(@my_fixture.acquisition_stream_specs.count,:>,0,"count "+message)
+	assert_operator(@my_fixture.acquisition_stream_specs.length,:>,0,"length "+message)
+	assert(!@my_fixture.acquisition_stream_specs.empty?,"empty "+message)
 end
 def test_aaa_test_assertions # aaa to output first
 	
 #	assert_equal([:table_spec_id,:acquisition_stream_spec_id],@possible_foreign_keys)
 #	http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html
-	assert(@model_class.instance_methods(false).include?('acquisition_stream_specs'))
-
 #	puts "self.class.instance_methods(false)=#{self.class.instance_methods(false).inspect}"
 
 #	puts " @loaded_fixtures['table_specs'].inspect=#{ @loaded_fixtures['table_specs'].inspect}"
@@ -103,20 +128,14 @@ def test_aaa_test_assertions # aaa to output first
 	puts " @loaded_fixtures['table_specs'].at(0).at(1).model_class.inspect=#{ @loaded_fixtures['table_specs'].at(0).at(1).model_class.inspect}"
 	
 #	assert_fixture_name(self,@model_name.to_sym)
-#	assert_public_instance_method(table_specs(:one),:acquisition_stream_specs)
-#	table_specs(:one).acquisition_interface_id=1 # kludge
 #	assert_association(AcquisitionStreamSpec,@model_class)
-#	assert_equal(table_specs(:one).acquisition_interface_id,acquisition_interfaces(:one).id)
-#	assert_equal(table_specs(:one).scheme,acquisition_interfaces(:one).scheme)
-#	assert_equal(table_specs(:one).scheme,acquisition_stream_specs(:one).acquisition_interface.scheme)
-#	testCall(table_specs(:one),:acquisition_stream_interfaces)
 end
 def test_id_equal
 		assert_equal(Fixtures::identify(:one),table_specs(:one).id,"identify != id")
 end #def
 def test_associated_id_equal
-	puts "table_specs(:one).inspect=#{table_specs(:one).inspect}"
-	puts "table_specs(:one).acquisition_stream_specs.inspect=#{table_specs(:one).acquisition_stream_specs.inspect}"
-	assert_equal(Fixtures::identify(:one),table_specs(:one).acquisition_stream_specs.first.table_spec_id,"identify != acquisition_stream_specs.first.table_spec_id")
+	puts "@my_fixture.inspect=#{@my_fixture.inspect}"
+	puts "@my_fixture.acquisition_stream_specs.inspect=#{@my_fixture.acquisition_stream_specs.inspect}"
+	assert_equal(Fixtures::identify(:one),@my_fixture.acquisition_stream_specs.first.table_spec_id,"identify != acquisition_stream_specs.first.table_spec_id")
 end #def
 end
