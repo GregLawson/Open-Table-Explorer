59aefa606925837b5915036c39b368f9bb552f01
Greg Lawson <GregLawson123@gmail.com>
2011-03-24 16:23:04 -0700

    Added new rake testing task for incremental testing and logging results.

--- changeTest.sh
+++
@@ -1,8 +0,0 @@
-pushd test
-ruby unit/acquisition_interface_test.rb 
-ruby unit/acquisition_stream_spec_test.rb 
-ruby unit/acquisition_test.rb 
-#ruby functional/acquisition_controller_test.rb 
-ruby functional/acquisition_stream_specs_controller_test.rb 
-ruby functional/acquisition_interfaces_controller_test.rb 
-popd
