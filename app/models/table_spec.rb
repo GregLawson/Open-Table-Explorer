86457d390fe8aa1b3a0193dcecf3cf6b2a0e1dd5
Greg Lawson <GregLawson123@gmail.com>
2011-03-27 10:16:44 -0700

    Logical primary key added.

--- app/models/table_spec.rb
+++ app/models/table_spec.rb
@@ -5,30 +5,5 @@ include Global
 def logical_primary_key
 	return model_class_name
 end #def
-def table2yaml(table_name)
-	primaryKeyValue=logical_primary_key
-#	primaryKey.to_s
-	i = "000"
-	limit=100
-	sql  = "SELECT * FROM %s LIMIT #{limit}"
-    	File.open("#{table_name}.yml.gen", 'w') do |file|
-      		data = ActiveRecord::Base.connection.select_all(sql % table_name)
-		puts "data.inspect=#{data.inspect}"
-		 file.write data.inject({}) { |hash, record|
-#			puts "#{primaryKeyExpression}.inspect=#{primaryKeyValue.inspect}"
-			record.delete('id') 
-			
-			puts "record.inspect=#{record.inspect}"
-			hash[primaryKeyValue] = record
-			hash
-		}.to_yaml
-	end# file open
-end #def
-def self.db2yaml
-	skip_tables = ["schema_info","tedprimaries","weathers"]
-  (	ActiveRecord::Base.connection.tables - skip_tables).each do |table_name|
-		table2yaml(table_name)
-	end #each
-end #def
 
 end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,4 +1,7 @@
 class Transfer < ActiveRecord::Base
 belongs_to :account
 include Global
+def logical_primary_key
+	return id
+end #def
 end
