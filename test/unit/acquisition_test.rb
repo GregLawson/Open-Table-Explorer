ef9ecbe397f1236149c83fdd5af740d3a31d3137
Greg Lawson <GregLawson123@gmail.com>
2011-04-08 08:39:53 -0700

    Update test logs.

--- test/unit/acquisition_test.rb
+++ test/unit/acquisition_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class AcquisitionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+def setup
+	define_association_names
 end
+def test_general_associations
+	assert_general_associations(@table_name)
+end
+def test_id_equal
+	@my_fixtures.each do |my_fixture|
+		assert_equal(Fixtures::identify(my_fixture.model_class_name),my_fixture.id,"identify != id")
+	end
+end #def
+test "specific, stable and working" do
+end #test
+def test_associatons
+	assert_public_instance_method(acquisition_stream_specs(:one),:acquisition_interface)
+	assert_public_instance_method(acquisition_stream_specs(:one),:table_spec)
+end #def
+end #class
--- test/unit/table_spec_test.rb
+++ test/unit/table_spec_test.rb
@@ -6,6 +6,83 @@ class TableSpecTest < ActiveSupport::TestCase
   #~ fixtures :acquisitions
   #~ fixtures :table_specs
   #~ fixtures :frequencies
+def setup
+	define_association_names
+end
+def test_general_associations
+	assert_general_associations(@table_name)
+end
+def test_id_equal
+	@my_fixtures.each do |my_fixture|
+		assert_equal(Fixtures::identify(my_fixture.model_class_name),my_fixture.id,"identify != id")
+	end
+end #def
+def test_associated_id_equal
+#	puts "@my_fixture.inspect=#{@my_fixture.inspect}"
+#	puts "@my_fixture.acquisition_stream_specs.inspect=#{@my_fixture.acquisition_stream_specs.inspect}"
+	assert_instance_of(Array,fixtures('table_specs'))
+	assert_operator(fixtures('table_specs').length,:>,0,"fixtures('table_specs')=#{fixtures('table_specs')}")
+	fixtures('table_specs').each do |my_fixture|
+		my_fixture.acquisition_stream_specs.each do |ar|
+			assert_equal(Fixtures::identify(my_fixture.model_class_name),ar.table_spec_id,"identify != acquisition_stream_specs.first.table_spec_id")
+		end #each
+	end #each
+end #def
+test "association empty" do
+#	assert_not_nil(acquisition_stream_specs,message)
+	frequencies.each do |my_fixture|
+		puts "my_fixture.inspect=#{my_fixture.inspect}"
+		assert_instance_of(Array,@my_fixtures)
+		my_fixture=@my_fixtures[rk.to_sym]
+		puts "my_fixture.frequency_id=#{my_fixture.frequency_id}"
+		message="#{my_fixture.inspect} but frequency not associated with #{frequencies.inspect}"
+		assert_equal(frequencies(rk.to_sym).id,my_fixture.frequency_id)
+		assert_operator(my_fixture.frequency.count,:>,0,"count "+message)
+		assert_operator(my_fixture.frequency.length,:>,0,"length "+message)
+		assert(!my_fixture.frequency.empty?,"empty "+message)
+	end #each
+end #def
+test "specific, stable and working" do
+	assert_equal('TableSpec',@model_name)
+	assert_equal(TableSpec,@model_class)
+	assert_equal('table_specs',@table_name)
+	assert(TableSpec.instance_methods(false).include?('acquisition_stream_specs'),"TableSpec.instance_methods(false).include?('acquisition_stream_specs')")
+	assert_include('acquisition_stream_specs',TableSpec.instance_methods(false))
+	assert_raise(Test::Unit::AssertionFailedError) do
+		assert_include('acquisition_stream_specs_Not_a_method',TableSpec.instance_methods(false))
+	end #assert_raise
+	explain_assert_respond_to(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	assert_respond_to(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	assert_association(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	assert(is_association?(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs),"is_association?(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)")
+	assert_equal(Set.new(@possible_associations),Set.new(['acquisition_stream_specs',"frequency"]))
+	assert_equal(@possible_foreign_keys,['frequency_id'])
+	assert_raise(Test::Unit::AssertionFailedError) {assert_public_instance_method(table_specs(:MULTIPLE_WEATHER),:acquisition_interfaces) }
+	assert_raise(Test::Unit::AssertionFailedError) { assert_public_instance_method(table_specs(:MULTIPLE_WEATHER),:cabbage) }
+	assert_public_instance_method(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	testCall(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	assert_association(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	assert_association(table_specs(:MULTIPLE_WEATHER),:frequency)
+	assert_association_one_to_many(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
+	assert_association_many_to_one(table_specs(:MULTIPLE_WEATHER),:frequency)
+	assert_public_instance_method(acquisition_stream_specs(:one),:table_spec)
+	assert_raise(Test::Unit::AssertionFailedError) {assert_public_instance_method(acquisition_stream_specs(:one),:table_specs) }
+	assert_association(acquisition_stream_specs(:one),:table_spec)
+	assert_association_to_one(acquisition_stream_specs(:one),:table_spec)
+	assert_association_many_to_one(acquisition_stream_specs(:one),:table_spec)
+	@my_fixtures.each do |my_fixture|
+		assert_association_to_many(my_fixture,:acquisition_stream_specs)
+		assert_association_one_to_many(my_fixture,:acquisition_stream_specs)
+		assert_respond_to(my_fixture,:acquisition_stream_specs)
+		associated_records=testCallResult(my_fixture,:acquisition_stream_specs)
+		#~ associated_records.all? do |ar|
+			#~ assert_equal(my_fixture.id,associated_foreign_key_id(ar,:table_spec))
+		#~ end #each
+		assert_public_instance_method(acquisition_stream_specs(:one),:table_spec)
+		assert_association_many_to_one(my_fixture,:frequency)
+		assert_equal(["frequency_id"],foreign_key_names(my_fixture.class),"foreign_key_names(my_fixture.class)=#{foreign_key_names(my_fixture.class)}")
+	end #each
+end #test
 def find_fixture
 	#~ puts "name=#{Global.objectName(table_specs(:MULTIPLE_WEATHER))}"
 	#~ puts "class=#{Global.objectClass(table_specs(:MULTIPLE_WEATHER))}"
@@ -59,53 +136,6 @@ def find_fixture
 	#~ end #each
 #	puts "@possible_foreign_keys=#{@possible_foreign_keys.inspect}"	
 end #def
-test "specific, stable and working" do
-	assert_equal('TableSpec',@model_name)
-	assert_equal(TableSpec,@model_class)
-	assert_equal('table_specs',@table_name)
-	assert(TableSpec.instance_methods(false).include?('acquisition_stream_specs'),"TableSpec.instance_methods(false).include?('acquisition_stream_specs')")
-	assert_include('acquisition_stream_specs',TableSpec.instance_methods(false))
-	assert_raise(Test::Unit::AssertionFailedError) do
-		assert_include('acquisition_stream_specs_Not_a_method',TableSpec.instance_methods(false))
-	end #assert_raise
-	explain_assert_respond_to(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	assert_respond_to(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	assert_association(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	assert(is_association?(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs),"is_association?(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)")
-	assert_equal(Set.new(@possible_associations),Set.new(['acquisition_stream_specs',"frequency"]))
-	assert_equal(@possible_foreign_keys,['frequency_id'])
-	assert_raise(Test::Unit::AssertionFailedError) {assert_public_instance_method(table_specs(:MULTIPLE_WEATHER),:acquisition_interfaces) }
-	assert_raise(Test::Unit::AssertionFailedError) { assert_public_instance_method(table_specs(:MULTIPLE_WEATHER),:cabbage) }
-	assert_public_instance_method(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	testCall(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	assert_association(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	assert_association(table_specs(:MULTIPLE_WEATHER),:frequency)
-	assert_association_one_to_many(table_specs(:MULTIPLE_WEATHER),:acquisition_stream_specs)
-	assert_association_many_to_one(table_specs(:MULTIPLE_WEATHER),:frequency)
-	assert_public_instance_method(acquisition_stream_specs(:one),:table_spec)
-	assert_raise(Test::Unit::AssertionFailedError) {assert_public_instance_method(acquisition_stream_specs(:one),:table_specs) }
-	assert_association(acquisition_stream_specs(:one),:table_spec)
-	assert_association_to_one(acquisition_stream_specs(:one),:table_spec)
-	assert_association_many_to_one(acquisition_stream_specs(:one),:table_spec)
-	@my_fixtures.each do |my_fixture|
-		assert_association_to_many(my_fixture,:acquisition_stream_specs)
-		assert_association_one_to_many(my_fixture,:acquisition_stream_specs)
-		assert_respond_to(my_fixture,:acquisition_stream_specs)
-		associated_records=testCallResult(my_fixture,:acquisition_stream_specs)
-		#~ associated_records.all? do |ar|
-			#~ assert_equal(my_fixture.id,associated_foreign_key_id(ar,:table_spec))
-		#~ end #each
-		assert_public_instance_method(acquisition_stream_specs(:one),:table_spec)
-		assert_association_many_to_one(my_fixture,:frequency)
-		assert_equal(["frequency_id"],foreign_key_names(my_fixture.class),"foreign_key_names(my_fixture.class)=#{foreign_key_names(my_fixture.class)}")
-	end #each
-end #def
-def setup
-	define_association_names
-end
-def test_general_associations
-	assert_general_associations(@table_name)
-end
 def test_aaa_test_assertions # aaa to output first
 	assert_equal(@my_fixtures,fixtures('table_specs'))
 	find_fixture
@@ -128,34 +158,4 @@ def test_aaa_test_assertions # aaa to output first
 	
 #	assert_association(AcquisitionStreamSpec,@model_class)
 end
-def test_id_equal
-	@my_fixtures.each do |my_fixture|
-		assert_equal(Fixtures::identify(my_fixture.model_class_name),my_fixture.id,"identify != id")
-	end
-end #def
-def test_associated_id_equal
-#	puts "@my_fixture.inspect=#{@my_fixture.inspect}"
-#	puts "@my_fixture.acquisition_stream_specs.inspect=#{@my_fixture.acquisition_stream_specs.inspect}"
-	assert_instance_of(Array,fixtures('table_specs'))
-	assert_operator(fixtures('table_specs').length,:>,0,"fixtures('table_specs')=#{fixtures('table_specs')}")
-	fixtures('table_specs').each do |my_fixture|
-		my_fixture.acquisition_stream_specs.each do |ar|
-			assert_equal(Fixtures::identify(my_fixture.model_class_name),ar.table_spec_id,"identify != acquisition_stream_specs.first.table_spec_id")
-		end #each
-	end #each
-end #def
-test "association empty" do
-#	assert_not_nil(acquisition_stream_specs,message)
-	frequencies.each do |my_fixture|
-		puts "my_fixture.inspect=#{my_fixture.inspect}"
-		assert_instance_of(Array,@my_fixtures)
-		my_fixture=@my_fixtures[rk.to_sym]
-		puts "my_fixture.frequency_id=#{my_fixture.frequency_id}"
-		message="#{my_fixture.inspect} but frequency not associated with #{frequencies.inspect}"
-		assert_equal(frequencies(rk.to_sym).id,my_fixture.frequency_id)
-		assert_operator(my_fixture.frequency.count,:>,0,"count "+message)
-		assert_operator(my_fixture.frequency.length,:>,0,"length "+message)
-		assert(!my_fixture.frequency.empty?,"empty "+message)
-	end #each
-end #def
 end #class
