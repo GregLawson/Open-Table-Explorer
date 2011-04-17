22e18cec1454f34e3c7118ca984d364c2afba73c
Greg Lawson <GregLawson123@gmail.com>
2011-04-17 16:45:29 -0700

    transfer test 8336 assertions all pass.

--- test/unit/transfer_test.rb
+++ test/unit/transfer_test.rb
@@ -29,71 +29,31 @@ end #def
 def Transfer.account_join
 	@@transfers.join(@@accounts).on(@@transfers[:account_id].eq(@@accounts[:id])).project(:open_tax_solver_line,:amount)
 end #def
+def Transfer.ots_lines
+	@@accounts.project(:open_tax_solver_line)
+end #def
 def Transfer.open_tax_solver
-	transfers = arel
 	ots_data="Title:  US Federal 1040 Tax Form - 2010 - Generated\n\nStatus     Married/Joint {Single, Married/Joint, Head_of_House, Married/Sep, Widow(er)}\nDependents     2         {Number of Dependents, self=1, spouse, etc.}\n{Income}\n"
-	#~ transfers.account_join.each do |transfer|
-		#~ ots_data="#{ots_data} #{number_to_currency(transfer.amount).to_s}<BR>\n"
-	#~ end # each
+	ots_lines.each do |ots_line|
+		ots_data="#{ots_data} #{transfer.amount.to_s}<BR>\n"
+	end # each
 end #def
 end
 test "open_tax_solver" do
-#	transfers=fixtures('transfers').values.first.class.arel
-#	transfers=Arel::Table.new(:transfers)
 	transfers=Transfer.transfers
-	puts "transfers.columns=#{transfers.columns.collect {|col| col.name}.inspect}"
-	puts transfers.class
-	puts Transfer.class
 	
-	@relation = Arel::Table.new(:users)
-	sm = @relation.skip 2
-        puts sm.to_sql
-	assert_not_empty(sm.to_sql)
-	explain_assert_respond_to(transfers,:join)
-	explain_assert_respond_to(sm,:to_sql)
-	assert_kind_of(Arel::Table,transfers)
-	tm=transfers.skip 2
-	explain_assert_respond_to(tm,:to_sql)
-
-	assert_kind_of(Arel::SelectManager,transfers.skip(2))
-	assert_kind_of(Arel::SelectManager,transfers.project(:open_tax_solver_line,:amount))
-	assert_kind_of(Arel::SelectManager,transfers.join(accounts))
-	assert_kind_of(Arel::SelectManager,transfers.join(accounts).project(:open_tax_solver_line,:amount))
-	assert_kind_of(Arel::SelectManager,transfers.project(:open_tax_solver_line,:amount).join(accounts))
-
 	assert_kind_of(Arel::Table,Transfer.transfers)
 	assert_kind_of(Arel::SelectManager,Transfer.account_join)
-
-	explain_assert_respond_to(transfers.skip(2),:to_sql)
-	explain_assert_respond_to(transfers.project(:open_tax_solver_line,:amount),:to_sql)
-	explain_assert_respond_to(transfers.join(accounts),:to_sql)
-	explain_assert_respond_to(transfers.join(accounts).project(:open_tax_solver_line,:amount),:to_sql)
-	explain_assert_respond_to(transfers.project(:open_tax_solver_line,:amount).join(accounts),:to_sql)
-	explain_assert_respond_to(Transfer.account_join,:to_sql)
-
-	testCall(transfers.skip(2),:to_sql)
-	testCall(transfers.project(:open_tax_solver_line,:amount),:to_sql)
-	assert_equal(transfers.project(:open_tax_solver_line,:amount).to_sql,"SELECT open_tax_solver_line, amount FROM \"transfers\"")
-	
+	explain_assert_respond_to(Transfer.account_join,:to_sql)	
 	testCall(Transfer.account_join,:to_sql)
 	assert_equal(Transfer.account_join.to_sql,"SELECT open_tax_solver_line, amount FROM \"transfers\" INNER JOIN \"accounts\" ON \"transfers\".\"account_id\" = \"accounts\".\"id\"")
-
-	#~ assert_raise(TypeError,testCall(transfers.project(:open_tax_solver_line,:amount).join(accounts),:to_sql))
-	#~ assert_raise(TypeError,testCall(transfers.join(accounts).project(:open_tax_solver_line,:amount),:to_sql))
-	#~ assert_raise(TypeError,testCall(transfers.join(accounts),:to_sql))
 
 	testCall(Transfer.joins(accounts),:to_sql)
 
 	relational_algebra=Transfer.account_join
-#	relational_algebra=transfers.join(accounts).project(:open_tax_solver_line,:amount)
 	testCall(relational_algebra,:to_sql)
 	
-	explain_assert_respond_to(transfers.project(:open_tax_solver_line,:amount).join(accounts),:to_sql)
-
-#	expected_sql=' select open_tax_solver_line,amount from transfers,accounts where accounts.id=transfers.account_id;'
 	expected_sql='SELECT open_tax_solver_line, amount FROM "transfers" INNER JOIN "accounts" ON "transfers"."account_id" = "accounts"."id"'
-#	relational_algebra=transfers.project(:open_tax_solver_line,:amount).join(accounts)
-#	relational_algebra=transfers.join(accounts).project(:open_tax_solver_line,:amount)
 	puts relational_algebra.to_sql
 	assert_equal(expected_sql,relational_algebra.to_sql)
 	arel_methods=["where","having","from","group","project",'joins','order']
@@ -110,12 +70,6 @@ test "open_tax_solver" do
 	
 	join=transfers.join(accounts).on(transfers[:account_id].eq(accounts[:id]))
 	assert_not_nil(join)
-#	assert_not_nil(transfers.includes(:accounts).where("accounts.open_tax_solver_line is not null"))
-	
-	assert_not_nil(transfers)
-#	assert_nil(transfers.class.instance_methods(false))
-	puts fixtures('transfers').values.first.class.arel.inspect
-	
 	puts fixtures('transfers').values.first.account_join.inspect
 	puts fixtures('transfers').values.first.class.open_tax_solver
 	explain_assert_respond_to(transfers,:join)
