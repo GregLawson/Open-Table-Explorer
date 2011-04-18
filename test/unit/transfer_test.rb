897e9d554d45f8eaa6061136775df58d0f12a91b
Greg Lawson <GregLawson123@gmail.com>
2011-04-18 04:03:38 -0700

    Split tests, renamed account_join to transfers_extended.

--- test/unit/transfer_test.rb
+++ test/unit/transfer_test.rb
@@ -26,7 +26,8 @@ include Generic_Table
 def Transfer.transfers
 	return @@transfers
 end #def
-def Transfer.account_join
+#scope :transfers_extended, join(@@accounts).on(@@transfers[:account_id].eq(@@accounts[:id])).project(:open_tax_solver_line,:amount)
+def Transfer.transfers_extended
 	@@transfers.join(@@accounts).on(@@transfers[:account_id].eq(@@accounts[:id])).project(:open_tax_solver_line,:amount)
 end #def
 def Transfer.ots_lines
@@ -45,23 +46,31 @@ def assert_relation(relation)
 	testCall(relation,:to_sql)
 	explain_assert_respond_to(relation,:each)	
 end #def
-test "open_tax_solver" do
+test "each" do
 cars = Transfer.where(:amount => 100.0) # No Query
 cars.each {|c| puts c.amount } # Fires "select * from cars where ..."
+	Transfer.transfers_extended.each do |t|
+		puts t.inspect
+	end # each
+end #test
+test "stable and working" do	
 	transfers=Transfer.transfers
 	
 	assert_kind_of(Arel::Table,Transfer.transfers)
-	assert_kind_of(Arel::SelectManager,Transfer.account_join)
-	explain_assert_respond_to(Transfer.account_join,:to_sql)	
-	testCall(Transfer.account_join,:to_sql)
-	assert_equal(Transfer.account_join.to_sql,"SELECT open_tax_solver_line, amount FROM \"transfers\" INNER JOIN \"accounts\" ON \"transfers\".\"account_id\" = \"accounts\".\"id\"")
+	assert_kind_of(Arel::SelectManager,Transfer.transfers_extended)
+	explain_assert_respond_to(Transfer.transfers_extended,:to_sql)	
+	testCall(Transfer.transfers_extended,:to_sql)
+	assert_equal(Transfer.transfers_extended.to_sql,"SELECT open_tax_solver_line, amount FROM \"transfers\" INNER JOIN \"accounts\" ON \"transfers\".\"account_id\" = \"accounts\".\"id\"")
 
 	testCall(Transfer.joins(accounts),:to_sql)
 
-	relational_algebra=Transfer.account_join
+	relational_algebra=Transfer.transfers_extended
 	
 	expected_sql='SELECT open_tax_solver_line, amount FROM "transfers" INNER JOIN "accounts" ON "transfers"."account_id" = "accounts"."id"'
 	assert_equal(expected_sql,relational_algebra.to_sql)
 	arel_methods=["where","having","from","group","project",'joins','order']
+end #test
+test "open_tax_solver" do
+	Transfer.open_tax_solver
 end #test
 end #class
