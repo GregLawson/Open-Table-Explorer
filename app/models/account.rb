24e9ed384a17c14e245819963f2fb566e7ec97ad
Greg Lawson <GregLawson123@gmail.com>
2011-04-22 00:02:07 -0700

    add simple scope to accounts.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 class Account < ActiveRecord::Base
 has_many :transfers
 include Generic_Table
+scope :ots_line_values, where(:open_tax_solver_line => 'L16a')
 def logical_primary_key
 	return :name
 end #def
