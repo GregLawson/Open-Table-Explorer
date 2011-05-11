e3a23357042e5673be9a98ee228090e387950070
Greg Lawson <GregLawson123@gmail.com>
2011-05-11 15:06:21 -0700

    Fixed name of foreign key acquisition_stream_id to acquisition_stream_spec_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110505193654) do
+ActiveRecord::Schema.define(:version => 20110511170200) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(:version => 20110505193654) do
 
   create_table "acquisitions", :force => true do |t|
     t.string   "acquisition_data"
-    t.datetime "created_at",            :null => false
+    t.datetime "created_at",                 :null => false
     t.string   "url"
-    t.integer  "acquisition_stream_id"
+    t.integer  "acquisition_stream_spec_id"
     t.string   "error"
     t.boolean  "acquisition_updated"
   end
