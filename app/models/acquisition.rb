da44563120ce281e98ee2b2f66d09ac94484d419
Greg Lawson <GregLawson123@gmail.com>
2011-05-13 16:01:17 -0700

    Delete acquire method from class Acquisition, better in Acquisitionnterface and AcqquisitionStreamSpecs.

--- app/models/acquisition.rb
+++ app/models/acquisition.rb
@@ -4,25 +4,6 @@ include Generic_Table
 def asociatedAcquisitionStreamSpec
 	acquisition_stream_spec=AcquisitionStreamSpec.find_by_id(self[:acquisition_stream_spec_id])
 end #def
-def acquire(acquisition_stream_spec_id)
-	@previousAcq=self[:acquisition_data] # change detection
-	self[:acquisition_stream_spec_id]=acquisition_stream_spec_id
-	acquisition_stream_spec=asociatedAcquisitionStreamSpec
-	self[:acquisition_data]=`#{acquisition_stream_spec.schemelessUrl} 2>&1`
-	if $?==0 then
-		self[:error]=nil
-	else
-		self[:error]=self[:acquisition_data]
-		self[:acquisition_data]=nil
-	end
-	return self
-rescue Exception => e
- 	self[:error]= "Exception: " + e.inspect + "couldn't acquire data from #{url}"
-	return self
-else
-	self[:error]= "Not subclass of Exception: " + "couldn't acquire data from #{url}"
-	return self
-end #def
 def acquisitionDuplicated?(acquisitionData=self[:acquisition_data])
 	return @previousAcq==acquisitionData
 end #def
