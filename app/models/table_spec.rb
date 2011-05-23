7414571135e828215bdeb18575cdbfdfb0785326
Greg Lawson <GregLawson123@gmail.com>
2011-05-23 16:34:44 -0700

    Add acquisitions association to table_spec.

--- app/models/table_spec.rb
+++ app/models/table_spec.rb
@@ -1,6 +1,7 @@
 class TableSpec < ActiveRecord::Base
 has_many :acquisition_stream_specs
 belongs_to :frequency
+has_many :acquisitions
 include Generic_Table
 def logical_primary_key
 	return :model_class_name
