5197d316143d7370381c7590718cbced605731e3
Greg Lawson <GregLawson123@gmail.com>
2011-05-25 14:34:59 -0700

    Delete old migrations that make new migrations fail.

--- db/migrate/20110319061837_add_primary_key_expression_to_table_spec.rb
+++
@@ -1,9 +0,0 @@
-class AddPrimaryKeyExpressionToTableSpec < ActiveRecord::Migration
-  def self.up
-    add_column :table_specs, :primary_key_expression, :string
-  end
-
-  def self.down
-    remove_column :table_specs, :primary_key_expression
-  end
-end
--- db/migrate/20110405004036_add_ots_line_to_accounts.rb
+++
@@ -1,9 +0,0 @@
-class AddOtsLineToAccounts < ActiveRecord::Migration
-  def self.up
-    add_column :accounts, :open_tax_solver_line, :string
-  end
-
-  def self.down
-    remove_column :accounts, :open_tax_solver_line
-  end
-end
