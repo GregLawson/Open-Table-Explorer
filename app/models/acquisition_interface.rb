1a6fd7506d39298d0b804b6ee9f14a1863d0d6bc
Greg Lawson <GregLawson123@gmail.com>
2011-05-31 16:00:18 -0700

    Made acquisition interfaces a sbclass of ruby_interfaces.

--- app/models/acquisition_interface.rb
+++ app/models/acquisition_interface.rb
@@ -5,12 +5,11 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-class AcquisitionInterface < ActiveRecord::Base
+class AcquisitionInterface < RubyInterface
 has_many :acquisition_stream_specs
 include Generic_Table
 #include Generic_Acquisition 
-attr_reader :acquisition
-after_initialize :setup
+attr_reader :interaction
 # functons of class only
 def logical_primary_key
 	return :acquisition_name
@@ -23,65 +22,41 @@ def scheme
 		return self[:name].downcase
 	end #if
 end #def
-def setup
-	codeBody
-end #def
 def delta(stream)
-	@previousAcq=@acquisition # change detection
-	@acquisition=Acquisition.new # reinitialize
+	@previousAcq=@interaction # change detection
+	@interaction=Acquisition.new # reinitialize
 	@stream=stream
-	@acquisition.acquisition_stream_spec=stream
-	@acquisition.acquisition_stream_spec_id=stream.id
+	@interaction.acquisition_stream_spec=stream
+	@interaction.acquisition_stream_spec_id=stream.id
 end #def
 def eval_method(name,code)
 	method_def= "def #{name}\n#{code}\nend\n"
 	return instance_eval(method_def)
 end #def
 		acquireBody="if $?==0 then\n"
-		acquireBody+="	@acquisition.error=nil\n"
+		acquireBody+="	@interaction.error=nil\n"
 		acquireBody+="else\n"
-		acquireBody+="	@acquisition.error=@acquisition.acquisition_data\n"
-		acquireBody+="	@acquisition.acquisition_data=nil\n"
+		acquireBody+="	@interaction.error=@interaction.acquisition_data\n"
+		acquireBody+="	@interaction.acquisition_data=nil\n"
 		acquireBody+="end\n"
 @@Default_Return_Code=acquireBody
-		acquireBody="rescue StandardError => exception_raised\n"
-#		acquireBody+="@acquisition.error= 'Error: ' + exception_raised.inspect + 'could not get data from '+stream.url\n"
-@@Default_Rescue_Code=acquireBody
 
-def codeBody
-	if library.nil? then
-		eval_method('acquire_method',acquire_data)
-	else
-		eval_method('acquire_method',"require '#{library}'\n#{acquire_data}")
-	end # if
-	
-	if return_error_code.nil? then
-		eval_method('error_return',@Default_Return_Code)
-	else
-		eval_method('error_return',return_error_code)
-	end #if
-	if rescue_code.nil? then
-		eval_method('rescue_method',@@Default_Rescue_Code)
-	else
-		eval_method('rescue_method',"rescue #{rescue_code}\n")
-	end
-end #def
 # functions parameterized by a acquisition_stream_spec and adding instance detail
 def acquire(stream)
 	delta(stream)
-	before_acquire=@acquisition
-	acquire_method
-	if before_acquire!=@acquisition then
+	before_acquire=@interaction.clone # save acquisition_data and error before new acquisition.
+	interface_method
+	if before_acquire==@interaction then
 		puts 'Nothing was acquired and no error was set'
-		@acquisition.error=['Nothing was acquired and no error was set']
+		@interaction.error=['Nothing was acquired and no error was set']
 	end #if
 	error_return
 	rescue  StandardError => exception_raised
 		rescue_method
 	else
-		@acquisition[:error]= "Not subclass of StandardError: " + "couldn't acquire data from #{url}"
+		@interaction[:error]= "Not subclass of StandardError: " + "couldn't acquire data from #{url}"
 	ensure
-		@acquisition.save
-		return @acquisition
+		@interaction.save
+		return @interaction
 end #def
 end # class
--- test/unit/acquisition_interface_test.rb
+++ test/unit/acquisition_interface_test.rb
@@ -8,7 +8,7 @@ def test_scheme
 	testAnswer(acquisition_interfaces(:HTTP),:scheme,'http')
 end #test
 def test_id_equal
-		assert_equal(acquisition_interfaces(:HTTP).id,Fixtures::identify(:HTTP),"id != Fixtures::identify(:one)")
+		assert_equal(acquisition_interfaces(:HTTP).id,Fixtures::identify(:HTTP),"id != Fixtures::identify(:HTTP)")
 end #def
 def test_id_equal
 		assert_equal(acquisition_interfaces(:HTTP).id,acquisition_stream_specs(@testURL.to_sym).acquisition_interface_id,"id != acquisition_stream_spec_id")
@@ -16,8 +16,8 @@ end #def
 def acq_and_rescue
 	stream=acquisition_stream_specs(@testURL.to_sym)
 	acq=acquisition_interfaces(:HTTP)
-	acq.acquire_method
-	assert(!acq.acquisition.error.nil? || !acq.acquisition.acquisition_data.nil?)
+	acq.interface_method
+	assert(!acq.interaction.error.nil? || !acq.interaction.acquisition_data.nil?)
 rescue  StandardError => exception_raised
 	puts 'Error: ' + exception_raised.inspect + ' could not get data from '+stream.url
 	puts "$!=#{$!}"
@@ -28,32 +28,31 @@ test "acquisition" do
 	acq=acquisition_interfaces(:HTTP)
 	assert_instance_of(AcquisitionInterface,acq)
 #	puts "acq.matching_methods(/code/).inspect=#{acq.matching_methods(/code/).inspect}"
-	acq.setup
+	acq.compile_code
 	assert_not_nil(acq)
 	assert_respond_to(acq,:acquire)
 
 	acq.delta(stream)
-	assert_not_nil(acq.acquisition)
-	assert_instance_of(Acquisition,acq.acquisition)
-	assert_instance_of(AcquisitionStreamSpec,acq.acquisition.acquisition_stream_spec)
-	assert_not_nil(acq.acquisition.acquisition_stream_spec_id)
-	assert_equal(stream.id,acq.acquisition.acquisition_stream_spec_id)
-	assert_instance_of(ActiveModel::Errors,acq.acquisition.errors)
-	assert_nil(acq.acquisition.error)
-	assert_nil(acq.acquisition.acquisition_data)
+	assert_not_nil(acq.interaction)
+	assert_instance_of(Acquisition,acq.interaction)
+	assert_instance_of(AcquisitionStreamSpec,acq.interaction.acquisition_stream_spec)
+	assert_not_nil(acq.interaction.acquisition_stream_spec_id)
+	assert_equal(stream.id,acq.interaction.acquisition_stream_spec_id)
+	assert_instance_of(ActiveModel::Errors,acq.interaction.errors)
+	assert_nil(acq.interaction.error)
+	assert_nil(acq.interaction.acquisition_data)
 	acq_and_rescue
-	assert_nothing_raised{acq.acquire_method}
-	assert_not_nil(acq.acquisition)
-	assert_equal({},acq.acquisition.errors)
-	assert_not_nil(acq.acquisition)
-	assert_not_nil(acq.acquisition)
+	assert_nothing_raised{acq.interface_method}
+	assert_not_nil(acq.interaction)
+	assert_equal({},acq.interaction.errors)
+	assert_not_nil(acq.interaction)
 #	assert_not_nil(acq.instance_variable_get(:stream))
 	assert_not_nil(acq.acquisition_stream_specs)
 #	assert_not_nil(acq.acquisition_stream_specs_ids)
 #	assert_equal(acq.acquisition_stream_spec.id,acq.acquisition_stream_spec_id)
 	assert_not_nil(acq.acquire(stream))
-	assert_not_nil(acq.acquisition.acquisition_stream_spec_id)
-	assert_equal(stream.id,acq.acquisition.acquisition_stream_spec_id)
+	assert_not_nil(acq.interaction.acquisition_stream_spec_id)
+	assert_equal(stream.id,acq.interaction.acquisition_stream_spec_id)
 	
 	assert_association(stream,:acquisition_interface)
 	assert_respond_to(stream,:associated_to_s)
@@ -87,41 +86,41 @@ test "default acquisition" do
 	acq=acquisition_interfaces(:HTTP)
 	acq.delta(stream)
 
-	acq.acquire_data='@acquisition[:acquisition_data]=Net::HTTP.get(@stream.uri)'
-	acq.codeBody # recompile eval code
-	assert_nothing_raised{acq.acquire_method}
-	assert(!acq.acquisition.error.nil? || !acq.acquisition.acquisition_data.nil?)
+	acq.interface_code='@interaction[:acquisition_data]=Net::HTTP.get(@stream.uri)'
+	acq.compile_code # recompile eval code
+	assert_nothing_raised{acq.interface_method}
+	assert(!acq.interaction.error.nil? || !acq.interaction.acquisition_data.nil?)
 
-	acq.return_error_code=''
-	acq.codeBody # recompile eval code
+	acq.return_code=''
+	acq.compile_code # recompile eval code
 	assert_nothing_raised{acq.error_return}
 
 	acq.rescue_code=''
-	acq.codeBody # recompile eval code
+	acq.compile_code # recompile eval code
 	assert_nothing_raised{acq.rescue_method}
 	assert_difference('Acquisition.count') do
-		acq.acquisition.save
+		acq.interaction.save
 	end #assert
 	puts "Acquisition.count=#{Acquisition.count}"
 
-	assert_not_nil(acq.acquisition.acquisition_stream_spec_id)
-	assert_equal(stream.id,acq.acquisition.acquisition_stream_spec_id)
+	assert_not_nil(acq.interaction.acquisition_stream_spec_id)
+	assert_equal(stream.id,acq.interaction.acquisition_stream_spec_id)
 	acq=acquisition_interfaces(:Shell)
 	stream=acquisition_stream_specs('/sbin/ifconfig'.to_sym)
 	acq.delta(stream)
 
-	acq.acquire_data='@acquisition[:acquisition_data]=`#{@stream.schemelessUrl} 2>&1`'
-	acq.codeBody # recompile eval code
-	assert_nothing_raised{acq.acquire_method}
-	assert(!acq.acquisition.error.nil? || !acq.acquisition.acquisition_data.nil?)
+	acq.interface_code='@interaction[:acquisition_data]=`#{@stream.schemelessUrl} 2>&1`'
+	acq.compile_code # recompile eval code
+	assert_nothing_raised{acq.interface_method}
+	assert(!acq.interaction.error.nil? || !acq.interaction.acquisition_data.nil?)
 
 	assert_difference('Acquisition.count') do
-		acq.acquisition.save
+		acq.interaction.save
 	end #assert
 	puts "Acquisition.count=#{Acquisition.count}"
 
-	assert_not_nil(acq.acquisition.acquisition_stream_spec_id)
-	assert_equal(stream.id,acq.acquisition.acquisition_stream_spec_id)
+	assert_not_nil(acq.interaction.acquisition_stream_spec_id)
+	assert_equal(stream.id,acq.interaction.acquisition_stream_spec_id)
 
 end #test
 end
