2bc663a2957ec32c09e1e5ae9bcbbc5d1827fa1d
Greg Lawson <GregLawson123@gmail.com>
2011-06-07 16:08:21 -0700

    Fix assumption that Generic_Table is included in all tables directly. In STI it is included in superclass.

--- test/unit/global_test.rb
+++ test/unit/global_test.rb
@@ -65,16 +65,37 @@ test 'matching methods' do
 	assert_equal('',acquisition_stream_spec.associated_to_s(:acquisition_interface,:name) )
 	
 end #def
+test '' do
+#~ def noninherited_modules
+	#~ if module? then
+		#~ return ancestors-[self]
+	#~ else
+		#~ return ancestors-[self]-superclass.ancestors
+	#~ end #if
+#~ end #def
+	assert(Generic_Table.module?)
+	assert(!AcquisitionStreamSpec.module?)
+	assert_include('Generic_Table',AcquisitionStreamSpec.ancestors.map{|a| a.name})
+	assert_equal([Generic_Table],Account.noninherited_modules)
+	assert_equal([Generic_Table],AcquisitionStreamSpec.noninherited_modules)
+	assert(AcquisitionStreamSpec.ancestors.map{|a| a.name}.include?('Generic_Table'),"Module not included in #{canonicalName} context.")
+	assert(AcquisitionInterface.ancestors.map{|a| a.name}.include?('Generic_Table'),"Module not included in #{canonicalName} context.")
+	testClass=Acquisition
+	assert_equal([Generic_Table],testClass.ancestors-[testClass]-testClass.superclass.ancestors)
+	assert_include(Generic_Table,AcquisitionInterface.ancestors-[AcquisitionInterface])
+	assert_equal([Generic_Table],AcquisitionInterface.ancestors-[AcquisitionInterface,RubyInterface]-AcquisitionInterface.superclass.superclass.ancestors)
+	assert_equal([],AcquisitionInterface.noninherited_modules) # stI at work
+end #test
 test 'matching methods in context' do
 	testClass=Acquisition
-	assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
-	assert_equal([testClass.canonicalName,testClass.matching_methods(//)],testClass.matching_methods_in_context(//)[0])
-	assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
-	assert_equal([Generic_Table],testClass.ancestors-[testClass]-testClass.superclass.ancestors)
+#error message too long	assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
+#error message too long		assert_equal([testClass.canonicalName,testClass.matching_methods(//)],testClass.matching_methods_in_context(//)[0])
+#error message too long			assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
 end #def
 test "Acquisition Stream Spec modules" do
 	assert(Generic_Table.module?)
-	assert_equal([Generic_Table],Account.noninherited_modules)
+	assert(!AcquisitionStreamSpec.module?)
+	assert_equal([Generic_Table],AcquisitionStreamSpec.noninherited_modules)
 	assert(AcquisitionStreamSpec.ancestors.map{|a| a.name}.include?('Generic_Table'),"Module not included in #{canonicalName} context.")
 	assert_include('Generic_Table',AcquisitionStreamSpec.ancestors.map{|a| a.name})
 	assert(AcquisitionStreamSpec.module_included?(:Generic_Table),"Module not included in #{canonicalName} context.")
@@ -82,8 +103,8 @@ test "Acquisition Stream Spec modules" do
 end #test
 test "Acquisition Interface modules" do
 	assert(Generic_Table.module?)
-	assert_equal([Generic_Table],AcquisitionInterface.noninherited_modules)
 	assert(AcquisitionInterface.ancestors.map{|a| a.name}.include?('Generic_Table'),"Module not included in #{canonicalName} context.")
+	assert_equal([],AcquisitionInterface.noninherited_modules) # because of STI Generic_Table is not directly included
 	assert_include('Generic_Table',AcquisitionInterface.ancestors.map{|a| a.name})
 	assert(AcquisitionInterface.module_included?(:Generic_Table),"Module not included in #{canonicalName} context.")
 	assert_module_included(AcquisitionInterface,:Generic_Table)
