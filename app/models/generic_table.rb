8c79a508bcacb26f67b071c170ca2ef6dfe2625c
Greg Lawson <GregLawson123@gmail.com>
2011-07-14 08:40:21 -0700

    Correct cut and pasted code message.

--- app/models/generic_table.rb
+++ app/models/generic_table.rb
@@ -473,6 +473,7 @@ def association_state(assName)
 		
 	when :not_generic_table
 		return "#{self.class.name} does not recognize #{assName} as association."
+		return "#{self.class.name} does not recognize #{assName} as a generic table."
 	when:not_an_association
 		return "#{self.class.name} does not recognize #{assName} as association."
 	else
