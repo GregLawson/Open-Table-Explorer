d251207d1b4fbc203593de1fbb1a9abcb33159e4
Greg Lawson <GregLawson123@gmail.com>
2011-09-23 02:35:42 -0700

    begin debugging streams

--- app/models/stream_method_call.rb
+++ app/models/stream_method_call.rb
@@ -9,4 +9,18 @@ class StreamMethodCall < ActiveRecord::Base # like a method call
 include Generic_Table
 belongs_to :stream_method
 has_many :stream_parameters
+def StreamMethodCall.schedule
+	StreamMethodCall.find(:all).each do |c|
+		c.fire
+	end #each
+end #def
+def StreamMethodCall.next_fire
+	StreamMethodCall.find(:all)
+end #def
+def fire
+	inputs=[]
+	inputs.fire
+	stream_method.execute
+	outputs.fire
+end #fire
 end #class
