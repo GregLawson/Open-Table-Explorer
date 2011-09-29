500bcbd8f2dff5f67e2fdc31a77d35fc2dcf067e
Greg Lawson <GregLawson123@gmail.com>
2011-09-29 08:43:01 -0700

    Add copyright notice.

--- test/unit/code_base_test.rb
+++ test/unit/code_base_test.rb
@@ -1,5 +1,11 @@
+###########################################################################
+#    Copyright (C) 2011 by Greg Lawson                                      
+#    <GregLawson@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
 require 'test_helper'
-#require 'lib/tasks/testing_file_patterns.rb'
 # executed in alphabetical orer? Longer names sort later.
 # place in order from low to high level and easy pass to harder, so that first fail is likely the cause.
 # move passing tests toward end
--- test/unit/test_run_test.rb
+++ test/unit/test_run_test.rb
@@ -1,8 +1,40 @@
+###########################################################################
+#    Copyright (C) 2011 by Greg Lawson                                      
+#    <GregLawson@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
 require 'test_helper'
 # executed in alphabetical orer? Longer names sort later.
 # place in order from low to high level and easy pass to harder, so that first fail is likely the cause.
 # move passing tests toward end
 class TestRunTest < ActiveSupport::TestCase
+def setup
+	@testURL='http://192.168.3.193/api/LiveData.xml'
+	define_model_of_test # allow generic tests
+	assert_module_included(@model_class,Generic_Table)
+	explain_assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
+	assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
+#	define_association_names #38271 associations
+end #def
+test "fixture_function" do  # aaa to output first
+	define_association_names #38271 associations
+	assert_equal(@my_fixtures,fixtures(@table_name))
+end #test
+def test_general_associations
+#	assert_general_associations(@table_name)
+end #test
+def test_id_equal
+	if @model_class.sequential_id? then
+	else
+		@my_fixtures.each_value do |ar_from_fixture|
+			message="Check that logical key (#{ar_from_fixture.class.logical_primary_key}) value (#{ar_from_fixture.logical_primary_key_value}) exactly matches yaml label for record."
+			message+=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
+			assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_value),ar_from_fixture.id,message)
+		end
+	end
+end #def
 def assert_logical_primary_key_defined(instance,message=nil)
 	message=build_message(message, "instance=?", instance.inspect)	
 	assert_not_nil(instance, message)
@@ -51,31 +83,6 @@ end #run
 test 'ruby_run_and_log' do
 #	TestRun.ruby_run_and_log('/dev/null','/dev/null')
 end #ruby_run_and_log
-def setup
-	@testURL='http://192.168.3.193/api/LiveData.xml'
-	define_model_of_test # allow generic tests
-	assert_module_included(@model_class,Generic_Table)
-	explain_assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
-	assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
-#	define_association_names #38271 associations
-end #def
-def test_general_associations
-#	assert_general_associations(@table_name)
-end #test
-def test_id_equal
-	if @model_class.sequential_id? then
-	else
-		@my_fixtures.each_value do |ar_from_fixture|
-			message="Check that logical key (#{ar_from_fixture.class.logical_primary_key}) value (#{ar_from_fixture.logical_primary_key_value}) exactly matches yaml label for record."
-			message+=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
-			assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_value),ar_from_fixture.id,message)
-		end
-	end
-end #def
 test "specific, stable and working" do
-end #test
-test "fixture_function" do  # aaa to output first
-	define_association_names #38271 associations
-	assert_equal(@my_fixtures,fixtures(@table_name))
 end #test
 end #class
