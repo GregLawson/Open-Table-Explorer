4a484b72a7b46a9333c2ee1f1d2d81800ec356ab
Greg Lawson <GregLawson123@gmail.com>
2011-10-04 22:50:58 -0700

    Debug stream links for EEG.

--- test/unit/stream_method_call_test.rb
+++ test/unit/stream_method_call_test.rb
@@ -9,19 +9,19 @@ require 'test_helper'
 
 class StreamMethodCallTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-test 'stream_parameters' do
+test 'stream_links' do
 	StreamMethodCall.find(:all).each do |smc|
-		assert(smc.stream_parameters.exists?,"smc=#{smc.inspect}, smc.stream_parameters=#{smc.stream_parameters.inspect} ")
+		assert(smc.inputs.exists?,"smc=#{smc.inspect}, smc.stream_links=#{smc.inputs.inspect} ")
 	end #each
-end #stream_parameters
+end #stream_links
 test 'EEG' do
 	streamCall=StreamMethodCall.first
 	assert_equal(64810937,streamCall.id)
-	assert_equal_sets(["stream_method","stream_parameters"], StreamMethodCall.association_names)
+	assert_equal_sets(["stream_method","stream_links"], StreamMethodCall.association_names)
 	assert_equal("695672806",StreamPattern.find_by_name('Acquisition').id.inspect)
 	assert_equal('File',StreamMethod.find_by_name('File').name)
-	assert_equal('Acquisition',StreamPatternArgument.find_by_stream_pattern('Acquisition').name)
-#	assert_equal([],StreamMethodArgument.find_by_stream_pattern().name)
+	assert_equal('Acquisition',StreamPatternArgument.where("name='Acquisition'").name)
+#	assert_equal([],StreamMethodArgument.where("stream_pattern='Acquisition'").name)
 	assert_equal('File',StreamMethod.first.name)
   end
 test 'fire' do
