c12c662f9745bf8f58ae580918a3b5197d0b1b57
Greg Lawson <GregLawson123@gmail.com>
2011-10-10 08:41:55 -0700

    Add uniqueness tests and exception recovery to method_model.

--- app/models/method_model.rb
+++ app/models/method_model.rb
@@ -2,27 +2,51 @@ class MethodModel < ActiveRecord::Base
 def self.method_record(m,owner,scope)
 	begin
 		mHash={}
-		mHash[:name]=m
-		theMethod=method(m.to_sym)
-		mHash[:method]=theMethod
-		mHash[:arity]=theMethod.arity
-		mHash[:owner]=theMethod.owner
-		mHash[:instance_variable_defined]=theMethod.instance_variable_defined?(('@'+m))
+		mHash[:name]=m.to_sym
+		mHash[:owner]=owner
+
+		if scope.to_sym==:class then
+			theMethod=owner.method(m.to_sym)
+		elsif owner.respond_to?(:new) then
+			begin
+#				theMethod=owner.new.method(m.to_sym)
+				theMethod=nil
+			rescue StandardError => exc
+				mHash[:exception]=exc
+			end #begin
+		else
+			puts "  invalid method:"
+			puts "m=:#{m}"
+			puts "scope='#{scope}'"
+			puts "owner=#{owner}"
+		end #if
+		if !theMethod.nil? then
+			mHash[:method]=theMethod
+			mHash[:arity]=theMethod.arity
+			mHash[:owner]=theMethod.owner
+			if theMethod.respond_to?(:source_location)
+				mHash[:source_location]=theMethod.source_location
+			end #if
+			if theMethod.respond_to?(:parameters)
+				mHash[:parameters]=theMethod.parameters
+			end #if
+		end #if
+		if m.to_s[/[a-zA-Z0-9_]+/,0]==m.to_s then
+			mHash[:instance_variable_defined]=theMethod.instance_variable_defined?(('@'+m.to_s))
+		end #if
 		mHash[:singleton]=singleton_methods.include?(m)
-		mHash[:scope]=self.class==Class ? 'Class': 'Instance'
+		mHash[:scope]=self.class==Class ? :class: :instance
 		mHash[:protected]=protected_method_defined?(m)
 		mHash[:private]=private_method_defined?(m)
-		mHash[:parameters]=theMethod.parameters
-		mHash[:source_location]=theMethod.source_location
 		mHash		
-	rescue StandardError => exc
-		mHash[:owner]=owner
-		mHash[:scope]=scope
-		mHash[:exception]=exc
+#	rescue StandardError => exc
+#		mHash[:owner]=owner
+#		mHash[:scope]=scope
+#		mHash[:exception]=exc
 		#~ puts "exc=#{exc.inspect}"
-		mHash		
+#		mHash		
 	end #begin
-end #def
+end #method_record
 def self.constantized
 	@@CONSTANTIZED||=Module.constants.map do |c|
 		begin
@@ -32,7 +56,7 @@ def self.constantized
 			 nil
 		 end #begin
 	end #map
-end #def
+end #constantized
 def self.classes
 	ret=[]
 	ObjectSpace.each_object(Class) do |c| 
@@ -40,24 +64,29 @@ def self.classes
 	end #each_object
 	ret=ret.sort{|x,y| (x.name)<=>(y.name)}
 	return ret.uniq
-end #def
+end #classes
 def self.modules
 	ret=[]
 	ObjectSpace.each_object(Module) do |mod| 
 		ret << mod
 	end #each_object
 	ret=ret.sort{|x,y| x.name<=>y.name}
-	return ret.uniq
-end #def
+	return ret-classes
+end #modules
 def self.classes_and_modules
 	return @@CLASSES_AND_MODULES||=classes+modules
-end #def
-def method_names
-	
-end #end
+end #classes_and_modules
 def self.all
 	@@ALL||=(classes_and_modules.map { |c| c.methods(false).map { |m| method_record(m,c,:class) } } +
 	classes_and_modules.map { |c| c.instance_methods(false).map { |m| method_record(m,c,:instance) } } +
 	classes_and_modules.map { |c| c.singleton_methods(false).map { |m| method_record(m,c,:singleton) } }).flatten
-end #def
+end #all
+def self.first
+	self.all.first
+end #first
+def self.find_by_name(name)
+	self.all.find_all{|i| i[:name].to_sym==name.to_sym}
+end #find_by_name
 end #class
+#class Stream < Enumerator
+#end #Stream
--- test/unit/method_model_test.rb
+++ test/unit/method_model_test.rb
@@ -1,8 +1,184 @@
 require 'test_helper'
 class GlobalTest < ActiveSupport::TestCase
+def test_method_record
+	owner=ActiveRecord::ConnectionAdapters::ColumnDefinition
+	scope=:instance
+	m=:to_sql
+	explain_assert_respond_to(owner.new,m)
+	assert_respond_to(owner.new,m)
+	assert_instance_of(Method,owner.new.method(m.to_sym))
+	assert_instance_of(Method,owner.new.method(m.to_sym))
+	mr=MethodModel.method_record(m,owner,scope)
+	assert_nil(mr[:exception])
+
+	assert_equal(mr[:name], :to_sql)
+	assert_equal(mr[:scope], :class)
+	assert_equal(mr[:protected], false)
+	assert_equal(mr[:instance_variable_defined], false)
+	assert_equal(mr[:private], false)
+	assert_equal(mr[:singleton], false)
+	assert_not_nil(mr[:owner],"owner is nil for m=#{m}")
+
+	owner=MethodModel
+	scope=:class
+	m=:inspect
+	assert_equal([],owner.instance_methods(false))
+	assert_equal_sets(["inspect", "instantiate_observers", "joins", "instance_method_already_implemented?"],owner.matching_class_methods(/ins/,false))
+#new	assert_instance_of(Method,owner.new.method(m.to_sym))
+#new	assert_instance_of(Method,owner.new.method(m.to_sym))
+	assert_nil(MethodModel.method_record(m,owner,scope)[:exception])
+
+end #method_record
+def test_constantized
+	assert_equal(['String'],Module.constants.map { |c| c.objectKind}.uniq)
+	assert_include('String',MethodModel.constantized.map { |c| c.objectKind}.uniq)
+	assert_operator(1000,:>,Module.constants.size)
+	assert_operator(MethodModel.constantized.size,:<,MethodModel.classes_and_modules.size)
+	assert_operator(100,:<,MethodModel.constantized.size)
+#	puts "Module.constants=#{Module.constants.inspect}"
+	method_list=Module.constants.map do |c|
+		if c.objectKind==:class || c.objectKind==:module then
+			method_record(c)
+		end #if
+	end #map
+	assert_operator(method_list.size,:<,1000)
+	assert_operator(100,:<,method_list.size)
+	assert_include( "Class",MethodModel.constantized.map { |c| c.objectKind}.uniq)
+	puts "pretty print"
+	#~ pp MethodModel.all
+	#~ assert_not_nil(method_record('object_id',Object,:methods))
+end #constantized
+test "size" do
+end #test
+def test_classes
+	assert_kind_of(Enumerable::Enumerator,ObjectSpace.each_object(Module))
+	assert_instance_of(Array,MethodModel.classes)
+	MethodModel.classes.each do |m|
+		assert_instance_of(Class,m)
+	end #each
+	assert_not_equal('',MethodModel.classes[0])
+	assert_equal(MethodModel.classes.size,MethodModel.classes.uniq.size)
+#	puts MethodModel.classes.inspect
+	assert_empty(MethodModel.classes.map { |c| c.name}.sort-MethodModel.classes.map { |c| c.name}.sort.uniq)
+	classNames=MethodModel.classes.map { |c| c.name}
+	uniqClasses=classNames.sort.uniq
+	duplicates=0 # found so far
+	classNames.each_index do |i|
+		if classNames[i].nil? then
+			puts "class name[#{i}] is nil, class=#{MethodModel.classes[i].inspect}"
+		end #if
+		if classNames[i].empty?
+			puts "class name[#{i}] is empty, class=#{MethodModel.classes[i].inspect}"
+		end #if
+		if classNames[i+duplicates]!=uniqClasses[i] then
+			puts "Duplicate class name[#{i}] = #{classNames[i+duplicates]}"
+			duplicates+=1
+		end # if
+	end #each
+	assert_include(String,MethodModel.classes)
+	assert_include(ActiveRecord::Base,MethodModel.classes)
+	
+end #classes
+def test_modules
+	assert_kind_of(Enumerable::Enumerator,ObjectSpace.each_object(Module))
+	assert_instance_of(Array,MethodModel.modules)
+	MethodModel.modules.each do |m|
+		if !m.instance_of?(Module) then
+			if MethodModel.classes.include?(m) then
+			else
+				puts "#{m} should be Module but is #{m.class}"
+			end #if
+		end #if
+#hope		assert_instance_of(Module,m)
+	end #each
+	MethodModel.modules.any? {|m| m.instance_of?(Module)}
+	assert_not_equal('',MethodModel.modules[0])
+	assert_equal(MethodModel.modules.size,MethodModel.modules.uniq.size)
+	assert_include(Generic_Table,MethodModel.modules)
+	assert_not_include(ActiveRecord::Base,MethodModel.modules)
+
+end #modules
+def test_classes_and_modules
+	assert_not_equal(MethodModel.classes.size,MethodModel.modules.size)
+	assert_empty((MethodModel.modules-MethodModel.classes)&MethodModel.classes)
+	assert_not_empty(MethodModel.classes_and_modules)
+	assert_not_empty(MethodModel.classes_and_modules.find_all{|i| i.to_s=='ActiveRecord::ConnectionAdapters::ColumnDefinition'})
+#	assert_equal([],MethodModel.classes_and_modules.find_all{|i| i.to_s=='ActiveRecord::ConnectionAdapters::ColumnDefinition'})
+
+end #classes_and_modules
+def test_all
+	all_records=MethodModel.all
+	assert_instance_of(Array,all_records)
+	assert_operator(100,:<,all_records.size)
+	all_records.each do |mr| 
+		assert_include(mr[:scope].to_sym, [:instance, :class, :singleton])
+	end #each
+	assert(all_records.all? {|mr| mr[:name]})
+	assert(all_records.all? {|mr| mr[:owner]})
+	assert(all_records.all? {|mr| mr[:scope]})
+	assert(all_records.all? {|mr| mr.has_key?(:singleton)})
+	assert(all_records.all? {|mr| mr.has_key?(:protected)})
+	assert(all_records.all? {|mr| mr.has_key?(:private)})
+	assert(all_records.any? {|mr| mr[:method]})
+	assert(all_records.any? {|mr| mr[:singleton]})
+	assert(all_records.any? {|mr| mr[:protected]})
+	assert(all_records.any? {|mr| mr[:private]})
+	assert(all_records.any? {|mr| mr[:arity]})
+	assert(all_records.any? {|mr| mr.has_key?(:instance_variable_defined)})
+	assert(!all_records.any? {|mr| mr.has_key?(:exception)})
+	assert(!all_records.any? {|mr| mr[:instance_variable_defined]})
+	assert(!all_records.any? {|mr| mr[:source_location]})
+	assert(!all_records.any? {|mr| mr[:parameters]})
+	puts all_records.map { |m| m.keys}.uniq.inspect
+#why?	assert_equal(Set.new([4,6,10]),Set.new(all_records.map { |m| m.keys.size}.uniq))
+	assert_not_empty(Set.new(all_records.map { |m| m.keys}.uniq))
+end #all
+def self.first
+	all_records=MethodModel.all
+	assert_instance_of(Hash,all_records[0])
+	assert_equal(all_records.first,all_records[0])
+	assert_not_nil(MethodModel.first)
+	assert_not_nil(all_records[0])
+	assert_equal(MethodModel.first,all_records[0])
+	assert_equal([],all_records[0])
+	assert_instance_of(Hash,MethodModel.first)
+	assert_equal([:scope, :class],all_records.first)
+	assert_equal([:scope, :class],all_records.first)
+	assert_equal([:scope, :class],MethodModel.first)
+	assert_instance_of(String,MethodModel.first[:owner].name)
+	assert_instance_of(String,MethodModel.first[:owner].name)
+end #first
+def test_find_by_name
+	assert_instance_of(Array,MethodModel.first.keys)
+	to_sqls=MethodModel.all.select {|m|m[:name].to_sym==:to_sql}
+	assert_equal(to_sqls,MethodModel.find_by_name(:to_sql))
+	assert_equal(to_sqls,MethodModel.all.find_all{|i| i[:name].to_sym==:to_sql})
+	MethodModel.find_by_name(:to_sql).each do |mr|
+		assert_equal(mr[:name], :to_sql)
+		assert_equal(mr[:scope], :class)
+		assert_equal(mr[:protected], false)
+		assert_equal(mr[:instance_variable_defined], false)
+		assert_equal(mr[:private], false)
+		assert_equal(mr[:singleton], false)
+		assert_not_nil(mr[:owner])
+		puts "#{mr[:owner]}:#{mr[:owner].object_id}"
+	end #each
+	to_sql_owners=to_sqls.map {|t|t[:owner]}
+	assert_equal(to_sql_owners.uniq,to_sql_owners,"No duplicates, please.")
+end #find_by_name
+def test_ExclusionValidator
+	ObjectSpace.each_object(Class) do |c| 
+		if c.name.match(/ExclusionValidator/) then
+			p c.inspect
+		end #if
+	end #each_object
+	#~ puts "ExclusionValidator.inspect=#{ExclusionValidator.inspect}"
+	#~ puts " 'ExclusionValidator'.constantized.inspect=#{'ExclusionValidator'.constantized.inspect}"
+end #ExclusionValidator
 test 'matching methods' do
 	testClass=Acquisition
-	assert_instance_of(Array,testClass.matching_methods(//))
+	assert_instance_of(Array,testClass.matching_class_methods(//))
+	assert_instance_of(Array,testClass.matching_instance_methods(//))
 end #test
 test '' do
 	assert(Generic_Table.module?)
@@ -18,7 +194,7 @@ test '' do
 	assert_equal([Generic_Table],AcquisitionInterface.ancestors-[AcquisitionInterface,RubyInterface]-AcquisitionInterface.superclass.superclass.ancestors)
 	assert_equal([],AcquisitionInterface.noninherited_modules) # stI at work
 end #test
-test 'matching methods in context' do
+def test_matching_methods_in_context
 	testClass=Acquisition
 #error message too long	assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
 #error message too long		assert_equal([testClass.canonicalName,testClass.matching_methods(//)],testClass.matching_methods_in_context(//)[0])
@@ -41,59 +217,5 @@ test "Acquisition Interface modules" do
 	assert(AcquisitionInterface.module_included?(:Generic_Table),"Module not included in #{canonicalName} context.")
 	assert_module_included(AcquisitionInterface,:Generic_Table)
 end #test
-def txest_method_model
-	assert_equal(['String'],Module.constants.map { |c| c.objectKind}.uniq)
-	assert_include('String',MethodModel.constantized.map { |c| c.objectKind}.uniq)
-	assert_operator(1000,:>,Module.constants.size)
-	ObjectSpace.each_object(Class) do |c| 
-		if c.name.match(/ExclusionValidator/) then
-			p c.inspect
-		end #if
-	end #each_object
-	#~ puts "ExclusionValidator.inspect=#{ExclusionValidator.inspect}"
-	#~ puts " 'ExclusionValidator'.constantized.inspect=#{'ExclusionValidator'.constantized.inspect}"
-	assert_operator(MethodModel.constantized.size,:<,MethodModel.classes_and_modules.size)
-	assert_operator(100,:<,MethodModel.constantized.size)
-#	puts "Module.constants=#{Module.constants.inspect}"
-	method_list=Module.constants.map do |c|
-		if c.objectKind=='Class' || c.objectKind=='Module' then
-			method_record(c)
-		end #if
-	end #map
-	assert_operator(method_list.size,:<,1000)
-	assert_operator(100,:<,method_list.size)
-	assert_include( "Class",MethodModel.constantized.map { |c| c.objectKind}.uniq)
-	puts "pretty print"
-	#~ pp MethodModel.all
-	#~ assert_not_nil(method_record('object_id',Object,:methods))
-	assert_equal(Set.new([4,6,10]),Set.new(MethodModel.all.map { |m| m.keys.size}.uniq))
-	assert_equal(Set.new([Set.new([:exception,:scope, :owner, :name]),
- Set.new([:exception,:scope, :method, :arity, :owner, :name]),
- Set.new([:exception,:scope,
-  :instance_variable_defined,
-  :method,
-  :singleton,
-  :protected,
-  :private,
-  :arity,
-  :owner,
-  :name])]),Set.new(MethodModel.all.map { |m| Set.new(m.keys)}.uniq))
-	puts MethodModel.all.map { |m| m.keys}.uniq.inspect
-	assert_equal(Set.new([:instance, :class, :singleton]),Set.new(MethodModel.all.map { |m| m[:scope]}.uniq))
-end #test
-test "size" do
-	assert_equal(MethodModel.classes.size,MethodModel.classes.uniq.size)
-#	puts MethodModel.classes.inspect
-	assert_empty(MethodModel.classes.map { |c| c.name}.sort-MethodModel.classes.map { |c| c.name}.sort.uniq)
-	classNames=MethodModel.classes.map { |c| c.name}
-	uniqClasses=classNames.sort.uniq
-	duplicates=0 # found so far
-	classNames.each_index do |i|
-		if classNames[i+duplicates]!=uniqClasses[i] then
-			puts "Duplicate class name = #{classNames[i+duplicates]}"
-			duplicates+=1
-		end # if
-	end #each
-	assert_equal(MethodModel.modules.size,MethodModel.modules.uniq.size)
-end #test
+
 end #test class
