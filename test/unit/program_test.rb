c04760c56bfcb2c75def6445b10ee6aafe50d535
Greg Lawson <GregLawson123@gmail.com>
2011-12-08 04:46:22 -0800

    Start link to MythTv MySql Programs table. But Mysql is not properly installed on Dell2.

---
+++ test/unit/program_test.rb
@@ -0,0 +1,25 @@
+###########################################################################
+#    Copyright (C) 2010-2011 by Greg Lawson                                      
+#    <GregLawson@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require 'test/test_helper'
+# executed in alphabetical order? Longer names sort later.
+# place in order from low to high level and easy pass to harder, so that first fail is likely the cause.
+# move passing tests toward end
+class Program < ActiveRecord::Base
+end #Program
+Program.establish_connection(
+  :adapter => "mysql2",
+  :encoding => "utf8",
+  :database => "mythconverg",
+  :pool => 5,
+  :username => "mythtv",
+  :password => "mythtv",
+  :socket => "/tmp/mysql.sock")
+  
+class ProgramTest < ActiveSupport::TestCase
+	assert_not_nil(Program.new)
+end #ProgramTest
