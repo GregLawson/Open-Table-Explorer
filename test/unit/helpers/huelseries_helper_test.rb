eab5bf50a06450540649d0ddef405c237ffd4f59
Greg Lawson <GregLawson123@gmail.com>
2011-12-21 01:30:17 -0800

    Helper tests, replacements. Don't use helper tests yet.

--- test/unit/helpers/huelseries_helper_test.rb
+++ test/unit/helpers/huelseries_helper_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class HuelseriesHelperTest < ActionView::TestCase
 end
--- test/unit/helpers/stream_patterns_helper_test.rb
+++ test/unit/helpers/stream_patterns_helper_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class StreamPatternsHelperTest < ActionView::TestCase
 end
