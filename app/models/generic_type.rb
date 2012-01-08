74ba436af3a8ea88dce5df77e93693f4e3fb800f
Greg Lawson <GregLawson123@gmail.com>
2012-01-08 04:40:23 -0800

    Some generic types tests pass.

--- app/models/generic_type.rb
+++ app/models/generic_type.rb
@@ -1,12 +1,22 @@
 class GenericType < ActiveRecord::Base
 include Generic_Table
-has_many :example_Types, :foreign_key => "import_class",
-	:conditions => "import_class = 'Timestamp_Column'"
-def example_types
-	return ExampleType.find_all_by_import_class(self[:import_class])
-end #example_types
-def parent
-	Generic_Table.where("search_sequence > ? ", self[:search_sequence])
+has_many :example_types
+has_many :specialize, :class_name => "GenericType"
+belongs_to :generalize, :class_name => "GenericType",
+    :foreign_key => "generalize_id"
+
+#def example_types
+#	return ExampleType.find_all_by_generic_type(self[:generic_type])
+#end #example_types
+def ancestors(candidates=GenericType.all)
+	
+	candidates.select {|c| c.example_types.all?{|e| e.valid_parent?(e[:example_string])}}
+end #ancestors
+#def generalize
+#	return GenericType.find(self[:generalize])
+#end #generalize
+def descendants(candidates=GenericType.all)
+	candidates.select {|c| c.example_types.all?{|e| !e.valid_parent?(e[:example_string])}}
   
-end #parent
+end #descendants
 end #GenericType
--- test/unit/generic_type_test.rb
+++ test/unit/generic_type_test.rb
@@ -1,7 +1,6 @@
 require 'test/test_helper'
 
 class GenericTypeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
 def test_example_types
     assert_not_nil(GenericType.all)
     assert_not_nil(GenericType.all[0])
@@ -13,5 +12,44 @@ def test_example_types
 	end #each
 #	assert_equal([], t.example_types.map{|t| t.attributes})
     end #each
-end #test_example_types
+end #example_types
+def test_ancestors
+	assert(GenericType.all.any? {|t| !t.ancestors.empty?})
+	assert_include("VARCHAR_Column", GenericType.find_by_import_class('Integer_Column').ancestors.map{|a| a.import_class})
+	assert_include("Text_Column", GenericType.find_by_import_class('Integer_Column').ancestors.map{|a| a.import_class})
+	GenericType.all.each do |t|
+		assert_instance_of(GenericType, t)
+		assert_instance_of(Array, t.ancestors)
+		if !t.ancestors.empty? then
+			assert_instance_of(GenericType, t.ancestors[0])
+		end #if
+	end #each
+	assert_equal(["VARCHAR_Column", "Text_Column"], GenericType.find_by_import_class('Integer_Column').ancestors.map{|a| a.import_class})
+end #ancestors
+def test_generalize
+	GenericType.all.each do |t|
+		assert_not_equal(t[:generalize_id], 0, "t=#{t.inspect}")
+	end #each
+	
+	assert(GenericType.all.any? {|t| !t.generalize.nil?})
+	GenericType.all.each do |t|
+		assert_instance_of(GenericType, t)
+		if !t.generalize.nil? then
+			assert_instance_of(GenericType, t.generalize)
+		end #if
+	end #each
+	assert_equal("VARCHAR_Column", GenericType.find_by_import_class('Integer_Column').generalize.import_class)
+end #generalize
+def test_descendants
+	assert(GenericType.all.any? {|t| !t.descendants.empty?})
+	GenericType.all.each do |t|
+		assert_instance_of(GenericType, t)
+		assert_instance_of(Array, t.descendants)
+		if !t.descendants.empty? then
+			assert_instance_of(GenericType, t.descendants[0])
+		end #if
+	end #each
+	assert_include('Integer_Column', GenericType.find_by_import_class("VARCHAR_Column").descendants.map{|a| a.import_class})
+	assert_include('Integer_Column', GenericType.find_by_import_class("Text_Column").descendants.map{|a| a.import_class})
+end #descendants
 end #GenericType
