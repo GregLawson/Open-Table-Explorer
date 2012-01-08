eaa376cfa9234930796b05cd1479d655deb1c9c9
Greg Lawson <GregLawson123@gmail.com>
2012-01-08 04:42:18 -0800

    Some example types tests pass.

--- app/models/example_type.rb
+++ app/models/example_type.rb
@@ -1,3 +1,19 @@
 class ExampleType < ActiveRecord::Base
 include Generic_Table
-end
+belongs_to :generic_type
+#def generic_type
+#	GenericType.find_by_import_class(self[:generic_type])
+#end #generic_type
+def valid?
+	valid_self? && valid_generalization? && valid_specialization?
+end #valid?
+def valid_self?(test_string=self[:example_string])
+	return Regexp.new(generic_type[:data_regexp]).match(test_string)	
+end #valid_self?
+def valid_generalization?(test_string=self[:example_string])
+	return Regexp.new(generic_type.generalize[:data_regexp]).match(test_string)
+end #valid_generalization?
+def valid_specialization?(test_string=self[:example_string])
+	return generic_type.specialize.all?{|t| Regexp.new(t[:data_regexp]).match(test_string)}
+end #valid_specialization?
+end #ExampleType
--- test/unit/example_type_test.rb
+++ test/unit/example_type_test.rb
@@ -1,8 +1,41 @@
 require 'test/test_helper'
 
 class ExampleTypeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-def test_the truth
-    assert true
-  end
-end
+def test_generic_type
+	ExampleType.all.each do |t|
+		assert_not_nil(t.generic_type)
+	end #each
+end #generic_type
+def test_valid?
+	ExampleType.all.each do |t|
+		assert_not_nil(t.valid?, "t=#{t.inspect}")
+	end #each
+end #valid?
+def test_valid_self
+	example_type=ExampleType.all[0]
+	assert_not_nil(example_type)
+	assert_not_nil(example_type.generic_type, "example_type=#{example_type.inspect}")
+	assert_not_nil(example_type.generic_type.generalize)
+	assert_not_nil(example_type.generic_type.generalize[:data_regexp])
+	assert_not_nil(example_type[:example_string])
+	assert(example_type.valid_self?(example_type[:example_string]))
+	ExampleType.all.each do |t|
+		assert_not_nil(t.valid_self?, "t=#{t.inspect}")
+	end #each
+end #valid_self?
+def test_valid_generalization
+	ExampleType.all.each do |t|
+		assert_instance_of(String, t[:example_string])
+		assert_not_nil(t.generic_type)
+		assert_not_nil(t.generic_type.generalize)
+		assert_not_nil(t.generic_type.generalize[:data_regexp])
+		assert(Regexp.new(t.generic_type.generalize[:data_regexp]).match(t[:example_string]), "t=#{t.inspect}, t.generic_type=#{t.generic_type.inspect}, t.generic_type.generalize=#{t.generic_type.generalize.inspect}")
+		assert_not_nil(t.valid_generalization?, "t=#{t.inspect}, t.generic_type=#{t.generic_type.inspect}, t.generic_type=#{t.generic_type.inspect}")
+	end #each
+end #valid_generalization?
+def test_valid_specialization
+	ExampleType.all.each do |t|
+		assert_not_nil(t.valid_specialization?, "t=#{t.inspect}")
+	end #each
+end #valid_specialization?
+end #ExampleType
