529ac1b34b06ae8944264bb62c0d4bac964fc1e4
Greg Lawson <GregLawson123@gmail.com>
2012-01-10 19:13:42 -0800

    All tests pass.

--- app/models/example_type.rb
+++ app/models/example_type.rb
@@ -1,47 +1,36 @@
 class ExampleType < ActiveRecord::Base
 include Generic_Table
+require 'test/assertions/ruby_assertions.rb'
 belongs_to :generic_type
 #def generic_type
 #	GenericType.find_by_import_class(self[:generic_type])
 #end #generic_type
 def valid_context?
-	valid? && valid?(:generalize) && valid(:specialize)
+	valid? && valid?(:generalize) && valid?(:specialize)
 end #valid_context
 def valid?(association=nil)
-	generic_type=which_generic_type(association)
-	generic_type[:data_regexp].match(self[:example_string])
+	gt=which_generic_type(association)
+	if gt.is_a?(Array) then #specialize
+		gt.all? do |g|
+			data_regexp=g[:data_regexp]
+			Regexp.new(data_regexp).match(self[:example_string])
+		end #all
+	else
+		data_regexp=gt[:data_regexp]
+		Regexp.new(data_regexp).match(self[:example_string])
+	end #if
 end #valid
 def which_generic_type(association=nil)
 	return case association
 	when nil
 		generic_type
 	when :generalize
-		generalize.generic_type
+		generic_type.generalize
 	when :specialize
-		specialize.generic_type
+		generic_type.specialize
 	else
-		raise "Unexpected value for association=#{association}"
+		raise "Unexpected value for association=#{association.inspect}"
 	end #case
 end #which_generic_type
-def assert_generic_type(association=nil, message=nil)
-	message=build_message(message, "example_type=?, association=?", self, association.inspect) 
-	case association
-	when nil
-		; # DO NOTHING
-	when :generalize, :specialize
-		assert_associations(self.class, association, message)
-	else
-		raise "Unexpected value for association=#{association}"
-	end #case
-	generic_type=which_generic_type(association)
-	assert_not_nil(generic_type, message)
-	assert_not_empty(generic_type[:data_regexp])
-end #generic_type
-def assert_example_type_valid(association=nil, message=nil)
-	message=build_message(message, "example_type=?, association=?", self, association.inspect) 
-#	assert_not_nil(example_type)
-	assert_not_empty(self[:example_string])
-	assert_generic_type(association)
-	assert(valid?(association))
-end #example_type_valid
+
 end #ExampleType
--- test/unit/example_type_test.rb
+++ test/unit/example_type_test.rb
@@ -1,5 +1,5 @@
 require 'test/test_helper'
-
+require 'test/assertions/example_type_assertions.rb'
 class ExampleTypeTest < ActiveSupport::TestCase
 def test_generic_type
 	ExampleType.all.each do |t|
@@ -14,18 +14,46 @@ end #valid_context
 def test_valid
 	example_type=ExampleType.all[0]
 	assert_not_nil(example_type)
+	assert_association(ExampleType, :generic_type)
 	assert_not_nil(example_type.generic_type, "example_type=#{example_type.inspect}")
 	assert_not_nil(example_type.generic_type.generalize)
 	assert_not_nil(example_type.generic_type.generalize[:data_regexp])
 	assert_not_nil(example_type[:example_string])
-	assert(example_type.valid?(example_type[:example_string]))
+	assert_match(Regexp.new(example_type.generic_type[:data_regexp]), example_type[:example_string])
+	assert(example_type.valid?)
+	assert(example_type.valid?(:generalize))
+	assert_not_nil(example_type.which_generic_type(:specialize))
+	assert_instance_of(Array, example_type.which_generic_type(:specialize))
+	assert(example_type.valid?(:specialize))
+	assert(example_type.valid_context?)
 	ExampleType.all.each do |t|
+		assert_match(Regexp.new(t.generic_type[:data_regexp]), t[:example_string])
+		generic_type=t.which_generic_type
+		assert_not_nil(generic_type)
+		assert_match(Regexp.new(t.generic_type[:data_regexp]), t[:example_string])
+		assert_match(Regexp.new(t.generic_type[:data_regexp]), t[:example_string])
 		assert_not_nil(t.valid?, "t=#{t.inspect}")
 	end #each
 end #valid
 def test_which_generic_type
 	ExampleType.all.each do |t|
-		t.assert_generic_type(:generalize, "t=#{t.inspect}, t.generic_type=#{t.generic_type.inspect}, t.generic_type=#{t.generic_type.inspect}")
+		message= "t=#{t.inspect}"
+		assert_not_nil(t.which_generic_type, message)
+		assert_not_nil(t.which_generic_type(:generalize), message)
+		assert_not_nil(t.which_generic_type(:specialize), message)
+		assert_not_nil(t.generic_type.specialize, message)
+		assert_instance_of(Array, t.generic_type.specialize, message)
+		assert_respond_to(t.generic_type.specialize, :class, message)
+		assert_not_nil(t.generic_type.specialize.class, message)
+		assert_not_nil(t.generic_type.specialize.class.name, message)
+#		assert_not_empty(t.generic_type.specialize, message)
+#		assert_not_empty(t.which_generic_type(:specialize), message)
+		assert_instance_of(GenericType, t.which_generic_type, message)
+		assert_instance_of(GenericType, t.which_generic_type(:generalize), message)
+		assert_instance_of(Array, t.which_generic_type(:specialize), message)
+#		t.assert_generic_type(nil, "t=#{t.inspect}, t.generic_type=#{t.generic_type.inspect}, t.generic_type=#{t.generic_type.inspect}", message)
+#		t.assert_generic_type(:generalize, "t=#{t.inspect}, t.generic_type=#{t.generic_type.inspect}, t.generic_type=#{t.generic_type.inspect}", message)
+#		t.assert_generic_type(:specialize, "t=#{t.inspect}, t.generic_type=#{t.generic_type.inspect}, t.generic_type=#{t.generic_type.inspect}", message)
 	end #each
 end #which_generic_type
 def test_assert_generic_type
@@ -35,9 +63,12 @@ def test_assert_generic_type
 		t.assert_generic_type(:specialize, 'specialize not valid')
 	end #each
 end #generic_type
-def test_example_type_valid
+def test_assert_example_type_valid
 	ExampleType.all.each do |t|
-		t.assert_example_type_valid(:specialize, "t=#{t.inspect}")
+		message= "t=#{t.inspect}"
+		t.assert_example_type_valid(nil, message)
+		t.assert_example_type_valid(:generalize, message)
+		t.assert_example_type_valid(:specialize, message)
 	end #each
 end #example_type_valid
 end #ExampleType
