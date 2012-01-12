0ef232c558da205bedefea51099c60b8926faf10
Greg Lawson <GregLawson123@gmail.com>
2012-01-12 07:08:08 -0800

    methods generalizations and specializations added.

--- app/models/generic_type.rb
+++ app/models/generic_type.rb
@@ -6,16 +6,38 @@ has_many :specialize, :class_name => "GenericType",
 belongs_to :generalize, :class_name => "GenericType",
     :foreign_key => "generalize_id"
 
-#def example_types
-#	return ExampleType.find_all_by_generic_type(self[:generic_type])
-#end #example_types
-def ancestors(candidates=GenericType.all)
-	
-	candidates.select {|c| c.example_types.all?{|e| e.valid_parent?(e[:example_string])}}
-end #ancestors
-#def generalize
-#	return GenericType.find(self[:generalize])
-#end #generalize
+def generalizations
+	if generalize==self then
+		return []
+	else
+		return generalize.generalizations << generalize
+	end #if
+end #generalizations
+def most_general?
+	return generalize==self || generalize.nil?
+end #most_general
+def unspecialized?
+	return specialize.empty?
+end #unspecialized
+# find Array of more specific types (tree children)
+def one_level_specializations
+	if most_general? then
+		return specialize-[self]
+	elsif unspecialized? then
+		return []
+	else
+		specialize
+	end #if
+end #one_level_specializations
+def specializations
+	if most_general? then
+		return (specialize-[self]).map{|s| s.specializations}.flatten + one_level_specializations
+	elsif unspecialized? then
+		return []
+	else
+		return specialize.map{|s| s.specializations}.flatten + one_level_specializations
+	end #if
+end #specializations
 def descendants(candidates=GenericType.all)
 	candidates.select {|c| c.example_types.all?{|e| !e.valid_parent?(e[:example_string])}}
   
--- test/unit/generic_type_test.rb
+++ test/unit/generic_type_test.rb
@@ -13,19 +13,57 @@ def test_example_types
 #	assert_equal([], t.example_types.map{|t| t.attributes})
     end #each
 end #example_types
-def test_ancestors
-	assert(GenericType.all.any? {|t| !t.ancestors.empty?})
-	assert_include("VARCHAR_Column", GenericType.find_by_import_class('Integer_Column').ancestors.map{|a| a.import_class})
-	assert_include("Text_Column", GenericType.find_by_import_class('Integer_Column').ancestors.map{|a| a.import_class})
+def test_generalizations
+	assert_instance_of(GenericType, GenericType.find_by_import_class('digit'))
+	assert_equal(["Text_Column", "VARCHAR_Column", "ascii", "print", "graph", "word", "alnum", "xdigit"], GenericType.find_by_import_class('digit').generalizations.map{|g| g.import_class})
+	assert(GenericType.all.any? {|g| !g.generalizations.empty?})
+	assert_include("VARCHAR_Column", GenericType.find_by_import_class('Integer_Column').generalizations.map{|a| a.import_class})
+	assert_include("Text_Column", GenericType.find_by_import_class('Integer_Column').generalizations.map{|a| a.import_class})
 	GenericType.all.each do |t|
 		assert_instance_of(GenericType, t)
-		assert_instance_of(Array, t.ancestors)
-		if !t.ancestors.empty? then
-			assert_instance_of(GenericType, t.ancestors[0])
+		assert_instance_of(Array, t.generalizations)
+		if !t.generalizations.empty? then
+			assert_instance_of(GenericType, t.generalizations[0])
 		end #if
 	end #each
-	assert_equal(["VARCHAR_Column", "Text_Column"], GenericType.find_by_import_class('Integer_Column').ancestors.map{|a| a.import_class})
-end #ancestors
+	assert_equal_sets(["VARCHAR_Column", "Text_Column"], GenericType.find_by_import_class('Integer_Column').generalizations.map{|a| a.import_class})
+end #generalizations
+def test_most_general
+	most_general=GenericType.find_by_import_class('Text_Column')
+	assert(most_general.most_general?)
+	
+end #most_general
+def test_unspecialized
+	digit=GenericType.find_by_import_class('digit')
+	assert_empty(digit.specialize)
+	assert(digit.unspecialized?)
+	most_general=GenericType.find_by_import_class('Text_Column')
+	assert_not_empty(most_general.specialize)
+	assert(!most_general.unspecialized?)
+end #unspecialized
+def test_one_level_specializations
+	assert(GenericType.all.any? {|t| !t.one_level_specializations.empty?})
+	GenericType.all.each do |t|
+		assert_instance_of(GenericType, t)
+		assert_instance_of(Array, t.one_level_specializations)
+		if !t.one_level_specializations.empty? then
+			assert_instance_of(GenericType, t.one_level_specializations[0])
+		end #if
+	end #each
+	assert_include('Integer_Column', GenericType.find_by_import_class("VARCHAR_Column").one_level_specializations.map{|a| a.import_class})
+	assert_not_include('Integer_Column', GenericType.find_by_import_class("Text_Column").one_level_specializations.map{|a| a.import_class})
+	assert_equal(["cntl", "print", "space"], GenericType.find_by_import_class('ascii').one_level_specializations.map{|g| g.import_class})
+end #one_level_specializations
+def test_specializations
+	assert_equal(["lower", "upper", "digit", "alpha", "xdigit", "alnum", "word", "punct", "graph", "blank", "cntl", "print", "space"], GenericType.find_by_import_class('ascii').specializations.map{|g| g.import_class})
+	most_general=GenericType.find_by_import_class('Text_Column')
+#	assert_equal(["lower", "upper", "digit", "alpha", "xdigit", "alnum", "word", "punct", "graph", "blank", "cntl", "print", "space"], GenericType.find_by_import_class('Text_Column').specializations.map{|g| g.import_class})
+
+#	assert_equal(["lower", "upper", "digit", "alpha", "xdigit", "alnum", "word", "punct", "graph", "blank", "cntl", "print", "space"], GenericType.find_by_import_class('Text_Column').specializations.map{|g| g.import_class})
+	GenericType.all.each do |g|
+		assert_instance_of(Array, g.specializations)
+	end #each
+end #specializations
 def test_generalize
 	GenericType.all.each do |t|
 		assert_not_equal(t[:generalize_id], 0, "t=#{t.inspect}")
@@ -41,15 +79,5 @@ def test_generalize
 	assert_equal("VARCHAR_Column", GenericType.find_by_import_class('Integer_Column').generalize.import_class)
 end #generalize
 def test_descendants
-	assert(GenericType.all.any? {|t| !t.descendants.empty?})
-	GenericType.all.each do |t|
-		assert_instance_of(GenericType, t)
-		assert_instance_of(Array, t.descendants)
-		if !t.descendants.empty? then
-			assert_instance_of(GenericType, t.descendants[0])
-		end #if
-	end #each
-	assert_include('Integer_Column', GenericType.find_by_import_class("VARCHAR_Column").descendants.map{|a| a.import_class})
-	assert_include('Integer_Column', GenericType.find_by_import_class("Text_Column").descendants.map{|a| a.import_class})
 end #descendants
 end #GenericType
