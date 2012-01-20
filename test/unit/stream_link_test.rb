d7589e2426a10ce034f08a51eb11c22f202e88b0
Greg Lawson <GregLawson123@gmail.com>
2012-01-20 07:08:14 -0800

    Test fixture names against logical primary keys.

--- test/unit/stream_link_test.rb
+++ test/unit/stream_link_test.rb
@@ -10,8 +10,16 @@ require 'test/test_helper'
 # place in order from low to high level and easy pass to harder, so that first fail is likely the cause.
 # move passing tests toward end
 class StreamLinkTest < ActiveSupport::TestCase
+@@test_name=self.name
+#        assert_equal('Test',@@test_name[-4..-1],"@test_name='#{@test_name}' does not follow the default naming convention.")
+@@model_name=@@test_name.sub(/Test$/, '').sub(/Controller$/, '')
+@@table_name=@@model_name.tableize
+ 
+fixtures @@table_name.to_sym
+fixtures :stream_method_arguments
+#assert_fixture_name(@@table_name)
+@@my_fixtures=fixtures(@@table_name)
 def setup
-	ActiveSupport::TestCase::fixtures :stream_links
 	ActiveSupport::TestCase::fixtures :stream_method_arguments
 	@testURL='http://192.168.3.193/api/LiveData.xml'
 	define_model_of_test # allow generic tests
@@ -20,7 +28,29 @@ def setup
 	assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
 #	define_association_names #38271 associations
 end #def
-  # Replace this with your real tests.
+def test_id_equal
+	assert_fixture_name(@@table_name)
+	assert(!@model_class.sequential_id?, "@model_class=#{@model_class}, should not be a sequential_id.")
+	assert_instance_of(Hash, fixtures(@@table_name))
+	assert_instance_of(Array, @@my_fixtures)
+	@@my_fixtures=fixtures(@@table_name)
+	assert_instance_of(Hash, @@my_fixtures)
+	if @model_class.sequential_id? then
+	else
+		fixtures(:stream_method_arguments).each_pair do |key, ar_from_fixture|
+			assert(Fixtures::identify(key), ar_from_fixture.id)
+			puts "'#{key}', #{ar_from_fixture.id}"
+		end #each
+		@@my_fixtures.each_pair do |key, ar_from_fixture|
+			message="Check that logical key (#{ar_from_fixture.class.logical_primary_key.inspect}) value (#{ar_from_fixture.logical_primary_key_value}) exactly matches yaml label(#{key}) for record."
+			message+=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
+			puts "'#{key}', #{ar_from_fixture.inspect}"
+			assert(Fixtures::identify(key), ar_from_fixture.id)
+			assert_equal(ar_from_fixture.logical_primary_key_recursive_value, key,message)
+			assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_recursive_value),ar_from_fixture.id,message)
+		end
+	end
+end #id_equal
 def test_association_names
   	assert_generic_table('StreamLink')
 	assert_equal_sets([:input_stream_method_argument_id,:output_stream_method_argument_id,:store_method_id,:next_method_id], StreamLink.foreign_key_names)
@@ -32,11 +62,14 @@ def test_association_names
 	end #each
 end #association_names
 def test_output_stream_method_argument
+	assert_not_empty(@@association_patterns)
+	assert_not_empty(StreamLink.association_patterns(:output_stream_method_argument))
 	assert_equal(@@association_patterns, StreamLink.association_patterns(:output_stream_method_argument))
 
 	assert_foreign_key_name(StreamLink,:output_stream_method_argument_id)
 	assert_association(StreamLink, :output_stream_method_argument)
-	assert_equal(:to_one,StreamLink.association_to_type(:output_stream_method_argument))
+	explain_assert_respond_to(StreamLink, :association_macro_type)
+	assert_equal(:belongs_to,StreamLink.association_macro_type(:output_stream_method_argument))
 	StreamLink.find(:all).each do |sl|	
 		assert_not_nil(sl)
 		assert_not_nil(StreamMethodArgument.where("id=?",sl.output_stream_method_argument_id))
@@ -51,7 +84,7 @@ def test_input_stream_method_argument
 	assert_equal(@@association_patterns, StreamLink.association_patterns(:input_stream_method_argument))
 	assert_foreign_key_name(StreamLink,:output_stream_method_argument_id)
 	assert_association(StreamLink, :input_stream_method_argument)
-	assert_equal(:to_one,StreamLink.association_to_type(:input_stream_method_argument))
+	assert_equal(:belongs_to,StreamLink.association_macro_type(:input_stream_method_argument))
 	StreamLink.find(:all).each do |sl|	
 		assert_not_nil(sl)
 		if !sl.association_has_data(:input_stream_method_argument) then
@@ -59,8 +92,11 @@ def test_input_stream_method_argument
 			assert_equal("",sl.association_state(:input_stream_method_argument))
 		end #if
 		message="StreamMethodArgument.where('id=?',#{sl.input_stream_method_argument_id})=#{StreamMethodArgument.where('id=?',sl.input_stream_method_argument_id).inspect}, sl.input_stream_method_argument_id=#{sl.input_stream_method_argument_id}, "
-		assert_equal('Input',StreamMethodArgument.where("id=?",sl.input_stream_method_argument_id).first.direction, message)
 		assert_not_nil(sl.input_stream_method_argument)
 	end #
 end #input_stream_method_argument
-end #class
+def test_store_method
+end #store_method
+def test_next_method
+end #next_method
+end #StreamLink
