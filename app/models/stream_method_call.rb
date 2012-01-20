df9a28c53c0876702b5e3c4538f590be1f42d8c5
Greg Lawson <GregLawson123@gmail.com>
2012-01-20 07:08:54 -0800

    Improve tests

--- app/models/stream_method_call.rb
+++ app/models/stream_method_call.rb
@@ -9,14 +9,30 @@ class StreamMethodCall < ActiveRecord::Base # like a method call
 include Generic_Table
 belongs_to :stream_method
 has_many :stream_links
-def StreamMethodCall.schedule
-	StreamMethodCall.find(:all).each do |c|
-		c.fire
-	end #each
-end #def
+def StreamMethodCall.find_by_name(name)
+	StreamMethodCall.find_by_stream_method(name)
+end #find_by_name
+def StreamMethodCall.acquisitions
+	acquisition_pattern=StreamPattern.find_by_name('Acquisition')
+	StreamMethod.find_by_pattern map do |m|
+			c.fire
+	end #map
+end #acquisitions
 def StreamMethodCall.next_fire
+	if @@fired.nil? then
+		
+	else
+	end #if
 	StreamMethodCall.next
 end #def
+def inputs
+	stream_links.where(:input_stream_method_argument_id => self.id, :direction => 'input')
+	
+end #inputs
+def outputs
+	stream_links.where('output_stream_method_argument_id=?',:id => self.id).find_by_direction('output')
+	
+end #outputs
 def fire
 	inputs=stream_links.where('input_stream_method_argument_id=?',:id => self.id)
 	inputs.each do |input|
--- test/unit/stream_method_call_test.rb
+++ test/unit/stream_method_call_test.rb
@@ -10,23 +10,26 @@ require 'test/test_helper'
 class StreamMethodCallTest < ActiveSupport::TestCase
   # Replace this with your real tests.
 def test_stream_links
-	StreamMethodCall.find(:all).each do |smc|
-		assert(smc.inputs.exists?,"smc=#{smc.inspect}, smc.stream_links=#{smc.inputs.inspect} ")
-	end #each
-end #stream_links
-def test_EEG
 	streamCall=StreamMethodCall.first
 	assert_equal(64810937,streamCall.id)
 	assert_equal_sets(["stream_method","stream_links"], StreamMethodCall.association_names)
-	assert_equal("695672806",StreamPattern.find_by_name('Acquisition').id.inspect)
-	assert_equal('File',StreamMethod.find_by_name('File').name)
-	assert_equal('Acquisition',StreamPatternArgument.where("name='Acquisition'").name)
-#	assert_equal([],StreamMethodArgument.where("stream_pattern='Acquisition'").name)
-	assert_equal('File',StreamMethod.first.name)
-  end
+	assert_equal('', StreamLink.all)
+	assert_equal('', streamCall)
+	assert_not_empty(streamCall.stream_links)
+	StreamMethodCall.find(:all).each do |smc|
+		assert_not_empty(smc.stream_links)
+	end #each
+end #stream_links
+def test_inputs
+	StreamMethodCall.find(:all).each do |smc|
+		assert(smc.inputs.exists?,"smc=#{smc.inspect}, smc.stream_links=#{smc.inputs.inspect} ")
+	end #each
+end #inputs
+def test_outputs
+end #outputs
 def test_fire
 	StreamMethodCall.find(:all).each do |smc|
 		smc.fire
 	end #each
-end #find
 end #fire
+end #StreamMethodCall
