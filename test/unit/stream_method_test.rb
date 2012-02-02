1b0ecfcc0a79f359000bb6e59f808d50cfc3de6c
Greg Lawson <GregLawson123@gmail.com>
2012-02-02 10:25:34 -0800

    Fix logical key recursive value assertion.

--- test/unit/stream_method_test.rb
+++ test/unit/stream_method_test.rb
@@ -190,7 +190,7 @@ def test_id_equal
 			message+=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
 			puts "'#{key}', #{ar_from_fixture.inspect}"
 			assert(Fixtures::identify(key), ar_from_fixture.id)
-			assert_equal(ar_from_fixture.logical_primary_key_recursive_value, key.to_s,message)
+			assert_equal(ar_from_fixture.logical_primary_key_recursive_value.join(','), key.to_s,message)
 			assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_recursive_value),ar_from_fixture.id,message)
 		end #each_pair
 	end #if
