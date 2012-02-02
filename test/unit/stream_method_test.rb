4614d1751e3caa0c747a8968f7afb6c337d28b09
Greg Lawson <GregLawson123@gmail.com>
2012-02-02 10:22:51 -0800

    Update more of standard test.

--- test/unit/stream_method_test.rb
+++ test/unit/stream_method_test.rb
@@ -185,12 +185,15 @@ def test_id_equal
 	assert_instance_of(Hash, @@my_fixtures)
 	if @model_class.sequential_id? then
 	else
-		@my_fixtures.each_value do |ar_from_fixture|
-			message="Check that logical key (#{ar_from_fixture.class.logical_primary_key}) value (#{ar_from_fixture.logical_primary_key_value}) exactly matches yaml label for record."
+		@@my_fixtures.each_pair do |key, ar_from_fixture|
+			message="Check that logical key (#{ar_from_fixture.class.logical_primary_key.inspect}) value (#{ar_from_fixture.logical_primary_key_value}) exactly matches yaml label(#{key}) for record."
 			message+=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
-			assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_value),ar_from_fixture.id,message)
-		end
-	end
+			puts "'#{key}', #{ar_from_fixture.inspect}"
+			assert(Fixtures::identify(key), ar_from_fixture.id)
+			assert_equal(ar_from_fixture.logical_primary_key_recursive_value, key.to_s,message)
+			assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_recursive_value),ar_from_fixture.id,message)
+		end #each_pair
+	end #if
 end #test_id_equal
 
 end #StreamMethod
