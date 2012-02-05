dcb7ef3a8f2e391c902fc7cd0953ccf300d7afea
Greg Lawson <GregLawson123@gmail.com>
2012-02-05 09:22:39 -0800

    Rename test to follow convention. Move test_unknown to end for alignment.

--- test/unit/generic_table_assertions_test.rb
+++ test/unit/generic_table_assertions_test.rb
@@ -1,30 +1,13 @@
 require 'test/test_helper'
-class TestHelperTest < ActiveSupport::TestCase
+class GenericTableAssertionTest < ActiveSupport::TestCase
 @@table_name='stream_patterns'
 fixtures @@table_name.to_sym
 fixtures :table_specs
 require 'test/test_helper_test_tables.rb'
-def test_unknown
-	class_reference=StreamMethodArgument
-	association_reference=:stream_method
-		klass=class_reference
-	association_reference=association_reference.to_sym
-	assert_not_empty(ActiveRecord::Base.instance_methods_from_class)
-	assert_not_include(association_reference.to_s,ActiveRecord::Base.instance_methods_from_class)
-	if (ActiveRecord::Base.instance_methods_from_class(true).include?(association_reference.to_s)) then
-		raise "# Don’t create associations that have the same name (#{association_reference.to_s})as instance methods of ActiveRecord::Base (#{ActiveRecord::Base.instance_methods_from_class.inspect})."
-	end #if
-	assert_instance_of(Symbol,association_reference,"assert_association")
-	if klass.module_included?(Generic_Table) then
-		association_type=klass.association_to_type(association_reference)
-		assert_not_nil(association_type)
-		assert_include(association_type,[:to_one,:to_many])
-	end #if
-	#~ explain_assert_respond_to(klass.new,(association_reference.to_s+'=').to_sym)
-	#~ assert_public_instance_method(klass.new,association_reference,"association_type=#{association_type}, ")
-	assert(klass.is_association?(association_reference),"fail is_association?, klass.inspect=#{klass.inspect},association_reference=#{association_reference}")
-	assert_association(class_reference,association_reference)
-end #test
+def test_assert_foreign_key_name
+end #assert_foreign_key_name
+def test_assert_association
+end #assert_association
 def test_assert_association_to_one
 	class_reference=StreamMethodArgument
 	association_reference=:stream_method
@@ -47,7 +30,7 @@ def test_assert_associations
 	assert(@@CLASS_WITH_FOREIGN_KEY.belongs_to_association?(@@FOREIGN_KEY_ASSOCIATION_SYMBOL) ,"StreamPatternArgument belongs_to stream_pattern")
 	assert(@@FOREIGN_KEY_ASSOCIATION_SYMBOL.to_s.classify.constantize.has_many_association?(@@TABLE_NAME_WITH_FOREIGN_KEY),"#{@@FOREIGN_KEY_ASSOCIATION_SYMBOL} does not has_many #{@@TABLE_NAME_WITH_FOREIGN_KEY}")
 	assert_associations(@@CLASS_WITH_FOREIGN_KEY,@@FOREIGN_KEY_ASSOCIATION_SYMBOL)
-	assert_associations(@@FOREIGN_KEY_ASSOCIATION_SYMBOL,@@CLASS_WITH_FOREIGN_KEY)
+	assert_associations(@@FOREIGN_KEY_ASSOCIATION_SYMBOL,@@CLASS_WITH_FOREIGN_KEY) # reverse arguments of above
 end #assert_associations
 def test_assert_general_associations
 	assert_general_associations(@@table_name)
@@ -83,6 +66,14 @@ def test_assert_not_active_record_method
 	assert(!ActiveRecord::Base.is_active_record_method?(method_name))
 	assert_not_active_record_method(method_name)
 end #assert_not_active_record_method
+def assert_table_exists(table_name)
+end #assert_table_exists
+def test_assert_table
+end #assert_table
+def test_assert_ActiveRecord_table(model_class_name)
+end #assert_ActiveRecord_table
+def test_assert_generic_table
+end #assert_generic_table
 def test_assert_matching_association
 #	assert_matching_association(TestTable,:full_associated_models)	
 #	assert(TestTable.is_matching_association?(:full_associated_models))
@@ -92,7 +83,7 @@ def test_assert_matching_association
 	end #assert_raised
 end  #assert_matching_association
 def test_handle_polymorphic
-	association_type=StreamMethodArgument.association_to_type(:parameter)
+	association_type=StreamMethodArgument.association_arity(:parameter)
 	assert_not_nil(association_type)
 #	assert_include(association_type,[:to_one,:to_many])
 #	assert_association(StreamMethodArgument,:parameter)
@@ -112,6 +103,27 @@ def test_various_assertions
 	assert_include('acquisition_stream_specs',TableSpec.instance_methods(false))
 	ar_from_fixture=table_specs(:ifconfig)
 	assert_not_nil ar_from_fixture.class.similar_methods(:acquisition_stream_spec)
+end #test
+def test_unknown
+	class_reference=StreamMethodArgument
+	association_reference=:stream_method
+		klass=class_reference
+	association_reference=association_reference.to_sym
+	assert_not_empty(ActiveRecord::Base.instance_methods_from_class)
+	assert_not_include(association_reference.to_s,ActiveRecord::Base.instance_methods_from_class)
+	if (ActiveRecord::Base.instance_methods_from_class(true).include?(association_reference.to_s)) then
+		raise "# Don’t create associations that have the same name (#{association_reference.to_s})as instance methods of ActiveRecord::Base (#{ActiveRecord::Base.instance_methods_from_class.inspect})."
+	end #if
+	assert_instance_of(Symbol,association_reference,"assert_association")
+	if klass.module_included?(Generic_Table) then
+		association_type=klass.association_arity(association_reference)
+		assert_not_nil(association_type)
+		assert_include(association_type,[:to_one,:to_many])
+	end #if
+	#~ explain_assert_respond_to(klass.new,(association_reference.to_s+'=').to_sym)
+	#~ assert_public_instance_method(klass.new,association_reference,"association_type=#{association_type}, ")
+	assert(klass.is_association?(association_reference),"fail is_association?, klass.inspect=#{klass.inspect},association_reference=#{association_reference}")
+	assert_association(class_reference,association_reference)
 end #test
 
 end #class
