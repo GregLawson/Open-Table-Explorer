8f1e2e54e8f652c26b9f2011bb934b9a7ad2eaeb
Greg Lawson <GregLawson123@gmail.com>
2012-02-10 15:49:11 -0800

    test/unit/stream_method_argument_test.rb

--- test/unit/stream_method_argument_test.rb
+++ test/unit/stream_method_argument_test.rb
@@ -10,6 +10,7 @@ require 'test/test_helper'
 class StreamMethodArgumentTest < ActiveSupport::TestCase
 set_class_variables
 def test_id_equal
+	assert(!@@model_class.sequential_id?, "@@model_class=#{@@model_class}, should not be a sequential_id.")
 	assert_test_id_equal
 end #test_id_equal
 end #StreamMethodArgument
