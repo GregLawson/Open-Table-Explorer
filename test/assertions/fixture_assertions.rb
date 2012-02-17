cd897dc43751ebff1333c97fd5f4938f7aa5727e
Greg Lawson <GregLawson123@gmail.com>
2012-02-17 07:51:05 -0800

    Factor out assertion of single fixture label.

--- test/assertions/fixture_assertions.rb
+++ test/assertions/fixture_assertions.rb
@@ -17,17 +17,24 @@ def assert_class_variables_defined
 	assert_fixture_name(@@table_name)
 	assert_instance_of(Hash, fixtures?(@@table_name))
 end #assert_class_variables_defined
+def assert_fixture_label(fixture_label, ar_from_fixture)
+	message="Check that logical key (#{ar_from_fixture.class.logical_primary_key.inspect}"
+	message+=" => #{ar_from_fixture.class.logical_primary_key_recursive.inspect})"
+	message+=" value (#{ar_from_fixture.logical_primary_key_value} "
+	message+="=> #{ar_from_fixture.logical_primary_key_recursive_value.inspect})"
+	message+=" exactly matches yaml label(#{fixture_label}) for record."
+	assert_equal(fixture_label.to_s, ar_from_fixture.logical_primary_key_recursive_value.join(','), message)
+	message=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
+#	puts "'#{fixture_label}', #{ar_from_fixture.inspect}"
+#	assert(Fixtures::identify(fixture_label), ar_from_fixture.id)
+	assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_recursive_value.join(',')),ar_from_fixture.id,message)
+end #assert_fixture_label
 def assert_fixture_labels
 	assert_kind_of(ActiveSupport::TestCase, self)
 #	assert_include('@@my_fixtures',ActiveSupport::TestCase.class_variables)
 #	assert_include('@@my_fixtures',self.class_variables)
-	fixtures?(@@table_name).each_pair do |key, ar_from_fixture|
-		message="Check that logical key (#{ar_from_fixture.class.logical_primary_key.inspect} => #{ar_from_fixture.class.logical_primary_key_recursive.inspect}) value (#{ar_from_fixture.logical_primary_key_value} => #{ar_from_fixture.logical_primary_key_recursive_value.inspect}) exactly matches yaml label(#{key}) for record."
-		assert_equal(key.to_s, ar_from_fixture.logical_primary_key_recursive_value.join(','), message)
-		message=" identify != id. ar_from_fixture.inspect=#{ar_from_fixture.inspect} ar_from_fixture.logical_primary_key_value=#{ar_from_fixture.logical_primary_key_value}"
-	#	puts "'#{key}', #{ar_from_fixture.inspect}"
-	#	assert(Fixtures::identify(key), ar_from_fixture.id)
-		assert_equal(Fixtures::identify(ar_from_fixture.logical_primary_key_recursive_value.join(',')),ar_from_fixture.id,message)
+	fixtures?(@@table_name).each_pair do |fixture_label, ar_from_fixture|
+		assert_fixture_label(fixture_label, ar_from_fixture)
 	end #each_pair
 end #assert_id_and_logical_primary_key
 def assert_test_id_equal
