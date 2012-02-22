652d6eff82eaf7480dd725c33436fd28286b8760
Greg Lawson <GregLawson123@gmail.com>
2012-02-21 16:13:23 -0800

    Delete test meant to fail to check error message.

--- test/unit/generic_table_test.rb
+++ test/unit/generic_table_test.rb
@@ -469,7 +469,6 @@ def test_defaulted_primary_logical_key
 	assert(StreamPattern.defaulted_primary_logical_key?, "StreamPattern should use the default :name logical_primary_key.")
 	assert_not_empty(StreamPattern.column_symbols)
 	assert_defaulted_primary_logical_key(StreamPattern)
-	assert_defaulted_primary_logical_key(Url)
 	assert_nil(Url.defaulted_primary_logical_key?, "Url uses :href rather than default :name.")
 end #defaulted_primary_logical_key
 def test_default_logical_primary_key
