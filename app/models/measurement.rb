d0b4355f51bb579e0a81cc82e04475377d870c7a
Greg Lawson <GregLawson123@gmail.com>
2012-02-22 08:19:59 -0800

    Add logical primary key.

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,7 @@
 class Measurement < ActiveRecord::Base
 include Generic_Table
-#belongs_to :nodes
-end
+belongs_to :load
+def self.logical_primary_key
+	return [:id]
+end #logical_primary_key
+end #Measurement
