c7e3f89be582743541db594d6c0a100624215ddd
Greg Lawson <GregLawson123@gmail.com>
2012-02-22 08:23:42 -0800

    Add logical primary key.

--- app/models/stream_parameter.rb
+++ app/models/stream_parameter.rb
@@ -9,4 +9,9 @@ class StreamParameter < ActiveRecord::Base # like the parameters of a method cal
 include Generic_Table
 belongs_to :stream_method_calls
 belongs_to :stream_method_arguments
+has_many :stream_links
+has_many :specifications
+def self.logical_primary_key
+	return [:stream_method_call_id, :stream_method_argument_id]
+end #logical_primary_key
 end #class
