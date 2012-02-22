65e43059195aa29888837ceda01b4db00b283378
Greg Lawson <GregLawson123@gmail.com>
2012-02-22 08:20:24 -0800

    Add logical primary key.

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,4 +2,7 @@ class Node < ActiveRecord::Base
 include Generic_Table
 belongs_to :branch , :polymorphic => true
 belongs_to :parent , :polymorphic => true
+def self.logical_primary_key
+	return [:id]
+end #logical_primary_key
 end #class
