4ea2864c6ee735f109f7c870fcea0f38a1f7a415
Greg Lawson <GregLawson123@gmail.com>
2012-02-22 16:13:13 -0800

    Not sure why this changed, other than to keep it working.

--- script/allservers.sh
+++ script/allservers.sh
@@ -1,4 +1,4 @@
-pkill -9 -f  "ruby1.8 script/rails server -e "
+pkill -9 -f  "/usr/bin/ruby1.8 script/rails server -e"
 rails server -e development -p 3002&
 rails server -e  test -p 3001 &
 rails server -e production -p 3000 &
