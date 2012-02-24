d46eaf1f736c409cdcda68a275872d83a8845ee4
Greg Lawson <GregLawson123@gmail.com>
2012-02-24 08:12:21 -0800

    The -e switch doesn't seem to work anymore, but RAILS_ENV environment variable does.

--- script/allservers.sh
+++ script/allservers.sh
@@ -1,5 +1,8 @@
 pkill -9 -f  "/usr/bin/ruby1.8 script/rails server -e"
+export RAILS_ENV=development
 rails server -e development -p 3002&
-rails server -e  test -p 3001 &
+export RAILS_ENV=production
 rails server -e production -p 3000 &
+export RAILS_ENV=test # last so interactive is test
+rails server -e  test -p 3001 &
 ps -C ruby1.8 -o pid,args
