04f4648fc75ec91e9a2e9633baf142c1807ac1f8
Greg Lawson <GregLawson123@gmail.com>
2012-03-04 09:47:08 -0800

    Correct spelling of file name.

--- app/models/battery_measurement.rb
+++ app/models/battery_measurement.rb
@@ -7,31 +7,16 @@
 ###########################################################################
 class BatteryMeasurement
 include NoDB
+extend NoDB::ClassMethods
 def self.all
 	fixtures = YAML::load( File.open('test/data_sources/battery_measurements.yml' ) )
 	fixtures.values #map
 end #all
-def self.sample(samples_wanted=100, sample_type=:first)
-	size=all.size
-	samples_returned=[samples_wanted, size].min
-	case sample_type
-	when :first
-		return all[0..samples_returned-1]
-	when :last
-		return all[size-samples_returned..size-1]
-	when :random
-		return all[0..samples_returned-1]
-	when :stratified
-		return all[0..samples_returned-1]
-	else
-		raise "Unknown sample type=#{sample_type}. Expected values are :sequential, :random, :stratified"
-	end #case
-end #sample
-def self.columns_present
+def self.column_symbols
 	column_names=sample.map do |r|
 		r.keys.map {|name| name.downcase.to_sym}
 	end.flatten.uniq #map
-end #columns_present
+end #column_symbols
 def self.column_remap
 end #column_remap
 end #BatteryMeasurement
