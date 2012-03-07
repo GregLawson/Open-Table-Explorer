1c263fd7b49d733e7dd43edcf0ea1ee94b5ebb78
Greg Lawson <GregLawson123@gmail.com>
2012-03-06 17:10:31 -0800

    Finished moving RegexpParser to separate file.

--- test/unit/regexp_tree_test.rb
+++ test/unit/regexp_tree_test.rb
@@ -5,97 +5,22 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-require 'test/test_helper.rb'
+require 'test_helper'
 # executed in alphabetical order. Longer names sort later.
 # place in order from low to high level and easy pass to harder, so that first fail is likely the cause.
 # move passing tests toward end
-require 'test/test_helper_test_tables.rb'
-require 'app/models/inlineAssertions.rb'
+#require 'test/test_helper_test_tables.rb'
 class RegexpTreeTest < ActiveSupport::TestCase
-require 'app/models/regexp_tree.rb'
-WhiteSpacePattern=' '
-WhiteEditor=RegexpParser.new(WhiteSpacePattern)	
+set_class_variables
+#require 'app/models/regexp_tree.rb'
 @@CONSTANT_PARSE_TREE=RegexpParser.new('K')
 @@keditor=@@CONSTANT_PARSE_TREE.clone
 @@CONSTANT_PARSE_TREE.freeze
 	assert_equal(['K'],@@CONSTANT_PARSE_TREE.to_a)
 
 KCeditor=RegexpParser.new('KC')
-RowsRegexp='(<tr.*</tr>)'
-RowsEditor=RegexpParser.new(RowsRegexp)
-RowsEdtor2=RegexpParser.new('\s*(<tr.*</tr>)')
 KCETeditor=RegexpParser.new('KCET[^
 ]*</tr>\s*(<tr.*</tr>).*KVIE')
-def test_RegexpParser
-	regexp_string='K.*C'
-	test_tree=RegexpParser.new(regexp_string)
-	assert_equal(regexp_string,test_tree.to_s)
-	assert_not_nil(test_tree.regexp_string)
-	assert_not_nil(RegexpParser.new(test_tree.rest).to_s)
-#	assert_not_nil(RegexpParser.new(nil))
-end #initialize
-def test_RegexpParser_to_a
-	assert_equal(['K','C'], KCeditor.to_a)
-end #to_a
-def test_RegexpParser_to_s
-	assert_equal('KC', KCeditor.to_s)
-end #to_s
-def test_nextToken
-
-	@@keditor.restartParse!
-	assert_equal(@@keditor.nextToken!,'K')
-	KCeditor.restartParse!
-	assert_equal('C',KCeditor.nextToken!)
-	RowsEditor.restartParse!
-	assert_equal(RowsEditor.nextToken!,')')
-end #nextToken!
-def test_rest
-	
-	@@keditor.restartParse!
-	assert_equal(@@keditor.rest,'K')
-
-	KCeditor.restartParse!
-	assert_equal(KCeditor.rest,'KC')
-end #rest
-def test_curlyTree
-end #curlyTree
-def test_parseOneTerm
-	
-
-	@@keditor.restartParse!
-	assert_equal(@@keditor.parseOneTerm!,'K')
-	KCeditor.restartParse!
-	assert_equal('C',KCeditor.parseOneTerm!)
-	KCETeditor.restartParse!
-	assert_equal('E',KCETeditor.parseOneTerm!)
-	assert_equal('I',KCETeditor.parseOneTerm!)
-	assert_equal('V',KCETeditor.parseOneTerm!)
-	assert_equal('K',KCETeditor.parseOneTerm!)
-	assert_equal(['.','*'],KCETeditor.parseOneTerm!)
-end #parseOneTerm!
-def test_regexpTree
-	
-	@@keditor.restartParse!
-	assert_equal(@@keditor.regexpTree!,['K'])
-	KCeditor.restartParse!
-	assert_equal(['K','C'],KCeditor.regexpTree!)
-	assert_equal(["(", "<", "t", "r", [".", "*"], "<", "/", "t", "r", ">"],RowsEditor.regexpTree!('('))
-end #regexpTree!
-def test_conservationOfCharacters
-	regexp_string='K.*C'
-	test_tree=RegexpParser.new(regexp_string)
-	assert_not_nil(test_tree.parseTree)
-	assert_not_nil(RegexpParser.new(''))
-	assert_equal('', RegexpParser.new(test_tree.rest).to_s)
-	assert_equal(["K", [".", "*"], "C"], test_tree.parseTree)
-	assert_not_nil(RegexpParser.new(["K", [".", "*"], "C"].to_s))
-	assert_not_nil(RegexpParser.new(test_tree.parseTree.to_s))
-	assert_not_nil(test_tree.rest.to_s+test_tree.parseTree.to_s)
-	assert_equal(test_tree.regexp_string, test_tree.rest.to_s+test_tree.parseTree.to_s)
-	assert_equal(test_tree.regexp_string, test_tree.rest+test_tree.parseTree.to_s)
-	test_tree.conservationOfCharacters
-end #conservationOfCharacters
-#end #RegexpParser
 def test_String_to_a
 	assert_equal(['a', 'b', 'c'],'abc'.to_a)
 	assert_not_equal(['b', 'b', 'c'],'abc'.to_a)
@@ -261,19 +186,19 @@ def test_macro_call
 	assert_equal(':', inner_colons[-1])
 	assert_equal('alnum', RegexpTree.macro_call?(parseTree[0]))
 end #macro_call?
-Nested_Test_Array=["t", "e", "s", "t", "/",
+Test_Pattern_Array=["t", "e", "s", "t", "/",
 	  	[["[", "a", "-", "z", "A", "-", "Z", "0", "-", "9", "_", "]"], "*"],
 	 	["[", ".", "]"],
 	 	"r",
 		[["[", "a", "-", "z", "]"], "*"]]
-Test_Pattern=RegexpTree.new(Nested_Test_Array)
+Test_Pattern=RegexpTree.new(Test_Pattern_Array)
 def test_to_filename_glob
 	assert_equal('*', RegexpTree.new(['.','*']).to_pathname_glob)
 	assert_equal('*', RegexpTree.new([['.','*']]).to_pathname_glob)
 	assert_equal('K*C', RegexpTree.new(['K',['.','*'],'C']).to_pathname_glob)
 	assert_equal('app/models/*[.]rb', RegexpTree.new('app/models/([a-zA-Z0-9_]*)[.]rb').to_pathname_glob)
-	assert_equal(Nested_Test_Array, RegexpTree.new('test/[a-zA-Z0-9_]*[.]r[a-z]*').to_a)
-	assert_equal(Nested_Test_Array, Test_Pattern.map_branches{|b| (b[0]=='('?RegexpTree.new(b[1..-2]):RegexpTree.new(b))})
+	assert_equal(Test_Pattern_Array, RegexpTree.new('test/[a-zA-Z0-9_]*[.]r[a-z]*').to_a)
+	assert_equal(Test_Pattern_Array, Test_Pattern.map_branches{|b| (b[0]=='('?RegexpTree.new(b[1..-2]):RegexpTree.new(b))})
 	assert_equal('test/*[.]r*', Test_Pattern.postfix_operator_walk{|p| '*'}.to_s)
 	assert_equal('test/*[.]r*', RegexpTree.new('test/[a-zA-Z0-9_]*[.]r[a-z]*').to_pathname_glob)
 end #to_pathname_glob
@@ -346,14 +271,8 @@ def test_editor
 	
 	regexpParserTest(KCETeditor)
 end #def
-def setup
-	define_model_of_test # allow generic tests
-#	assert_module_included(@model_class,Generic_Table)
-#	explain_assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
-#	assert_respond_to(@model_class,:sequential_id?,"#{@model_name}.rb probably does not include include Generic_Table statement.")
-end #def
 def test_zero_parameter_new
 	assert_nothing_raised{RegexpTree.new} # 0 arguments
-	assert_not_nil(@model_class)
+	assert_not_nil(@@model_class)
 end #test_name_correct
 end #test class
