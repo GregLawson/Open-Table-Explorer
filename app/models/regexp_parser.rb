6861bbde36fffd95ef99c42a8f9c8c457e03c953
Greg Lawson <GregLawson123@gmail.com>
2012-03-06 17:06:39 -0800

    Move RegexpParser to separate file.

---
+++ app/models/regexp_parser.rb
@@ -0,0 +1,103 @@
+###########################################################################
+#    Copyright (C) 2010-2012 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+# parse tree internal format is nested Arrays.
+# Postfix operators and brackets end embeddded arrays
+require 'app/models/inlineAssertions.rb'
+class RegexpParser
+attr_reader :regexp_string,:tokenIndex,:parseTree
+def initialize(regexp_string)
+	raise "RegexpParser.new currently only handles String arguments. regexp_string=#{regexp_string.inspect}" unless regexp_string.kind_of?(String)
+	@regexp_string=regexp_string
+	restartParse!
+	@parseTree=regexpTree!
+end #initialize
+def to_a
+	return @parseTree
+end #to_a
+def to_s
+	return to_a.join
+end #to_s
+def restartParse! # primarily for testing
+	if @regexp_string.nil? then
+		@tokenIndex=-1 # start at end
+	else
+		@tokenIndex=@regexp_string.length-1 # start at end
+	end #if
+	@parseTree=[]
+end #def
+def nextToken!
+	if beyondString? then
+		raise RuntimeError, "method nextToken! called after end of regexp_string."
+	elsif @tokenIndex>1 && @regexp_string[@tokenIndex-1..@tokenIndex-1]=='\\' then
+		ret='\\'+@regexp_string[@tokenIndex..@tokenIndex]
+		@tokenIndex=@tokenIndex-2
+	else
+		ret=@regexp_string[@tokenIndex..@tokenIndex]
+		@tokenIndex=@tokenIndex-1
+	end
+	return ret
+end #nextToken!
+def rest
+	if beyondString? then
+		return ''
+	else
+		return @regexp_string[0..@tokenIndex]
+	end #if
+end #rest
+def advanceToken!(increment)
+	@tokenIndex=@tokenIndex+increment
+end #advanceToken!
+# test if parsing has gone beyond end of string and should stop
+def beyondString?(testPos=@tokenIndex)
+	@regexp_string.nil? || testPos<0 || testPos>@regexp_string.length-1
+end
+# handle {2,3}-style specification of repetitions
+# not currently used, since numbers are not interpreted
+# currently handled identically to parenthesis and square brackets
+def curlyTree!(regexp_string)
+	remaining=rest
+	matchData=/\{(\d*)(,(\d*))?\}/.match(remaining)
+	increment=matchData[1].length+matchData[2].length+1
+	advanceToken!(increment)
+	return ['{',[matchData[1],matchData[2]],'}']
+end #curlyTree
+# parse matching brackets, postfix operator, or single character
+def parseOneTerm!
+	ch=nextToken!
+	index=RegexpTree.ClosingBrackets.index(ch)
+	if index then
+		return  regexpTree!(RegexpTree.OpeningBrackets[index].chr) << ch
+	else
+		index=RegexpTree.PostfixOperators.index(ch)
+		if index then
+			return  [parseOneTerm!, ch]
+		else
+			return ch
+		end #if
+	end #if
+end #parseOneTerm!
+def regexpTree!(terminator=nil)
+	ret=[]
+	begin
+		if !beyondString? then
+			term=parseOneTerm!
+			ret << term
+		else
+			return ret.reverse
+		end #if
+	end until term==terminator
+#not recursive	conservationOfCharacters(ret.reverse)
+	return ret.reverse
+end #regexpTree!
+# test 
+def conservationOfCharacters
+	message="Regexp parse error: regexp_string=#{@regexp_string},rest=#{rest},parseTree.inspect=#{parseTree.inspect}."
+	raise message if @regexp_string!=(rest+@parseTree.to_s)
+end #conservationOfCharacters
+
+end #RegexpParser
---
+++ test/unit/regexp_parser_test.rb
@@ -0,0 +1,98 @@
+###########################################################################
+#    Copyright (C) 2010-2012 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require 'test_helper'
+# executed in alphabetical order. Longer names sort later.
+# place in order from low to high level and easy pass to harder, so that first fail is likely the cause.
+# move passing tests toward end
+#require 'test/test_helper_test_tables.rb'
+class RegexpParerTest < ActiveSupport::TestCase
+set_class_variables
+#require 'app/models/regexp_tree.rb'
+WhiteSpacePattern=' '
+WhiteEditor=RegexpParser.new(WhiteSpacePattern)	
+@@CONSTANT_PARSE_TREE=RegexpParser.new('K')
+@@keditor=@@CONSTANT_PARSE_TREE.clone
+@@CONSTANT_PARSE_TREE.freeze
+	assert_equal(['K'],@@CONSTANT_PARSE_TREE.to_a)
+
+KCeditor=RegexpParser.new('KC')
+RowsRegexp='(<tr.*</tr>)'
+RowsEditor=RegexpParser.new(RowsRegexp)
+RowsEdtor2=RegexpParser.new('\s*(<tr.*</tr>)')
+KCETeditor=RegexpParser.new('KCET[^
+]*</tr>\s*(<tr.*</tr>).*KVIE')
+def test_RegexpParser
+	regexp_string='K.*C'
+	test_tree=RegexpParser.new(regexp_string)
+	assert_equal(regexp_string,test_tree.to_s)
+	assert_not_nil(test_tree.regexp_string)
+	assert_not_nil(RegexpParser.new(test_tree.rest).to_s)
+#	assert_not_nil(RegexpParser.new(nil))
+end #initialize
+def test_RegexpParser_to_a
+	assert_equal(['K','C'], KCeditor.to_a)
+end #to_a
+def test_RegexpParser_to_s
+	assert_equal('KC', KCeditor.to_s)
+end #to_s
+def test_nextToken
+
+	@@keditor.restartParse!
+	assert_equal(@@keditor.nextToken!,'K')
+	KCeditor.restartParse!
+	assert_equal('C',KCeditor.nextToken!)
+	RowsEditor.restartParse!
+	assert_equal(RowsEditor.nextToken!,')')
+end #nextToken!
+def test_rest
+	
+	@@keditor.restartParse!
+	assert_equal(@@keditor.rest,'K')
+
+	KCeditor.restartParse!
+	assert_equal(KCeditor.rest,'KC')
+end #rest
+def test_curlyTree
+end #curlyTree
+def test_parseOneTerm
+	
+
+	@@keditor.restartParse!
+	assert_equal(@@keditor.parseOneTerm!,'K')
+	KCeditor.restartParse!
+	assert_equal('C',KCeditor.parseOneTerm!)
+	KCETeditor.restartParse!
+	assert_equal('E',KCETeditor.parseOneTerm!)
+	assert_equal('I',KCETeditor.parseOneTerm!)
+	assert_equal('V',KCETeditor.parseOneTerm!)
+	assert_equal('K',KCETeditor.parseOneTerm!)
+	assert_equal(['.','*'],KCETeditor.parseOneTerm!)
+end #parseOneTerm!
+def test_regexpTree
+	
+	@@keditor.restartParse!
+	assert_equal(@@keditor.regexpTree!,['K'])
+	KCeditor.restartParse!
+	assert_equal(['K','C'],KCeditor.regexpTree!)
+	assert_equal(["(", "<", "t", "r", [".", "*"], "<", "/", "t", "r", ">"],RowsEditor.regexpTree!('('))
+end #regexpTree!
+def test_conservationOfCharacters
+	regexp_string='K.*C'
+	test_tree=RegexpParser.new(regexp_string)
+	assert_not_nil(test_tree.parseTree)
+	assert_not_nil(RegexpParser.new(''))
+	assert_equal('', RegexpParser.new(test_tree.rest).to_s)
+	assert_equal(["K", [".", "*"], "C"], test_tree.parseTree)
+	assert_not_nil(RegexpParser.new(["K", [".", "*"], "C"].to_s))
+	assert_not_nil(RegexpParser.new(test_tree.parseTree.to_s))
+	assert_not_nil(test_tree.rest.to_s+test_tree.parseTree.to_s)
+	assert_equal(test_tree.regexp_string, test_tree.rest.to_s+test_tree.parseTree.to_s)
+	assert_equal(test_tree.regexp_string, test_tree.rest+test_tree.parseTree.to_s)
+	test_tree.conservationOfCharacters
+end #conservationOfCharacters
+end #RegexpParerTest
