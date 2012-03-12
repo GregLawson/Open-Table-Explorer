ad6017566e2b4df8b2cbee20e89fa3b9575064de
Greg Lawson <GregLawson123@gmail.com>
2012-03-12 13:55:23 -0700

    Coumn symbols has already been moved to more generic NoDB module.

--- test/unit/battery_measurement_test.rb
+++ test/unit/battery_measurement_test.rb
@@ -14,10 +14,4 @@ def test_all
 	records=@@model_class.all
 	assert_not_empty(records)
 end #all
-def test_column_symbols
-	assert_include('sample', BatteryMeasurement.methods)
-	wanted_columns=[:multimeter_id, :id, :created_at, :updated_at, :load_current_ma, :battery_id, :load_current_mA, :voltage, :status, :closed_circuit_current_ma]
-	column_names=@@model_class.column_symbols
-	assert_equal([], column_names-wanted_columns, "Unwanted columns:")
-end #column_symbols
 end #BatteryTest
