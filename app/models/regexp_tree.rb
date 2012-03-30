50ba673bad40ea527e7411616d63df5a2872efba
Greg Lawson <GregLawson123@gmail.com>
2012-03-30 13:42:35 -0700

    Move String monkee patch to autoloaded directory.

--- app/models/regexp_tree.rb
+++ app/models/regexp_tree.rb
@@ -12,27 +12,6 @@ require 'app/models/nested_array.rb'
 class RegexpTree < NestedArray
 Default_options=Regexp::EXTENDED | Regexp::MULTILINE
 end #RegexpTree
-class String
-# Create Regexp that exactly matches String.
-# Special Regexp characters are escaped
-# Other special cases may exist like '<'
-def to_exact_regexp(options=RegexpTree::Default_options)
-	return Regexp.new(Regexp.escape(self), options)
-end #to_exact_regexp
-# Convert String to Array of one character Strings
-# Why would this be useful? It doesn't distinquish Regexp special characters
-# It was designed for unpacking binary data into Arrays
-# See http://ruby-doc.org/core-1.9.3/String.html#method-i-unpack
-# it could be a check on parsing:
-# string.to_a=string.to_regexp_exact.source
-def to_a(format='a', packed_length=1)
-	array_length=size/packed_length
-	ret=(0..array_length-1).to_a.map do |i|
-		self[i*packed_length,packed_length].unpack(format)[0]
-	end #map
-	return ret
-end #to_a
-end #String
 class RegexpTree < NestedArray # reopen
 include Inline_Assertions
 def self.OpeningBrackets
