3fa17704154a37e4103c66075a3b0b3e24a2cc0d
Greg Lawson <GregLawson123@gmail.com>
2012-04-19 13:09:08 -0700

    Remove extraneous modules.

--- app/models/regexp_match_assertions.rb
+++ app/models/regexp_match_assertions.rb
@@ -5,47 +5,11 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-# There is a rails method that does this; forgot name
-module Squeeze_Display
-@@Max_Context=100
-@@Max_Lines=5
-def maxedDisplay(str,maxLength=@@Max_Context)
-	if str.length>maxLength then
-		return str[0,maxLength]+' ...'
-	else
-		return str
-	end #if	
-end #def
-def squeezeDisplay(str)
-	if str.nil? then
-		return 'nil'
-	elsif str.empty? then
-		return ''
-	elsif str.length<2*@@Max_Context then
-		return str
-	else
-		return 'Starts with:'+str[0,@@Max_Context]+'\nand ends with: ... '+str[-@@Max_Context,str.length]
-	end
-end #def
-def matchDisplay(regexp)
-	matchData=matchRescued(regexp)
-#	assert_instance_of(matchData,MatchData)
-	if matchData.nil? then
-		# Global::log.info("For /#{regexp}/ no matches.")
-	else
-		# Global::log.info("regexp=/#{regexp}/ has #{matchData.size} matches.")
-		for i in 0..matchData.size-1
-			# Global::log.info("matchData[#{i}]=#{squeezeDisplay(matchData[i]).inspect}")
-		end
-	end
-end #def
-end #module
-module RegexpMatchAssertions
+module RegexpMatchAssertions #file context
 # Assertions (validations)
 include Test::Unit::Assertions
 require 'rails/test_help'
-include Squeeze_Display
-#include Match_Addressing
+#include Squeeze_Display
 def assert_regexp_match(regexp_match=self)
 	assert_respond_to(regexp_match,:consecutiveMatches)
 	assert_not_nil(regexp_match.consecutiveMatches(+1,0,0))
