b78fe6e78301ea6cee2442d9fd90336cad94587a
GregLawson <GregLawson123@gmail.com>
2012-10-29 09:43:12 -0700

    Helpers of rails be gone!

--- test/unit/helpers/example_types_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test/test_helper'
-
-class ExampleTypesHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/stream_method_arguments_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test/test_helper'
-
-class StreamMethodArgumentsHelperTest < ActionView::TestCase
-end
