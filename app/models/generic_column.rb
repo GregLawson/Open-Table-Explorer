2f9b0cca2548252e6018a4d0eecdabda94c551d5
GregLawson <GregLawson123@gmail.com>
2012-10-29 22:29:15 -0700

    Will be part of no_rails

--- app/models/generic_column.rb
+++
@@ -1,6 +0,0 @@
-class GenericColumn < ActiveRecord::Base
-include Generic_Table
-def logical_primary_key
-	return [:model_class, :column_name]
-end #logical_primary_key
-end
--- script/rdoc.sh
+++
@@ -1,6 +0,0 @@
-rdoc --op ../Open-Table-Explorer-github-pages/doc/ app test lib
-pushd ../Open-Table-Explorer-github-pages/
-git branch
-git add doc
-git push
-popd
