bd87d9631006d743b1be6e5cd93cf05a33635a17
GregLawson <GregLawson123@gmail.com>
2012-11-09 13:59:42 -0800

    Rename class tests generically to override

--- test/unit/unbounded_fixnum_assertions_test.rb
+++ test/unit/unbounded_fixnum_assertions_test.rb
@@ -8,15 +8,17 @@
 require_relative 'test_environment'
 require_relative 'default_assertions_test.rb'
 require_relative '../../test/assertions/unbounded_fixnum_assertions.rb'
+def test_CLASS_assert_pre_conditions
 end #assert_pre_conditions
-def test_assert_UnboundedFixnum_invariant_conditions
 	UnboundedFixnum.assert_invariant_conditions	
+#test_CLASS_assert_pre_conditions
+def test_assert_Class_invariant_conditions
 end #assert_UnboundedFixnum_invariant_conditions
 
-def test_assert_UnboundedFixnum_post_conditions
 	UnboundedFixnum::Inf.assert_post_conditions
 	UnboundedFixnum::Neg_inf.assert_post_conditions
 	UnboundedFixnum.assert_post_conditions	
+def test_assert_Class_post_conditions
 end #assert_UnboundedFixnum_post_conditions
 def test_assert_invariant_conditions
 end #assert_invariant_conditions
