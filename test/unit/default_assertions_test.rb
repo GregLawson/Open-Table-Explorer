7c25954e52edd74a28365ee9dfee902db7a6cbe7
GregLawson <GregLawson123@gmail.com>
2012-11-16 12:23:03 -0800

    Comment out debugging puts and fails.

--- test/unit/default_assertions_test.rb
+++ test/unit/default_assertions_test.rb
@@ -40,27 +40,28 @@ def test_test_environment
 	assert_not_include(self.methods(false), :explain_assert_respond_to)
 	assert_not_include(self.class.methods(false), :explain_assert_respond_to)
 	assert_equal([], self.class.methods(false))
-	puts "model_class?::Examples.inspect=#{model_class?::Examples.inspect}"
-	puts "model_class?::Examples.constants.inspect=#{model_class?::Examples.constants.inspect}"
-	puts "model_class?::Examples.instance_methods.inspect=#{model_class?::Examples.instance_methods.inspect}"
-	puts "model_class?::Examples.methods.inspect=#{model_class?::Examples.methods.inspect}"
-	puts "model_class?::Assertions.inspect=#{model_class?::Assertions.inspect}"
-	puts "model_class?::Assertions.constants.inspect=#{model_class?::Assertions.constants.inspect}"
-	puts "model_class?::Assertions.instance_methods.inspect=#{model_class?::Assertions.instance_methods.inspect}"
-	puts "model_class?::Assertions.methods.inspect=#{model_class?::Assertions.methods.inspect}"
+#	puts "model_class?::Examples.inspect=#{model_class?::Examples.inspect}"
+#	puts "model_class?::Examples.constants.inspect=#{model_class?::Examples.constants.inspect}"
+#	puts "model_class?::Examples.instance_methods.inspect=#{model_class?::Examples.instance_methods.inspect}"
+#	puts "model_class?::Examples.methods.inspect=#{model_class?::Examples.methods.inspect}"
+#	puts "model_class?::Assertions.inspect=#{model_class?::Assertions.inspect}"
+#	puts "model_class?::Assertions.constants.inspect=#{model_class?::Assertions.constants.inspect}"
+#	puts "model_class?::Assertions.instance_methods.inspect=#{model_class?::Assertions.instance_methods.inspect}"
+#	puts "model_class?::Assertions.methods.inspect=#{model_class?::Assertions.methods.inspect}"
 	assert_include(model_class?.included_modules, model_class?::Assertions)
 	assert_include(model_class?.included_modules, Test::Unit::Assertions)
 #	assert_equal('Test::Unit::Assertions', self.class.name)
 #	assert_equal([MiniTest::Assertions], self.class.included_modules)
 #	assert_equal([Module, Object, Test::Unit::Assertions, MiniTest::Assertions, PP::ObjectMixin, Kernel, BasicObject], self.class.ancestors)
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end #test_test_case
 def test_assertion_inclusion
 	assert_include(model_class?.included_modules, model_class?::Assertions)
 	assert_include(model_class?.ancestors, Test::Unit::Assertions)
-	assert_include(model_class?.ancestors, model_class?::Examples, "module #{model_class?}::Examples  should exist in class #{model_class?}")
+	assert_include(model_class?.ancestors, model_class?::Examples, "module #{model_class?}::Examples  should exist in class #{model_class?}.\nPlace 'include Examples' within class #{model_class?} scope in assertions file.")
+	assert_include(model_class?.ancestors, DefaultAssertions, "module DefaultAssertions  should exist in class #{model_class?}.\nPlace 'include DefaultAssertions' within class #{model_class?} scope in assertions file.")
 	assert_include(model_class?.included_modules, model_class?::Examples, "module Examples  should be included in class #{model_class?}")
-	assert_include(model_class?.methods, :example_constants_by_class, "model_class?=#{model_class?}")
+	assert_include(model_class?.methods, :example_constants_by_class, "module DefaultAssertions::ClassMethods (including :example_constants_by_class) should exist in class #{model_class?}.\nPlace 'extend DefaultAssertions::ClassMethods' within class #{model_class?} scope in assertions file.")
 	assert_respond_to(model_class?, :example_constants_by_class, "model_class?=#{model_class?}")
 #	assert_respond_to(model_class?, :example_constants_by_class)
 #	assert_include(model_class?.methods, :example_constants_by_class, "model_class?=#{model_class?}")
@@ -68,34 +69,34 @@ end #test_assertion_inclusion
 def test_class_assert_invariant
 	#puts "self.class.methods(true)=#{self.class.methods(true)}"
 	model_class?.assert_invariant
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end # class_assert_invariant
 def test_class_assert_pre_conditions
 	model_class?.assert_pre_conditions
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end #class_assert_pre_conditions
 def test_class_assert_post_conditions
 	model_class?.example_constants_by_class(model_class?).each do |c|
 		c.assert_pre_conditions
 	end #each
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end #class_assert_post_conditions
 def test_assert_pre_conditions
 	model_class?.example_constants_by_class(model_class?).each do |c|
 		c.assert_pre_conditions
 	end #each
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end #assert_pre_conditions
 def test_assert_invariant
 	model_class?.example_constants_by_class(model_class?).each do |c|
 		c.assert_invariant
 	end #each
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end #def assert_invariant
 def test_assert_post_conditions
 	model_class?.example_constants_by_class(model_class?).each do |c|
 		c.assert_post_conditions
 	end #each
-	fail "got to end of default test."
+#	fail "got to end of default test."
 end #assert_post_conditions
 end #DefaultAssertionTests
