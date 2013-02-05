3eaacf5268485611db781b1cf579fe8bfcf8478c
GregLawson <GregLawson123@gmail.com>
2013-02-05 10:04:35 -0800

    Add 2013 to copyright notices

--- test/unit/unbounded_fixnum_test.rb
+++ test/unit/unbounded_fixnum_test.rb
@@ -1,5 +1,5 @@
 ###########################################################################
-#    Copyright (C) 2010-2012 by Greg Lawson                                      
+#    Copyright (C) 2010-2013 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
