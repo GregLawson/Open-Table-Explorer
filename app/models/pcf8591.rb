124462275936c20c1cbc8aa1c0e350cfe478726e
Greg Lawson <GregLawson123@gmail.com>
2013-03-04 08:59:10 -0800

    All tests pass most of the time.

--- app/models/pcf8591.rb
+++ app/models/pcf8591.rb
@@ -6,25 +6,108 @@
 #
 ###########################################################################
 class PCF8591
+module Constants
+module CB
+	# 0-3 analog channels
+	Auto_increment=0x04
+	# single ended or differntial 
+	All_single_ended=0x00
+	Differntial_from_3=0x10
+	Single_single_diff=0x20
+	Differential_pairs=0x30
+	#
+	Analog_output_enable=0x40 # stabilizes internal oscillator
+end #CB
+	CB_Mask=0x77 # zeros in reserved bits
+	Default_control_byte=CB::Auto_increment|CB::All_single_ended|CB::Analog_output_enable
+end #Constants
+include Constants
 def initialize(bus=1, address=0x48)
 	@bus=bus.to_s
 	@address='0x'+address.to_s(16)
+	@last_value=[] # detect disconnected lines and power on
+	@last_control_byte=nil
 end #initialize
 def command_string(command=:get, *args)
-	puts "in command_string, args="+args.inspect
+#	puts "in command_string, args="+args.inspect
 	command_start="sudo i2c#{command.to_s} -y #{@bus} #{@address}"
 	([command_start]+args).map{|a| a.to_s}.join(' ')
 end #command_string
-def adc_start(analog=1)
-	sysout=command_string(:get, analog)
-end
+def select_channel_string(control_byte=Default_control_byte)
+	command_string(:set, '0x'+control_byte.to_s(16)) # set control_byte
+end #select_channel_string
+def get_value_string
+	command_string(:get)
+end #get_value_string
+def control_byte(channel, output_enable, differential)
+end #control_byte
+def adc_read(control_byte=Default_control_byte, burst_length=1)
+	if @last_control_byte!=control_byte then
+		sysout=`#{select_channel_string(control_byte)}` # set control_byte
+		@last_control_byte=control_byte
+	end #if
+#	puts "select_channel_string="+select_channel_string(control_byte) # set control_byte
+	@discard=`#{get_value_string}`.chomp.hex #clear latched value
+#	puts "discard=", @discard
+	ret=Array.new(burst_length) do |index| 
+		sysout=`#{get_value_string}`
+#		puts "get_value_string="+get_value_string
+#		puts sysout
+		sysout.chomp.hex
+	end #Array.new
+	sysout=`#{select_channel_string(control_byte&~CB::Analog_output_enable)}` # set control_byte
+	@last_value[control_byte]=ret[-1]
+	return ret
+end #adc_read
+require_relative '../../test/assertions/default_assertions.rb'
+require_relative '../../test/assertions/ruby_assertions.rb'
 module Assertions
+include DefaultAssertions
+extend DefaultAssertions::ClassMethods
+include Constants
 def assert_pre_conditions
 	assert_instance_of(Class, self)
 end #assert_pre_conditions
+def assert_constant(control_byte=Default_control_byte, samples=64, expected_value=nil)
+	scan=adc_read(control_byte, samples)
+	if (control_byte&CB::Analog_output_enable)==CB::Analog_output_enable then
+		scan=scan.drop_while {|e| e==@discard}
+	end #if
+	unique_values=scan.uniq
+	assert_equal(1, unique_values.size,message)
+	if !expected_value.nil? then
+		assert_equal(expected_value, unique_values[0], message)
+	end #if
+end #assert_constant
+def assert_range(control_byte=Default_control_byte, samples=64, min=0, max=255, max_range=nil)
+	scan=adc_read(control_byte, samples)
+	message="\ncontrol_byte=#{control_byte}\n#{self.inspect} #{scan.inspect}\ndecay=#{@discard.to_f/scan[0].to_f}\n#{caller_lines}"
+	if (control_byte&CB::Analog_output_enable)==CB::Analog_output_enable then
+		scan=scan.drop_while {|e| e==@discard}
+	end #if
+	assert_operator(min, :<=, scan.min, message)
+	assert_operator(max, :>=, scan.max, message)
+	if !max_range.nil? then
+		assert_operator(max_range, :>=, scan.max-scan.min, message)
+	end #if
+end #assert_range
+def assert_disconnected(control_byte=Default_control_byte, samples=64)
+	scan=adc_read(control_byte, samples)
+	message="\ncontrol_byte=#{control_byte}\n#{self.inspect} #{scan.inspect}\ndecay=#{@discard.to_f/scan[0].to_f}\n#{caller_lines}"
+	if (control_byte&CB::Analog_output_enable)==CB::Analog_output_enable then
+		scan=scan.drop_while {|e| e==@discard}
+	end #if
+	assert_operator(@discard/4.0, :<=, scan[0], "under "+message)
+	assert_operator(@discard/2.6, :>=, scan[0], "over "+message)
+	scan[1..-1].each do |value|
+		assert_operator(value, :<=, 1, message)
+	end #each
+end #assert_disconnected
 end #Assertions
-require_relative '../../test/assertions/default_assertions.rb'
-include DefaultAssertions
-extend DefaultAssertions::ClassMethods
+include Assertions
+extend Assertions::ClassMethods
+module Examples
+	YL_40=PCF8591.new(1, 0x48) # all address bits tied low
+end #Examples
 end #PCF8591
 
--- test/unit/pcf8591_test.rb
+++ test/unit/pcf8591_test.rb
@@ -10,6 +10,8 @@ require_relative 'default_test_case.rb'
 require_relative '../../app/models/pcf8591.rb'
 class PCF8591Test < DefaultTestCase2
 include DefaultTests2
+include PCF8591::Examples
+include PCF8591::Constants
 def test_initialize
 	assert_not_nil(PCF8591.new)
 end #initialize
@@ -18,4 +20,27 @@ def test_command_string
 	assert_equal('', [].map{|a| a.to_s}.join(' '))
 	assert_equal("sudo i2cget -y 1 0x48", PCF8591.new.command_string)
 end #command_string
+def test_select_channel_string
+	assert_equal('sudo i2cset -y 1 0x48 0x44', YL_40.select_channel_string)
+end #select_channel_string
+def test_get_value_string
+	assert_equal('sudo i2cget -y 1 0x48', YL_40.get_value_string)
+end #get_value_string
+def test_adc_read
+	analog=1
+	burst_length=512
+	miniboard=PCF8591.new
+	miniboard.assert_range(0+CB::Analog_output_enable, burst_length, 143, 227, 8)	
+	miniboard.assert_range(1+CB::Analog_output_enable, burst_length, 216, 220, 2)
+	miniboard.assert_disconnected(2+CB::Analog_output_enable, burst_length)	
+	miniboard.assert_constant(3+CB::Analog_output_enable, burst_length)
+
+	miniboard.assert_constant(3, burst_length)
+	miniboard.assert_range(1, burst_length, 216, 219, 2)	
+	miniboard.assert_range(0, burst_length, 143, 225, 8)	
+	miniboard.assert_disconnected(2, burst_length)	
+	miniboard.assert_constant(1, 7, 0)	
+	miniboard.assert_constant(3, 7)	
+	miniboard.assert_range(2, 64, 0, 81)	
+end #adc_read
 end #PCF8591
