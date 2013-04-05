9bbd09bd62befa208d6caefd320668ef19847141
GregLawson <GregLawson123@gmail.com>
2013-04-05 12:58:44 -0700

    New open_tax_form filler test_parse passes.

---
+++ app/models/open_tax_form_filler.rb
@@ -0,0 +1,154 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative '../../app/models/no_db.rb'
+class OpenTaxFormFiller
+include NoDB
+extend NoDB::ClassMethods
+module Constants
+Default_tax_year=2012
+Open_tax_filler_directory="../OpenTaxFormFiller/#{Default_tax_year}"
+OTF_definition_filenames="#{Open_tax_filler_directory}/definition/Federal/f*.json"
+Data_source_directory='test/data_sources'
+OTF_SQL_dump_filename="#{Data_source_directory}/OTF_SQL_dump_{Default_tax_year}.sql"
+Symbol_pattern='^ ?([-A-Za-z0-9?]+)'
+Delimiter='\s+'
+Specific_types=['\?\?', '0', ';', '0\s+;', 'Yes']
+Type_pattern='(\s+|'+Specific_types.map{|a| '('+Delimiter+'('+a+')'+Delimiter+')'}.join('|')+')'
+Description_pattern='\{(.+)\}'
+Symbol_regexp=/#{Symbol_pattern}/
+Type_regexp=/#{Symbol_pattern}#{Type_pattern}/
+Description_regexp=/#{Description_pattern}/
+Full_regexp=/#{Symbol_pattern}#{Type_pattern}#{Description_pattern}/
+end #Constants
+# returns array of hashes
+def self.parse(acquisition) #acquisition=next
+	json=JSON[acquisition]
+	entries=[]
+	json['fields'].each_pair do |key, value|
+		flat={}
+		flat[:form]=json['form']
+		flat[:year]=json['year']
+		flat[:line]=key
+		flat[:type]=value
+		entries.push(flat)
+	end #each_pair
+	entries.map do |hash|
+		OpenTaxFormFiller.new(hash, [String, Fixnum, String, String])
+	end #map
+end #parse
+def assert_json_string(acquisition)
+	assert_not_nil(acquisition)
+	assert_instance_of(String, acquisition)
+	json=JSON[acquisition]
+	assert_instance_of(Hash, json)
+end #assert_json_string
+def self.raw_acquisitions
+	all_files=Dir[OTF_definition_filenames]
+	all_files.map do |filename|
+		json=JSON[IO.read(filename)]
+		entries=json['fields'].each_pair do |key, value|
+			flat={}
+			flat[:form]=json['form']
+			flat[:year]=json['year']
+			flat[:line]=key
+			flat[:type]=value
+			flat
+		end #each_pair
+		entries
+	end.compact # map
+end #raw_acquisitions
+def self.coarse_filter
+	raw_acquisitions.select do |acquisition|
+		Type_regexp.match(acquisition) && Description_regexp.match(acquisition)
+	end #select
+end #coarse_filter
+def self.coarse_rejections
+	raw_acquisitions.select do |acquisition|
+		!(Type_regexp.match(acquisition) && Description_regexp.match(acquisition))
+	end #select
+end #coarse_rejections
+def self.all(tax_year=Default_tax_year)
+	coarse_filter.map do |r| #map
+		matchData=Full_regexp.match(r)
+		if matchData then
+			otff=parse(r, Full_regexp)
+			otff[:tax_year]=tax_year
+			otff
+		else
+			nil
+		end #if
+	end.compact #map
+end #all
+def self.fine_rejections
+	coarse_filter.select do |r| #map
+		!Full_regexp.match(r)
+	end #select
+end #fine_rejections
+module Examples
+Simple_acquisition="{\"year\":2012,\"form\":\"f1040\",\"fields\":[{}]}"
+
+
+end #Examples
+require_relative '../../test/assertions/default_assertions.rb'
+
+module Assertions
+include Test::Unit::Assertions
+extend Test::Unit::Assertions
+def assert_pre_conditions
+		assert_instance_of(OpenTaxFormFiller, self)
+		assert_instance_of(Hash, self.attributes)
+		assert_respond_to(self.attributes, :values)
+		assert_scope_path(:DefaultAssertions, :ClassMethods)
+		assert_constant_instance_respond_to(:NoDB, :insert_sql)
+		assert_include(self.class.included_modules, NoDB)
+#		assert_include(NoDB.methods, :insert_sql)
+		assert_includes(self.methods, :insert_sql)
+		explain_assert_respond_to(self, :insert_sql)
+		assert_respond_to(self, :insert_sql)
+		assert_instance_of(Array, attributes.values)
+end #assert_pre_conditions
+module ClassMethods
+include OpenTaxFormFiller::Constants
+include OpenTaxFormFiller::Examples
+include Test::Unit::Assertions
+extend Test::Unit::Assertions
+include DefaultAssertions::ClassMethods
+def assert_pre_conditions
+	assert_scope_path(:DefaultAssertions, :ClassMethods)
+	assert_include(included_modules, NoDB, "")
+end #assert_pre_conditions
+def assert_post_conditions
+#	assert_constant_instance_respond_to(:DefaultAssertions, :ClassMethods, :value_of_example?) #, "In assert_post_conditions calling assert_constant_instance_respond_to"
+	Examples.constants.each do |name|
+		example_acquisition=OpenTaxFormFiller.value_of_example?(name)
+		assert_match(/#{Symbol_pattern}/, example_acquisition)
+		assert_match(/#{Delimiter}/, example_acquisition)
+		assert_match(/#{Type_pattern}/, example_acquisition)
+		assert_match(/#{Description_pattern}/, example_acquisition)
+		assert_match(Symbol_regexp, example_acquisition)
+		assert_match(Type_regexp, example_acquisition)
+		assert_match(Description_regexp, example_acquisition)
+		assert_match(Full_regexp, example_acquisition)
+	end #each
+#hit	fail "end of CLASS assert_post_conditions"
+end #assert_post_conditions
+def assert_json_string(acquisition)
+	assert_not_nil(acquisition)
+	assert_instance_of(String, acquisition)
+	json=JSON[acquisition]
+	assert_instance_of(Hash, json)
+end #assert_json_string
+end #ClassMethods
+end #Assertions
+require_relative '../../test/assertions/default_assertions.rb'
+include Assertions
+include Examples
+include Constants
+extend Assertions::ClassMethods
+end #OpenTaxFormFiller
+
---
+++ test/unit/open_tax_filler_test.rb
@@ -0,0 +1,130 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'test_environment'
+require_relative 'default_test_case.rb'
+require_relative '../../app/models/open_tax_form_filler.rb'
+class OpenTaxFormFillerTest < DefaultTestCase2
+include DefaultTests2
+include OpenTaxFormFiller::Constants
+include OpenTaxFormFiller::Examples
+def test_CLASS_constants
+	
+	assert(File.exists?(Open_tax_filler_directory), Dir["#{Open_tax_filler_directory}/*"].inspect)
+	assert(File.exists?(OTF_definition_filename), "File #{OTF_definition_filename} doesnot exist.")
+	assert(File.exists?(Data_source_directory), Dir["#{Data_source_directory}/*"].inspect)
+	assert(File.exists?(OTF_SQL_dump_filename), Dir["#{Data_source_directory}/*"].inspect)
+	assert_match(/#{Symbol_pattern}/, Simple_acquisition)
+	assert_match(/#{Delimiter}/, Simple_acquisition)
+	assert_match(/#{Type_pattern}/, Simple_acquisition)
+	assert_match(/#{Description_pattern}/, Simple_acquisition)
+	assert_match(Symbol_regexp, Simple_acquisition)
+	assert_match(Type_regexp, Simple_acquisition)
+	assert_match(Description_regexp, Simple_acquisition)
+	assert_match(Full_regexp, Simple_acquisition)
+	OpenTaxFormFiller.assert_post_conditions
+
+end #Constants
+def test_initialize
+	assert_not_nil(OpenTaxFormFiller.new)
+end #initialize
+def test_parse
+	hash={:year => Default_tax_year, :form => 'f1040', 'fields'=> {'L1' => "Amount"}}
+	assert_instance_of(Hash, hash)
+	acquisition=JSON[hash]
+	puts acquisition.inspect
+	assert_not_nil(acquisition)
+	assert_instance_of(String, acquisition)
+	json=JSON[acquisition]
+	assert_instance_of(Hash, json)
+	assert_include(json.keys, 'year')
+	assert_include(json.keys, 'fields')
+	OpenTaxFormFiller::assert_json_string(acquisition)
+	otff=OpenTaxFormFiller.parse(acquisition)
+	assert_equal({:form=>"f1040", :year=>2012, :line=>"L1", :type=>"Amount"},otff[0].attributes, otff[0].inspect)
+	assert_equal([:form, :year, :line, :type],otff[0].attributes.keys, otff[0].inspect)
+	assert_equal(["f1040", 2012, "L1", "Amount"],otff[0].attributes.values, otff[0].inspect)
+	assert_equal('f1040',otff[0][:form])
+	assert_equal(Default_tax_year,otff[0][:year])
+	assert_equal('Amount', otff[0][:type])
+end #parse
+def test_raw_acquisitions
+	all_files=Dir[OTF_definition_filenames]
+	all_files.map do |filename|
+		json=JSON[IO.read(filename)]
+		entries=json['fields'].each_pair do |key, value|
+			flat={}
+			flat[:form]=json['form']
+			flat[:year]=json['year']
+			flat[:line]=key
+			flat[:type]=value
+			assert_equal([:form, :year, :line, :type], flat.keys, flat.keys.inspect)
+			flat
+		end #each_pair
+		entries
+	end.compact # map
+	raw_acquisitions=OpenTaxFormFiller.raw_acquisitions
+	assert_instance_of(Array, raw_acquisitions, raw_acquisitions.inspect)
+	assert_instance_of(Hash, raw_acquisitions[0], raw_acquisitions[0].inspect)
+	assert_equal(9, OpenTaxFormFiller.raw_acquisitions.size, raw_acquisitions.inspect)
+	assert_equal([:form, :year, :line, :type], raw_acquisitions[0].keys, raw_acquisitions.inspect)
+end #raw_acquisitions
+def test_coarse_filter
+	assert_not_empty(OpenTaxFormFiller.coarse_filter.compact, OpenTaxFormFiller.coarse_filter.inspect)
+	assert_operator(84, :==, OpenTaxFormFiller.coarse_filter.size, OpenTaxFormFiller.coarse_filter.inspect)
+end #coarse_filter
+def test_coarse_rejections
+	OpenTaxFormFiller.coarse_rejections.each do |acquisition|
+		puts acquisition if Type_regexp.match(acquisition) 
+		puts acquisition if Description_regexp.match(acquisition)
+	end #select
+	assert_operator(31, :==, OpenTaxFormFiller.coarse_rejections.size, OpenTaxFormFiller.coarse_rejections.inspect)
+end #coarse_rejections
+def test_all
+	ret=OpenTaxFormFiller.coarse_filter.map do |r| #map
+		matchData=Full_regexp.match(r)
+		if matchData then
+			OpenTaxFormFiller.assert_full_match(r)
+			otff=OpenTaxFormFiller.parse(r, Full_regexp)
+			otff[:tax_year]=Default_tax_year
+		else
+			nil
+		end #if
+	end.compact #select
+	assert_not_empty(ret.compact, ret.inspect)
+	assert_operator(84, :==, OpenTaxFormFiller.all.size, OpenTaxFormFiller.fine_rejections.inspect)
+	OpenTaxFormFiller.all(Default_tax_year).each do |ots|
+		assert_instance_of(OpenTaxFormFiller, ots)
+		assert_instance_of(Hash, ots.attributes)
+		assert_respond_to(ots.attributes, :values)
+		assert_scope_path(:DefaultAssertions, :ClassMethods)
+		assert_constant_instance_respond_to(:NoDB, :insert_sql)
+		assert_include(ots.class.included_modules, NoDB)
+#		assert_include(NoDB.methods, :insert_sql)
+		assert_includes(OpenTaxFormFiller.methods, :insert_sql)
+		explain_assert_respond_to(OpenTaxFormFiller, :insert_sql)
+		assert_respond_to(OpenTaxFormFiller, :insert_sql)
+		assert_instance_of(Array, ots.attributes.values)
+		ots.assert_pre_conditions
+		values=ots.insert_sql
+	end #each
+	assert_instance_of(Array, OpenTaxFormFiller.dump)
+	assert_instance_of(String, OpenTaxFormFiller.dump[0])
+	assert_not_equal('"', OpenTaxFormFiller.dump[0][0], OpenTaxFormFiller.dump[0][0..20])
+	assert_equal("\n", OpenTaxFormFiller.dump[0][-1], OpenTaxFormFiller.dump[0][0..20])
+	IO.binwrite(OTF_SQL_dump_filename, OpenTaxFormFiller.dump.join(''))
+end #all
+def test_fine_rejections
+	OpenTaxFormFiller.fine_rejections.each do |r|
+		OpenTaxFormFiller.assert_full_match(r)
+	end #each
+end #fine_rejections
+def test_assert_json_string
+	OpenTaxFormFiller::assert_json_string(Simple_acquisition)
+
+end #assert_full_match
+end #OpenTaxFormFiller
