13b9542e2d1487583f84b6a50cbf1384f71f04ab
GregLawson <GregLawson123@gmail.com>
2013-04-22 11:44:34 -0700

    Rename ShellCommand to ShellCommands

--- app/models/open_tax_solver.rb
+++ app/models/open_tax_solver.rb
@@ -60,12 +60,12 @@ def run_open_tax_solver
 	open_tax_solver_sysout="#{open_tax_solver_data_directory}/US_1040_Lawson_sysout.txt"
 	command="#{Open_tax_solver_binary} #{open_tax_solver_input} >#{open_tax_solver_sysout}"
 	shell_command(command)
-	results=ShellCommand.new(command)
+	results=ShellCommands.new(command)
 end #run_open_tax_solver
 def run_open_tax_solver_to_filler
 	command="nodejs #{@open_Tax_Filler_Directory}/script/json_ots.js #{@open_tax_solver_sysout} > #{Data_source_directory}/US_1040_OTS.json"
 	shell_command(command)
-	results=ShellCommand.new(command)
+	results=ShellCommands.new(command)
 end #run_open_tax_solver_to_filler
 def 	run_tax_form_filler_node
 # The following comments are copied from the README files:
@@ -85,7 +85,7 @@ def 	run_tax_form_filler_node
 #node script/apply_values.js ${YEAR_DIR}/definition/${FORM}.json \
 #       ${YEAR_DIR}/transform/${FORM}.json ${DATA} > /tmp/output.fdf
 	command="nodejs #{Open_Tax_Filler_Directory}/script/apply_values.js #{Open_Tax_Filler_Directory}/#{year_dir}/definition/#{form}.json #{Open_Tax_Filler_Directory}/#{year_dir}/transform/#{form}.json #{data} > #{fdf}"
-	results=ShellCommand.new(command)
+	results=ShellCommands.new(command)
 end #run_tax_form_filler_node
 def 	run_fillin_pdf_form
 #pdftk ${YEAR_DIR}/PDF/${FORM}.pdf fill_form ${fdf} output $3
