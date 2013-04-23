1a7a17e73a3bf1c61cf6c7af8b8e44b04b2a461d
GregLawson <GregLawson123@gmail.com>
2013-04-23 15:22:19 -0700

    Relatively empty StreamMethodArgument
    8 tests, 2 assertions, 0 failures, 0 errors, 0 skips

--- app/models/stream_method_argument.rb
+++ app/models/stream_method_argument.rb
@@ -5,6 +5,8 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
+require_relative '../../app/models/no_db.rb'
+require_relative '../../app/models/generic_table.rb' # in test_helper?
 class StreamMethodArgument < ActiveRecord::Base # like the arguments of a methed def
 include Generic_Table
 belongs_to :stream_method
@@ -12,5 +14,25 @@ has_many :stream_links
 def self.logical_primary_key
 	return [:stream_method_id, :name]
 end #logical_key
-
+module Examples
+end #Examples
+include Examples
+require_relative '../../test/assertions/default_assertions.rb'
+module Assertions
+include DefaultAssertions
+module ClassMethods
+include DefaultAssertions::ClassMethods
+def assert_pre_conditions
+	StreamMethodArgument.all.map do |u|
+	end #map
+#	fail "end of class assert_pre_conditions "
+end #assert_pre_conditions
+end #ClassMethods
+def assert_pre_conditions
+	assert_instance_of(StreamMethodArgument, self)
+#	fail "end of instance assert_pre_conditions"
+end #assert_pre_conditions
+end #Assertions
+include Assertions
+extend Assertions::ClassMethods
 end #StreamMethodArgument
--- test/unit/stream_method_argument_test.rb
+++ test/unit/stream_method_argument_test.rb
@@ -5,12 +5,14 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-require 'test/test_helper'
+require_relative 'test_environment'
+require_relative 'default_test_case.rb'
+require_relative '../../app/models/stream_method_argument.rb' # in test_helper?
 
-class StreamMethodArgumentTest < ActiveSupport::TestCase
-set_class_variables
+class StreamMethodArgumentTest < DefaultTestCase2
+include DefaultTests2
 def test_id_equal
-	assert(!@@model_class.sequential_id?, "@@model_class=#{@@model_class}, should not be a sequential_id.")
-	assert_test_id_equal
+	assert(!model_class?.sequential_id?, "model_class?=#{model_class?}, should not be a sequential_id.")
+#	assert_test_id_equal
 end #test_id_equal
 end #StreamMethodArgument
