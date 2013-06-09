6fa96c80a1e21661c596139f241684277472e0b3
GregLawson <GregLawson123@gmail.com>
2013-06-09 16:44:55 -0700

    Add 3 version editing

---
+++ dct.rb
@@ -0,0 +1,41 @@
+case ARGV.size
+when 0 then puts "dct <branch> <file>; where <branch> is development, compiles, or master"; exit
+end #case
+Edit_branch=ARGV[0]
+File=ARGV[1]
+Test_file=ARGV[2]
+puts ARGV.inspect
+def self.revison_tag(branch)
+	if branch.to_s==Edit_branch then
+		return ''
+	else
+		return '-r '+branch.to_s
+	end #if
+end #revison_tag
+def file_versions(filename)
+	" #{revison_tag(:master)} #{filename} #{revison_tag(:compiles)} #{filename} #{revison_tag(:development)} #{filename}"
+end #file_versions
+command_line="git checkout #{Edit_branch}"
+sysout=`#{command_line}`
+if $?.success? then
+else
+	puts "command_line=#{command_line}, $?=#{$?.inspect}, $?.success?=#{$?.success?}";exit
+end #if
+command_line="diffuse"+ file_versions(File) + ' -t' +file_versions(Test_file)
+sysout=`#{command_line}`
+if $?.success? then
+	command_line="ruby "+ Test_file
+	sysout=`#{command_line}`
+	if $?.success? then
+		puts "command_line=#{command_line}, $?=#{$?.inspect}, $?.success?=#{$?.success?}";exit
+		command_line="git-cola "
+		sysout=`#{command_line}`
+		puts "command_line=#{command_line}, $?=#{$?.inspect}, $?.success?=#{$?.success?}";exit
+	else
+		puts "command_line=#{command_line}, $?=#{$?.inspect}, $?.success?=#{$?.success?}";exit
+	end #if
+else
+	puts "command_line=#{command_line}, $?=#{$?.inspect}";exit
+end #if
+puts command_line
+puts sysout
