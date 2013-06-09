196675d0a17c9c5d95e10a83f08bb41d82729a79
GregLawson <GregLawson123@gmail.com>
2013-06-09 16:43:19 -0700

    Add 3NF sub tables Chemistry, FormFactor, and Brand
    Conflicts:
    	app/models/battery_type.rb

--- app/models/battery_type.rb
+++ app/models/battery_type.rb
@@ -5,13 +5,14 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-require_relative '../../app/models/generic_table.rb'
+#require_relative '../../app/models/generic_table.rb'
+require_relative '../../app/models/no_db.rb'
 require_relative '../../test/assertions/default_assertions.rb'
-class Chemistry < ActiveRecord
+class Chemistry < ActiveRecord::Base
 end #Chemistry
-class FormFactor < ActiveRecord
+class FormFactor < ActiveRecord::Base
 end #FormFactor
-class Brand < ActiveRecord
+class Brand < ActiveRecord::Base
 end #Brand
 class BatteryType
 include NoDB
