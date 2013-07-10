e9107e55033166d774254defeb9d747506a776c7
GregLawson <GregLawson123@gmail.com>
2013-07-10 16:12:20 -0700

    New gnome test

---
+++ test/unit/gnome_test.rb
@@ -0,0 +1,23 @@
+require_relative 'test_environment.rb'
+require_relative '../../app/models/shell_command.rb'
+class Gnome
+module Constants
+Data_Sources_Dir=TE.data_sources_directory?+'gnome_hangs/'
+end #Constants
+include Constants
+end #Gnome
+class GnomeTest < TestCase
+include DefaultTests
+include Gnome::Constants
+#assert_equal(DefaultTestCase0, TestCase)
+#assert_equal(DefaultTestCase0, self)
+def test_lsof
+	lsof=ShellCommands.new('lsof')
+	IO.binwrite(Data_Sources_Dir+'test.lsof', lsof.output)
+end #lsof
+def test_ps
+	ps=ShellCommands.new('ps -ef')
+	IO.binwrite(Data_Sources_Dir+'test.ps', ps.output)
+end #lsof
+end #GnomeTest
+
