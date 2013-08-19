c7ea2b5cf36610848fc2d91477a76dbc93afa77c
Greg Lawson <GregLawson123@gmail.com>
2013-08-19 13:15:41 -0700

    Add homerun unit

---
+++ app/models/homerun.rb
@@ -0,0 +1,47 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative '../../app/models/shell_command.rb'
+class HomeRun
+module ClassMethods
+end #ClassMethods
+extend ClassMethods
+#include DefaultAssertions
+#extend DefaultAssertions::ClassMethods
+def initialize(id='10311E80')
+	@id=id
+end #initialize
+def scan
+	scan=ShellCommands.new("hdhomerun_config #{@id} scan").execute
+end #scan
+require 'test/unit'
+module Assertions
+include Test::Unit::Assertions
+module ClassMethods
+include Test::Unit::Assertions
+def assert_post_conditions
+end #assert_post_conditions
+end #ClassMethods
+def assert_pre_conditions
+end #assert_pre_conditions
+def assert_post_conditions
+	assert_include(self.instance_variables, :@id, "self=#{self.inspect}")
+	assert_not_nil(@id)
+end #assert_post_conditions
+end #Assertions
+include Assertions
+#TestWorkFlow.assert_pre_conditions
+module Constants
+Discover=ShellCommands.new('hdhomerun_config discover', :delay_execution)
+end #Constants
+include Constants
+module Examples
+include Constants
+Default_hdhr=HomeRun.new
+end #Examples
+include Examples
+end #HomeRun
---
+++ test/unit/homerun_test.rb
@@ -0,0 +1,25 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'test_environment.rb'
+require_relative '../../app/models/homerun.rb'
+class HomeRunTest  < TestCase
+include HomeRun::Examples
+def test_initialize
+	Default_hdhr.assert_post_conditions
+end #initialize
+def test_scan
+	scan=Default_hdhr.scan
+	assert_match(/^unable to connect to device\n|^$/, scan.errors, "scan=#{scan.inspect}")
+end
+def test_Constants
+	Discover.execute
+	assert_operator(0, :<, Discover.output.size, "Discover=#{Discover.inspect}")
+	assert_match(/^no devices found\n|^hdhomerun device /, Discover.output, "Discover=#{Discover.inspect}")
+end #Constants
+
+end #HomeRun
