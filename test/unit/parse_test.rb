455d9bbe84d3570c21ee7650aa7f45c921b579c2
Greg Lawson <GregLawson123@gmail.com>
2013-09-02 11:28:50 -0700

    fixup! Regexp

--- test/unit/parse_test.rb
+++ test/unit/parse_test.rb
@@ -1,3 +1,10 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
 require_relative 'test_environment'
 require_relative '../../app/models/parse.rb'
 class ParseTest < TestCase
