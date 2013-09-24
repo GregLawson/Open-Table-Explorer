743c720c5ca1bd2844a833b49533a9973b680510
Greg Lawson <GregLawson123@gmail.com>
2013-09-24 11:25:48 -0700

    fixup! Repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -95,11 +95,11 @@ def downgrade(executable=related_files.model_test_pathname?)
 	downgrade_commit(deserving_branch?(executable), executable)
 end #downgrade
 def stage(target_branch, tested_files)
+	git_command("stash save").assert_post_conditions
 	if current_branch_name? ==target_branch then
 		push_branch=target_branch # no need for stash popping
 	else
 		push_branch=current_branch_name?
-		git_command("stash save").assert_post_conditions
 		switch_branch=git_command("checkout "+target_branch.to_s).execute
 		message="#{current_branch_name?.inspect}!=#{target_branch.inspect}\n"
 		message+="current_branch_name? !=target_branch=#{current_branch_name? !=target_branch}\n"
