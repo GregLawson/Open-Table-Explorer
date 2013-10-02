2924eb5b96c7663f06bd2bc6f84b40a441abd85c
Greg Lawson <GregLawson123@gmail.com>
2013-10-02 15:10:56 -0700

    fixup! OpenTaxFormFiller

--- app/models/open_tax_form_filler.rb
+++ app/models/open_tax_form_filler.rb
@@ -53,7 +53,9 @@ end #GenericJson
 module OpenTaxFormFiller
 module Constants
 Default_tax_year=2012
+Data_source_directory='test/data_sources'
 end #Constants
+
 class Definitions
 include NoDB
 extend NoDB::ClassMethods
@@ -62,9 +64,57 @@ extend GenericJsons::ClassMethods
 include GenericJsons::Assertions
 extend GenericJsons::Assertions::ClassMethods
 module Constants
-Default_tax_year=2012
+include OpenTaxFormFiller::Constants
 Open_tax_filler_directory="../OpenTaxFormFiller/#{Default_tax_year}"
 Input_filenames="#{Open_tax_filler_directory}/definition/Federal/f*.json"
+OTF_SQL_dump_filename="#{Data_source_directory}/OTF_SQL_dump_#{Default_tax_year}.sql"
+end #Constants
+include Constants
+def self.input_file_names
+	Input_filenames
+end #input_file_names
+def table_name?
+	'Definitions'
+end #model_name?
+# returns array of hashes
+def self.parse
+	coarse= raw_acquisitions.map do |acquisition|
+		json=JSON[acquisition]
+		entries=[]
+		json['fields'].each_pair do |key, value|
+			flat={}
+			flat[:form]=json['form']
+			flat[:year]=json['year']
+			flat[:line]=key
+			flat[:type]=value
+			entries.push(flat)
+		end #each_pair
+		entries
+	end.flatten #map
+end #parse
+def self.all
+	All
+end #all
+module Examples
+Simple_acquisition="{\"year\":2012,\"form\":\"f1040\",\"fields\":[{}]}"
+All=Definitions.all_initialize
+
+
+end #Examples
+require_relative '../../test/assertions/default_assertions.rb'
+
+include Examples
+end #Definitions
+
+class Transforms
+include NoDB
+extend NoDB::ClassMethods
+include GenericJsons
+extend GenericJsons::ClassMethods
+module Constants
+include OpenTaxFormFiller::Constants
+Open_tax_filler_directory="../OpenTaxFormFiller/#{Default_tax_year}"
+Input_filenames="#{Open_tax_filler_directory}/transform/Federal/f*.json"
 Data_source_directory='test/data_sources'
 OTF_SQL_dump_filename="#{Data_source_directory}/OTF_SQL_dump_#{Default_tax_year}.sql"
 Symbol_pattern='^ ?([-A-Za-z0-9?]+)'
@@ -177,6 +227,32 @@ end #parse
 def self.all
 	All
 end #all
+# array of indices  to match
+# nil value means need insertion of any characters
+def self.match_regexp_array(combination_indices, acquisition)
+	rest=acquisition
+	regexp_string=Full_regexp_array[combination_indices[0]]
+	combination_indices.each_cons(2) do |pair|
+		if pair[0]+1==pair[1] then # consecutive match
+			added_regexp=Full_regexp_array[pair[1]]
+		else #mismatch deleted
+			added_regexp="(?<error_#{pair[0]}>.*)"
+		end #if
+		regexp_string+=added_regexp
+	end #each_cons
+	regexp=Regexp.new(regexp_string)
+	matchData=regexp.match(acquisition)
+end #match_regexp_array
+def self.leftmost_match(regexp_array, acquisition)
+	[0..regexp_array.size-1].times.find do |i|
+		match_regexp_array(regexp_array[0..i], acquisition)
+	end #find
+end #leftmost_match
+def self.reverse_array_match(regexp_array, acquisition)
+	Array.new(regexp_array.size){|i| regexp_array.size-i}.find do |i|
+		match_regexp_array(regexp_array[i..-1], acquisition)
+	end #find
+end #reverse_array_match
 def self.subset_regexp(size)
 	longest=Full_regexp_array.size
 	Full_regexp_array.combination(longest) do |c|
@@ -198,7 +274,6 @@ end #coarse_filter
 
 include Assertions
 extend Assertions::ClassMethods
-#Pjsons.assert_pre_conditions
 #Pjsons.assert_pre_conditions
 module Examples
 Simple_acquisition="{\"year\":2012,\"form\":\"f1040\",\"fields\":[{}]}"
--- test/unit/open_tax_form_filler_test.rb
+++ test/unit/open_tax_form_filler_test.rb
@@ -32,6 +32,9 @@ include DefaultTests2
 def test_initialize
 	assert_not_nil(OpenTaxFormFiller.new)
 end #initialize
+def model_class?
+	OpenTaxFormFiller::Definitions
+end #model_name?
 def test_parse
 	hash={:year => Default_tax_year, :form => 'f1040', 'fields'=> {'L1' => "Amount"}}
 	assert_instance_of(Hash, hash)
@@ -43,14 +46,14 @@ def test_parse
 	assert_instance_of(Hash, json)
 	assert_include(json.keys, 'year')
 	assert_include(json.keys, 'fields')
-	OpenTaxFormFiller::assert_json_string(acquisition)
-	otff=OpenTaxFormFiller.parse(acquisition)
-	assert_equal({:form=>"f1040", :year=>2012, :line=>"L1", :type=>"Amount"},otff[0].attributes, otff[0].inspect)
-	assert_equal([:form, :year, :line, :type],otff[0].attributes.keys, otff[0].inspect)
-	assert_equal(["f1040", 2012, "L1", "Amount"],otff[0].attributes.values, otff[0].inspect)
-	assert_equal('f1040',otff[0][:form])
-	assert_equal(Default_tax_year,otff[0][:year])
-	assert_equal('Amount', otff[0][:type])
+	Definitions::assert_json_string(acquisition)
+	parsed=Definitions.parse
+	assert_equal({:form=>"1040sa", :year=>2012, :line=>"L1", :type=>"Amount"},parsed[0], parsed[0].inspect)
+	assert_equal([:form, :year, :line, :type],parsed[0].keys, parsed[0].inspect)
+	assert_equal(["1040sa", 2012, "L1", "Amount"],parsed[0].values, parsed[0].inspect)
+	assert_equal('1040sa',parsed[0][:form])
+	assert_equal(Default_tax_year, parsed[0][:year])
+	assert_equal('Amount', parsed[0][:type])
 end #parse
 def test_raw_acquisitions
 	all_files=Dir[OTF_definition_filenames]
@@ -128,3 +131,149 @@ def test_assert_json_string
 
 end #assert_full_match
 end #OpenTaxFormFiller
+class TransformsTest < DefaultTestCase2
+include OpenTaxFormFiller::Transforms::Constants
+include OpenTaxFormFiller::Transforms::Examples
+include OpenTaxFormFiller
+def model_class?
+	OpenTaxFormFiller::Transforms
+end #model_name?
+include DefaultTests2
+def test_parse
+	model_class?.raw_acquisitions.each do |r|
+		assert_equal(["form", "title", "year", "pdfSum", "fields"], JSON[r].keys)
+	end #each
+	hash={:year => Default_tax_year, :form => 'f1040', 'fields'=> {'L1' => "Amount"}}
+	assert_instance_of(Hash, hash)
+	acquisition=JSON[hash]
+#	puts acquisition.inspect
+	assert_not_nil(acquisition)
+	assert_instance_of(String, acquisition)
+	json=JSON[acquisition]
+	assert_instance_of(Hash, json)
+	assert_include(json.keys, 'year')
+	assert_include(json.keys, 'fields')
+	model_class?.assert_json_string(acquisition)
+	parsed=model_class?.parse
+	assert_equal('1040',parsed[0][:form])
+	assert_equal(Default_tax_year,parsed[0][:year])
+end #parse
+def test_dump_sql_to_file
+	filename="#{Data_source_directory}/#{model_name?}_#{Default_tax_year}.sql"
+	assert_equal(:Transforms, model_name?)
+	model_class?.dump_sql_to_file(filename)
+end #dump_sql_to_file
+end #Transforms
+
+class PjsonsTest < DefaultTestCase2
+include OpenTaxFormFiller::Pjsons::Constants
+include OpenTaxFormFiller::Pjsons::Examples
+include OpenTaxFormFiller
+def model_class?
+	OpenTaxFormFiller::Pjsons
+end #model_name?
+include DefaultTests2
+def test_parse
+	array_of_hashes=[]
+	model_class?.raw_acquisitions.each do |acquisition|
+		begin
+		assert_not_nil(acquisition)
+		assert(!(acquisition.nil?), "acquisition should not be nil at start of parse")
+		assert(!(acquisition.size==0), "acquisition should not have a size of zero at start of parse")
+		assert(!(acquisition.empty?), "acquisition should not be empty at start of parse")
+		assert_instance_of(String, acquisition)
+		model_class?.assert_match_regexp_array(acquisition)
+		hash={}
+		longest=Full_regexp_array.size
+		
+		regexp=Regexp.new(Full_regexp_array.join)
+		assert_instance_of(Regexp, regexp)
+		matchData=regexp.match(acquisition)
+		if matchData then
+			matchData.names.map do |n|
+				hash[n.to_sym]=matchData[n]
+			end #map
+			acquisition=matchData.post_match
+			assert(!(acquisition.nil?), "acquisition should not be nil after match")
+#ok at end			assert(!(acquisition.size==0), "acquisition should not have a size of zero after match")
+#ok at end			assert(!(acquisition.empty?), "acquisition should not be empty after match")
+		else
+			acquisition=nil
+			assert(acquisition.nil? | acquisition.empty? | acquisition.size==0)
+		end #if
+		assert_instance_of(Hash, hash)
+		array_of_hashes << hash
+#		puts hash.inspect
+#		puts "'#{acquisition}', length=#{acquisition.size}"
+		end until (acquisition.nil?) | (acquisition.empty?) | (acquisition.size==0)
+		assert((acquisition.nil?) | (acquisition.empty?) | (acquisition.size==0))
+		array_of_hashes
+	end.flatten #map
+	assert_operator(array_of_hashes.size, :>, model_class?.raw_acquisitions.size)
+	assert_array_of(array_of_hashes, Hash) #flatten array
+	parsed=model_class?.parse
+	assert_array_of(parsed, Hash) #flatten array
+	model_class?.assert_parsed
+	assert_operator(parsed.size, :>=, 2080)
+	assert_operator(parsed.uniq.size, :>=, 414)
+	assert_no_duplicates(array_of_hashes)
+	assert_no_duplicates(parsed)
+	assert_equal(parsed.uniq.size, raw_acquisitions.size/4, "parse should be one quarter of raw_acquisitions.")
+	assert_equal(parsed.uniq.size, parsed.size, "parse produces duplicates.")
+	assert_equal(array_of_hashes.uniq.size, array_of_hashes.size, "array_of_hashes produces duplicates.")
+	assert_equal(array_of_hashes.size, 2080)
+end #parse
+def test_match_regexp_array
+	acquisition=model_class?.raw_acquisitions[0]
+	rest=acquisition
+	combination_indices=[0, 1, 2] #passes
+	combination_indices=[0, 1, 2, 4] #passes
+	combination_indices=[0, 1, 2, 3] #fails
+	regexp_string=Full_regexp_array[combination_indices[0]]
+	assert_match(/#{regexp_string}/, acquisition)
+	combination_indices.each_cons(2) do |pair|
+		if pair[0]+1==pair[1] then # consecutive match
+			added_regexp=Full_regexp_array[pair[1]]
+		else #mismatch deleted
+			added_regexp="(?<error_#{pair[0]}>.*)"
+		end #if
+		regexp_string+=added_regexp
+		if matchData=/#{regexp_string}/.match(acquisition) then
+			rest=matchData.post_match
+		else
+			message="regexp_string=/#{regexp_string}/ did not match acquisition"
+			message+="\n#{acquisition[0..100]}"
+			message="\npair=/#{pair}" 
+			message+="\nadding /#{added_regexp}/ did not match '#{rest[0..100]}'"
+			raise message
+		end #if
+	end #each_cons
+	regexp=Regexp.new(regexp_string)
+	assert_match(/#{regexp_string}/, acquisition)
+	matchData=regexp.match(acquisition)
+	assert_not_nil(matchData)
+	acquisition=matchData.post_match
+	assert(matchData=model_class?.match_regexp_array(combination_indices, acquisition))
+	acquisition=matchData.post_match
+	model_class?.assert_match_regexp_array(acquisition,combination_indices)
+end #match_regexp_array
+def test_subset_regexp
+	model_class?.raw_acquisitions.map do |acquisition|
+		assert_instance_of(String, acquisition)
+		longest=Full_regexp_array.size
+		Array.new(Full_regexp_array.size){|i| i}.combination(longest) do |c|
+			matchData=model_class?.match_regexp_array(c, acquisition)
+			if matchData then
+				acquisition=matchData.post_match
+			end #if
+		end #combinations
+		hash
+	end.flatten #map
+end #subset_regexp
+def test_dump_sql_to_file
+	filename="#{Data_source_directory}/#{model_name?}_#{Default_tax_year}.sql"
+	assert_equal(:Pjsons, model_name?)
+	assert_respond_to(model_class?, :dump_sql_to_file)
+	model_class?.dump_sql_to_file(filename)
+end #dump_sql_to_file
+end #Pjsons
