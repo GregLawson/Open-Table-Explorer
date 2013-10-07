bc8eb9bc4cb1b0dcb7c871a325c52d39085b2e40
GregLawson <GregLawson123@gmail.com>
2013-10-07 16:53:51 -0700

    Release

---
+++ app/models/release.rb
@@ -0,0 +1,13 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'test_environment'
+#require_relative '../../app/models/release.rb'
+class ReleaseTest < TestCase
+include DefaultTests
+include TE.model_class?::Examples
+end #Release
