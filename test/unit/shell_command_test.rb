af8bf00cb940c418ab87d533da3c75430e3254eb
GregLawson <GregLawson123@gmail.com>
2013-10-07 06:01:10 -0700

    fixup! ShellCommand

--- test/unit/shell_command_test.rb
+++ test/unit/shell_command_test.rb
@@ -9,14 +9,16 @@ require_relative 'test_environment'
 require_relative '../../app/models/shell_command.rb'
 require_relative '../../app/models/default_test_case.rb'
 class ShellCommandsTest < DefaultTestCase2
+include DefaultTests
 include ShellCommands::Examples
 def test_initialize
 	assert_equal(COMMAND_STRING, EXAMPLE.command_string)
 	assert_equal("1 2;3 4\n", EXAMPLE.output)
 	assert_equal("", EXAMPLE.errors)
-	assert_equal(0, EXAMPLE.exit_status.exitstatus)
-	assert_equal(EXAMPLE.exit_status.pid, EXAMPLE.pid)
+	assert_equal(0, EXAMPLE.process_status.exitstatus)
+	assert_equal(EXAMPLE.process_status.pid, EXAMPLE.pid)
 	assert_equal("Hello World\n", Hello_world.output)
+	Hello_world.assert_post_conditions
 end #initialize
 def test_system_output
 	ret=[] #make method scope not block scope so it can be returned
@@ -26,10 +28,10 @@ def test_system_output
 		stdout.close
 		errors=stderr.read
 		stderr.close
-		exit_status = wait_thr.value  # Process::Status object returned.
+		process_status = wait_thr.value  # Process::Status object returned.
 		pid = wait_thr.pid # pid of the started process.
-		exit_status = wait_thr.value # Process::Status object returned.
-		ret=[output, errors, exit_status, pid]
+		process_status = wait_thr.value # Process::Status object returned.
+		ret=[output, errors, process_status, pid]
 	}
 	ret
 end #system_output
@@ -44,6 +46,10 @@ def test_inspect
 end #inspect
 def test_puts
 	assert_equal(Example_output, EXAMPLE.output)
+	assert_kind_of(Enumerable, caller)
+	assert_instance_of(Array, caller)
+	explain_assert_respond_to(caller, :grep)
+	shorter_callers=caller.grep(/^[^\/]/)
 	assert_equal(EXAMPLE, EXAMPLE.puts) #allow command chaining
 end #puts
 def test_assert_post_conditions
