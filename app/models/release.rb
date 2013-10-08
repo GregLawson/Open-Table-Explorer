e0545af750183b5adb3f7f21f1b7043bd5aa9700
GregLawson <GregLawson123@gmail.com>
2013-10-07 22:57:01 -0700

    Release

---
+++ app/models/release.rb
@@ -0,0 +1,35 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+class Release
+module Constants
+end #Constants
+include Constants
+module ClassMethods
+end #ClassMethods
+extend ClassMethods
+#include DefaultAssertions
+#extend DefaultAssertions::ClassMethods
+module Assertions
+include Test::Unit::Assertions
+module ClassMethods
+include Test::Unit::Assertions
+def assert_post_conditions
+end #assert_post_conditions
+end #ClassMethods
+def assert_pre_conditions
+end #assert_pre_conditions
+def assert_post_conditions
+end #assert_post_conditions
+end #Assertions
+include Assertions
+extend Assertions::ClassMethods
+#self.assert_pre_conditions
+module Examples
+include Constants
+end #Examples
+end #Release
