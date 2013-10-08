5464e01f1a3768f024a2c449ade117ebe0667e21
GregLawson <GregLawson123@gmail.com>
2013-10-07 23:02:29 -0700

    fixup! WorkFlow

--- script/work_flow.rb
+++ script/work_flow.rb
@@ -29,8 +29,20 @@ OptionParser.new do |opts|
   opts.on("-b", "--[no-]best", "Best. Merge down, no conflicts. ") do |t|
     commands+=[:test] if t
   end
-  opts.on("-x", "--[no-]emacs", "Emacs unit edit. ") do |t|
+  opts.on("-m", "--[no-]emacs", "Emacs unit edit. ") do |t|
     commands+=[:emacs] if t
+  end
+  opts.on("-x", "--[no-]execute", "Unit test and edit. ") do |t|
+    commands+=[:execute] if t
+  end
+  opts.on("-p", "--[no-]passed", "Stage file to passed. ") do |t|
+    commands+=[:passed] if t
+  end
+  opts.on("-g", "--[no-]testing", "Stage file to testing. ") do |t|
+    commands+=[:testing] if t
+  end
+  opts.on("-i", "--[no-]edited", "Stage file to edited. ") do |t|
+    commands+=[:edited] if t
   end
 end.parse!
 
@@ -51,14 +63,17 @@ argv.each do |f|
 	editTestGit=WorkFlow.new(f)
 	commands.each do |c|
 		case c.to_sym
-		when :execute then editTestGit.execute
+		when :execute then editTestGit.execute(f)
 		when :edit then editTestGit.edit
-		when :test then editTestGit.test
-		when :upgrade then editTestGit.upgrade
-		when :downgrade then editTestGit.downgrade
-		when :best then editTestGit.best
-		when :emacs then editTestGit.emacs
+		when :test then editTestGit.test(f)
+		when :upgrade then editTestGit.upgrade(f)
+		when :downgrade then editTestGit.downgrade(f)
+		when :best then editTestGit.best(f)
+		when :emacs then editTestGit.emacs(f)
+		when :passed then editTestGit.repository.stage_files(:passed, [f])
+		when :testing then editTestGit.repository.stage_files(:testing, [f])
+		when :edited then editTestGit.repository.stage_files(:edited, [f])
 		end #case
-		editTestGit.repository.git_command('status').puts
+		$stdout.puts editTestGit.repository.git_command('status').inspect
 	end #each
 end #each
