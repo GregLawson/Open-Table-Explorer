51beacab2cc2d720a4bcf84c38579bad8e2d6030
GregLawson <GregLawson123@gmail.com>
2013-10-27 07:17:48 -0700

    fixup! Minimal4

--- app/models/minimal4.rb
+++ app/models/minimal4.rb
@@ -1,1 +1,15 @@
-minimal3.rb
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+class Minimal4
+module Constants
+end #Constants
+include Constants
+module ClassMethods
+end #ClassMethods
+extend ClassMethods
+end #Minimal
