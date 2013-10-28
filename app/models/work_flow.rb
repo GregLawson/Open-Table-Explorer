7af525ac50c81b5a304d9a6c0564dabbe93d0de3
GregLawson <GregLawson123@gmail.com>
2013-10-28 10:46:46 -0700

    fixup! WorkFlow

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -126,10 +126,11 @@ def unit_test(executable=@related_files.model_test_pathname?)
 		edit
 	end until !@repository.something_to_commit? 
 end #unit_test
-def all
-	pattern=FilePattern.find_by_name(:test)
+def all(pattern_name=:test)
+	pattern=FilePattern.find_by_name(pattern_name)
 	glob=pattern.pathname_glob
 	tests=Dir[glob]
+	puts tests.inspect if $VERBOSE
 	tests.each do |test|
 		WorkFlow.new(test).unit_test
 	end #each
