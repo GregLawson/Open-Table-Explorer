d1eda1f7427f02fcb7d8ee981e1df07a15f18393
GregLawson <GregLawson123@gmail.com>
2013-11-02 17:21:58 -0700

    fixup! TestMethod

---
+++ test/unit/test_method_test.rb
@@ -0,0 +1,18 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'test_environment'
+require_relative '../../app/models/test_method.rb'
+class TestMethodTest < TestCase
+include DefaultTests
+include TE.model_class?::Examples
+def test_initialize
+	grep=ShellCommands.new('grep "def test_" test/unit/work_flow_test.rb')
+	parse(grep.output, /def test_/.capture(:method_name))
+	assert_equal([:initialize], TestMethod.new($0))
+end #initialize
+end #TestMethod
