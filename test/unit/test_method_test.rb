58f198f4613b98b21f368e1eb361dadd24c4b33a
GregLawson <GregLawson123@gmail.com>
2013-11-03 11:32:55 -0800

    fixup! TestMethod

--- test/unit/test_method_test.rb
+++ test/unit/test_method_test.rb
@@ -12,7 +12,7 @@ include DefaultTests
 include TE.model_class?::Examples
 def test_initialize
 	grep=ShellCommands.new('grep "def test_" test/unit/work_flow_test.rb')
-	parse(grep.output, /def test_/.capture(:method_name))
+	Parse.parse(grep.output, /def test_/.capture(:method_name))
 	assert_equal([:initialize], TestMethod.new($0))
 end #initialize
 end #TestMethod
