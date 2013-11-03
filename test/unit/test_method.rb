eff968469fadf5666061bdb525c9cb085c05a365
GregLawson <GregLawson123@gmail.com>
2013-11-02 17:04:42 -0700

    fixup! TestMethod

---
+++ test/unit/test_method.rb
@@ -0,0 +1,44 @@
+###########################################################################
+#    Copyright (C) 2013 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative '../../app/models/no_db.rb'
+require_relative '../../app/models/shell_command.rb'
+require_relative '../../app/models/regexp.rb'
+class TestMethod
+module ClassMethods
+end #ClassMethods
+extend ClassMethods
+module Constants
+end #Constants
+include Constants
+# attr_reader
+def initialize(test_executable)
+	grep=ShellCommand.new(' grep "def test_" '+test_executable)
+	@test_executable=test_executable
+	@method_test_names=grep.output.parse(/def test_/.capture(:method_name))
+end #initialize
+module Assertions
+include Test::Unit::Assertions
+module ClassMethods
+include Test::Unit::Assertions
+def assert_pre_conditions
+end #assert_pre_conditions
+def assert_post_conditions
+end #assert_post_conditions
+end #ClassMethods
+def assert_pre_conditions
+end #assert_pre_conditions
+def assert_post_conditions
+end #assert_post_conditions
+end #Assertions
+include Assertions
+extend Assertions::ClassMethods
+#self.assert_pre_conditions
+module Examples
+include Constants
+end #Examples
+end #TestMethod
