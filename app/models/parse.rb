5809870539b9cf421e06099e10f4bca64e18e632
GregLawson <GregLawson123@gmail.com>
2013-11-05 16:22:22 -0800

    fixup! Parse

--- app/models/parse.rb
+++ app/models/parse.rb
@@ -52,8 +52,8 @@ end #parse_string
 # Splits pattern match captures into an array of parses
 # Uses Regexp capture mechanism in String#split
 def parse_split(string, pattern=Terminated_line, ending=:optional)
-	case ending
 	ret=string.split(pattern)
+	case ending
 	when :optional then 
 		if ret[-1].nil? then
 			ret[0..-2] #drop empty
@@ -67,7 +67,6 @@ def parse_split(string, pattern=Terminated_line, ending=:optional)
 		else
 			ret
 		end #if 
-	else
 	end #case
 end #parse_split
 def parse_array(string_array, pattern=WORDS)
