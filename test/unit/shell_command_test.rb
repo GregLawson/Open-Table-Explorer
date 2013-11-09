0841d24298fd2a1c2c6e77b6843ef755b2bfcb3a
GregLawson <GregLawson123@gmail.com>
2013-11-09 12:47:06 -0800

    fixup! ShellCommand Test command as Array.

--- test/unit/shell_command_test.rb
+++ test/unit/shell_command_test.rb
@@ -18,6 +18,7 @@ def test_initialize
 	assert_equal(0, EXAMPLE.process_status.exitstatus)
 	assert_equal("Hello World\n", Hello_world.output)
 	Hello_world.assert_post_conditions
+	assert_equal('', ShellCommands.new(['cd', '/tmp', ';', 'echo', '$SECONDS', '>', 'blank in filename.shell_command']).output)
 end #initialize
 def test_system_output
 	ret=[] #make method scope not block scope so it can be returned
