d2b150d85b0c789fc2e1af7679fef052f3828f48
GregLawson <GregLawson123@gmail.com>
2013-11-10 19:14:14 -0800

    fixup! ShellCommand

--- test/unit/shell_command_test.rb
+++ test/unit/shell_command_test.rb
@@ -11,6 +11,8 @@ require_relative '../../app/models/default_test_case.rb'
 class ShellCommandsTest < DefaultTestCase2
 include DefaultTests
 include ShellCommands::Examples
+def test_execute
+end #execute
 def test_initialize
 	assert_equal(COMMAND_STRING, EXAMPLE.command_string)
 	assert_equal("1 2;3 4\n", EXAMPLE.output)
@@ -19,23 +21,20 @@ def test_initialize
 	assert_equal("Hello World\n", Hello_world.output)
 	Hello_world.assert_post_conditions
 	assert_not_equal('', ShellCommands.new([['cd', '/tmp'], ';', ['echo', '$SECONDS']]).output)
-	assert_equal("# On branch testing\nnothing to commit, working directory clean\n", ShellCommands.new([['cd', File.dirname($0)], ';', ['git', 'status']]).output)
+	assert_pathname_exists($0)
+	guaranteed_existing_directory=File.expand_path(File.dirname($0))+'/'
+	guaranteed_existing_basename=File.basename($0)
+	cd_command=['cd', guaranteed_existing_directory]
+	cd_command={:command => 'cd', :in => guaranteed_existing_directory}
+	relative_command=['pwd']
+#	relative_command=['ls', guaranteed_existing_basename]
+#	relative_command=['ls', 'guaranteed_existing_basename', '>', 'blank in filename.shell_command']
+	shell_execution2=ShellCommands.new([relative_command]).assert_post_conditions(shell_execution2.inspect)
+	shell_execution=ShellCommands.new([cd_command, '&&', relative_command])
+	shell_execution.assert_post_conditions
+	assert_equal(guaranteed_existing_directory+"\n", shell_execution.output, shell_execution.inspect)
 	assert_equal("$SECONDS > blank in filename.shell_command\n", ShellCommands.new([['cd', '/tmp'], ';', ['echo', '$SECONDS', '>', 'blank in filename.shell_command']]).output)
 end #initialize
-def test_system_output
-	ret=[] #make method scope not block scope so it can be returned
-	Open3.popen3(COMMAND_STRING) {|stdin, stdout, stderr, wait_thr|
-		stdin.close  # stdin, stdout and stderr should be closed explicitly in this form.
-		output=stdout.read
-		stdout.close
-		errors=stderr.read
-		stderr.close
-		process_status = wait_thr.value  # Process::Status object returned.
-		process_status = wait_thr.value # Process::Status object returned.
-		ret=[output, errors, process_status]
-	}
-	ret
-end #system_output
 def test_success?
 	assert(EXAMPLE.success?)
 end #success
