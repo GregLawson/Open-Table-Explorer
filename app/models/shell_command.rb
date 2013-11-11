f7b1cd47257f4aac3e49c6a7b5f0f1285d54cd97
GregLawson <GregLawson123@gmail.com>
2013-11-10 18:43:40 -0800

    fixup! ShellCommand

--- app/models/shell_command.rb
+++ app/models/shell_command.rb
@@ -25,8 +25,20 @@ def initialize(command)
 				@command_string=Shellwords.join(e)
 			elsif e.instance_of?(Hash) then
 				command_array=[]
-				e.each_pair do |key, value|
-					command_array+=Shellwords.escape(value)
+				e.each_pair do |key, word|
+					case key
+					when :command then command_array << Shellwords.escape(word)
+					when :in then 
+						raise "Input file '#{word}' does not exist." if !File.exists?(word)
+						command_array << Shellwords.escape(word)
+					when :out then command_array << Shellwords.escape(word)
+					when :inout then command_array << Shellwords.escape(word)
+					when :glob then 
+						raise "Input pathname glob '#{word}' does not exist." if !Dir(word)==[]
+						command_array << Shellwords.escape(word)
+					else
+						command_array << word
+					end #case
 				end #each_pair
 				@command_string=command_array.join(' ')
 			else
