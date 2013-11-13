74a72cf52501a6bf367b7572a95362bd84a85815
GregLawson <GregLawson123@gmail.com>
2013-11-12 19:23:04 -0800

    fixup! ShellCommand

--- app/models/shell_command.rb
+++ app/models/shell_command.rb
@@ -59,7 +59,7 @@ extend ClassMethods
 attr_reader :command_string, :output, :errors, :process_status
 # execute same command again (also called by new.
 def execute
-	puts "@command="+@command.inspect
+	info "@command="+@command.inspect
 	Open3.popen3(@command_string) {|stdin, stdout, stderr, wait_thr|
 		stdin.close  # stdin, stdout and stderr should be closed explicitly in this form.
 		@output=stdout.read
