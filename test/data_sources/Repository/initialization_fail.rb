8d118076135295b2eb11d436b20d85ef0b80c514
GregLawson <GregLawson123@gmail.com>
2013-11-19 07:32:16 -0800

    fixup! Repository

---
+++ test/data_sources/Repository/initialization_fail.rb
@@ -0,0 +1,4 @@
+class Test<TestCase
+def test_initialize
+	fail
+end #initializeend #Test
---
+++ test/data_sources/Repository/single_test_fail.rb
@@ -0,0 +1,6 @@
+class Test<TestCase
+def test_initialize
+end #initializedef test_fail
+	fail
+end #initialize
+end #Test
