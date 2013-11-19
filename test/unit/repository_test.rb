d3f23aea6f09f712ae53ed6e634951744e56c0cd
GregLawson <GregLawson123@gmail.com>
2013-11-19 00:50:20 -0800

    fixup! Repository

--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -110,7 +110,10 @@ def test_error_score?
 		assert_equal("Syntax OK\n", syntax_test.output, syntax_test.inspect)
 	assert_equal(0, SELF_code_Repo.error_score?('test/unit/minimal2_test.rb'))
 	SELF_code_Repo.assert_deserving_branch(:passed, executable)
-
+	Error_classification.each_pair do |key, value|
+		executable=Data_source_directory+'/'+model_name?+value.to_s+'.rb'
+		assert_equal(key, error_score?(executable), executable)
+	end #each
 end #error_score
 def test_deserving_branch
 	executable='/etc/mtab' #force syntax error with non-ruby text
