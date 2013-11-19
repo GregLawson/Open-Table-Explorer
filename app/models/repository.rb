9a280acc4925e82cea348b5c37d4cd645ab5a3d2
GregLawson <GregLawson123@gmail.com>
2013-11-18 17:34:09 -0800

    fixup! Repository
    test/unit/repository_test.rb:195: syntax error, unexpected tSYMBEG
    ...al_repository.path+'README']. :echo)
    ...                               ^
    test/unit/repository_test.rb:256: syntax error, unexpected $end,
    expecting keyword_end

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -178,7 +178,7 @@ def unit_names?(files)
 		FilePattern.path2model_name?(f).to_s
 	end #map
 end #unit_names?
-def validate_commit(changes_branch, files)
+def validate_commit(changes_branch, files, interact=:interactive)
 	puts files.inspect if $VERBOSE
 	files.each do |p|
 		puts p.inspect  if $VERBOSE
@@ -190,7 +190,13 @@ def validate_commit(changes_branch, files)
 			commit_message+= "\n"+@recent_test.errors if !@recent_test.errors.empty?
 		end #if
 		IO.binwrite('.git/GIT_COLA_MSG', commit_message)	
-		git_command('cola').assert_post_conditions
+		case interact
+		when :interactive then
+			git_command('cola').assert_post_conditions
+		when :echo then
+			puts "changes_branch="+changes_branch
+			puts "files="+files.inspect
+		end #case
 #		git_command('rebase --autosquash --interactive')
 	end #if
 end #validate_commit
@@ -255,6 +261,72 @@ end #force_change
 def revert_changes
 	git_command('reset --hard')
 end #revert_changes
+module Assertions
+include Test::Unit::Assertions
+module ClassMethods
+include Test::Unit::Assertions
+def assert_post_conditions
+end #assert_post_conditions
+end #ClassMethods
+def assert_pre_conditions
+	assert_pathname_exists(path)
+	assert_pathname_exists(path+'.git/')
+	assert_pathname_exists(path+'.git/logs/')
+	assert_pathname_exists(path+'.git/logs/refs/')
+end #assert_pre_conditions
+def assert_post_conditions
+end #assert_post_conditions
+def assert_nothing_to_commit(message='')
+	status=@grit_repo.status
+	message+="git status=#{inspect}\n@grit_repo.status=#{@grit_repo.status.inspect}"
+	assert_equal({}, status.added, 'added '+message)
+	assert_equal({}, status.changed, 'changed '+message)
+	assert_equal({}, status.deleted, 'deleted '+message)
+end #assert_nothing_to_commit
+def assert_something_to_commit(message='')
+	message+="git status=#{inspect}\n@grit_repo.status=#{@grit_repo.status.inspect}"
+	assert(something_to_commit?, message)
+end #assert_something_to_commit
+def assert_deserving_branch(branch_expected, executable, message='')
+	deserving_branch=deserving_branch?(executable)
+	recent_test=shell_command("ruby "+executable)
+	message+="\nrecent_test="+recent_test.inspect
+	message+="\nrecent_test.process_status="+recent_test.process_status.inspect
+	syntax_test=shell_command("ruby -c "+executable)
+	message+="\nsyntax_test="+syntax_test.inspect
+	message+="\nsyntax_test.process_status="+syntax_test.process_status.inspect
+	message+="\nbranch_expected=#{branch_expected.inspect}"
+	message+="\ndeserving_branch=#{deserving_branch.inspect}"
+	case deserving_branch
+	when :edited then
+		assert_equal(1, recent_test.process_status.exitstatus, message)
+		assert_not_equal("Syntax OK\n", syntax_test.output, message)
+		assert_equal(1, syntax_test.process_status.exitstatus, message)
+	when :testing then
+		assert_operator(1, :<=, recent_test.process_status.exitstatus, message)
+		assert_equal("Syntax OK\n", syntax_test.output, message)
+	when :passed then
+		assert_equal(0, recent_test.process_status.exitstatus, message)
+		assert_equal("Syntax OK\n", syntax_test.output, message)
+	end #case
+	assert_equal(deserving_branch, branch_expected, message)
+end #deserving_branch
+end #Assertions
+include Assertions
+#TestWorkFlow.assert_pre_conditions
+include Constants
+module Examples
+include Constants
+Removable_Source='/media/greg/SD_USB_32G/Repository Backups/'
+Repo= Grit::Repo.new(Root_directory)
+SELF_code_Repo=Repository.new(Root_directory)
+Empty_Repo_path=Source+'test_repository/'
+Empty_Repo=Repository.replace_or_create(Empty_Repo_path)
+Modified_path=Empty_Repo_path+'/README'
+Unique_repository_directory_pathname=Source+Time.now.strftime("%Y-%m-%d %H:%M:%S.%L")
+
+end #Examples
+include Examples
 end #Repository
 
 
