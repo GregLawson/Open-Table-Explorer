0701e78e24b72b3a00dcf7c6380abc5d0485e5cb
GregLawson <GregLawson123@gmail.com>
2013-11-21 10:03:59 -0800

    fixup! WorkFlow
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/work_flow.rb:40:in
    `initialize': undefined local variable or method `argv' for
    #<WorkFlow:0x8ad1af8> (NameError)
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:222:in `new'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:222:in `<module:Examples>'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:220:in `<class:WorkFlow>'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:13:in `<top (required)>'
    	from test/unit/work_flow_test.rb:9:in `require_relative'
    	from test/unit/work_flow_test.rb:9:in `<main>'

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -34,15 +34,16 @@ extend ClassMethods
 # parametized by related files, repository, branch_number, executable
 # record error_score, recent_test, time
 attr_reader :related_files, :edit_files, :repository
-def initialize(testable_file, related_files=nil, 
-	repository=Repository.new(@related_files.project_root_dir))
+def initialize(testable_file, related_files=nil)
 	@testable_file=testable_file
 	path2model_name=FilePattern.path2model_name?(@testable_file)
-	@related_files=RelatedFile.new(path2model_name, FilePattern.project_root_dir?(argv[0]))
+	related_files=RelatedFile.new(path2model_name, FilePattern.project_root_dir?(argv[0]))
+	@related_files=related_files
 	message= "edit_files do not exist\n argv=#{argv.inspect}" 
 	message+= "\n @related_files.edit_files=#{@related_files.edit_files.inspect}" 
 	message+= "\n @related_files.missing_files=#{@related_files.missing_files.inspect}" 
 #	raise message if  @related_files.edit_files.empty?
+	repository=Repository.new(related_files.project_root_dir)
   @repository=repository
 end #initialize
 def version_comparison(files=nil)
