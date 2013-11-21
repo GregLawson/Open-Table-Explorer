fbd5db26f8d154a6526545f4e5796d510b40c37d
GregLawson <GregLawson123@gmail.com>
2013-11-21 09:53:52 -0800

    fixup! WorkFlow
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/work_flow.rb:38:in
    `initialize': wrong number of arguments (1 for 2) (ArgumentError)
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:221:in `new'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:221:in `<module:Examples>'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:219:in `<class:WorkFlow>'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:13:in `<top (required)>'
    	from test/unit/work_flow_test.rb:9:in `require_relative'
    	from test/unit/work_flow_test.rb:9:in `<main>'

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -16,30 +16,34 @@ module Constants
 Branch_enhancement=[:passed, :testing, :edited]
 Branch_compression={:success	=> 0,
 			:single_test_fail 	=> 1,
-			:multiple_tests_fail	=> 1,
+			:multiple_tests_fail	=> 2,
 			:initialization_fail => 2,
 			:syntax_error        => 2
 			}
 end #Constants
-attr_reader :related_files, :edit_files, :repository
+include Constants
+@@cached_unit_versions={}
 module ClassMethods
 def revison_tag(branch)
 		return '-r '+branch.to_s
 end #revison_tag
 end #ClassMethods
 extend ClassMethods
-def initialize(*argv)
-	argv=argv.flatten
-	raise "Arguments (argv) for WorkFlow.initialize cannot be empty" if argv.empty? 
-	raise "argv must be an array." if !argv.instance_of?(Array)
-	raise "argv[0]=#{argv[0].inspect} must be an String." if !argv[0].instance_of?(String)
-	path2model_name=FilePattern.path2model_name?(argv[0])
+# Define related (unit) versions
+# Use as current, lower/upper bound, branch history
+# parametized by related files, repository, branch_number, executable
+# record error_score, recent_test, time
+attr_reader :related_files, :edit_files, :repository
+def initialize(testable_file, related_files, 
+	repository=Repository.new(@related_files.project_root_dir))
+	@testable_file=testable_file
+	path2model_name=FilePattern.path2model_name?(@testable_file)
 	@related_files=RelatedFile.new(path2model_name, FilePattern.project_root_dir?(argv[0]))
 	message= "edit_files do not exist\n argv=#{argv.inspect}" 
 	message+= "\n @related_files.edit_files=#{@related_files.edit_files.inspect}" 
 	message+= "\n @related_files.missing_files=#{@related_files.missing_files.inspect}" 
 #	raise message if  @related_files.edit_files.empty?
-  @repository=Repository.new(@related_files.project_root_dir)
+  @repository=repository
 end #initialize
 def version_comparison(files=nil)
 	if files.nil? then
@@ -89,10 +93,11 @@ def minimal_comparison?
 		end #if
 	end.compact.join #map
 end #minimal_comparison
-def deserving_branch?
-	error_score=@repository.error_score?(executable=@related_files.model_test_pathname?)
-	error_classification=Error_classification.fetch(error_score, :multiple_tests_fail)
-	Branch_enhancement[Branch_compression[error_classification]]
+def deserving_branch?(executable=@related_files.model_test_pathname?)
+		error_score=@repository.error_score?(executable)
+		error_classification=Repository::Error_classification.fetch(error_score, :multiple_tests_fail)
+		branch_compression=Branch_compression[error_classification]
+		branch_enhancement=Branch_enhancement[branch_compression]
 end #deserving_branch
 def edit
 	edit=ShellCommands.new("diffuse"+ version_comparison + test_files)
@@ -111,7 +116,7 @@ def emacs(executable=@related_files.model_test_pathname?)
 end #emacs
 def test(executable=@related_files.model_test_pathname?)
 	begin
-		deserving_branch=@repository.deserving_branch?(executable)
+		deserving_branch=deserving_branch?(executable)
 		puts deserving_branch if $VERBOSE
 		@repository.safely_visit_branch(deserving_branch) do |changes_branch|
 			@repository.validate_commit(changes_branch, @related_files.tested_files(executable))
@@ -122,7 +127,7 @@ def test(executable=@related_files.model_test_pathname?)
 end #test
 def unit_test(executable=@related_files.model_test_pathname?)
 	begin
-		deserving_branch=@repository.deserving_branch?(executable)
+		deserving_branch=deserving_branch?(executable)
 		if @repository.recent_test.success? then
 			break
 		end #if
@@ -151,7 +156,7 @@ def execute(executable=@related_files.model_test_pathname?)
 	push_branch=@repository.current_branch_name?
 	@repository.git_command("stash save").assert_post_conditions
 #	@repository.stage(:edited, @related_files.tested_files(executable))
-	deserving_branch=@repository.deserving_branch?(executable)
+	deserving_branch=deserving_branch?(executable)
 	@repository.stage(deserving_branch, @related_files.tested_files(executable))
 	@repository.git_command('checkout #{push_branch}')
 	@repository.git_command('stash apply')
--- test/unit/work_flow_test.rb
+++ test/unit/work_flow_test.rb
@@ -67,14 +67,23 @@ def test_minimal_comparison
 	assert_equal(' -t app/models/regexp_parse.rb app/models/minimal4.rb -t test/unit/regexp_parse_test.rb test/unit/minimal4_test.rb -t test/assertions/regexp_parse_assertions.rb test/assertions/minimal4_assertions.rb -t test/unit/regexp_parse_assertions_test.rb test/unit/minimal4_assertions_test.rb', WorkFlow.new('test/unit/regexp_parse_test.rb').minimal_comparison?)
 end #minimal_comparison
 def test_deserving_branch?
-	Error_classification.each_pair do |key, value|
-		executable=data_source_directory?+'/'+value.to_s+'.rb'
-		error_score=SELF_code_Repo.error_score?(executable)
-		assert_equal(key, error_score, SELF_code_Repo.recent_test.inspect)
-		error_classification=Error_classification.fetch(error_score, :multiple_tests_fail)
-		branch_compression=Branch_compression[error_classification]]
-		branch_enhancement= Branch_enhancement[branch_compression]
+	error_classifications=[]
+	branch_compressions=[]
+	branch_enhancements=[]
+	Repository::Error_classification.each_pair do |key, value|
+		executable=data_source_directory?('Repository')+'/'+value.to_s+'.rb'
+		error_score=TestWorkFlow.repository.error_score?(executable)
+		assert_equal(key, error_score, TestWorkFlow.repository.recent_test.inspect)
+		error_classification=Repository::Error_classification.fetch(error_score, :multiple_tests_fail)
+		error_classifications<<error_classification
+		branch_compression=Branch_compression[error_classification]
+		branch_compressions<<branch_compression
+		branch_enhancement=Branch_enhancement[branch_compression]
+		branch_enhancements<<branch_enhancement
 	end #each
+	assert_equal(4, error_classifications.uniq.size, error_classifications.inspect)
+	assert_equal(3, branch_compressions.uniq.size, branch_compressions.inspect)
+	assert_equal(3, branch_enhancements.uniq.size, branch_enhancements.inspect)
 #	error_classification=Error_classification.fetch(error_score, :multiple_tests_fail)
 #	assert_equal(:passed, Branch_enhancement[Branch_compression[error_classification]])
 end #deserving_branch
