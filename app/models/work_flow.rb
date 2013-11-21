3844900ccbf4056d08fc821ce300768425d11611
GregLawson <GregLawson123@gmail.com>
2013-11-21 10:14:49 -0800

    fixup! WorkFlow

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -34,13 +34,13 @@ extend ClassMethods
 # parametized by related files, repository, branch_number, executable
 # record error_score, recent_test, time
 attr_reader :related_files, :edit_files, :repository
-def initialize(testable_file, related_files=nil)
-	related_files=RelatedFile.new_from_path?(testable_file)
+def initialize(testable_file, 
+	related_files=RelatedFile.new_from_path?(testable_file),
+	repository=Repository.new(related_files.project_root_dir))
 #	message= "edit_files do not exist\n argv=#{argv.inspect}" 
 #	message+= "\n related_files.edit_files=#{related_files.edit_files.inspect}" 
 #	message+= "\n related_files.missing_files=#{related_files.missing_files.inspect}" 
 #	raise message if  @related_files.edit_files.empty?
-	repository=Repository.new(related_files.project_root_dir)
 	@testable_file=testable_file
 	@related_files=related_files
 	@repository=repository
