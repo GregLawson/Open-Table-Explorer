3ad40c0ac52aaa52e0de674205cddf3299978463
GregLawson <GregLawson123@gmail.com>
2013-11-21 12:02:08 -0800

    Global

--- app/models/global.rb
+++ app/models/global.rb
@@ -57,12 +57,11 @@ def matching_class_methods(regexp,all=false)
 end #def
 end #module
 class Object
-#~ require 'IncludeModuleClassMethods.rb'
- #~ mixin_class_methods { |klass|
- #~ puts "Module Acquisition has been included by #{klass}" if $VERBOSE
- #~ }
-#~ define_class_methods {
-#~ } #define_class_methods
+def info(message)
+	if $VERBOSE then
+		$stdout.puts message
+	end #if
+end #info     
 def object_identities
 	return "<#{objectClass} \##{object_id}{#{hash}},#{to_s},#{inspect}>"
 end #object_identities
