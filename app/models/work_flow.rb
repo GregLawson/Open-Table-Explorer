9f2484a7649da0b8bf7d69cf452f51aa02adaf7f
GregLawson <GregLawson123@gmail.com>
2013-11-22 11:50:38 -0800

    fixup! WorkFlow

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -24,6 +24,15 @@ end #Constants
 include Constants
 @@cached_unit_versions={}
 module ClassMethods
+def all(pattern_name=:test)
+	pattern=FilePattern.find_by_name(pattern_name)
+	glob=pattern.pathname_glob
+	tests=Dir[glob]
+	puts tests.inspect if $VERBOSE
+	tests.each do |test|
+		WorkFlow.new(test).unit_test
+	end #each
+end #test_unit_test_all
 def revison_tag(branch)
 		return '-r '+branch.to_s
 end #revison_tag
@@ -148,15 +157,6 @@ def unit_test(executable=@related_files.model_test_pathname?)
 		edit
 	end until !@repository.something_to_commit? 
 end #unit_test
-def all(pattern_name=:test)
-	pattern=FilePattern.find_by_name(pattern_name)
-	glob=pattern.pathname_glob
-	tests=Dir[glob]
-	puts tests.inspect if $VERBOSE
-	tests.each do |test|
-		WorkFlow.new(test).unit_test
-	end #each
-end #test_unit_test_all
 def execute(executable=@related_files.model_test_pathname?)
 	begin
 	push_branch=@repository.current_branch_name?
