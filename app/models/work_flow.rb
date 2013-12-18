546d2651b84568e0ec9e468217824cd648def7df
GregLawson <GregLawson123@gmail.com>
2013-12-18 03:53:06 -0800

    fixup! WorkFlow
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/work_flow.rb:17:in
    `<module:Constants>': uninitialized constant
    WorkFlow::Branch_enhancement (NameError)
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:15:in `<class:WorkFlow>'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:13:in `<top (required)>'
    	from test/unit/work_flow_test.rb:9:in `require_relative'
    	from test/unit/work_flow_test.rb:9:in `<main>'

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -14,6 +14,7 @@ class WorkFlow
 #include Grit
 module Constants
 Branch_enhancement=[:passed, :testing, :edited]
+Last_slot_index=WorkFlow::Branch_enhancement.size
 Branch_compression={:success	=> 0,
 			:single_test_fail 	=> 1,
 			:multiple_tests_fail	=> 2,
@@ -85,9 +86,19 @@ def version_comparison(files=nil)
 end #version_comparison
 def goldilocks(filename, middle_branch=@repository.current_branch_name?.to_sym)
 	current_index=WorkFlow::Branch_enhancement.index(middle_branch)
-	last_slot_index=WorkFlow::Branch_enhancement.size-1
-	right_index=[current_index+1, last_slot_index].min
-	left_index=right_index-1 
+	right_index=(current_index+1..Last_slot_index).first do
+		true #default
+	end #first
+	if right_index.nil? then
+		right_index=Last_slot_index
+	end #if
+	left_index=(current_index+1..-1).first do
+		true #default
+	end #first
+	if left_index.nil? then
+		left_index=-1
+	end #if
+	left_index=[-1, right_index-1].max 
 	relative_filename=	Pathname.new(filename).relative_path_from(Pathname.new(Dir.pwd)).to_s
 
 	" -t #{WorkFlow.revison_tag(WorkFlow::Branch_enhancement[left_index])} #{relative_filename} #{relative_filename} #{WorkFlow.revison_tag(WorkFlow::Branch_enhancement[right_index])} #{relative_filename}"
