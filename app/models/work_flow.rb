2129afd0773c6990b3244b6a0f265cf2969f4b64
GregLawson <GregLawson123@gmail.com>
2013-12-25 16:18:57 -0800

    fixup! WorkFlow

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -89,7 +89,7 @@ def version_comparison(files=nil)
 end #version_comparison
 def working_different_from?(filename, branch_index)
 	raise filename+" does not exist." if !File.exists?(filename)
-	diff_run=@repository.git_command("diff --summary --shortstat #{WorkFlow::Branch_enhancement[branch_index]} -- "+filename).assert_post_conditions
+	diff_run=@repository.git_command("diff --summary --shortstat #{WorkFlow::Branch_enhancement[branch_index]} -- "+filename)
 	if diff_run.output!='' then
 		false # no difference
 	elsif diff_run.output.split("\n").size>=2 then
