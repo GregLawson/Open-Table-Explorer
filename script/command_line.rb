1b7e3658015595e451574c335b75ab007e47f5ee
GregLawson <GregLawson123@gmail.com>
2013-12-30 17:23:55 -0800

    fixup! CommandLine

--- script/command_line.rb
+++ script/command_line.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 ###########################################################################
 #    Copyright (C) 2013 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
@@ -5,8 +6,10 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
+# @see http://ruby-doc.org/stdlib-2.0.0/libdoc/optparse/rdoc/OptionParser.html#method-i-make_switch
 require 'pp'
 require_relative '../app/models/command_line.rb'
+pp ARGV if $VERBOSE
 commands = []
 OptionParser.new do |opts|
   opts.banner = "Usage: work_flow.rb --<command> files"
