d9625fed3217006889a6275f5cde2b440016e8ef
GregLawson <GregLawson123@gmail.com>
2013-12-31 11:01:19 -0800

    fixup! WorkFlow
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/work_flow.rb:10:in
    `require_relative': /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/repository.rb:204: syntax error, unexpected tLSHFT,
    expecting keyword_end (SyntaxError)
    <<<<<<< HEAD
      ^
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/repository.rb:212:
    syntax error, unexpected tEQQ, expecting keyword_end
    =======
       ^
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/repository.rb:214:
    syntax error, unexpected tRSHFT, expecting keyword_end
    >>>>>>> passed
      ^
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/work_flow.rb:10:in `<top (required)>'
    	from test/unit/work_flow_test.rb:9:in `require_relative'
    	from test/unit/work_flow_test.rb:9:in `<main>'

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -201,17 +201,7 @@ def validate_commit(changes_branch, files, interact=:interactive)
 			commit_message+= "\n"+@recent_test.errors if !@recent_test.errors.empty?
 		end #if
 		IO.binwrite('.git/GIT_COLA_MSG', commit_message)	
-<<<<<<< HEAD
-		case interact
-		when :interactive then
-			git_command('cola').assert_post_conditions
-		when :echo then
-			puts "changes_branch="+changes_branch.to_s
-			puts "files="+files.inspect
-		end #case
-=======
 		confirm_commit(interact)
->>>>>>> passed
 #		git_command('rebase --autosquash --interactive')
 	end #if
 end #validate_commit
