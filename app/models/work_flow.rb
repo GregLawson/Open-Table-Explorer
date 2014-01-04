81c16da0946b2b78343c22779042e35aa6c99879
GregLawson <GregLawson123@gmail.com>
2014-01-04 11:11:24 -0800

    fixup! WorkFlow

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -5,7 +5,7 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-require_relative 'default_test_case.rb'
+#require_relative 'default_test_case.rb'
 require_relative 'related_file.rb'
 require_relative 'repository.rb'
 class WorkFlow
