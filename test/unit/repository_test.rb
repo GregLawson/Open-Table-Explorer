02ccedfa829339a328273e36a608c66e7604ed9b
GregLawson <GregLawson123@gmail.com>
2014-01-04 01:26:58 -0800

    fixup! Repository
    /home/greg/Desktop/src/Open-Table-Explorer/app/models/repository.rb:268:
    warning: character class has '-' without escape: /[a-z0-9A-Z-_]+/

--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -208,6 +208,8 @@ end #revert_changes
 
 
 #ShellCommands.new("rsync -a #{Temporary}recover /media/greg/B91D-59BB/recover").assert_post_conditions
+def test_merge_conflict_files?
+end #merge_conflict_files?
 def test_branches?
 	assert_equal(:master, Minimal_repository.current_branch_name?)
 	explain_assert_respond_to(Parse, :parse_split)
@@ -217,4 +219,6 @@ end #branches?
 def test_remotes?
 	assert_includes(SELF_code_Repo.remotes?, Minimal_repository.current_branch_name?)
 end #branches?
+def test_rebase!
+end #rebase!
 end #Repository
