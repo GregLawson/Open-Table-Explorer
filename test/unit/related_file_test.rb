c7aa01afa70ec110810b4a7b72c85073bbd6126d
GregLawson <GregLawson123@gmail.com>
2014-01-08 17:11:04 -0800

    fixup! RelatedFile

--- test/unit/related_file_test.rb
+++ test/unit/related_file_test.rb
@@ -57,12 +57,13 @@ def test_assertions_test_pathname
 end #assertions_test_pathname?
 def test_data_sources_directory
 	message='TE.data_sources_directory?='+TE.data_sources_directory?+"\n"
-	message+='Dir[TE.data_sources_directory?]='+Dir[TE.data_sources_directory?]+"\n"
+	message+='Dir[TE.data_sources_directory?]='+Dir[TE.data_sources_directory?].inspect+"\n"
 	assert_not_empty(TE.data_sources_directory?, message)
 	assert_empty(Dir[TE.data_sources_directory?], message)
 	related_file=RelatedFile.new('test/unit/tax_form_test.rb')
 	message='related_file='+related_file.inspect+"\n"
 	message+='related_file.data_sources_directory?='+related_file.data_sources_directory?+"\n"
+	message+='Dir[related_file.data_sources_directory?]='+Dir[related_file.data_sources_directory?].inspect+"\n"
 	assert_not_empty(Dir[related_file.data_sources_directory?], message)
 end #data_sources_directory
 def test_pathnames
