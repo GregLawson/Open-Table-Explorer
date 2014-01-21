869bbe72111ed60a96e4719ce2d995238c6e0422
GregLawson <GregLawson123@gmail.com>
2014-01-21 01:35:23 -0800

    fixup! TaxForm

--- test/unit/tax_form_test.rb
+++ test/unit/tax_form_test.rb
@@ -19,6 +19,7 @@ include OpenTableExplorer::Finance::TaxForm::Examples
 def test_Constants
 	assert_pathname_exists(Data_source_directory, 'mkdir test/data_sources/tax_form')
 	assert_pathname_exists(Open_Tax_Filler_Directory)
+#	assert_equal('', 'test/data_sources/tax_form/US_1040/US_1040_example1_out.txt')
 #	assert_pathname_exists(Open_tax_solver_examples_directory)
 #	assert_pathname_exists(Open_tax_solver_data_directory)
 #	assert_pathname_exists(Open_tax_solver_input)
@@ -51,21 +52,36 @@ def test_initialize
 	assert_pathname_exists(CA540_template.open_tax_solver_data_directory+'/'+CA540_template.taxpayer_basename_with_year+'.txt')
 	assert_equal('CA_540_2012_template', CA540_template.taxpayer_basename)
 end #initialize
-def test_build
+def test_example
 	US1040_example.build.assert_build.assert_pdf_to_jpeg
+	CA540_example.build
+	CA540_example.assert_open_tax_solver
+	CA540_example.assert_ots_to_json
+	CA540_example.assert_json_to_fdf
+	CA540_example.assert_pdf_to_jpeg
+	US1040_example.commit_minor_change!(Dir['test/data_sources/tax_form/*/*'], 'fixup! TaxForm update timestamps')
+	CA540_example.build.assert_build
 	CA540_example.build.assert_build.assert_pdf_to_jpeg
+	CA540_example.assert_build
+end #example
+def test_user
 	US1040_user.build.assert_build.assert_pdf_to_jpeg
 	CA540_user.build.assert_build.assert_pdf_to_jpeg
+end #user
+def test_example1
 	US1040_example1.build.assert_build.assert_open_tax_solver
+end #build
+def test_template
 	US1040_template.build.assert_build.assert_open_tax_solver
 	CA540_template.build.assert_build.assert_open_tax_solver
 #	Repository.new(Data_source_directory).git_command('git diff edited -- test/data_sources/tax_form/CA_540/CA_540_2012_example_out.txt').assert_post_conditions
-	commit_minor_change(Dir['test/data_sources/tax_form/*/*'], 'fixup! TaxForm update timestamps')
 end #build
 def test_commit_minor_change!
 	file='test/data_sources/tax_form/CA_540/CA_540_2012_example_out.txt'
-	diff_run=Repository::This_code_repository.git_command('diff -- '+file)
-	assert_operator(diff_run.output.split.size, :>=, 4, diff_run.inspect)
+	current_branch_name=Repository::This_code_repository.current_branch_name?
+	diff_run=Repository::This_code_repository.git_command('diff stash -- '+file).assert_post_conditions
+	diff_run=Repository::This_code_repository.git_command("diff #{current_branch_name.to_s} -- "+file).assert_post_conditions
+	assert_operator(diff_run.output.split.size, :<=, 8, diff_run.inspect)
 	
 #        modified:   test/data_sources/tax_form/CA_540/CA_540_2012_template_out.txt
 #        modified:   test/data_sources/tax_form/US_1040/US_1040_example1_out.txt
@@ -119,11 +135,11 @@ def test_run_json_to_fdf
 #       ${YEAR_DIR}/transform/${FORM}.json ${DATA} > /tmp/output.fdf
 
 #pdftk ${YEAR_DIR}/PDF/${FORM}.pdf fill_form ${FDF} output $3
-	US1040_template.run_json_to_fdf.assert_json_to_fdf
+#	US1040_template.run_json_to_fdf.assert_json_to_fdf
 	US1040_example.run_json_to_fdf.assert_json_to_fdf
 #	CA540_template.run_json_to_fdf.assert_json_to_fdf
 #	CA540_example.run_json_to_fdf.assert_json_to_fdf
-	US1040_user.run_json_to_fdf.assert_json_to_fdf
+#	US1040_user.run_json_to_fdf.assert_json_to_fdf
 #	CA540_user.run_json_to_fdf.assert_json_to_fdf
 end #run_json_to_fdf
 def test_run_fdf_to_pdf
@@ -142,6 +158,42 @@ def 	test_run_pdf_to_jpeg
 #	US1040_user.run_pdf_to_jpeg.assert_pdf_to_jpeg
 #	CA540_user.run_pdf_to_jpeg.assert_pdf_to_jpeg
 end #run_pdf_to_jpeg
+def test_assert_open_tax_solver
+	CA540_example.build
+	peculiar_status=CA540_example.open_tax_solver_run.process_status.exitstatus
+	if File.exists?(CA540_example.open_tax_solver_sysout) then
+		message=IO.binread(CA540_example.open_tax_solver_sysout)
+	else
+		message="file=#{CA540_example.open_tax_solver_sysout} does not exist"
+	end #if
+	message+=CA540_example.open_tax_solver_run.errors
+	CA540_example.open_tax_solver_run.puts
+	puts "peculiar_status=#{peculiar_status}"
+	puts "message='#{message}'"
+	case peculiar_status
+	when 0 then 
+		CA540_example.open_tax_solver_run.assert_post_conditions('else peculiar_status '+message)
+	when 1 then
+		# fed not found
+		message+="\nfed input not found"
+		message+="\nUS1040_example.open_tax_solver_output=#{US1040_example.open_tax_solver_output}\n"
+		CA540_example.open_tax_solver_run.assert_post_conditions('else peculiar_status '+message)
+	when 2 then
+		assert_pathname_exists(CA540_example.open_tax_solver_output)
+		assert_pathname_exists(CA540_example.open_tax_solver_sysout)
+		CA540_example.open_tax_solver_run.assert_post_conditions('else peculiar_status '+message)
+	else
+		warn(message)
+		warn('!CA540_example.open_tax_solver_run.success?='+(!CA540_example.open_tax_solver_run.success?).to_s)
+	end #case
+	CA540_example.build.assert_pdf_to_jpeg
+	CA540_example.build.assert_build
+	CA540_example.build.assert_build.assert_pdf_to_jpeg
+end #assert_open_tax_solver
+def test_assert_build
+	CA540_example.build.assert_build
+	CA540_example.build.assert_build.assert_pdf_to_jpeg
+end #build
 def test_Examples
 	OpenTableExplorer::Finance::TaxForm::Examples.constants.each do |e|
 		value=eval(e.to_s)
