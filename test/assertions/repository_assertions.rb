1fdda80ddef0aa5004485fac8383d053a16446ee
GregLawson <GregLawson123@gmail.com>
2014-01-27 23:32:38 -0800

    fixup! Repository

--- test/assertions/repository_assertions.rb
+++ test/assertions/repository_assertions.rb
@@ -46,7 +46,6 @@ include Constants
 Removable_Source='/media/greg/SD_USB_32G/Repository Backups/'
 #Repo= Grit::Repo.new(Root_directory)
 SELF_code_Repo=Repository.new(Root_directory)
-	SELF_code_Repo.assert_pre_conditions
 Empty_Repo_path=Source+'test_repository/'
 Empty_Repo=Repository.create_test_repository(Empty_Repo_path)
 Modified_path=Empty_Repo_path+'/README'
