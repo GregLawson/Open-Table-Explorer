80d54c78e4429867516d3c1b1c574679e7285194
GregLawson <GregLawson123@gmail.com>
2014-02-02 10:20:19 -0800

    fixup! MinimalNoTestEnvironment

--- test/unit/minimal_no_test_environment_test.rb
+++ test/unit/minimal_no_test_environment_test.rb
@@ -1,11 +1,12 @@
 ###########################################################################
-#    Copyright (C) 2013 by Greg Lawson                                      
+#    Copyright (C) 2013-2014 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
 #require_relative 'test_environment' # avoid recursive requires
+require 'test/unit'
 require_relative '../../app/models/default_test_case.rb'
 require_relative '../../test/assertions/ruby_assertions.rb'
 require_relative '../../app/models/related_file.rb'
@@ -14,5 +15,5 @@ DefaultTests=eval(TE.default_tests_module_name?)
 TestCase=eval(TE.test_case_class_name?)
 class MinimalTest < TestCase
 include DefaultTests
-include TE.model_class?::Examples
+#include TE.model_class?::Examples
 end #Minimal
