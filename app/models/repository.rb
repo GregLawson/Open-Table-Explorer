6e1752b8f02585019ee01ddfe24663742b0405c9
GregLawson <GregLawson123@gmail.com>
2014-02-17 16:44:28 -0800

    fixup! Repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -175,7 +175,7 @@ def safely_visit_branch(target_branch, &block)
 			puts git_command('status').output
 			puts git_command('stash show').output
 		else
-			apply_run.assert_post_conditions
+			apply_run.assert_post_conditions('unexpected stash apply fail')
 		end #if
 		merge_conflict_files?.each do |conflict|
 			shell_command('diffuse -m '+conflict[:file])
