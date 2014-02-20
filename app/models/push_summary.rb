8db8ce829ca1ed3230cf7e43e1c78aaff9c27fdf
GregLawson <GregLawson123@gmail.com>
2014-02-20 10:27:25 -0800

    fixup! PushSummary

---
+++ app/models/push_summary.rb
@@ -0,0 +1,45 @@
+###########################################################################
+#    Copyright (C) 2013-2014 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+#require_relative '../../app/models/no_db.rb'
+require_relative '../unit/test_environment'
+require_relative '../../test/assertions/repository_assertions.rb'
+class PushSummary
+module ClassMethods
+end #ClassMethods
+extend ClassMethods
+module Constants
+Hex_number=/[0-9a-f]+/
+end #Constants
+include Constants
+# attr_reader
+def initialize(repository)
+	@repository=repository
+end #initialize
+module Assertions
+include Test::Unit::Assertions
+module ClassMethods
+include Test::Unit::Assertions
+def assert_pre_conditions(message='')
+	message+="In assert_pre_conditions, self=#{inspect}"
+end #assert_pre_conditions
+def assert_post_conditions(message='')
+	message+="In assert_post_conditions, self=#{inspect}"
+end #assert_post_conditions
+end #ClassMethods
+def assert_pre_conditions(message='')
+end #assert_pre_conditions
+def assert_post_conditions(message='')
+end #assert_post_conditions
+end #Assertions
+include Assertions
+extend Assertions::ClassMethods
+#self.assert_pre_conditions
+module Examples
+include Constants
+end #Examples
+end # PushSummary
