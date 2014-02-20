b934e5ce2ea0599fb7dad5f9d8694765d173161d
GregLawson <GregLawson123@gmail.com>
2014-02-20 10:24:49 -0800

    fixup! PushSummary
    /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/related_file.rb:131:in `eval': uninitialized
    constant RelatedFile::PushSummary (NameError)
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/related_file.rb:131:in `eval'
    	from /home/greg/Desktop/src/Open-Table-
    Explorer/app/models/related_file.rb:131:in `model_class?'
    	from test/unit/push_summary_test.rb:12:in
    `<class:PushSummaryTest>'
    	from test/unit/push_summary_test.rb:10:in `<main>'

--- test/unit/push_summary_test.rb
+++ test/unit/push_summary_test.rb
@@ -10,4 +10,9 @@ require_relative '../../app/models/minimal2.rb'
 class PushSummaryTest < TestCase
 include DefaultTests
 include TE.model_class?::Examples
+def test_rebase
+			if !something_to_commit? then
+				git_command('cola rebase origin/'+current_branch_name?.to_s)
+			end # if
+end #
 end #Rebase
