c262424cbbc3790c95de287e78c00178595d267a
GregLawson <GregLawson123@gmail.com>
2014-02-24 09:46:42 -0800

    fixup! PushSummary

--- app/models/push_summary.rb
+++ app/models/push_summary.rb
@@ -7,7 +7,34 @@
 ###########################################################################
 #require_relative '../../app/models/no_db.rb'
 require_relative 'repository.rb'
-class PushSummary
+class Branch
+module ClassMethods
+def branches?
+	branch_output=git_command('branch --list').assert_post_conditions.output
+#?	Parse.parse_into_array(branch_output, /[* ]/*/[a-z0-9A-Z_-]+/.capture*/\n/, ending=:optional)
+end #branches?
+def remotes?
+	git_command('branch --list --remote').assert_post_conditions.output.split("\n")
+end #remotes?
+end #ClassMethods
+extend ClassMethods
+def initialize(repository=This_code_repository, branch=current_branch_name?, remote_branch=nil)
+	@branch=branch
+	if remote_branch.nil? then
+		@remote_branch=find_origin(@branch)
+	else
+		@remote_branch=remote_branch
+	end # if
+end # initialize
+def find_origin
+	if remotes?.include?(current_branch_name?) then
+		'origin/'+@branch.to_s
+	else
+		nil
+	end #if
+end # find_origin
+end # Branch
+class TodoList
 module ClassMethods
 end #ClassMethods
 extend ClassMethods
@@ -16,9 +43,36 @@ Hex_number=/[0-9a-f]+/
 end #Constants
 include Constants
 # attr_reader
-def initialize(repository)
-	@repository=repository
+def initialize(branch, onto=branch.remote_branch)
+	@branch=branch
+	@onto=onto
 end #initialize
+def todo_list
+	git_command('git shortlog '+find_origin+'..'+@branch.to_s).output
+end # todo_list
+def flip_start_fixup
+	todo_cache=todo_list # relatively expensive call
+	squashable_list=todo_cache[0]
+	todo_cache.cons(2) do |consecutive_lines|
+		previous_commit=consecutive_lines[0]
+		fixup_header=consecutive_lines[1]
+		if fixup_header!=previous_commit then # change
+			squashable_list << fixup_header.gsub(/fixup! |squash! /, '') # delete header on first
+		else
+			squashable_list << consecutive_lines[1]
+		end # if
+	end # map
+end # flip_start_fixup
+def fixup_until_fail
+	run=git_command('git rebase --continue')
+end # fixup_until_fail
+def rebase!
+	if remotes?.include?(current_branch_name?) then
+		git_command('rebase --interactive origin/'+current_branch_name?).assert_post_conditions.output.split("\n")
+	else
+		puts current_branch_name?.to_s+' has no remote branch in origin.'
+	end #if
+end #rebase!
 module Assertions
 include Test::Unit::Assertions
 module ClassMethods
@@ -40,5 +94,6 @@ extend Assertions::ClassMethods
 #self.assert_pre_conditions
 module Examples
 include Constants
+
 end #Examples
 end # PushSummary
--- test/unit/push_summary_test.rb
+++ test/unit/push_summary_test.rb
@@ -10,8 +10,16 @@ require_relative '../../app/models/push_summary.rb'
 require_relative '../../test/assertions/repository_assertions.rb'
 class PushSummaryTest < TestCase
 include DefaultTests
-include TE.model_class?::Examples
+#include TE.model_class?::Examples
 include Repository::Examples
+def test_find_origin
+end # find_origin
+def test_todo_list
+end # todo_list
+def test_flip_start_fixup
+end # flip_start_fixup
+def test_fixup_until_fail
+end # fixup_until_fail
 def test_rebase
 			if !This_code_repository.something_to_commit? then
 				This_code_repository.git_command('cola rebase origin/'+This_code_repository.current_branch_name?.to_s)
