1309156e6eac570ee689a43e3e74efa94163f541
GregLawson <GregLawson123@gmail.com>
2014-02-26 13:24:22 -0800

    fixup! Repository

--- test/unit/test_environment_default_test_case.rb
+++ test/unit/test_environment_default_test_case.rb
@@ -11,7 +11,7 @@ require 'active_support/all'
 require_relative '../../app/models/default_test_case.rb'
 require_relative '../../test/assertions/ruby_assertions.rb'
 require_relative '../../app/models/related_file.rb'
-TE=RelatedFile.new
+TE=Unit.new
 DefaultTests=eval(TE.default_tests_module_name?)
 TestCase=eval(TE.test_case_class_name?)
 # AssertionFailedError=Test::Unit::AssertionFailedError
