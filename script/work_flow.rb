d2475b09362ef86066b57153ec248eac77281920
GregLawson <GregLawson123@gmail.com>
2014-03-07 12:48:20 -0800

    fixup! WorkFlow

--- script/work_flow.rb
+++ script/work_flow.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 ###########################################################################
-#    Copyright (C) 2013 by Greg Lawson                                      
+#    Copyright (C) 2013-2014 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
@@ -11,6 +11,13 @@ require 'ostruct'
 require 'pp'
 require_relative '../app/models/work_flow.rb'
 require_relative '../app/models/command_line.rb'
+scripting_workflow=WorkFlow.new($0)
+if File.exists?('.git/MERGE_HEAD') then
+	scripting_workflow.repository.merge_conflict_recovery
+else
+end
+# good enough for edited; no syntax error
+scripting_workflow.script_deserves_commit!(:edited)
 commands = []
 OptionParser.new do |opts|
   opts.banner = "Usage: work_flow.rb --<command> files"
@@ -18,13 +25,13 @@ OptionParser.new do |opts|
   opts.on("-e", "--[no-]edit", "Edit related files and versions in diffuse") do |e|
     commands+=[:edit] if e
   end
-  opts.on("-d", "--[no-]downgrade", "Test downgraded related files in git branches") do |d|
-    commands+=[:downgrade] if d
+  opts.on("-d", "--[no-]merge-down", "Test downgraded related files in git branches") do |d|
+    commands+=[:merge_down] if d
   end
   opts.on("-u", "--[no-]upgrade", "Test upgraded related files in git branches") do |u|
     commands+=[:upgrade] if u
   end
-  opts.on("-t", "--[no-]test", "Test. No commit. ") do |t|
+  opts.on("-t", "--[no-]test", "Test, commit. ") do |t|
     commands+=[:test] if t
   end
   opts.on("-b", "--[no-]best", "Best. Merge down, no conflicts. ") do |t|
@@ -51,20 +58,35 @@ OptionParser.new do |opts|
   opts.on("-a", "--[no-]all", "All files tested ") do |t|
     commands+=[:all] if t
   end
-  opts.on("-l", "--[no-]minimal", "edit with minimal comparison. ") do |t|
+  opts.on("-n", "--[no-]minimal", "edit with minimal comparison. ") do |t|
     commands+=[:minimal] if t
+  end
+  opts.on("-l", "--[no-]loop", "Test, commit, edit, loop.") do |t|
+    commands+=[:loop] if t
+  opts.on("-s", "--[no-]split", "split off new class and edit") do |t|
+    commands+=[:split] if t
+  end
   end
   opts.on("-r", "--[no-]related", "Related files") do |t|
     commands+=[:related] if t
   end
+  opts.on("-c", "--[no-]merge-conflict-recovery", "merge_conflict_recovery") do |t|
+    commands+=[:merge_conflict_recovery] if t
+  end
+  
 end.parse!
 
-commands=[:test] if commands.empty?
+if commands.empty? then
+	commands=[:test]
+	puts 'No command; assuming test.'
+end #if
+# good enough for testing; no syntax error
+scripting_workflow.script_deserves_commit!(:testing)
+
 pp commands
 pp ARGV
 
-
-case ARGV.size
+case ARGV.size # paths after switch removal?
 when 0 then # scite testing defaults command and file
 	puts "work_flow --<command> <file>"
 	this_file=File.expand_path(__FILE__)
@@ -80,35 +102,48 @@ commands.each do |c|
 #			WorkFlow.all(:assertions)
 			WorkFlow.all(:assertions_test)
 			WorkFlow.all(:long_test)
-			ShellCommands.new('yard doc')
+			ShellCommands.new('yard doc').assert_post_conditions
 			work_flow=WorkFlow.new($0)
-			work_flow.merge(:master, :passed) 
-			work_flow.merge(:passed, :master) 
-			work_flow.merge(:testing, :master) 
-			work_flow.merge(:edited, :master) 
+			current_branch=work_flow.repository.current_branch_name?
+			if current_branch==:passed then
+				work_flow.merge(:master, :passed) 
+			end #if
+			work_flow.merge_down(current_branch) 
+		when :merge_down then 
+			work_flow=WorkFlow.new($0)
+			work_flow.merge_down
+		when :merge_conflict_recovery then 
+			work_flow=WorkFlow.new($0)
+			work_flow.repository.merge_conflict_recovery
+		when :split then
+			work_flow.split(argv[0], argv[1])
 	else argv.each do |f|
 		work_flow=WorkFlow.new(f)
 		case c.to_sym
 		when :execute then work_flow.execute(f)
 		when :edit then work_flow.edit
-		when :test then work_flow.test(f)
+		when :test then 
+			deserving_branch = work_flow.test(f)
+			work_flow.merge_down(deserving_branch)
+		when :loop then work_flow.loop(f)
 		when :upgrade then work_flow.upgrade(f)
-		when :downgrade then work_flow.downgrade(f)
 		when :best then work_flow.best(f)
 		when :emacs then work_flow.emacs(f)
 		when :passed then work_flow.repository.stage_files(:passed, [f])
 		when :testing then work_flow.repository.stage_files(:testing, [f])
 		when :edited then work_flow.repository.stage_files(:edited, [f])
 		when :deserve then 
-			$stdout.puts  'deserving branch='+work_flow.deserving_branch?(f).to_s
+			deserving_branch=work_flow.deserving_branch?(f).to_s
 			$stdout.puts  work_flow.repository.recent_test.inspect
+			$stdout.puts  'deserving branch='+deserving_branch.to_s
 		when :minimal then work_flow.minimal_edit
 		when :related then
 			puts work_flow.related_files.inspect
 			puts "diffuse"+ work_flow.version_comparison + work_flow.test_files + work_flow.minimal_comparison? if $VERBOSE
 		end #case
-		work_flow.repository.stage_files(:passed, work_flow.related_files.tested_files($0))
+		scripting_workflow.script_deserves_commit!(:passed)
 		$stdout.puts work_flow.repository.git_command('status --short --branch').inspect
 	end #each
 	end #case
 end #each
+1 # successfully completed
