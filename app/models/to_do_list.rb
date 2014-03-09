1a684282578d7001cd548172830b9fcafea1c905
GregLawson <GregLawson123@gmail.com>
2014-03-08 22:50:28 -0800

    fixup! ToDoList

--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -16,6 +16,8 @@ SED_SEQUENCE_EDITOR="sed -i -re 's/^pick /e /' | git rebase -i"
 COLA_SEQUENCE_EDITOR='/usr/share/git-cola/bin/git-xbase'
 EMACS_SEQUENCE_EDITOR='emacs'
 DEFAULT_SEQUENCE_EDITOR=EMACS_SEQUENCE_EDITOR
+# git-cola interface to rebase not working yet
+GIT_COLA_TRACE='full' # (for commands and status) or trace (to cola console widget) or unset (no output) or other values (commands)
 end #Constants
 include Constants
 attr_reader :branch, :onto
@@ -56,25 +58,34 @@ end #rebase_editor?
 def rebase!(sequence_editor=rebase_editor?)
 	# rebase only on clean working directory
 	if !@branch.repository.something_to_commit? then
-		@branch.repository.shell_command(sequence_editor, command_line_rebase_string?) # only on configured remote
+		@run = ShellCommands.named({env: sequence_editor, command: command_line_rebase_string?, opts: {chdir: branch.repository.path}}) # only on configured remote
+		if @run.success? then
+		else
+			@abort_status = @branch.repository.git_command('rebase --abort').puts
+		end # if
 	end # if
+	self
 end # rebase!
+def puts
+		puts @run.inspect
+		puts @branch.repository.state?.inspect
+end # puts
 def command_line_rebase_string?
 	command_string = 'git rebase ' # beginning
 	if @branch.repository.interactive == :interactive then
 		command_string += '--interactive '
 	end # if
-		command_string +=  @branch.to_s + ' --onto '
+	command_string +=  @branch.to_s
 	if @onto.nil? then
 		if @branch.remote_branch.nil? then
-			command_string += ' --root '
+			command_string += ' --root'
 		else
-			command_string += @branch.remote_branch.to_s
+			command_string += ' --onto ' + @branch.remote_branch.to_s
 		end
 	elsif @onto.instance_of?(Fixnum) then
-		command_string += @branch.to_s + '~' + @onto.to_s
+		command_string += ' --onto ' + @branch.to_s + '~' + @onto.to_s
 	else # normal String ref
-		command_string += @onto.to_s
+		command_string += ' --onto ' + @onto.to_s
 	end # if
 end # command_line_rebase_string?
 module Assertions
@@ -98,11 +109,12 @@ extend Assertions::ClassMethods
 #self.assert_pre_conditions
 module Examples
 include Constants
+include Branch::Examples
 Test_rebase = ToDoList.new(Branch::Examples::Empty_repo_master_branch)
 Test_rebase_4 = ToDoList.new(Branch::Examples::Empty_repo_master_branch, 4)
-Test_rebase_passed = ToDoList.new(Branch::Examples::Empty_repo_master_branch, :passed)
+Test_rebase_passed = ToDoList.new(Branch::Examples::Empty_repo_passed_branch)
 Executing_rebase = ToDoList.new(Branch::Examples::Executing_master_branch)
 Executing_rebase_4 = ToDoList.new(Branch::Examples::Executing_master_branch, 4)
-Executing_rebase_passed = ToDoList.new(Branch::Examples::Executing_master_branch, :passed)
+Executing_rebase_passed = ToDoList.new(Branch::Examples::Executing_passed_branch)
 end #Examples
 end # ToDoList
