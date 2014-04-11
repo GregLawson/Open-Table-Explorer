f47ae100e649367b31fb80136703eb8420cd6289
GregLawson <GregLawson123@gmail.com>
2014-04-11 08:00:25 -0700

    fixup! partition

---
+++ test/unit/partition_test.rb
@@ -0,0 +1,18 @@
+###########################################################################
+#    Copyright (C) 2012-2014 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'test_environment'
+require_relative '../../app/models/partition.rb'
+class PartitionTest < TestCase
+#include DefaultTests
+include TE.model_class?::Examples
+def test_initialize
+
+	partition_run=ShellCommands.new('cat /proc/partitions').assert_pre_conditions
+	partition_run.output
+end # initialize
+end # Partition
