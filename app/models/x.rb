4eb5dd8aea0d5afa7b8591b71d09879c7d261540
Greg Lawson <GregLawson123@gmail.com>
2014-04-21 16:06:53 -0700

    By default does emacs VC only find files in same directory?

--- app/models/x.rb
+++ app/models/x.rb
@@ -6,16 +6,37 @@
 #
 ###########################################################################
 #require_relative '../../app/models/no_db.rb'
+require_relative '../../app/models/parse.rb'
 class X
 module ClassMethods
+def logs
+  logs = Dir['/var/log/X*']
+end # logs
 end #ClassMethods
 extend ClassMethods
 module Constants
+include Regexp::Constants
+Logs = X::logs
+Prefix = %r{/var/log/Xorg.}
+Sequence = /[0-9]+/.capture(:sequence)
+Suffix = /\.old/.capture(:old)
+File_pattern = Prefix * Sequence * /.log/ * Suffix.group * Optional
 end #Constants
 include Constants
-# attr_reader
-def initialize
+# attr_reader :sequence, :old
+def initialize(log_file)
+  @log_file = log_file
+  @parse_filename = Parse.parse_string(log_file, File_pattern)
+  @sequence = @parse_filename[:sequence].to_i
+  @old = @parse_filename[:old] ? true : false
+  assert_equal({sequence: '1', old: '.old'}, Parse.parse_string('/var/log/Xorg.1.log.old', File_pattern))
+  @log = IO.read(@log_file)
 end #initialize
+def log
+  logs = Dir['/var/log/X*']
+  log_file = logs[0]
+
+end # log
 module Assertions
 include Test::Unit::Assertions
 module ClassMethods
