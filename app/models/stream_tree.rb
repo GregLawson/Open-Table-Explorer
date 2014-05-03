815ae0863fa9194b15a87de42e3f61df73d4e7ac
Greg Lawson <GregLawson123@gmail.com>
2014-05-03 08:53:05 -0700

    stream_tree

--- app/models/stream_tree.rb
+++ app/models/stream_tree.rb
@@ -10,11 +10,36 @@
 # Or is it duck typing modules without inheritance?
 # A Stream is a generalization of Array, Enumerable allowing infinite length part of which can be a Tree data store
 # A tree is a generalization of Tables and Nested Arrays and Hashes 
+# in context see http://rubydoc.info/gems/gratr/0.4.3/file/README
+# simple construction would give a tree
+# sharing nodes would give a directed acyclic graph
+# loops might be possible since ruby objects are references and self references are possible
 #require_relative '../../app/models/no_db.rb'
-module Stream
+# make as many methods in common between Array and Hash
+# [] is the obvious method in common
+# each_index and each_pair seem synonyms
+# map should be added analogously to Hash
+class Array
+def each_pair
+end # each_pair
+def values
+end # values
+def keys
+end # keys
+end # Array
+class Hash
+def each_index
+end # each_index
+def map
+end # map
+end # Hash
+module Stream # see http://rgl.rubyforge.org/stream/classes/Stream.html
 include Enumerable
 end # Stream
+module Graph # see http://rubydoc.info/gems/gratr/0.4.3/file/README
+end # Graph
 module Tree
+include Graph
 # delegate to Array, Enumable and Hash
 end # Tree
 module StreamTree
@@ -31,9 +56,9 @@ def initialize
 end #initialize
 require_relative '../../test/assertions.rb'
 module Assertions
-include Minitest::Assertions
+include Test::Unit::Assertions
 module ClassMethods
-include Minitest::Assertions
+include Test::Unit::Assertions
 def assert_pre_conditions(message='')
 	message+="In assert_pre_conditions, self=#{inspect}"
 end #assert_pre_conditions
