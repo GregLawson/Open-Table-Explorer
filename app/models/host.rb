1250b6cac50c4ec46b2156084fa4e3a3aa87e971
Greg Lawson <GregLawson123@gmail.com>
2014-06-04 15:48:20 -0700

    fiup! host

--- app/models/host.rb
+++ app/models/host.rb
@@ -5,11 +5,18 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
+require_relative '../../app/models/no_db.rb'
+#require_relative '../../app/models/generic_table.rb'
 class Host < ActiveRecord::Base
-require_relative '../../app/models/generic_table.rb'
-include Generic_Table
+#include Generic_Table
 has_many :ports
 has_many :routers
+module ClassMethods
+end # ClassMethods
+extend ClassMethods
+module Constants
+end # Constants
+include Constants
 def self.logical_primary_key
 	return [:name]
 end #logical_primary_key
@@ -96,4 +103,28 @@ def Host.scanNmapSummary(s)
 	puts "nmap_execution_time=#{nmap_execution_time}" if $DEBUG
 	return [up,nmap_execution_time]
 end
+# attr_reader
+def initialize
+end # initialize
+require_relative '../../test/assertions.rb'
+module Assertions
+module ClassMethods
+def assert_pre_conditions(message='')
+	message+="In assert_pre_conditions, self=#{inspect}"
+end #assert_pre_conditions
+def assert_post_conditions(message='')
+	message+="In assert_post_conditions, self=#{inspect}"
+end #assert_post_conditions
+end #ClassMethods
+def assert_pre_conditions(message='')
+end #assert_pre_conditions
+def assert_post_conditions(message='')
+end #assert_post_conditions
+end # Assertions
+include Assertions
+extend Assertions::ClassMethods
+#self.assert_pre_conditions
+module Examples
+include Constants
+end # Examples
 end #Host
