07b7f10e8d26f8c482de9f7b0b39bd360faf1d37
GregLawson <GregLawson123@gmail.com>
2014-07-24 19:31:01 -0700

    usb

--- test/unit/usb_test.rb
+++ test/unit/usb_test.rb
@@ -1,13 +1,21 @@
 ###########################################################################
-#    Copyright (C) 2012-2013 by Greg Lawson                                      
+#    Copyright (C) 2014 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-require_relative 'test_environment_ruby'
+require_relative 'test_environment'
 require_relative '../../app/models/usb.rb'
 class UsbBbusTest < TestCase
 def test_usb_gem
 end #usb_gem
-end #MinimalTest
+def test_find_devices
+	devs = ShellCommands.new('ls /dev/bus/usb/*/*').assert_post_conditions
+	lsusb = ShellCommands.new('lsusb').assert_post_conditions
+	disk_devs = ShellCommands.new('ls -l /dev/disk/by-*|grep usb').assert_post_conditions
+	drivers = ShellCommands.new('ls -l /sys/bus/usb/drivers/*').assert_post_conditions
+	events = ShellCommands.new('find /dev -name "*event*"').assert_post_conditions
+
+end # find_devices
+end # UsbBus
