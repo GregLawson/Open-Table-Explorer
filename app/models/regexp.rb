148681ef53d904c31fc66574bd6384b84feb8d99
GregLawson <GregLawson123@gmail.com>
2014-08-03 11:39:44 -0700

    fixup! regexp

--- app/models/regexp.rb
+++ app/models/regexp.rb
@@ -12,26 +12,28 @@ class Regexp
 #include Comparable
 extend ActiveModel::Naming # allow ActiveModel / ActiveRecord style error attributes. Naming clash?
 module Constants
-Default_options=Regexp::EXTENDED | Regexp::MULTILINE
+Default_options = 0 # Regexp::EXTENDED | Regexp::MULTILINE # only make sense for literals
 Ascii_characters=(0..127).to_a.map { |i| i.chr}
 Binary_bytes=(0..255).to_a.map { |i| i.chr}
 Any_binary_char_string='[\000-\377]'
-Any='*'
-Many='+'
+Any = 0..Float::INFINITY
+Many = 1..Float::INFINITY
 Optional='?'
 Start_string=/\A/
 End_string=/\z/
 End_string_less_newline=/\Z/
+US_ASCII_encoding = Encoding::US_ASCII # Encoding.find('US-ASCII')
+Binary_encoding = Encoding::ASCII_8BIT
 end #Constants
 include Constants
 module ClassMethods
-def [](array_form)
-	array_form.reduce(:*) do |regexp|
-		case regexp.class
-		when Regexp then regexp
-		when String then Regexp.new(Regexp.escape(node))
+def [](*array_form)
+	array_form.reduce(//, :*) do |regexp|
+		case regexp.class.to_s
+		when 'Regexp' then regexp
+		when 'String' then Regexp.new(Regexp.escape(node))
 		else
-			raise "unexpected node = #{node.inspect}"
+			raise "unexpected node = #{regexp.inspect}"
 		end # case
 	end # reduce
 end # []
@@ -59,12 +61,13 @@ def regexp_rescued(regexp_string, options=Regexp::Default_options)
 	return Regexp.new(regexp_string, options)
 rescue  RegexpError => exception_raised
 	@regexp_string = regexp_string
-	errors.add(name, 'RegexpError: ' + exception_raised.inspect, options = {}) 
+	@errors = {:RegexpError => exception_raised}
 	return nil
 end #regexp_rescued
-def regexp_error(regexp_string, options=Default_options)
-	raise "expecting regexp_string=#{regexp_string.inspect}" unless regexp_string.instance_of?(String)
-	return Regexp.new(regexp_string, options)
+def regexp_error(regexp_string)
+	raise "Argument to regexp_error is expected to be a String; but regexp_string=#{regexp_string.inspect}" unless regexp_string.instance_of?(String)
+	Regexp.new(regexp_string) # test
+	return nil # if no RegexpError
 rescue RegexpError => exception
 	return exception
 end #regexp_error
@@ -78,17 +81,37 @@ def delimiter_regexp(delimiter)
 	raise "delimiters must be single characters not #{delimiter.inspect}." if delimiter.length!=1
 	/([^#{delimiter}]*)(?:#{delimiter}([^#{delimiter}]*))*/
 end #delimiter_regexp
+def propagate_options(regexp)
+	if regexp.instance_of?(Regexp) then
+		ret= [(regexp.casefold? ? Regexp::CASE_FOLD : 0)]
+		ret += [regexp.encoding]
+	else
+		[Regexp::Default_options, Encoding::US_ASCII]
+	end # if
+end #propagate_options
 end #ClassMethods
 extend ClassMethods
+# Modeled on Numeric.coerce
+# If a +alternative_form is the same type as self, returns an array containing alternative_form and self.
+# Otherwise, returns an array with both a alternative_form and self represented as Regexp objects or to_regexp_escaped_string.
+# This coercion mechanism is modeled on Ruby's handleing of mixed-type numeric operations: 
+# it is intended to find a compatible common type between the two operands of the operator.
+# 1.coerce(2.5)   #=> [2.5, 1.0]
+# 1.2.coerce(3)   #=> [3.0, 1.2]
+# 1.coerce(2)     #=> [2, 1]
+def coerce_escaped_string(alternative_form)
+	if self.class == alternative_form.class then
+		[alternative_form, self]
+	else
+		[Regexp.to_regexp_escaped_string(alternative_form), Regexp.to_regexp_escaped_string(self)]
+	end # if
+end # coerce_escaped_string
 def unescaped_string
 	"#{source}"
 end #unescape
-def propagate_options(regexp=self)
-	ret=(regexp.casefold? ? Regexp::CASE_FOLD : 0)
-	encoding=regexp.encoding
-	[ret, encoding]
-end #propagate_options
 def *(other)
+	coerced_arguments = coerce_escaped_string(other)
+	options = [Regexp.propagate_options(other), Regexp.propagate_options(self)]
 	case other
 	when Regexp then return Regexp.new(self.unescaped_string + other.unescaped_string)
 	when String then return Regexp.new(self.unescaped_string + Regexp.escape(other))
@@ -140,6 +163,8 @@ def assert_pre_conditions
 end #assert_pre_conditions
 def assert_post_conditions
 end #assert_post_conditions
+def assert_no_exception(message)
+end # assert_no_exception
 end #Assertions
 include Assertions
 extend Assertions::ClassMethods
@@ -148,5 +173,20 @@ include Constants
 Ip_number_pattern=/\d{1,3}/
 Escape_string='\d'
 Back_reference=((/[aeiou]/.capture(:vowel)*/./).back_reference(:vowel)*/./).back_reference(:vowel)
+Regexp_exception = Regexp.regexp_error('[')
 end #Examples
 end #Regexp
+class RegexpError
+def assert_pre_conditions
+	assert_instance_of(RegexpError, Regexp.regexp_error('['))
+	assert_instance_of(String, backtrace[0])
+	assert_match(/regexp/, backtrace[0])
+	assert_instance_of(Thread::Backtrace::Location, backtrace_locations[0])
+	assert_equal('initialize', backtrace_locations[0].base_label)
+	assert_equal('initialize', backtrace_locations[0].label)
+	assert_instance_of(Fixnum, backtrace_locations[0].lineno)
+	assert_match(/[a-z.]+/, backtrace_locations[0].path)
+	assert_match(/[a-z.\/]+/, backtrace_locations[0].absolute_path)
+	assert_equal(message, self.message)
+end #assert_pre_conditions
+end # RegexpError
--- test/unit/regexp_test.rb
+++ test/unit/regexp_test.rb
@@ -14,6 +14,8 @@ extend DefaultTests
 #include Minitest::Assertions
 include Regexp::Examples
 def test_square_brackets
+	assert_equal(/a/, Regexp[/a/])
+	assert_equal(/a/, Regexp['a'])
 	assert_equal(/ab/, ['a', /b/].reduce(//, :*))
 end # []
 def test_to_regexp_escaped_string
@@ -29,21 +31,53 @@ def test_promote
 	assert_raises(RuntimeError) {Regexp.promote(2..5)}
 end #promote
 def test_regexp_rescued
-	assert_equal(/]/mx, Regexp.regexp_rescued(']'))
+	assert_equal(/]/, Regexp.regexp_rescued(']'))
 	assert_raises(RuntimeError) {assert_equal(/]/, Regexp.regexp_rescued(/]/))} # String only
 	assert_equal(nil, Regexp.regexp_rescued('['))
 	assert_equal(/}/, Regexp.regexp_rescued('}'))
-	assert_equal(/}/, Regexp.regexp_rescued('{'))
+	assert_equal(/{/, Regexp.regexp_rescued('{'))
 	assert_equal(nil, Regexp.regexp_rescued(')'))
 	assert_equal(nil, Regexp.regexp_rescued('('))
 end #regexp_rescued
 def test_regexp_error
-	assert_equal('', Regexp.regexp_error(/]/))
+	assert_equal(nil, Regexp.regexp_error(']'))
+
+	assert_instance_of(RegexpError, Regexp_exception)
+	assert_equal('premature end of char-class: /[/', Regexp.regexp_error('[').message)
+	assert_instance_of(Thread::Backtrace::Location, Regexp.regexp_error('[').backtrace_locations[0])
+	assert_instance_of(String, Regexp.regexp_error('[').backtrace[0])
+	assert_equal(nil, Regexp.regexp_error('}'))
+	assert_equal(nil, Regexp.regexp_error('{'))
+	assert_instance_of(RegexpError, Regexp.regexp_error(')'))
+	assert_instance_of(RegexpError, Regexp.regexp_error('('))
+	assert_equal(nil, Regexp.regexp_error(']'))
+	assert_raises(RuntimeError) {Regexp.regexp_error(/]/)} # String only
+	assert_nothing_raised(RuntimeError) {Regexp.regexp_error(']')} # String only
 end #regexp_error
 def test_terminator_regexp
 end #terminator_regexp
 def test_delimiter_regexp
 end #delimiter_regexp
+def test_propagate_options
+	
+	assert_equal([0, US_ASCII_encoding], Regexp.propagate_options(/a/))
+
+    assert_equal([0, Encoding::UTF_8], Regexp.propagate_options(/pat/u)) # UTF-8
+
+    assert_equal([0, Encoding::EUC_JP], Regexp.propagate_options(/pat/e)) # EUC-JP
+
+    assert_equal([0, Encoding::Windows_31J], Regexp.propagate_options(/pat/s)) # Windows-31J
+    assert_equal(Encoding::ASCII_8BIT, /pat/n.encoding) # ASCII-8BIT
+    assert_equal([0, Encoding::BINARY], Regexp.propagate_options(/pat/n)) # ASCII-8BIT
+    assert_equal([0, Encoding::ASCII_8BIT], Regexp.propagate_options(/pat/n)) # ASCII-8BIT
+	
+
+	assert(defined? Regexp)
+#	assert(defined? Regexp::CASE_FOLD)
+	assert_equal([0, Encoding.find('US-ASCII')], /a/x.propagate_options)
+end #propagate_options
+def test_coerce_escaped_string
+end # coerce_escaped_string
 def test_unescaped_string
 	assert_equal(/#{Escape_string}/, Regexp.new(Escape_string))
 	assert_equal(Escape_string, Regexp.new(Escape_string).source)
@@ -56,11 +90,6 @@ def test_unescaped_string
 	ip_pattern=Regexp.new(Array.new(4, Ip_number_pattern.unescaped_string).join('.'))
 	assert_match(ip_pattern, '123.2.3.4')
 end #unescape
-def test_propagate_options
-	assert(defined? Regexp)
-#	assert(defined? Regexp::CASE_FOLD)
-	assert_equal([0, Encoding.find('US-ASCII')], /a/x.propagate_options)
-end #propagate_options
 def test_sequence
   assert_equal('(?-mix:a)', /a/.to_s)
   assert_equal('/a/', /a/.inspect)
@@ -117,4 +146,22 @@ def test_group
 	message="matchData.inspect=#{matchData.inspect}"
 	assert_equal('2', matchData[0], message)
 end #group
+def test_assert_pre_conditions
+
+	assert_instance_of(RegexpError, Regexp_exception)
+	assert_instance_of(String, Regexp_exception.backtrace[0])
+	assert_match(/regexp/, Regexp_exception.backtrace[0])
+	assert_instance_of(Thread::Backtrace::Location, Regexp_exception.backtrace_locations[0])
+	assert_equal('initialize', Regexp_exception.backtrace_locations[0].base_label)
+	assert_equal('initialize', Regexp_exception.backtrace_locations[0].label)
+	assert_instance_of(Fixnum, Regexp_exception.backtrace_locations[0].lineno)
+	assert_match(/[a-z.]+/, Regexp_exception.backtrace_locations[0].path)
+	assert_match(/[a-z.\/]+/, Regexp_exception.backtrace_locations[0].absolute_path)
+	assert_equal('premature end of char-class: /[/', Regexp_exception.message)
+	Regexp.new('}').assert_pre_conditions
+	Regexp.new('{').assert_pre_conditions
+	Regexp.regexp_error(')').assert_pre_conditions
+	Regexp.regexp_error('(').assert_pre_conditions
+	Regexp.new(']').assert_pre_conditions
+end # assert_pre_conditions
 end #Regexp
