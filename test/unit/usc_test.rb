1a8ee911f50db21ff6bf6cf0e2aa1c90a60888d9
GregLawson <GregLawson123@gmail.com>
2014-09-15 18:27:25 -0700

    fixup! usc

--- test/unit/usc_test.rb
+++ test/unit/usc_test.rb
@@ -1,3 +1,14 @@
-IO.foreach('/dev/ttyUSB0') do |line|
-	puts line
-end # foreach
+###########################################################################
+#    Copyright (C) 2014 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'test_environment'
+require_relative '../../app/models/usc.rb'
+class UscTest < TestCase
+include DefaultTests
+include TE.model_class?::Examples
+#Usc_file.gets 
+end # Usc
