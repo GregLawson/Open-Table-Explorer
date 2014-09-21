af69d1890e82c55ef3b49f8ce930e808e0be0cb7
GregLawson <GregLawson123@gmail.com>
2014-09-20 23:22:45 -0700

    fixup! network

--- test/unit/network_test.rb
+++ test/unit/network_test.rb
@@ -7,7 +7,6 @@
 ###########################################################################
 require_relative 'test_environment'
 require_relative '../../app/models/network.rb'
-require_relative '../../app/models/parse.rb'
 class NetworkTest < TestCase
 #include DefaultTests
 include TE.model_class?::Examples
@@ -16,6 +15,7 @@ def test_whereAmI
 	private_C = ifconfig.parse(Private_C)
 	private_network = ifconfig.parse(Private_Network_Pattern)
 	context = ifconfig.parse(Context_Pattern)
+	captures = ifconfig.capture?(IP_Pattern)
 	ip = ifconfig.parse(IP_Pattern)
 	netmask = ifconfig.parse(Netmask_Pattern)
 #	acquire=StreamPattern.find_by_name('Acquisition')
@@ -27,23 +27,23 @@ def test_whereAmI
 	Network.whereAmI
 end #whereAmI
 def test_NetworkInterface
-	lines=Parse.parse_into_array(IFCONFIG.output, Parse::Examples::LINE)
-	double_lines=IFCONFIG.output.split("\n\n")
+	lines = IFCONFIG.output.parse(Capture::Examples::LINE)
+	double_lines = IFCONFIG.output.split("\n\n")
 	assert_instance_of(Array, double_lines)
 	assert_operator(2, :<=, double_lines.size)
 	assert_equal('eth0', double_lines[0].split(' ')[0])
-	words=Parse.parse_into_array(double_lines[0], Parse::Examples::WORD)
-	assert_equal({:word=>"eth0"}, words[0])
+	words=double_lines[0].parse(Capture::Examples::WORD)
+	assert_equal({:word=>"eth0"}, words, words.inspect)
 #	assert_equal('Link', words[1], "words=#{words.inspect}, lines=#{lines.inspect}")
 	puts "words=#{words.inspect}, double_lines=#{double_lines.inspect}"
 	words=double_lines.map do |row|
-		words=Parse.parse_into_array(row, Parse::Examples::WORD)
+		words = row.parse(Capture::Examples::WORD)
 		puts "words=#{words.inspect}, row=#{row.inspect}"
-		assert_match(words[0], /eth0|lo|wlan0/, "row=#{row.inspect}, words=#{words.inspect}")
+		assert_match(/eth0|lo|wlan0/, words[:word], "row=#{row.inspect}, words=#{words.inspect}")
 	end #map
-	Parse.parse_into_array(IFCONFIG.output, Parse::Examples::LINES).map  do |row| 
-		Parse.parse_into_array(row, Parse::Examples::WORD)
-	end #map
+#	IFCONFIG.output.enumerate(:map) parse(Capture::Examples::LINE).map  do |row| 
+#		row.parse(row, Capture::Examples::WORD)
+#	end #map
 #	assert_equal('', IFCONFIG.rows_and_columns)
 #	assert_equal('eth0,', IFCONFIG.inspect)
 #	assert_equal('', IFCONFIG.output)
