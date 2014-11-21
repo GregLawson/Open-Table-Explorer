9c1e0be0ce70ed2545c0719dd566212dd3857044
Greg Lawson <greg@acer-desk-deb.lan>
2014-11-20 22:11:05 -0800

    fixup! dependancies

--- test/unit/dependancies_test.rb
+++ test/unit/dependancies_test.rb
@@ -1,10 +1,33 @@
-sudo apt-get install pmount
-sudo apt-get install active_model
-sudo apt-get install activemodel
-sudo apt-get install vrms
-sudo gem install grit
-sudo gem install regexp_parser
-sudo gem install virtus
-sudo gem install mysql
-sudo wajig install  ruby-activemodel-3.2
-bundle install
+###########################################################################
+#    Copyright (C) 2014 by Greg Lawson                                      
+#    <GregLawson123@gmail.com>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+#require_relative 'test_environment' # avoid recursive requires
+require 'test/unit'
+TestCase=Test::Unit::TestCase
+require_relative '../../app/models/default_test_case.rb'
+require_relative '../../test/assertions/ruby_assertions.rb'
+#require_relative '../../app/models/related_file.rb'
+require_relative '../../app/models/shell_command.rb'
+class DependanciesTest < TestCase
+require 'pathname'
+def test_script
+	install_execution=ShellCommands.new('sudo apt-get install pmount').assert_post_conditions
+	install_execution=ShellCommands.new('sudo apt-get install active_model').assert_post_conditions
+	install_execution=ShellCommands.new('sudo apt-get install activemodel').assert_post_conditions
+	install_execution=ShellCommands.new('sudo apt-get install vrms').assert_post_conditions
+	install_execution=ShellCommands.new('sudo gem install grit').assert_post_conditions
+	install_execution=ShellCommands.new('sudo gem install regexp_parser').assert_post_conditions
+	install_execution=ShellCommands.new('sudo gem install virtus').assert_post_conditions
+	install_execution=ShellCommands.new('sudo gem install mysql').assert_post_conditions
+	install_execution=ShellCommands.new('sudo wajig install  ruby-activemodel-3.2').assert_post_conditions
+	install_execution=ShellCommands.new('bundle install').assert_post_conditions
+end # script
+def test_dpkg_lock
+	pathname = '/var/cache/apt/archives/lock'
+	assert(!File.exist?(pathname), pathname)
+end # dpkg_lock
+end # Dependancies
