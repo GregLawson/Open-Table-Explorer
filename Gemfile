1cdda5ddadfca820e64c697e39fe71171ce89bde
Greg Lawson <greg@acer-desk-deb.lan>
2014-11-20 21:30:06 -0800

    dependancies

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
-# A sample Gemfile
 source "https://rubygems.org"
 gem 'active_support'
 gem 'i18n'
 gem 'grit'
+gem 'rserve-client', '~> 0.3.1'
 # gem "rails"
---
+++ test/unit/dependancies_test.rb
@@ -0,0 +1,10 @@
+sudo apt-get install pmount
+sudo apt-get install active_model
+sudo apt-get install activemodel
+sudo apt-get install vrms
+sudo gem install grit
+sudo gem install regexp_parser
+sudo gem install virtus
+sudo gem install mysql
+sudo wajig install  ruby-activemodel-3.2
+bundle install
