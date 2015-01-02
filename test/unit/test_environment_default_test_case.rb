fd84320a5f6ec9d31e119636ae92de86524f420f
Greg Lawson <greg@acer-desk-deb.lan>
2015-01-01 16:46:49 -0800

    fixup! test_environment

--- test/unit/test_environment_default_test_case.rb
+++ test/unit/test_environment_default_test_case.rb
@@ -14,3 +14,6 @@ TE=Unit.new
 DefaultTests=eval(TE.default_tests_module_name?)
 TestCase=eval(TE.test_case_class_name?)
 #AssertionFailedError=Test::Unit::AssertionFailedError
+AssertionFailedError = RuntimeError
+#AssertionFailedError = MiniTest::Assertion
+assert_global_name(:AssertionFailedError)
