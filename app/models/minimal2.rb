a913e8835547c3a00e55e7ddea6e555206556871
Greg Lawson <greg@acer-desktop.lan>
2015-01-04 13:17:01 -0800

    fixup! minimal

--- app/models/minimal2.rb
+++ app/models/minimal2.rb
@@ -20,8 +20,32 @@ end # initialize
 require_relative '../../test/assertions.rb'
 module Assertions
 module ClassMethods
+def test_nested_scope_modules?
+	nested_constants = Version.class.constants
+	message = ''
+	assert_include(included_modules, :Assertions, message)
+	assert_equal([:Constants, :Assertions, :ClassMethods], Version.nested_scope_modules?)
+end # nested_scopes
+def assert_nested_scope_submodule(module_symbol, context = self, message='')
+	message+="\nIn assert_nested_scope_submodule for class #{context.name}, "
+	message += "make sure module Constants is nested in #{context.class.name.downcase} #{context.name}"
+	message += " but not in #{context.nested_scope_modules?.inspect}"
+	assert_include(constants, :Contants, message)
+end # assert_included_submodule
+def assert_included_submodule(module_symbol, context = self, message='')
+	message+="\nIn assert_included_submodule for class #{self.name}, "
+	message += "make sure module Constants is nested in #{self.class.name.downcase} #{self.name}"
+	message += " but not in #{self.nested_scope_modules?.inspect}"
+	assert_include(included_modules, :Contants, message)
+end # assert_included_submodule
+def asset_nested_and_included(module_symbol, context = self, message='')
+	assert_nested_scope_submodule(module_symbol)
+	assert_included_submodule(module_symbol)
+end # asset_nested_and_included
 def assert_pre_conditions(message='')
-	message+="In assert_pre_conditions, self=#{inspect}"
+	asset_nested_and_included(:ClassMethods, self)
+	asset_nested_and_included(:Constants, self)
+	asset_nested_and_included(:Assertions, self)
 end #assert_pre_conditions
 def assert_post_conditions(message='')
 	message+="In assert_post_conditions, self=#{inspect}"
