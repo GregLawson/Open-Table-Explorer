6297cd8c71ac4a578d1a406c4dacb14ed790eca7
Greg Lawson <greg@acer-desktop.lan>
2015-01-09 09:31:55 -0800

    fixup! work_flow

--- script/work_flow.rb
+++ script/work_flow.rb
@@ -133,7 +133,7 @@ commands.each do |c|
 		when :testing then work_flow.repository.stage_files(:testing, [f])
 		when :edited then work_flow.repository.stage_files(:edited, [f])
 		when :deserve then 
-			deserving_branch=work_flow.deserving_branch?(f).to_s
+			deserving_branch = work_flow.deserving_branch?(f).to_s
 			$stdout.puts  work_flow.repository.recent_test.inspect
 			$stdout.puts  'deserving branch='+deserving_branch.to_s
 		when :minimal then work_flow.minimal_edit
