5349c046bc7aaffec26fd95f4fad7b291312db28
Greg Lawson <greg@acer-desktop.lan>
2015-01-13 06:19:52 -0800

    fixup! branch
    temporarily remove repository dependency while not needed by workflow.rb

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -6,8 +6,8 @@
 #
 ###########################################################################
 #require_relative '../../app/models/no_db.rb'
-require_relative '../../test/assertions/repository_assertions.rb'
-assert_global_name(:Repository)
+#require_relative '../../test/assertions/repository_assertions.rb'
+#assert_global_name(:Repository)
 require_relative '../../app/models/parse.rb'
 class Branch
 #include Repository::Constants
