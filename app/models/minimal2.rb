affe6f024f11c3933543e0945ccda509f8e1694b
Greg Lawson <greg@acer-desktop.lan>
2015-01-13 19:19:14 -0800

    fixup! minimal

--- app/models/minimal2.rb
+++ app/models/minimal2.rb
@@ -1,5 +1,5 @@
 ###########################################################################
-#    Copyright (C) 2013 by Greg Lawson                                      
+#    Copyright (C) 2013-2014 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
@@ -8,18 +8,17 @@
 #require_relative '../../app/models/no_db.rb'
 class Minimal2
 module ClassMethods
-end #ClassMethods
+end # ClassMethods
 extend ClassMethods
 module Constants
-end #Constants
+end # Constants
 include Constants
 # attr_reader
 def initialize
-end #initialize
+end # initialize
+require_relative '../../test/assertions.rb'
 module Assertions
-include Test::Unit::Assertions
 module ClassMethods
-include Test::Unit::Assertions
 def assert_pre_conditions(message='')
 	message+="In assert_pre_conditions, self=#{inspect}"
 end #assert_pre_conditions
@@ -31,11 +30,11 @@ def assert_pre_conditions(message='')
 end #assert_pre_conditions
 def assert_post_conditions(message='')
 end #assert_post_conditions
-end #Assertions
+end # Assertions
 include Assertions
 extend Assertions::ClassMethods
 #self.assert_pre_conditions
 module Examples
 include Constants
-end #Examples
-end #Minimal2
+end # Examples
+end # Minimal2
