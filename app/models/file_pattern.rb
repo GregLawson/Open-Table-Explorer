0d18b4fccb17209a8c7cc688ede630b71e6cf0cf
Greg Lawson <greg@acer-desktop.lan>
2015-01-14 11:21:44 -0800

    fixup! file_pattern

--- app/models/file_pattern.rb
+++ app/models/file_pattern.rb
@@ -5,11 +5,11 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-require 'test/unit'
 require 'pathname'
 require_relative 'regexp.rb'
 require 'active_support/all'
-class FilePattern <  ActiveSupport::HashWithIndifferentAccess
+
+class FilePattern # <  ActiveSupport::HashWithIndifferentAccess
 module Constants
 # ordered from ambiguous to specific, common to rare
 Patterns=[
@@ -20,9 +20,12 @@ Patterns=[
 	{:suffix =>'_test.rb', :name => :long_test, :prefix => 'test/long_test/', :example_file => 'test/long_test/repository_test.rb'}, 
 	{:suffix =>'_assertions.rb', :name => :assertions, :prefix => 'test/assertions/', :example_file => 'test/assertions/repository_assertions.rb'}, 
 	{:suffix =>'_assertions_test.rb', :name => :assertions_test, :prefix => 'test/unit/', :example_file => 'test/unit/repository_assertions_test.rb'},
+	{:suffix =>'.log', :name => :library_log, :prefix => 'log/library/', :example_file => 'log/library/repository.log'},
+	{:suffix =>'.log', :name => :assertions_log, :prefix => 'log/assertions/', :example_file => 'log/assertions/repository.log'},
+	{:suffix =>'.log', :name => :integration_log, :prefix => 'log/integration/', :example_file => 'log/integration/repository.log'},
+	{:suffix =>'.log', :name => :long_log, :prefix => 'log/long/', :example_file => 'log/long/repository.log'},
 	{:suffix =>'', :name => :data_sources_dir, :prefix => 'test/data_sources/', :example_file => 'test/data_sources/tax_form/CA540'}
 	]
-All=Patterns.map {|s| FilePattern.new(s)}	
 include Regexp::Constants
 Directory_delimiter=/\//
 Basename_character_regexp=/[[:word:]\. -]/
@@ -32,26 +35,30 @@ Relative_pathname_regexp=Start_string*Pathname_character_regexp*Many*End_string
 Absolute_pathname_regexp=Start_string*Directory_delimiter*Pathname_character_regexp*Many*End_string
 Relative_directory_regexp=Start_string*Pathname_character_regexp*Many*End_string
 Absolute_directory_regexp=Start_string*Directory_delimiter*Pathname_character_regexp*Many*End_string
-end  #Constants
+end # Constants
 include Constants
 module ClassMethods
-def all
-	Constants::All
-end #all
+def executing_path?
+	squirrely_string = $PROGRAM_NAME
+	class_name = self.name.to_s
+	test_name = 'test_executing_path?'
+	extra_at_end = ' ' + class_name + '#' + test_name	
+	extra_length = extra_at_end.length
+	regexp = Relative_pathname_regexp
+	squirrely_string[0..-(extra_length+2)]
+end # executing_path?
 def path2model_name?(path=$0)
 	raise "path=#{path.inspect} must be a string" if !path.instance_of?(String)
-	path=File.expand_path(path)
-	basename=File.basename(path)
-	matches=all.map do |s| 
-		if s.suffix_match(path) && s.prefix_match(path) then
-			name_length=basename.size-s[:suffix].size
-			basename[0,name_length].camelize.to_sym
-		else
-			nil
-		end #if	
-	end #map
-	matches.compact.last
+	unit_base_name?(path).to_s.camelize.to_sym
 end #path2model_name
+def unit_base_name?(path=$0)
+	raise "path=#{path.inspect} must be a string" if !path.instance_of?(String)
+	path=File.expand_path(path)
+	matched_pattern = find_from_path(path)
+	basename=File.basename(path)
+	name_length=basename.size-matched_pattern[:suffix].size
+	basename[0,name_length].to_sym
+end # unit_base_name
 # searches up pathname for .git sub-directory
 # returns nil if not in a git repository
 def repository_dir?(path=$0)
@@ -78,7 +85,8 @@ end #repository_dir?
 # returns nil if file does not follow any pattern
 def project_root_dir?(path=$0)
 	path=File.expand_path(path)
-	roots=FilePattern::All.map do |p|
+	matched_pattern = find_from_path(path)
+	roots=Constants::Patterns.map do |p|
 		matchData=Regexp.new(p[:prefix]).match(path)
 		if matchData.nil? then
 			nil
@@ -96,15 +104,43 @@ def project_root_dir?(path=$0)
 	end #if
 end #project_root_dir
 def find_by_name(name)
-	Constants::All.find do |s|
+	Constants::Patterns.find do |s|
 		s[:name]==name
 	end #find
 end #find_by_name
+def match_path(pattern, path)
+	p = pattern.clone
+		p[:path] = path
+		p[:prefix_match] = Regexp.new(p[:prefix]).match(path)
+		p[:full_match] = p[:prefix_match] && path[-p[:suffix].size, p[:suffix].size] == p[:suffix]
+	p
+end # match_path
+def match_all?(path)
+	path=File.expand_path(path)
+	ret = Constants::Patterns.map do |p|
+		match = match_path(p, path)
+	end # map
+	ret
+end # match_all
+def find_all_from_path(path)
+	ret = match_all?(path).select do |p|
+		p[:full_match]
+	end # select
+	ret
+end #find_from_path
 def find_from_path(path)
-	Constants::All.find do |p|
-		p.prefix_match(path) && p.suffix_match(path)
+	match_all?(path).find do |p|
+		p[:full_match]
 	end #find
 end #find_from_path
+def path?(pattern, unit_base_name)
+	raise pattern.inspect if !pattern.instance_of?(Hash)
+	raise pattern.inspect if !pattern[:prefix].instance_of?(String)
+	raise "unit_base_name-#{unit_base_name.inspect}" if !unit_base_name.respond_to?(:to_s)
+	raise "" if !pattern[:suffix].instance_of?(String)
+	pattern[:prefix]+unit_base_name.to_s+pattern[:suffix]
+end # path?
+#returns Array of all possible pathnames for a unit_base_name
 def pathnames?(model_basename)
 #	raise "project_root_dir" if FilePattern.class_variable_get(:@@project_root_dir).nil?
 	raise "model_basename" if model_basename.nil?
@@ -112,53 +148,60 @@ def pathnames?(model_basename)
 		p.path?(model_basename)
 	end #
 end #pathnames
+def new_from_path(path)
+	raise path.inspect unless path.instance_of?(String) || path.instance_of?(Pathname)
+	path=File.expand_path(path)
+	pattern = FilePattern.find_from_path(path)
+	FilePattern.new(pattern,
+						FilePattern.unit_base_name?(path),
+						FilePattern.project_root_dir?(path),
+						FilePattern.repository_dir?(path))
+end # new_from_path
 #FilePattern.assert_pre_conditions
 #assert_include(FilePattern.included_modules, :Assertions)
 #assert_pre_conditions
 end #ClassMethods
 extend ClassMethods
-module Constants
-Project_root_directory=FilePattern.project_root_dir?($0)
-end #Constants
-def initialize(hash)
-	@pattern=hash
-	super(hash)
+attr_reader :pattern, :project_root_dir, :repository_dir, :unit_base_name
+def initialize(pattern,
+					 unit_base_name = '*',
+					 project_root_dir = Library.project_root_dir,
+					 repository_dir = project_root_dir)
+	raise 'initialize' unless pattern.instance_of?(Hash)
+	@unit_base_name = unit_base_name
+	@pattern = pattern
+	@project_root_dir = project_root_dir
+	@repository_dir = repository_dir
 end #initialize
 #def inspect
-#	message="FilePattern<instance_variables=#{instance_variables.inspect}, self=#{self.inspect}>"
-#end #inspect
-def suffix_match(path)
-	path[-self[:suffix].size, self[:suffix].size] == self[:suffix]
-end #suffix_match
-def prefix_match(path)
-	path=File.expand_path(path)
-	matchData=Regexp.new(self[:prefix]).match(path)
-#	prefix=File.dirname(path)
-#	expected_prefix=self[:prefix][0..-2] # drops trailing /
-#	prefix[-expected_prefix.size,expected_prefix.size]==expected_prefix
-end #prefix_match
-def path?(model_basename)
-#	raise "" if !@@project_root_dir.instance_of?(String)
-	raise self.inspect if !self.instance_of?(FilePattern)
-	raise self.inspect if !self[:prefix].instance_of?(String)
-	raise "model_basename-#{model_basename.inspect}" if !model_basename.instance_of?(String)
-	raise "" if !self[:suffix].instance_of?(String)
-	self[:prefix]+model_basename.to_s+self[:suffix]
+#	message="FilePattern<instance_variables=#{instance_variables.inspect}>"
+#e#nd #inspect
+def path?(unit_base_name=@basename)
+	raise @pattern.inspect unless @pattern.instance_of?(Hash)
+	raise @pattern.inspect if !@pattern[:prefix].instance_of?(String)
+	raise "unit_base_name-#{unit_base_name.inspect}" unless unit_base_name.instance_of?(String) || unit_base_name.instance_of?(Symbol)
+	raise "" if !@pattern[:suffix].instance_of?(String)
+	@project_root_dir + '/' + @pattern[:prefix]+unit_base_name.to_s+@pattern[:suffix]
 end #path
 def parse_pathname_regexp
-	Absolute_directory_regexp.capture(:project_root_directory)*self[:prefix]+/[[:word:]]+/.capture(:model_basename)+self[:suffix]
+	Absolute_directory_regexp.capture(:project_root_directory)*@pattern[:prefix]+/[[:word:]]+/.capture(:unit_base_name)+@pattern[:suffix]
 end #parse_pathname_regexp
-def pathname_glob(model_basename='*')
-	Project_root_directory+self[:prefix]+model_basename+self[:suffix]
+def pathname_glob(unit_base_name='*', project_root_directory = @project_root_dir)
+	project_root_directory+@pattern[:prefix]+unit_base_name.to_s+@pattern[:suffix]
 end #pathname_glob
-def relative_path?(model_basename)
-	Pathname.new(path?(model_basename)).relative_path_from(Pathname.new(Dir.pwd))
+def relative_path?(unit_base_name)
+	Pathname.new(path?(unit_base_name)).relative_path_from(Pathname.new(Dir.pwd))
 end #relative_path
+module Constants
+Library = FilePattern.new_from_path(__FILE__)
+Executable = FilePattern.new_from_path($0)
+end #Constants
 include Constants
+require_relative '../../test/assertions.rb'
 module Assertions
-include Test::Unit::Assertions
+
 module ClassMethods
-include Test::Unit::Assertions
+
 # conditions that are always true (at least atomically)
 def assert_invariant
 #	fail "end of assert_invariant "
@@ -179,15 +222,13 @@ def assert_post_conditions
 #	assert_pathname_exists(FilePattern.class_variable_get(:@@project_root_dir))
 end #class_assert_post_conditions
 def assert_pattern_array(array, array_message='')
+	message = "\ndefault FilePattern.project_root_dir?=#{FilePattern.project_root_dir?.inspect}"
 	successes=array.map do |p|
 		p[:example_file].match(p[:prefix])
+		p[:example_file].match(p[:suffix])
 	end #map
 	assert(successes.all?, successes.inspect+"\n"+array.inspect)
 	assert_not_empty(array, array_message)
-	array.each_with_index do |n, i| 
-		message=array_message+" \n n=#{n.inspect}"
-		n.assert_pre_conditions(message)
-	end #map
 end #assert_pattern_array
 end #ClassMethods
 # conditions that are always true (at least atomically)
@@ -196,37 +237,35 @@ def assert_invariant
 end #assert_invariant
 # assertions true after instance is initialized
 def assert_pre_conditions(message='')
-	assert_kind_of(FilePattern, self)
-	message+="\n self=#{self.inspect}\n self=#{self.inspect}"
-	assert_not_equal('{}',self.inspect, message)
-	assert_not_nil(self, message)
-	assert_instance_of(FilePattern, self, message)
-	assert(!self.keys.empty?, message)
-	assert_not_empty(self.values, message)
-	assert_include(self.keys, :suffix.to_s, inspect)
+#	assert_not_nil(@path, 'Path is nil'+ self.inspect)
+#	assert_not_empty(@path)
+	message+="\n @pattern=#{@pattern.inspect}\n @pattern=#{@pattern.inspect}"
+	assert_not_equal('{}',@pattern.inspect, message)
+	assert_not_nil(@pattern, message)
+	assert_instance_of(Hash, @pattern, message)
+	assert(!@pattern.keys.empty?, message)
+	assert_not_empty(@pattern.values, message)
+#	assert_include(@pattern.keys, :suffix, inspect)
+#	assert_equal(@path[-@pattern[:suffix].size, @pattern[:suffix].size], @pattern[:suffix])
 #	fail message+"end of assert_pre_conditions "
 end #assert_pre_conditions
 # assertions true after any instance operations
-def assert_post_conditions
-	message+="\ndefault FilePattern.project_root_dir?=#{FilePattern.project_root_dir?.inspect}"
+def assert_post_conditions(message = 'self = '+self.inspect)
 	assert_not_empty(@project_root_dir, message)
+	assert_pathname_exists(@project_root_dir)
+#	assert(File.exists?(@path))
+#	find_all_from_path = FilePattern.find_all_from_path(@path)
+#	assert_equal(1, find_all_from_path.size, find_all_from_path)
 end #assert_post_conditions
-def assert_naming_convention_match(path)
-	path=File.expand_path(path)
-	assert_equal(path[-self[:suffix].size, self[:suffix].size], self[:suffix], caller_lines)
-	assert(self.suffix_match(path), self.inspect+caller_lines)
-	assert(self.prefix_match(path), self.inspect+caller_lines)
-	message="self=#{self.inspect}, path=#{path.inspect}"
-end #naming_convention_match
 end #Assertions
 include Assertions
 extend Assertions::ClassMethods
 module Examples
 DCT_filename='script/dct.rb'
-#DCT=FilePattern.new(FilePattern.path2model_name?(DCT_filename), FilePattern.project_root_dir?(DCT_filename))
+#DCT=FilePattern.new_from_path(FilePattern.path2model_name?(DCT_filename), FilePattern.project_root_dir?(DCT_filename))
 SELF_Model=__FILE__
 SELF_Test=$0
-#SELF=FilePattern.new(FilePattern.path2model_name?(SELF_Model), FilePattern.project_root_dir?(SELF_Model))
-Data_source_example='test/data_sources/tax_form/examples_and_templates/US_1040/US_1040_example_sysout.txt'
+#SELF=FilePattern.new_from_path(FilePattern.path2model_name?(SELF_Model), FilePattern.project_root_dir?(SELF_Model))
+Data_source_example='test/data_sources/tax_form/2012/examples_and_templates/US_1040/US_1040_example_sysout.txt'
 end #Examples
 end #FilePattern
