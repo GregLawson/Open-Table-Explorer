c3a03b8cc6fb510df84b90e3451dd273515aa867
Greg Lawson <greg@acer-desktop.lan>
2015-02-05 11:51:27 -0800

    Create sub module/branch regexp

--- test/unit/test_environment_ruby.rb
+++ test/unit/test_environment_ruby.rb
@@ -1,5 +1,5 @@
 ###########################################################################
-#    Copyright (C) 2012-2013 by Greg Lawson                                      
+#    Copyright (C) 2012-2014 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
@@ -7,4 +7,8 @@
 ###########################################################################
 require 'test/unit'
 TestCase=Test::Unit::TestCase
-AssertionFailedError=MiniTest::Assertion
+require_relative '../../test/assertions/ruby_assertions.rb'
+#AssertionFailedError=Test::Unit::AssertionFailedError
+AssertionFailedError = RuntimeError
+#AssertionFailedError = MiniTest::Assertion
+#assert_global_name(:AssertionFailedError)
