8aed65850ba3fa68490b2327e0d067889797e8f9
Greg Lawson <greg@acer-desktop.lan>
2015-03-29 12:47:30 -0700

    Merge branch 'testing' into edited

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -53,13 +53,6 @@ def new_from_git_branch_line(git_branch_line)
 end # new_from_git_branch_line
 end #ClassMethods
 extend ClassMethods
-def find_origin
-	if Branch.remotes?(@repository).include?(@repository.current_branch_name?) then
-		'origin/'+@branch.to_s
-	else
-		nil
-	end #if
-end # find_origin
 attr_reader :repository, :branch, :remote_branch
 def initialize(repository, branch=repository.current_branch_name?, remote_branch=nil)
 	fail "Branch.new first argument must be of type Repository" unless repository.instance_of?(Repository)
@@ -81,7 +74,20 @@ end # to_s
 def to_sym
 	@branch.to_sym
 end # to_s
+def find_origin
+	if Branch.remotes?(@repository).include?(@repository.current_branch_name?) then
+		'origin/'+@branch.to_s
+	else
+		nil
+	end #if
+end # find_origin
 def rebase!
+	if remotes?.include?(current_branch_name?) then
+		git_command('rebase --interactive origin/'+current_branch_name?).assert_post_conditions.output.split("\n")
+	else
+		puts current_branch_name?.to_s+' has no remote branch in origin.'
+	end #if
+end #rebase!
 require_relative '../../test/assertions.rb'
 module Assertions
 module ClassMethods
