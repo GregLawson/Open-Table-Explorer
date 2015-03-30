2f79d406a688abfc55fa19460b4ab13af93f29ca
Greg Lawson <greg@acer-desktop.lan>
2015-03-29 18:43:36 -0700

    fixup! branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -30,36 +30,40 @@ include Constants
 module ClassMethods
 #include Repository::Constants
 include Constants
-def branch_command?(repository, git_command)
-	branch_output = repository.git_command(git_command).assert_post_conditions.output
-	parse = branch_output.parse(Branch_regexp)
+def branch_capture?(repository, branch_command = '--list')
+	branch_output = repository.git_command('branch ' + branch_command).assert_post_conditions.output
+	branch_output.capture?(Branch_regexp)
 end # branch_command?
 def current_branch_name?(repository)
-	branch_output= repository.git_command('branch --list').assert_post_conditions.output
-	parse = branch_output.parse(Branch_regexp)
-	parse[:branch].to_sym
+	branch_capture = branch_capture?(repository, '--list')
+	if branch_capture.success? then
+		branch_capture.output?.map {|c| c[:branch].to_sym}
+	else
+		fail Exception.new('git branch parse failed = ' + branch_capture.inspect)
+	end # if
 end #current_branch_name
 def branches?(repository)
-	branch_output = branch_command?(repository, 'branch --list').assert_post_conditions.output
-	parse = branch_output.parse(Branch_regexp)
-	parse.map {|e| Branch.new(self, e[:branch].to_sym)}
+	branch_capture = branch_capture?(repository, '--list')
+	if branch_capture.success? then
+		branch_capture.output?.map do |c| 
+			Branch.new(repository, c[:branch].to_sym)
+		end # map
+	else
+		fail Exception.new('git branch parse failed = ' + branch_capture.inspect)
+	end # if
 end #branches?
-def remotes?
+def remotes?(repository)
 	pattern=/  /*(/[a-z0-9\/A-Z]+/.capture(:remote))
-	git_parse('branch --list --remote', pattern).map{|h| h[:remote]}
+	repository.git_parse('branch --list --remote', pattern).map{|h| h[:remote]}
 end #remotes?
+def branch_names?(repository)
+	branches?(repository).map {|b| b.branch}
+end # 
 def new_from_git_branch_line(git_branch_line)
 
 end # new_from_git_branch_line
 end #ClassMethods
 extend ClassMethods
-def find_origin
-	if @repository.remotes?.include?(@repository.current_branch_name?) then
-		'origin/'+@branch.to_s
-	else
-		nil
-	end #if
-end # find_origin
 attr_reader :repository, :branch, :remote_branch
 def initialize(repository, branch=repository.current_branch_name?, remote_branch=nil)
 	fail "Branch.new first argument must be of type Repository" unless repository.instance_of?(Repository)
@@ -69,9 +73,9 @@ def initialize(repository, branch=repository.current_branch_name?, remote_branch
 	@repository=repository
 	@branch=branch
 	if remote_branch.nil? then
-		@remote_branch=find_origin
-	else
 		@remote_branch=remote_branch
+	else
+		@remote_branch=find_origin
 	end # if
 end # initialize
 # Allows Branch objects to be used in most contexts where a branch name Symbol is expected
@@ -81,6 +85,20 @@ end # to_s
 def to_sym
 	@branch.to_sym
 end # to_s
+def find_origin
+	if Branch.remotes?(@repository).include?(@repository.current_branch_name?) then
+		'origin/'+@branch.to_s
+	else
+		nil
+	end #if
+end # find_origin
+def rebase!
+	if remotes?.include?(current_branch_name?) then
+		git_command('rebase --interactive origin/'+current_branch_name?).assert_post_conditions.output.split("\n")
+	else
+		puts current_branch_name?.to_s+' has no remote branch in origin.'
+	end #if
+end #rebase!
 require_relative '../../test/assertions.rb'
 module Assertions
 module ClassMethods
--- test/unit/branch_test.rb
+++ test/unit/branch_test.rb
@@ -7,6 +7,7 @@
 ###########################################################################
 require_relative '../unit/test_environment'
 require_relative '../../test/assertions/ruby_assertions.rb'
+require_relative '../../test/assertions/branch_assertions.rb'
 #require_relative '../../test/assertions/repository_assertions.rb'
 require_relative '../../app/models/branch.rb'
 class BranchTest < TestCase
@@ -23,23 +24,24 @@ end # branch_command?
 def test_current_branch_name?
 #	assert_include(WorkFlow::Branch_enhancement, WorkFlow.current_branch_name?, Repo.head.inspect)
 	branch_output= Empty_Repo.git_command('branch --list').assert_post_conditions.output
-	assert_equal(:master, Branch.current_branch_name?(Empty_Repo))
+	assert_equal([:master, :passed], Branch.current_branch_name?(Empty_Repo))
 end #current_branch_name
 def test_branches?
 #?	explain_assert_respond_to(Parse, :parse_split)
 	branch_output = Empty_Repo.git_command('branch --list').assert_post_conditions.output
 	Patterns.each do |p|
 		assert_match(p, branch_output)
-		branches=branch_output.parse(p)
-		assert_equal([{:branch=>"master"}, {:branch=>"passed"}], branches, p.inspect)
+		branches = branch_output.capture?(p)
+		puts branches.inspect if branches.success?
+		assert_equal([{:branch=>"master"}, {:branch=>"passed"}], branches.output?, branches.inspect)
 	end # each
 	
-	assert_includes(Empty_Repo.branches?.map{|b| b.branch}, Empty_Repo.current_branch_name?)
-	assert_equal([:master, :passed], Empty_Repo.branches?.map{|b| b.branch})
-	assert_includes(This_code_repository.branch_names?.map{|b| b.branch}, This_code_repository.current_branch_name?)
+	assert_includes(Branch.branches?(Empty_Repo).map{|b| b.branch}, Empty_Repo.current_branch_name?)
+	assert_equal([:master, :passed], Branch.branches?(Empty_Repo).map{|b| b.branch})
+	assert_includes(Branch.branch_names?(This_code_repository), This_code_repository.current_branch_name?)
 end #branches?
 def test_remotes?
-	assert_empty(Empty_Repo.remotes?)
+	assert_empty(Branch.remotes?(Empty_Repo))
 end #remotes?
 def test_initialize
 	assert_equal(This_code_repository, Branch.new(This_code_repository).repository)
