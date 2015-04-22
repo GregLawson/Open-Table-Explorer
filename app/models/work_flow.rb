c1e3fc7ac07b02f495d92e6adf609fce4f3fc5d8
Greg Lawson <GregLawson123@gmail.com>
2015-04-22 09:39:52 -0700

    fixup! work_flow

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -55,68 +55,6 @@ def initialize(specific_file,
 		@branch_index = index
 	end # if
 end # initialize
-def version_comparison(files = nil)
-	if files.nil? then
-		files = [@repository.log_path?(@related_files.model_test_pathname?)].concat(@related_files.edit_files)
-	end # if
-	ret = files.map do |f|
-		goldilocks(f)
-	end # map
-	ret.join(' ')
-end # version_comparison
-def goldilocks(filename, middle_branch = @repository.current_branch_name?.to_sym)
-	if File.exists?(filename) then
-		current_index = UnitMaturity.branch_index?(middle_branch)
-		left_index, right_index = @unit_maturity.bracketing_versions?(filename, current_index)
-		relative_filename = Pathname.new(File.expand_path(filename)).relative_path_from(Pathname.new(Dir.pwd)).to_s
-		ret = ' -t '
-		if left_index.nil? then
-			ret += " #{relative_filename} "
-		else
-			ret += "#{UnitMaturity.revison_tag?(left_index)} #{relative_filename} "
-		end # if
-		ret += relative_filename
-		if right_index.nil? then
-			ret += " #{relative_filename} "
-		else
-			ret += " #{UnitMaturity.revison_tag?(right_index)} #{relative_filename}"
-		end # if
-	else
-		ret = ''
-	end # if
-#	ret += ' -r ' + @unit_maturity.last_change?(filename) + ' ' + filename
-end # goldilocks
-def test_files(edit_files = @related_files.edit_files)
-	pairs = @related_files.functional_parallelism(edit_files).map do |p|
-
-		' -t ' + p.map do |f|
-			Pathname.new(f).relative_path_from(Pathname.new(Dir.pwd)).to_s
-
-		end.join(' ') # map
-	end # map
-	pairs.join(' ')
-end # test_files
-def minimal_comparison?
-	if @related_files.edit_files == [] then
-		unit_pattern = FilePattern.new_from_path(_FILE_)
-	else
-		unit_pattern = FilePattern.new_from_path(@related_files.edit_files[0])
-	end # if
-	unit_name = unit_pattern.unit_base_name
-	FilePattern::Constants::Patterns.map do |p|
-		pattern = FilePattern.new(p)
-		pwd = Pathname.new(Dir.pwd)
-		default_test_class_id = @related_files.default_test_class_id?.to_s
-		min_path = Pathname.new(pattern.path?('minimal' + default_test_class_id))
-		unit_path = Pathname.new(pattern.path?(unit_name))
-#		path = Pathname.new(start_file_pattern.pathname_glob(@related_files.model_basename)).relative_path_from(Pathname.new(Dir.pwd)).to_s
-#		puts "File.exists?('#{min_path}')==#{File.exists?(min_path)}, File.exists?('#{path}')==#{File.exists?(path)}" if $VERBOSE
-		if File.exists?(min_path)  then
-			' -t ' + unit_path.relative_path_from(pwd).to_s + ' ' + 
-				min_path.relative_path_from(pwd).to_s
-		end # if
-	end.compact.join # map
-end # minimal_comparison
 def deserving_branch?(executable = @related_files.model_test_pathname?)
 	if File.exists?(executable) then
 		@error_score = @repository.error_score?(executable)
@@ -188,44 +126,6 @@ def merge(target_branch, source_branch, interact=:interactive)
 		@repository.confirm_commit(interact)
 	end # safely_visit_branch
 end # merge
-def edit(context = nil)
-	if context.nil? then
-	else
-	end # if
-	@repository.recent_test.puts if !@repository.recent_test.nil?
-	if @related_files.edit_files.empty? then
-		command_string = 'diffuse' + version_comparison([@specific_file]) + test_files
-	else
-		command_string = 'diffuse' + version_comparison + test_files
-	end # if
-	puts command_string if $VERBOSE
-	edit = @repository.shell_command(command_string)
-	edit = edit.tolerate_status_and_error_pattern(0, /Warning/)
-	status =edit
-#	status.assert_post_conditions
-end # edit
-def split(executable, new_base_name)
-	related_files = work_flow.related_files
-	new_unit = Unit.new(new_base_name, project_root_dir)
-	related_files.edit_files. map do |f|
-		pattern_name = FilePattern.find_by_file(f)
-		split_tab += ' -t ' + f + new_unit.pattern?(pattern_name)
-		@repository.shell_command('cp ' + f +  new_unit.pattern?(pattern_name))
-	end #map
-	edit = @repository.shell_command('diffuse' + version_comparison + test_files + split_tab)
-	puts edit.command_string
-	edit.assert_post_conditions
-end # split
-def minimal_edit
-	edit = @repository.shell_command('diffuse' + version_comparison + test_files + minimal_comparison?)
-	puts edit.command_string
-	edit.assert_post_conditions
-end # minimal_edit
-def emacs(executable = @related_files.model_test_pathname?)
-	emacs = @repository.shell_command('emacs --no-splash ' + @related_files.edit_files.join(' '))
-	puts emacs.command_string
-	emacs.assert_post_conditions
-end # emacs
 def merge_down(deserving_branch = @repository.current_branch_name?)
 	UnitMaturity.merge_range(deserving_branch).each do |i|
 		@repository.safely_visit_branch(UnitMaturity::Branch_enhancement[i]) do |changes_branch|
--- test/unit/work_flow_test.rb
+++ test/unit/work_flow_test.rb
@@ -66,19 +66,6 @@ def test_goldilocks
 	assert_data_file(relative_filename)
 end #goldilocks
 include WorkFlow::Examples
-def test_execute
-	assert_include(TestWorkFlow.related_files.edit_files, TestFile)
-#	assert_equal('', TestWorkFlow.version_comparison)
-#	assert_equal('', TestWorkFlow.test_files)
-end #execute
-def test_test_files
-	assert_equal('', TestWorkFlow.test_files([]))
-# 	assert_equal(' -t /home/greg/Desktop/src/Open-Table-Explorer/app/models/work_flow.rb /home/greg/Desktop/src/Open-Table-Explorer/test/unit/work_flow_test.rb', TestWorkFlow.test_files([TestWorkFlow.edit_files]))
-end #test_files
-def test_minimal_comparison
-	assert_equal(' -t app/models/work_flow.rb app/models/minimal2.rb -t test/unit/work_flow_test.rb test/unit/minimal2_test.rb -t script/work_flow.rb script/minimal2.rb -t test/integration/work_flow_test.rb test/integration/minimal2_test.rb -t test/long_test/work_flow_test.rb test/long_test/minimal2_test.rb -t test/assertions/work_flow_assertions.rb test/assertions/minimal2_assertions.rb -t test/unit/work_flow_assertions_test.rb test/unit/minimal2_assertions_test.rb -t log/library/work_flow.log log/library/minimal2.log -t log/assertions/work_flow.log log/assertions/minimal2.log -t log/integration/work_flow.log log/integration/minimal2.log -t log/long/work_flow.log log/long/minimal2.log -t test/data_sources/work_flow test/data_sources/minimal2', TestWorkFlow.minimal_comparison?)
-	assert_equal(' -t app/models/regexp_parse.rb app/models/minimal4.rb -t test/unit/regexp_parse_test.rb test/unit/minimal4_test.rb -t script/regexp_parse.rb script/minimal4.rb -t test/integration/regexp_parse_test.rb test/integration/minimal4_test.rb -t test/long_test/regexp_parse_test.rb test/long_test/minimal4_test.rb -t test/assertions/regexp_parse_assertions.rb test/assertions/minimal4_assertions.rb -t test/unit/regexp_parse_assertions_test.rb test/unit/minimal4_assertions_test.rb -t log/library/regexp_parse.log log/library/minimal4.log -t log/assertions/regexp_parse.log log/assertions/minimal4.log -t log/integration/regexp_parse.log log/integration/minimal4.log -t log/long/regexp_parse.log log/long/minimal4.log -t test/data_sources/regexp_parse test/data_sources/minimal4', WorkFlow.new('test/unit/regexp_parse_test.rb').minimal_comparison?)
-end #minimal_comparison
 def test_merge_conflict_recovery
 end # merge_conflict_recovery
 def test_merge
@@ -86,17 +73,6 @@ def test_merge
 	TestWorkFlow.repository.edited_superset_of_testing.assert_post_conditions
 	TestWorkFlow.merge(:edited, :testing) # not too long or too dangerous
 end #merge
-def test_edit
-#(context = nil)
-end # edit
-def test_split
-#(executable, new_base_name)
-end # split
-def test_minimal_edit
-end # minimal_edit
-def test_emacs
-#(executable = @related_files.model_test_pathname?)
-end # emacs
 def test_merge_down
 #(deserving_branch = @repository.current_branch_name?)
 end # merge_down
