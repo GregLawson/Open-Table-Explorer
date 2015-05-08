be8b9a0077438d105cb9398987cef3911e4c7ea3
Greg Lawson <GregLawson123@gmail.com>
2015-05-08 06:48:28 -0700

    fixup! test_run

---
+++ app/models/editor.rb
@@ -0,0 +1,172 @@
+###########################################################################
+#    Copyright (C) 2013-15 by Greg Lawson
+#    <GregLawson123@gmail.com>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'unit.rb'
+#require_relative 'repository.rb'
+require_relative 'unit_maturity.rb'
+class Editor
+module Constants
+end # Constants
+include Constants
+module ClassMethods
+include Constants
+end # ClassMethods
+extend ClassMethods
+# Define related (unit) versions
+# Use as current, lower/upper bound, branch history
+# parametized by related files, repository, branch_number, executable
+# record error_score, recent_test, time
+attr_reader :related_files, :edit_files, :repository, :unit_maturity
+def initialize(specific_file,
+	related_files = Unit.new_from_path?(specific_file),
+	repository = Repository.new(FilePattern.repository_dir?, :interactive))
+
+	@specific_file = specific_file
+	@unit_maturity = UnitMaturity.new(repository, related_files)
+	@related_files = related_files
+	@repository = repository
+	index = UnitMaturity::Branch_enhancement.index(repository.current_branch_name?)
+	if index.nil? then
+		@branch_index = UnitMaturity::First_slot_index
+	else
+		@branch_index = index
+	end # if
+end # initialize
+def version_comparison(files = nil)
+	if files.nil? then
+		files = [@repository.log_path?(@related_files.model_test_pathname?)].concat(@related_files.edit_files)
+	end # if
+	ret = files.map do |f|
+		goldilocks(f)
+	end # map
+	ret.join(' ')
+end # version_comparison
+def goldilocks(filename, middle_branch = @repository.current_branch_name?.to_sym)
+	if File.exists?(filename) then
+		current_index = UnitMaturity.branch_index?(middle_branch)
+		left_index, right_index = @unit_maturity.bracketing_versions?(filename, current_index)
+		relative_filename = Pathname.new(File.expand_path(filename)).relative_path_from(Pathname.new(Dir.pwd)).to_s
+		ret = ' -t '
+		if left_index.nil? then
+			ret += " #{relative_filename} "
+		else
+			ret += "#{UnitMaturity.revison_tag?(left_index)} #{relative_filename} "
+		end # if
+		ret += relative_filename
+		if right_index.nil? then
+			ret += " #{relative_filename} "
+		else
+			ret += " #{UnitMaturity.revison_tag?(right_index)} #{relative_filename}"
+		end # if
+	else
+		ret = ''
+	end # if
+#	ret += ' -r ' + BranchReference.last_change?(filename, repository) + ' ' + filename
+end # goldilocks
+def test_files(edit_files = @related_files.edit_files)
+	pairs = @related_files.functional_parallelism(edit_files).map do |p|
+
+		' -t ' + p.map do |f|
+			Pathname.new(f).relative_path_from(Pathname.new(Dir.pwd)).to_s
+
+		end.join(' ') # map
+	end # map
+	pairs.join(' ')
+end # test_files
+def minimal_comparison?
+	if @related_files.edit_files == [] then
+		unit_pattern = FilePattern.new_from_path(_FILE_)
+	else
+		unit_pattern = FilePattern.new_from_path(@related_files.edit_files[0])
+	end # if
+	unit_name = unit_pattern.unit_base_name
+	FilePattern::Constants::Patterns.map do |p|
+		pattern = FilePattern.new(p)
+		pwd = Pathname.new(Dir.pwd)
+		default_test_class_id = @related_files.default_test_class_id?.to_s
+		min_path = Pathname.new(pattern.path?('minimal' + default_test_class_id))
+		unit_path = Pathname.new(pattern.path?(unit_name))
+#		path = Pathname.new(start_file_pattern.pathname_glob(@related_files.model_basename)).relative_path_from(Pathname.new(Dir.pwd)).to_s
+#		puts "File.exists?('#{min_path}')==#{File.exists?(min_path)}, File.exists?('#{path}')==#{File.exists?(path)}" if $VERBOSE
+		if File.exists?(min_path)  then
+			' -t ' + unit_path.relative_path_from(pwd).to_s + ' ' + 
+				min_path.relative_path_from(pwd).to_s
+		end # if
+	end.compact.join # map
+end # minimal_comparison
+def edit(context = nil)
+	if context.nil? then
+	else
+	end # if
+	@repository.recent_test.puts if !@repository.recent_test.nil?
+	if @related_files.edit_files.empty? then
+		command_string = 'diffuse' + version_comparison([@specific_file]) + test_files
+	else
+		command_string = 'diffuse' + version_comparison + test_files
+	end # if
+	puts command_string if $VERBOSE
+	edit = @repository.shell_command(command_string)
+	edit = edit.tolerate_status_and_error_pattern(0, /Warning/)
+	status =edit
+#	status.assert_post_conditions
+end # edit
+def split(executable, new_base_name)
+	related_files = work_flow.related_files
+	new_unit = Unit.new(new_base_name, project_root_dir)
+	related_files.edit_files. map do |f|
+		pattern_name = FilePattern.find_by_file(f)
+		split_tab += ' -t ' + f + new_unit.pattern?(pattern_name)
+		@repository.shell_command('cp ' + f +  new_unit.pattern?(pattern_name))
+	end #map
+	edit = @repository.shell_command('diffuse' + version_comparison + test_files + split_tab)
+	puts edit.command_string
+	edit.assert_post_conditions
+end # split
+def minimal_edit
+	edit = @repository.shell_command('diffuse' + version_comparison + test_files + minimal_comparison?)
+	puts edit.command_string
+	edit.assert_post_conditions
+end # minimal_edit
+def emacs(executable = @related_files.model_test_pathname?)
+	emacs = @repository.shell_command('emacs --no-splash ' + @related_files.edit_files.join(' '))
+	puts emacs.command_string
+	emacs.assert_post_conditions
+end # emacs
+require_relative '../../test/assertions.rb'
+module Assertions
+
+module ClassMethods
+
+def assert_pre_conditions
+end # assert_pre_conditions
+def assert_post_conditions
+#	assert_pathname_exists(TestFile, "assert_post_conditions")
+end # assert_post_conditions
+end # ClassMethods
+def assert_pre_conditions
+	assert_not_nil(@related_files)
+	assert_not_empty(@related_files.edit_files, "assert_pre_conditions, @test_environmen=#{@test_environmen.inspect}, @related_files.edit_files=#{@related_files.edit_files.inspect}")
+	assert_kind_of(Grit::Repo, @repository.grit_repo)
+	assert_respond_to(@repository.grit_repo, :status)
+	assert_respond_to(@repository.grit_repo.status, :changed)
+end # assert_pre_conditions
+def assert_post_conditions
+	odd_files = Dir['/home/greg/Desktop/src/Open-Table-Explorer/test/unit/*_test.rb~HEAD*']
+	assert_empty(odd_files, 'Editor#assert_post_conditions')
+end # assert_post_conditions
+end # Assertions
+include Assertions
+extend Assertions::ClassMethods
+# TestEditor.assert_pre_conditions
+include Constants
+module Examples
+TestFile = File.expand_path($PROGRAM_NAME)
+TestEditor = Editor.new(TestFile)
+include Constants
+end # Examples
+include Examples
+end # Editor
--- test/unit/test_run_test.rb
+++ test/unit/test_run_test.rb
@@ -11,13 +11,14 @@ require_relative '../../app/models/regexp.rb'
 require_relative '../../app/models/test_run.rb'
 # executed in alphabetical order. Longer names sort later.
 class TestRunTest < TestCase
+include TestExecutable::Examples
 include TestRun::Examples
 include Repository::Constants
 def test_virtus_initialize
-	assert_equal(:unit, Odd_plural_testRun.test_type)
-	assert_equal('code_base', Odd_plural_testRun.singular_table)
-	assert_equal('code_bases', Odd_plural_testRun.plural_table)
-	assert_equal(nil, Odd_plural_testRun.test)
+	assert_equal(:unit, Odd_plural_executable.test_type)
+	assert_equal('code_base', Odd_plural_executable.singular_table)
+	assert_equal('code_bases', Odd_plural_executable.plural_table)
+	assert_equal(nil, Odd_plural_executable.test)
 end # virtus_initialize
 def test_TestRun_initialize
 	testRun=TestExecutable.new
@@ -39,35 +40,42 @@ def test_TestRun_initialize
 	assert_equal('string_hash', testRun.singular_table)
 	assert_equal('string_hash', testRun[:singular_table])
 	
-#	Singular_testRun.assert_logical_primary_key_defined
-#	Stream_pattern_testRun.assert_logical_primary_key_defined()
-#	Unit_testRun.assert_logical_primary_key_defined()
+#	Singular_executable.assert_logical_primary_key_defined
+#	Stream_pattern_executable.assert_logical_primary_key_defined()
+#	Unit_executable.assert_logical_primary_key_defined()
 end #initialize
+def test_new_from_pathname
+	unit = Unit.new_from_path?(executable_file_file)
+	new_executable_file = TestExecutable.new(executable_file: executable_file, unit: unit)
+end # new_from_pathname
 def test_log_path?
-	executable = $PROGRAM_NAME
-	assert_equal('log/unit/1.9/1.9.3p194/quiet/repository.log', This_code_repository.log_path?(executable))
+	executable_file = $PROGRAM_NAME
+	assert_equal('log/unit/1.9/1.9.3p194/quiet/repository.log', This_code_repository.log_path?(executable_file))
 #	assert_equal('log/unit/1.9/1.9.3p194/quiet/repository.log', This_code_repository.log_path?)
 end # log_path?
 def test_ruby_test_string
-	executable = $PROGRAM_NAME
-	ruby_test_string = This_code_repository.ruby_test_string(executable)
-	assert_match(executable, ruby_test_string)
+	executable_file = $PROGRAM_NAME
+	ruby_test_string = This_code_repository.ruby_test_string(executable_file)
+	assert_match(executable_file, ruby_test_string)
 end # ruby_test_string
 def test_TestExecutable_initialize
 end #initialize
 def test_log_file
 	test_virtus_initialize
-	assert_equal(:unit, Odd_plural_testRun.test_type)
-	assert_equal('code_base', Odd_plural_testRun.singular_table)
-	assert_equal(:code_base, Odd_plural_testRun.unit?.model_class_name, Odd_plural_testRun.inspect)
-	assert_equal(:code_base, Odd_plural_testRun.unit?.model_class_name.to_s.underscore.to_sym, Odd_plural_testRun.inspect)
+	assert_equal(:unit, Odd_plural_executable.test_type)
+	assert_equal('code_base', Odd_plural_executable.singular_table)
+	assert_equal(:code_base, Odd_plural_executable.unit?.model_class_name, Odd_plural_executable.inspect)
+	assert_equal(:code_base, Odd_plural_executable.unit?.model_class_name.to_s.underscore.to_sym, Odd_plural_executable.inspect)
 
-	assert_equal(:code_base, Odd_plural_testRun.unit?.model_basename, Odd_plural_testRun.inspect)
-	assert_equal(File.expand_path('log/library/code_base.log'), Odd_plural_testRun.log_file, Odd_plural_testRun.inspect)
+	assert_equal(:code_base, Odd_plural_executable.unit?.model_basename, Odd_plural_executable.inspect)
+	assert_equal(File.expand_path('log/library/code_base.log'), Odd_plural_executable.log_file, Odd_plural_executable.inspect)
 end #log_file
 def test_test_file?
-	assert_equal('test/unit/code_base_test.rb',Odd_plural_testRun.test_file?)
+	assert_equal('test/unit/code_base_test.rb',Odd_plural_executable.test_file?)
 end #test_file?
+def test_unit_names?
+	assert_equal(['repository'], Minimal_repository.unit_names?([$0]))	
+end #unit_names?
 def test_ruby_run_and_log
 #	executable=This_code_repository.related_files.model_test_pathname?
 	executable='/etc/mtab' #force syntax error with non-ruby text
@@ -93,7 +101,7 @@ def test_shell
 	assert_not_empty(TestRun.shell('pwd'){|run| run.inspect})
 end #shell
 def test_file_bug_reports
-	header,errors,summary=TestRun.parse_log_file(Odd_plural_testRun.log_file?)
+	header,errors,summary=TestRun.parse_log_file(Odd_plural_executable.log_path?)
 	headerArray=header.split("\n")
 	assert_instance_of(Array, headerArray)
 	sysout=headerArray[0..-2]
@@ -112,7 +120,7 @@ def test_file_bug_reports
 	assert_operator(run_time, :>=, 0)
 end #file_bug_reports
 def test_parse_log_file
-	log_file = Default_testRun.log_file?
+	log_file = Default_executable.log_path?
 	blocks=IO.read(log_file).split("\n\n")# delimited by multiple successive newlines
 #	puts "blocks='#{blocks.inspect}'"
 	header= blocks[0]
@@ -135,7 +143,7 @@ def test_parse_log_file
 	sysout,run_time=TestRun.parse_header(header)
 	assert_not_nil(run_time)
 	assert_operator(run_time, :>=, 0)
-	header,errors,summary=TestRun.parse_log_file(testRun.log_file?)
+	header,errors,summary=TestRun.parse_log_file(testRun.log_path?)
 	assert_not_nil(header)
 	assert_not_nil(summary)
 end #parse_log_file
@@ -146,7 +154,7 @@ end # summarize
 def test_parse_summary
 end #parse_summary
 def test_parse_header
-	header,errors,summary=TestRun.parse_log_file(Odd_plural_testRun.log_file?)
+	header,errors,summary=TestRun.parse_log_file(Odd_plural_executable.log_path?)
 	assert_operator(header.size,:>,0)
 	headerArray=header.split("\n")
 	assert_instance_of(Array, headerArray)
@@ -162,30 +170,41 @@ def test_parse_header
 	assert_not_nil(run_time)
 	assert_operator(run_time, :>=, 0)
 end #parse_header
+def test_TestRun_initialize
+	assert_equal(TestExecutable::Examples::Default_executable, Default_testRun.executable)
+	assert_equal(TestExecutable::Examples::Default_executable.executable_file, Default_testRun.executable.executable_file)
+	assert_equal($PROGRAM_NAME, Default_testRun.executable.executable_file)
+	assert_equal(TestExecutable::Examples::Default_executable.unit, Default_testRun.executable.unit)
+end # test_TestRun_initialize
 def test_error_score?
-	executable='/etc/mtab' #force syntax error with non-ruby text
-	test_executable = TestExecutable.new(executable: executable)
-	ruby_test_string = TestExecutable.ruby_test_string(executable)
+	executable_file = '/etc/mtab' #force syntax error with non-ruby text
+	test_executable = TestExecutable.new(executable_file: executable_file)
+	ruby_test_string = test_executable.ruby_test_string
 	recent_test = This_code_repository.shell_command(ruby_test_string)
 	error_message = recent_test.process_status.inspect+"\n"+recent_test.inspect
 	assert_equal(1, recent_test.process_status.exitstatus, error_message)
 	assert_equal(false, recent_test.success?, error_message)
 	assert(!recent_test.success?, error_message)
-		syntax_test=This_code_repository.shell_command("ruby -c "+executable)
+		syntax_test=This_code_repository.shell_command("ruby -c "+executable_file)
 		assert_not_equal("Syntax OK\n", syntax_test.output, syntax_test.inspect)
-	assert_equal(10000, TestRun.new(executable: executable).error_score?, This_code_repository.recent_test.inspect)
-#	This_code_repository.assert_deserving_branch(:edited, executable)
+#	test_run = TestRun.new(:executable => executable)
+	test_run = TestRun.new(executable: TestExecutable.new(executable_file: executable_file))
+	assert_equal(nil, Unit.new_from_path?(executable_file))
+	assert_equal(nil, test_run.executable.unit, test_run.inspect)
+	assert_equal(10000, test_run.error_score?, recent_test.inspect)
+#	This_code_repository.assert_deserving_branch(:edited, executable_file)
 
-	executable='test/unit/minimal2_test.rb'
-		recent_test=This_code_repository.shell_command("ruby "+executable)
+	executable_file ='test/unit/minimal2_test.rb'
+	test_executable = TestExecutable.new(executable_file: executable_file)
+		recent_test=This_code_repository.shell_command("ruby "+executable_file)
 		assert_equal(recent_test.process_status.exitstatus, 0, recent_test.inspect)
-		syntax_test=This_code_repository.shell_command("ruby -c "+executable)
+		syntax_test=This_code_repository.shell_command("ruby -c "+executable_file)
 		assert_equal("Syntax OK\n", syntax_test.output, syntax_test.inspect)
-	assert_equal(0, TestRun.new(executable: executable).error_score?('test/unit/minimal2_test.rb'))
-#	This_code_repository.assert_deserving_branch(:passed, executable)
+	assert_equal(0, TestRun.new(executable: test_executable).error_score?)
+#	This_code_repository.assert_deserving_branch(:passed, executable_file)
 	Error_classification.each_pair do |key, value|
-		executable = Repository_Unit.data_sources_directory?+'/'+value.to_s+'.rb'
-		assert_equal(key, This_code_repository.error_score?(executable), This_code_repository.recent_test.inspect)
+		executable_file = Repository_Unit.data_sources_directory?+'/'+value.to_s+'.rb'
+		assert_equal(key, TestRun.new(executable: TestExecutable.new(executable_file: executable_file)).error_score?, This_code_repository.recent_test.inspect)
 	end #each
 end # error_score
 def test_run
