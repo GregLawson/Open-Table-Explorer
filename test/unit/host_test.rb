d103e7dc3596f63706c9a86a3b2854e10df35ade
Greg Lawson <GregLawson123@gmail.com>
2015-05-23 09:53:15 -0700

    fixup! host

--- test/unit/host_test.rb
+++ test/unit/host_test.rb
@@ -11,6 +11,13 @@ require_relative '../../app/models/host.rb'
 class HostTest < TestCase
 #include DefaultTests
 include TE.model_class?::Examples
+def test_Constants
+end # Constants
+def test_nmap
+	ip_range = '192.168.0.1-254'
+	nmap_run = ShellCommand.new('nmap ' + ip_range).assert_post_conditions
+	nmap_hash = nmap_run.output.parse(Start_line)
+end # nmap
 def test_nmapScan
 #	candidateIP = 
 #	assert_test_id_equal
