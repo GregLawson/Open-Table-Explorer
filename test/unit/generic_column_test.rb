23c33c362819f240f2de3093b9031fc05926327a
Greg Lawson <GregLawson123@gmail.com>
2015-07-01 04:37:17 -0700

    fixup! generic_column

--- test/unit/generic_column_test.rb
+++ test/unit/generic_column_test.rb
@@ -16,7 +16,7 @@ def test_GenericVariable_name
 	assert_equal(:Var, Var.name)
 end # name
 def test_equal
-	assert_equal(Name_0, GenericColumn.promote(:name))
+	assert_equal(Name, GenericVariable.promote(:name))
 end # ==
 def test_GenericVariable_header
 	assert_equal('Col', Col.header)
@@ -26,11 +26,14 @@ end # header
 def test_promote 
 	assert_equal(Name_0, GenericColumn.promote(Name_0))
 	assert_equal(Name_0, GenericColumn.promote(Name))
-	assert_equal(Name_0.variable.ruby_type.name, GenericColumn.promote(:name).variable.ruby_type.name)
+	assert_equal(Name_0.variable.ruby_type, GenericColumn.promote(:name).variable.ruby_type)
 
 #suprise	assert_equal(Name_0.variable.ruby_type, GenericColumn.promote(:name).variable.ruby_type)
 	assert_equal(Name_0, GenericColumn.promote(:name))
 end # promote
+def test_equal
+	assert_equal(Name_0, GenericColumn.promote(:name))
+end # ==
 def test_GenericColumn_name
 	assert_equal(:name, Name_0.name)
 	assert_equal(:name_3, Name3.name)
@@ -46,8 +49,13 @@ end # header
 def test_to_hash
 	assert_equal({Col_1 => '123'}, Col_1.to_hash('123'))
 	memory_address_regexp = /[[:xdigit:]]{14}/
-	assert_match(memory_address_regexp, Col_1.to_hash('123').inspect)
-	ruby_type_regexp = /ruby_type=#<Class:0x/ * memory_address_regexp * />/
+#	assert_equal('\', Regexp.new(Regexp.to_regexp_escaped_string('\')).source)
+#	assert_equal('\"', Regexp.new(Regexp.to_regexp_escaped_string('\"')).source)
+#	assert_equal('\"', /\\"/.source)
+	string_quote_inspected_regexp = Regexp.new(Regexp.to_regexp_escaped_string('\"'))
+	name_string_regexp = /:[A-Z][a-z0-9]+/
+	assert_match(name_string_regexp, Col_1.to_hash('123').inspect)
+	ruby_type_regexp = /ruby_type=/ * name_string_regexp
 	assert_match(ruby_type_regexp, Col_1.to_hash('123').inspect)
 	name_inspect_regexp = /name=:Col /
 	assert_match(name_inspect_regexp, Col_1.to_hash('123').inspect)
