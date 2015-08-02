7b1b208b1299378c11e49b50b371c3d7ad9d0ce8
Greg Lawson <GregLawson123@gmail.com>
2015-08-02 13:14:19 -0700

    fixup! editor
    edit script works

--- script/editor.rb
+++ script/editor.rb
@@ -7,19 +7,18 @@
 #
 ###########################################################################
 # @see http://ruby-doc.org/stdlib-2.0.0/libdoc/optparse/rdoc/OptionParser.html#method-i-make_switch
+require_relative '../app/models/editor.rb' # before command_line
 require_relative '../app/models/command_line.rb'
 #require_relative '../app/models/unit.rb'
-#require_relative '../app/models/test_executable.rb'
 scripting_executable = TestExecutable.new_from_path($0)
 require_relative "../app/models/#{scripting_executable.unit.model_basename}"
 script_class = Unit::Executing_Unit.model_class?
 
-pp ARGV if $VERBOSE
 script = CommandLine.new($0)
 pp ARGV if $VERBOSE
 pp script.options if $VERBOSE
 
 run = script.run do
 end # do run
-puts "run returns "+ run.inspect
+puts "run returns "+ run.inspect if CommandLine::Command_line_opts[:inspect]
 1 # successfully completed
