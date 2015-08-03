d718a481bd259dd4eb7dd6c1f2caec699538a4bd
Greg Lawson <GregLawson123@gmail.com>
2015-08-03 08:33:20 -0700

    fixup! minimal

--- script/minimal2.rb
+++ script/minimal2.rb
@@ -6,18 +6,14 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
+require_relative '../app/models/minimal2.rb' # before command_line
 require_relative '../app/models/command_line.rb'
-#require_relative '../app/models/unit.rb'
-#require_relative '../app/models/test_executable.rb'
 scripting_executable = TestExecutable.new_from_path($0)
 require_relative "../app/models/#{scripting_executable.unit.model_basename}.rb"
 script_class = Unit::Executing_Unit.model_class?
 
-pp ARGV if $VERBOSE
 script = CommandLine.new($0)
-pp ARGV if $VERBOSE
-
 run = script.run do
 end # do run
-puts "run returns "+ run.inspect
+puts "run returns "+ run.inspect if CommandLine::Command_line_opts[:inspect]
 1 # successfully completed
