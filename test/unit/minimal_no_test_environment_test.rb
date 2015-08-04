03379ec79f8409cb8bd49ea15ef56a1490805af9
Greg Lawson <GregLawson123@gmail.com>
2015-08-04 15:38:56 -0700

    fixup! minimal

--- test/unit/minimal_no_test_environment_test.rb
+++ test/unit/minimal_no_test_environment_test.rb
@@ -1,14 +1,16 @@
 ###########################################################################
-#    Copyright (C) 2013-2014 by Greg Lawson                                      
+#    Copyright (C) 2013-2015 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
+# gem install mintest
+require "minitest/autorun"
+require 'active_support/all'
 #require_relative 'test_environment' # avoid recursive requires
-require 'test/unit'
-#require_relative '../../test/assertions/ruby_assertions.rb'
-TestCase=BaseTestCase=Test::Unit::TestCase
+require_relative '../../test/assertions/ruby_assertions.rb'
+TestCase = MiniTest::Unit::TestCase
 class MinimalTest < TestCase
 #include TE.model_class?::Examples
 end #Minimal
