1595b4529baf739884b5902c369fbdae837cca69
Greg Lawson <GregLawson123@gmail.com>
2015-08-22 11:36:24 -0700

    fixup! test_environment

---
+++ app/models/test_environment_minitest.rb
@@ -0,0 +1,19 @@
+###########################################################################
+#    Copyright (C) 2012-2015 by Greg Lawson
+#    <GregLawson123@gmail.com>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+# gem install mintest
+require "minitest/autorun"
+require 'active_support/all'
+require_relative '../../test/assertions/ruby_assertions.rb'
+require_relative '../../app/models/unit.rb'
+BaseTestCase = MiniTest::Unit::TestCase 
+TestCase = BaseTestCase
+AssertionsModule = MiniTest::Assertions
+AssertionFailedError = RuntimeError
+#AssertionFailedError=Test::Unit::AssertionFailedError
+#AssertionFailedError = MiniTest::Assertion
+#assert_global_name(:AssertionFailedError)
