c9e1ff204ab8b8b25dd60e8943ca42bd79ab0a41
Greg Lawson <GregLawson123@gmail.com>
2015-09-02 05:44:15 -0700

    fixup! editor

--- script/editor.rb
+++ script/editor.rb
@@ -6,10 +6,9 @@
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-# @see http://ruby-doc.org/stdlib-2.0.0/libdoc/optparse/rdoc/OptionParser.html#method-i-make_switch
-require_relative '../app/models/editor.rb' # before command_line
+require_relative '../app/models/unit.rb' # before command_line
+require_relative "../app/models/#{Unit::Executable.model_basename}"
 require_relative '../app/models/command_line.rb'
-#require_relative '../app/models/unit.rb'
 scripting_executable = TestExecutable.new_from_path($0)
 require_relative "../app/models/#{scripting_executable.unit.model_basename}"
 script_class = Unit::Executable.model_class?
@@ -18,7 +17,6 @@ script = CommandLine.new($0, script_class)
 pp ARGV if $VERBOSE
 pp script.options if $VERBOSE
 
-run = script.run do
+run = CommandLine::Script_command_line.run do
 end # do run
-puts "run returns "+ run.inspect if CommandLine::Command_line_opts[:inspect]
 1 # successfully completed
