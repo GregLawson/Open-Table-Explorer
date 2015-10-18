4431d1426e425bed5a35d88d9b3677cbeef3a8c2
greg <GregLawson123@gmail.com>
2015-10-18 13:41:55 -0700

    fixup! test_executable

--- app/models/test_executable.rb
+++ app/models/test_executable.rb
@@ -6,25 +6,56 @@
 #
 ###########################################################################
 require_relative '../../app/models/test_executable.rb'
-require_relative '../../app/models/no_db.rb'
+#require_relative '../../app/models/no_db.rb'
 require 'virtus'
 require 'fileutils'
 require_relative '../../app/models/repository.rb'
-require_relative '../../app/models/ruby_interpreter.rb'
-require_relative '../../app/models/shell_command.rb'
-require_relative '../../app/models/branch.rb'
-class TestExecutable
+#require_relative '../../app/models/ruby_interpreter.rb'
+#require_relative '../../app/models/shell_command.rb'
+#require_relative '../../app/models/branch.rb'
+class FileArgument
+include Virtus.model
+	attribute :executable_file, String
+	attribute :unit, Unit
+	attribute :repository, Repository, :default => Repository::This_code_repository
+module Examples
+end # Examples
+end # FileArgument
+
+class RailsUnit < FileArgument # naming conventions typical of Ruby Rails S.B. deprecated
 include Virtus.model
 	attribute :test_type, Symbol, :default => 'unit' # is this a virtus bug? automatic String to Symbol conversion
 	attribute :singular_table, String
 	attribute :plural_table, String, :default => nil
+def test_file?
+	case @test_type
+	when :unit
+		return "test/unit/#{@singular_table}_test.rb"
+	when :controller
+		return "test/functional/#{@plural_table}_controller_test.rb"
+	else raise "Unnown @test_type=#{@test_type} for #{self.inspect}"
+	end #case
+end #test_file?
+def unit?
+	Unit.new(@singular_table)
+end # unit?
+module Examples
+#include Constants
+Unit_executable = RailsUnit.new(:test_type => :unit)
+Plural_executable = RailsUnit.new({:test_type => :unit, :plural_table => 'test_runs'})
+Singular_executable = RailsUnit.new(:test_type => :unit,  :singular_table => 'test_run')
+Odd_plural_executable = RailsUnit.new(:test_type => :unit, :singular_table => :code_base, :plural_table => :code_bases, :test => nil)
+end # Examples
+end # RailsUnit
+
+class TestExecutable < RailsUnit # executable / testable ruby unit with executable
+include Virtus.model
 	attribute :test, String, :default => nil # all tests in file
 	attribute :test_command, String, :default => 'ruby'
 	attribute :processor_version, String, :default => nil # system version
 	attribute :options, String, :default => '-W0'
 	attribute :timestamp, Time, :default => Time.now
-	attribute :repository, Repository, :default => Repository::This_code_repository
-	attribute :executable_file, String
+#	attribute :executable_file, String
 	attribute :unit, Unit
 module ClassMethods
 def new_from_path(executable_file,
@@ -35,7 +66,7 @@ def new_from_path(executable_file,
 end # new_from_path
 end # ClassMethods
 extend ClassMethods
-def log_path?(logging = :quiet,
+def log_path?(logging = :silence,
 		minor_version = '1.9',
 		patch_version = '1.9.3p194')
 	@unit = Unit.new_from_path(executable_file)
@@ -50,7 +81,7 @@ def log_path?(logging = :quiet,
 		@log_path += '/' + @unit.model_basename.to_s + '.log'
 		end # if
 end # log_path?
-def ruby_test_string(logging = :quiet,
+def ruby_test_string(logging = :silence,
 		minor_version = '1.9',
 		patch_version = '1.9.3p194')
 	case logging 
@@ -79,27 +110,11 @@ def write_commit_message(recent_test,files)
 	end #if
 	IO.binwrite('.git/GIT_COLA_MSG', commit_message)	
 end # write_commit_message
-def unit?
-	Unit.new(@singular_table)
-end # unit?
 # log_file => String
 # Filename of log file from test run
-def test_file?
-	case @test_type
-	when :unit
-		return "test/unit/#{@singular_table}_test.rb"
-	when :controller
-		return "test/functional/#{@plural_table}_controller_test.rb"
-	else raise "Unnown @test_type=#{@test_type} for #{self.inspect}"
-	end #case
-end #test_file?
 module Examples
 #include Constants
 Default_executable = TestExecutable.new_from_path($PROGRAM_NAME)
-Unit_executable = TestExecutable.new(:test_type => :unit)
-Plural_executable = TestExecutable.new({:test_type => :unit, :plural_table => 'test_runs'})
-Singular_executable = TestExecutable.new(:test_type => :unit,  :singular_table => 'test_run')
-Odd_plural_executable = TestExecutable.new(:test_type => :unit, :singular_table => :code_base, :plural_table => :code_bases, :test => nil)
 end # Examples
 end # TestExecutable
 
