339c963888a12b790d547b3e16c3dd3d657ef472
greg <GregLawson123@gmail.com>
2016-03-03 11:50:47 -0800

    fixup! test_executable

--- app/models/test_executable.rb
+++ app/models/test_executable.rb
@@ -14,50 +14,100 @@ require_relative '../../app/models/ruby_interpreter.rb'
 #require_relative '../../app/models/branch.rb'
 class FileArgument
 include Virtus.model
-	attribute :executable_file, String
-	attribute :unit, Unit, :default => 	lambda { |argument, attribute| Unit.new_from_path(argument.executable_file) }
-#	attribute :test_type, Symbol, :default => 'unit' # is this a virtus bug? automatic String to Symbol conversion
-	attribute :pattern, Symbol, :default => 	lambda { |argument, attribute| FilePattern.find_from_path(argument.executable_file) }
-	attribute :test_type, Symbol, :default => 	lambda { |argument, attribute| (argument.pattern.nil? ? :non_unit : argument.pattern[:name]) }
+	attribute :argument_path, Pathname
+	attribute :unit, Unit, :default => 	lambda { |argument, attribute| Unit.new_from_path(argument.argument_path) }
+	attribute :pattern, Symbol, :default => 	lambda { |argument, attribute| FilePattern.find_from_path(argument.argument_path) }
+#	attribute :test_type, Symbol, :default => 	lambda { |argument, attribute| (argument.pattern.nil? ? :non_unit : argument.pattern[:name]) }
 	attribute :repository, Repository, :default => Repository::This_code_repository
 module Examples
-TestSelf = FileArgument.new(executable_file: $PROGRAM_NAME)
-Not_unit = FileArgument.new(executable_file: '/dev/null')
-TestMinimal  = FileArgument.new(executable_file: 'test/unit/minimal2_test.rb')
-Non_executable = FileArgument.new(executable_file: 'log/unit/2.2/2.2.3p173/silence/test_executable.log')
+TestSelf = FileArgument.new(argument_path: $PROGRAM_NAME)
+Not_unit = FileArgument.new(argument_path: '/dev/null')
+Not_unit_executable = FileArgument.new(argument_path: 'test/data_sources/unit_maturity/success.rb')
+TestMinimal  = FileArgument.new(argument_path: 'test/unit/minimal2_test.rb')
+Unit_non_executable = FileArgument.new(argument_path: 'log/unit/2.2/2.2.3p173/silence/test_executable.log')
 end # Examples
-def testable?(recursion_danger = nil)
-		if @unit.nil? then # probably can't test if not in a unit
-			nil
-		elsif !recursion_danger.nil? &&(@executable_file == $PROGRAM_NAME) then
-			false # terminate recursion
-		elsif @test_type != :unit then
+def unit_file_type
+	 if pattern.nil? then
+		:non_unit
+	else
+		pattern[:name]
+	end #if
+end # unit_file_type
+# argument path is in a unit andis a generatable file.
+def unit_file?
+		if unit_file_type == :non_unit then
+			false
+		elsif @unit.nil? || @unit.project_root_dir.nil? then # probably can't test if not in a unit
 			false
 		else
 			true
 		end # if
-end # testable?
+end # unit_file?
+def generatable_unit_file?
+		if unit_file_type == :non_unit then
+			false
+		elsif @unit.nil? || @unit.project_root_dir.nil? then # probably can't test if not in a unit
+			false
+		elsif @pattern[:generate] then
+			true
+		else
+			false
+		end # if
+end # generatable_unit_file?
+def regression_unit_test_file
+	if unit_file? then
+			@unit.pathname_pattern?(@test_type) # unit_test_path
+	else
+		File.expand_path(@argument_path) # nonunit file
+	end # if
+end # regression_unit_test_file
+def recursion_danger?
+	File.expand_path(regression_unit_test_file) == File.expand_path($PROGRAM_NAME)
+end # recursion_danger?
 end # FileArgument
 
 
 class TestExecutable < FileArgument # executable / testable ruby unit with executable
-  include Virtus.value_object
-  values do
-  attribute :ruby_interpreter, RubyInterpreter, :default => RubyInterpreter::Preferred
+include Virtus.value_object
+values do
+	attribute :test_type, Symbol, :default => 'unit' # is this a virtus bug? automatic String to Symbol conversion
+	attribute :ruby_interpreter, RubyInterpreter, :default => RubyInterpreter::Preferred
 	attribute :test, String, :default => nil # all tests in file
-	end # values
+end # values
 module ClassMethods
-def new_from_path(executable_file,
+def new_from_path(argument_path,
 		test_type = :unit,
 		repository = Repository::This_code_repository)
-	unit = Unit.new_from_path(executable_file)
-	new_executable = TestExecutable.new(executable_file: executable_file, 
+	unit = Unit.new_from_path(argument_path)
+	new_executable = TestExecutable.new(argument_path: argument_path, 
 								unit: unit,
 								test_type: test_type,
 								repository: repository)
 end # new_from_path
 end # ClassMethods
 extend ClassMethods
+# test dirty working directory for needed regression test
+# return nil if not in unit since regression testing is then impossible
+def testable?(recursion_danger = nil)
+	if unit_file? then # probably can't test if not in a unit
+		if !recursion_danger.nil? &&(@argument_path == $PROGRAM_NAME) then
+			false # terminate recursion
+		elsif @test_type == :unit then
+			true
+		elsif @pattern[:suffix][-8..-1] == "_test.rb" then
+			true
+		else
+			false
+		end # if
+	else
+		nil # return nil if not in unit since regression testing is then impossible
+	end # if
+end # testable?
+def regression_test
+	if testable? then
+		test_run = TestRun.new(TestExecutable.new(argument_path: unit_test_path)).error_score?
+	end # if
+end # regression_test
 def log_path?
 	if @unit.nil? then
 		@log_path = '' # empty file string
@@ -69,7 +119,7 @@ def log_path?
 		@log_path += '/' + @ruby_interpreter.logging.to_s
 		Pathname.new(@log_path).mkpath
 		@log_path += '/' + @unit.model_basename.to_s + '.log'
-		end # if
+	end # if
 	@log_path
 end # log_path?
 def ruby_test_string
@@ -79,7 +129,7 @@ def ruby_test_string
 	when :verbose then @ruby_test_string = 'ruby -v -W2 '
 	else fail Exception.new(logging.to_s + ' is not a valid logging type.')
 	end # case
-	@ruby_test_string += executable_file
+	@ruby_test_string += regression_unit_test_file
 end # ruby_test_string
 def error_file(recent_test)
 	ret = @repository.current_branch_name?.to_s
@@ -108,6 +158,11 @@ end # write_commit_message
 module Examples
 #include Constants
 TestTestExecutable = TestExecutable.new_from_path($PROGRAM_NAME)
+TestSelf = TestExecutable.new(argument_path: $PROGRAM_NAME)
+Not_unit = TestExecutable.new(argument_path: '/dev/null')
+Not_unit_executable = TestExecutable.new(argument_path: 'test/data_sources/unit_maturity/success.rb')
+TestMinimal  = TestExecutable.new(argument_path: 'test/unit/minimal2_test.rb')
+Unit_non_executable = TestExecutable.new(argument_path: 'log/unit/2.2/2.2.3p173/silence/test_executable.log')
 end # Examples
 end # TestExecutable
 
--- test/unit/test_executable_test.rb
+++ test/unit/test_executable_test.rb
@@ -10,21 +10,38 @@ require_relative 'test_environment'
 require_relative '../../app/models/test_executable.rb'
 class FileArgumentTest < TestCase
 include FileArgument::Examples
-def test_Examples
-	assert_equal(:unit, TestSelf.test_type)
-	assert_equal(:unit, TestMinimal.test_type)
-	assert_equal(:non_unit, Not_unit.test_type)
+def test_unit_file_type
+	assert_equal(:unit, TestSelf.unit_file_type)
+	assert_equal(:unit, TestMinimal.unit_file_type)
+	assert_equal(:non_unit, Not_unit.unit_file_type)
 	assert_nil(Not_unit.pattern)
-	refute_nil(Non_executable.pattern, Non_executable.inspect)
-	assert_equal(:unit_log, Non_executable.test_type)
-end # Examples
-def test_testable?
-	assert_equal(true, TestSelf.testable?)
-	assert_equal(false, TestSelf.testable?(:recursion_danger))
-	assert_equal(true, TestMinimal.testable?)
-	assert_equal(true, TestMinimal.testable?(:recursion_danger))
-	assert_equal(false, Non_executable.testable?)
-end # testable?
+	refute_nil(Unit_non_executable.pattern, Unit_non_executable.inspect)
+	assert_equal(:unit_log, Unit_non_executable.unit_file_type)
+	assert_equal(:data_sources_dir, Not_unit_executable.unit_file_type)
+end # unit_file_type
+def test_unit_file?
+	assert_equal(true, TestSelf.unit_file?)
+	assert_equal(true, TestMinimal.unit_file?)
+	assert_equal(true, Unit_non_executable.unit_file?)
+	assert_equal(false, Not_unit.unit_file?)
+	assert_equal(false, Not_unit_executable.unit_file?)
+end # unit_file?
+def generatable_unit_file?
+	assert_equal(true, TestSelf.generatable_unit_file?)
+	assert_equal(true, TestMinimal.generatable_unit_file?)
+	assert_equal(false, Unit_non_executable.generatable_unit_file?)
+	assert_equal(false, Not_unit.generatable_unit_file?)
+	assert_equal(false, Not_unit_executable.generatable_unit_file?)
+end # generatable_unit_file?
+def test_regression_unit_test_file
+	assert_equal(File.expand_path($PROGRAM_NAME), TestSelf.regression_unit_test_file)
+	assert_equal(File.expand_path(TestMinimal.argument_path), TestMinimal.regression_unit_test_file)
+	assert_equal(File.expand_path(Not_unit.argument_path), Not_unit.regression_unit_test_file)
+	assert_equal(File.expand_path(Unit_non_executable.unit.pathname_pattern?(:unit)), Unit_non_executable.regression_unit_test_file)
+	assert_equal(File.expand_path(Not_unit_executable.unit.pathname_pattern?(:unit)), Not_unit_executable.regression_unit_test_file)
+end # regression_unit_test_file
+def test_recursion_danger?
+end # recursion_danger?
 end # FileArgument
 
 class TestExecutableTest < TestCase
@@ -42,25 +59,36 @@ def test_TestExecutable_initialize
 #	Unit_executable.assert_logical_primary_key_defined()
 	assert_equal(:unit, TestTestExecutable.test_type, TestTestExecutable.inspect)
 	assert_equal(:unit, TestTestExecutable.test_type)
-end #initialize
+end # values
 def test_new_from_path
-	executable_file = $0
-	unit = Unit.new_from_path(executable_file)
+	argument_path = $0
+	unit = Unit.new_from_path(argument_path)
 	repository = Repository::This_code_repository
-	new_executable = TestExecutable.new(executable_file: executable_file, 
+	new_executable = TestExecutable.new(argument_path: argument_path, 
 								unit: unit, repository: repository)
 	new_from_path_executable = TestExecutable.new_from_path($0)
 	assert_instance_of(TestExecutable, new_executable)
 end # new_from_path
+def test_testable?
+	assert_equal(true, TestSelf.testable?)
+	assert_equal(false, TestSelf.testable?(:recursion_danger))
+	assert_equal(true, TestMinimal.testable?)
+	assert_equal(true, TestMinimal.testable?(:recursion_danger))
+	assert_equal(nil, Not_unit.testable?)
+	assert_equal(true, Not_unit_executable.testable?, Unit_non_executable.inspect)
+	assert_equal(true, Unit_non_executable.testable?, Unit_non_executable.inspect)
+end # testable?
+def test_regression_test
+end # regression_test
 def test_log_path?
 	unit = Unit.new_from_path($0)
 	refute_nil(unit)
 	assert_equal('log/unit/2.2/2.2.3p173/silence/test_executable.log', TestTestExecutable.log_path?)
 end # log_path?
 def test_ruby_test_string
-	executable_file = $PROGRAM_NAME
+	argument_path = $PROGRAM_NAME
 	ruby_test_string = TestTestExecutable.ruby_test_string
-	assert_match(executable_file, ruby_test_string)
+	assert_match(argument_path, ruby_test_string)
 end # ruby_test_string
 def test_write_error_file
 	recent_test = ShellCommands.new('pwd')
@@ -73,5 +101,10 @@ def test_write_commit_message
 end # write_commit_message
 def test_Examples
 	assert_equal(:unit, TestTestExecutable.test_type)
+	assert_equal(:unit, TestSelf.test_type)
+	assert_equal(:unit, TestMinimal.test_type)
+	assert_equal(:unit, Not_unit.test_type)
+	assert_equal(:unit, Unit_non_executable.test_type)
+	assert_equal(:unit, Not_unit_executable.test_type)
 end # Examples
 end # TestExecutable
