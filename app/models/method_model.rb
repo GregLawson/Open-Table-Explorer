20140d4f4b5fc4923b9280ec67d67e2200a98b15
greg <GregLawson123@gmail.com>
2016-04-27 06:17:57 -0700

    fixup! method_model

--- app/models/method_model.rb
+++ app/models/method_model.rb
@@ -6,244 +6,245 @@
 #
 ###########################################################################
 require 'virtus'
-#require_relative '../../app/models/no_db.rb'
+# require_relative '../../app/models/no_db.rb'
 class Method # monkey patch
+  def default_arguments?
+    if arity < 0
+      true
+    else
+      false
+    end # if
+    end # default_arguments
 
-def default_arguments?
-	if arity < 0 then
-		true
-	else
-		false
-	end # if
-
-
-end # default_arguments
-def required_arguments
-
-	if default_arguments? then
-		-(arity+1)
-	else
-		arity
-	end # if
-end # required_arguments
+  def required_arguments
+    if default_arguments?
+      -(arity + 1)
+    else
+      arity
+    end # if
+  end # required_arguments
 end # Method
-class MethodModel  #<ActiveRecord::Base
-module DefinitionalConstants # constant parameters of the type (suggest all CAPS)
-end # DefinitionalConstants
-include DefinitionalConstants
-module ClassMethods
-#include DefinitionalConstants
-def init_path(m,owner=nil,scope=nil)
-	if !m.nil? && owner.nil? && scope.nil? then # only one argument (method)
-		theMethod=m
-		init_path = [:object_space_method]
-		if theMethod.respond_to?(:source_location)
-			init_path << :source_location
-		else
-			init_path << :not_source_location
-		end #if
-		if theMethod.respond_to?(:parameters)
-			init_path << :parameters
-		end #if
-	else #3 arguments
-		init_path = [:init]
+class MethodModel # <ActiveRecord::Base
+  module DefinitionalConstants # constant parameters of the type (suggest all CAPS)
+  end # DefinitionalConstants
+  include DefinitionalConstants
+  module ClassMethods
+    # include DefinitionalConstants
+    def init_path(m, owner = nil, scope = nil)
+      if !m.nil? && owner.nil? && scope.nil? # only one argument (method)
+        theMethod = m
+        init_path = [:object_space_method]
+        init_path << if theMethod.respond_to?(:source_location)
+                       :source_location
+                     else
+                       :not_source_location
+                     end # if
+        if theMethod.respond_to?(:parameters)
+          init_path << :parameters
+        end # if
+      else # 3 arguments
+        init_path = [:init]
 
-		if scope.to_sym==:class then
-			init_path << :class
-		else
-			theMethod=MethodModel.method_query(m, owner)
-		end #if
-		if !theMethod.nil? then
-			init_path << :theMethod_not_nil
-			if theMethod.respond_to?(:source_location)
-				init_path << :source_location
-			else
-				init_path << :not_source_location
-			end #if
-			if theMethod.respond_to?(:parameters)
-				init_path << :parameters
-			end #if
-		else
-			init_path << :theMethod_nil
-		end #if
-	end #if
-	begin
-		attributes[:protected]=protected_method_defined?(m)
-		attributes[:private]=private_method_defined?(m)
-		init_path << :protected
-	rescue StandardError => exc
-		init_path << :rescue_protected
-	end #if
-	if m.to_s[/[a-zA-Z0-9_]+/,0]==m.to_s then
-		init_path << :alphanumeric
-	else
-		init_path << :not_alphanumeric
-	end #if
-init_path
-end # init_path
-def method_query(m, owner)
-	ObjectSpace.each_object(owner) do |object| 
-		if object.respond_to?(m.to_sym) then
-		begin
-			theMethod = object.method(m.to_sym)
-			return theMethod
-		rescue ArgumentError, NameError => exc
-			puts "exc=#{exc}, object=#{object.inspect}"
-		end #begin
-		end #if
-	end #each_object
-	return nil #no object found, new has side effects
-end # method_query
-def constantized
-	@@CONSTANTIZED||=Module.constants.map do |c|
-		begin
-			c=c.constantize
-		rescue
-			 puts "constant #{c.inspect} fails constanization"
-			 nil
-		 end #begin
-	end #map
-end #constantized
-end # ClassMethods
-extend ClassMethods
+        if scope.to_sym == :class
+          init_path << :class
+        else
+          theMethod = MethodModel.method_query(m, owner)
+        end # if
+        if !theMethod.nil?
+          init_path << :theMethod_not_nil
+          init_path << if theMethod.respond_to?(:source_location)
+                         :source_location
+                       else
+                         :not_source_location
+                       end # if
+          if theMethod.respond_to?(:parameters)
+            init_path << :parameters
+          end # if
+        else
+          init_path << :theMethod_nil
+        end # if
+      end # if
+      begin
+        attributes[:protected] = protected_method_defined?(m)
+        attributes[:private] = private_method_defined?(m)
+        init_path << :protected
+      rescue StandardError => exc
+        init_path << :rescue_protected
+      end # if
+      init_path << if m.to_s[/[a-zA-Z0-9_]+/, 0] == m.to_s
+                     :alphanumeric
+                   else
+                     :not_alphanumeric
+                   end # if
+      init_path
+    end # init_path
+
+    def method_query(m, owner)
+      ObjectSpace.each_object(owner) do |object|
+        next unless object.respond_to?(m.to_sym)
+        begin
+          theMethod = object.method(m.to_sym)
+          return theMethod
+        rescue ArgumentError, NameError => exc
+          puts "exc=#{exc}, object=#{object.inspect}"
+        end # begin
+        # if
+      end # each_object
+      nil # no object found, new has side effects
+    end # method_query
+
+    def constantized
+      @@CONSTANTIZED ||= Module.constants.map do |c|
+        begin
+           c = c.constantize
+         rescue
+           puts "constant #{c.inspect} fails constanization"
+           nil
+         end # begin
+      end # map
+    end # constantized
+  end # ClassMethods
+  extend ClassMethods
   include Virtus.value_object
   values do
- 	attribute :name, Symbol
-	attribute :owner, Object
-	attribute :scope, Class, :default => nil
-	attribute :new_from_method, Method, :default => nil
-end # values
-module Constructors # such as alternative new methods
-include DefinitionalConstants
-def new_from_method(method_object)
-	MethodModel.new(name: method_object.name, owner: method_object.owner, scope: method_object.owner.class, new_from_method: method_object)
-end # new_from_method
-end # Constructors
-extend Constructors
-def inspect
-	@name.inspect + ' sends to ' + @owner.inspect + ', scope= ' + @scope.inspect + ' ' + @new_from_method.inspect + "\n"
-end # inspect
-#include NoDB
-def theMethod
-	if @scope.to_sym==:class then
-		@owner.method(@name.to_sym)
-	else # look it up! Why? Beause can't create fully? Existence check?
-		MethodModel.method_query(@name.to_sym, @owner)
-	end #if
-end # theMethod
-def source_location
-		if theMethod.respond_to?(:source_location)
-			theMethod.source_location
-		else
-			nil
-		end #if
-end # source_location
-def parameters
-	if theMethod.respond_to?(:parameters)
-		theMethod.parameters
-	else
-	 	nil
-	end #if
+    attribute :name, Symbol
+    attribute :owner, Object
+    attribute :scope, Class, default: nil
+    attribute :new_from_method, Method, default: nil
+  end # values
+  module Constructors # such as alternative new methods
+    include DefinitionalConstants
+    def new_from_method(method_object)
+      MethodModel.new(name: method_object.name, owner: method_object.owner, scope: method_object.owner.class, new_from_method: method_object)
+    end # new_from_method
+  end # Constructors
+  extend Constructors
+  def inspect
+    @name.inspect + ' sends to ' + @owner.inspect + ', scope= ' + @scope.inspect + ' ' + @new_from_method.inspect + "\n"
+  end # inspect
 
-end # parameters
-def find_example?(unit_class)
-	examples = Example.find_by_class(unit_class, unit_class)
-	if examples.empty? then
-		nil
-	else
-		examples.first
-	end # if
-end # find_example?
-def make_executable_object(file_argument)
-	if @unit_class.included_modules.include?(Virtus::InstanceMethods) then
-		@unit_class.new(executable: TestExecutable.new(executable_file: file_argument))
-	else
-		@unit_class.new(TestExecutable.new_from_path(file_argument))
-	end # if
-end # make_executable_object
-def executable_object(file_argument = nil)
-	example = find_example?
-	if file_argument.nil? then
-		if example.nil? then # default
-			if number_of_arguments == 0 then
-				make_executable_object($0) # script file
-			else
-				make_executable_object(@argv[1])
-			end # if
-		else
-			example.value
-		end # if
-	else
-		make_executable_object(file_argument)
-	end # if
-	
-end # executable_object
-def executable_method?(method_name, argument = nil)
-	executable_object = executable_object(argument)
-	ret = if executable_object.respond_to?(method_name) then
-		method = executable_object.method(method_name)
-	else
-		nil
-	end # if
-end # executable_method?
-def method_exception_string(method_name)
-		message = "#{method_name.to_s} is not an instance method of #{executable_object.class.inspect}"
-		message += "\n candidate_commands = "
-		message += candidate_commands_strings.join("\n")
-#		message += "\n\n executable_object.class.instance_methods = " + executable_object.class.instance_methods(false).inspect
-end # method_exception_string
-def arity(method_name)
-	executable_method = executable_method?(method_name)
-	ret = if executable_method.nil? then
-		message = "#{method_name} is not an instance method of #{executable_object.class.inspect}"
-		message = candidate_commands_strings.join("\n")
-#		message += "\n candidate_commands = " + candidate_commands.inspect
-#		message += "\n\n executable_object.class.instance_methods = " + executable_object.class.instance_methods(false).inspect
-		fail Exception.new(message)
-	else
-		executable_method.arity
-	end # if
-end # arity
-def default_arguments?(method_name)
-	if arity(method_name) < 0 then
-		true
-	else
-		false
-	end # if
+  # include NoDB
+  def theMethod
+    if @scope.to_sym == :class
+      @owner.method(@name.to_sym)
+    else # look it up! Why? Beause can't create fully? Existence check?
+      MethodModel.method_query(@name.to_sym, @owner)
+    end # if
+  end # theMethod
 
+  def source_location
+    if theMethod.respond_to?(:source_location)
+      theMethod.source_location
+    end # if
+  end # source_location
 
-end # default_arguments
-def required_arguments(method_name)
+  def parameters
+    if theMethod.respond_to?(:parameters)
+      theMethod.parameters
+    end # if
+  end # parameters
 
-	method_arity = arity(method_name)
-	if default_arguments?(method_name) then
-		-(method_arity+1)
-	else
-		method_arity
-	end # if
-end # required_arguments
-module Examples
-	class EmptyClass
-	end
-end # Examples
+  def find_example?(unit_class)
+    examples = Example.find_by_class(unit_class, unit_class)
+    if examples.empty?
+      nil
+    else
+      examples.first
+    end # if
+  end # find_example?
+
+  def make_executable_object(file_argument)
+    if @unit_class.included_modules.include?(Virtus::InstanceMethods)
+      @unit_class.new(executable: TestExecutable.new(executable_file: file_argument))
+    else
+      @unit_class.new(TestExecutable.new_from_path(file_argument))
+    end # if
+  end # make_executable_object
+
+  def executable_object(file_argument = nil)
+    example = find_example?
+    if file_argument.nil?
+      if example.nil? # default
+        if number_of_arguments == 0
+          make_executable_object($PROGRAM_NAME) # script file
+        else
+          make_executable_object(@argv[1])
+        end # if
+      else
+        example.value
+      end # if
+    else
+      make_executable_object(file_argument)
+    end # if
+  end # executable_object
+
+  def executable_method?(method_name, argument = nil)
+    executable_object = executable_object(argument)
+    ret = if executable_object.respond_to?(method_name)
+            method = executable_object.method(method_name)
+          end # if
+  end # executable_method?
+
+  def method_exception_string(method_name)
+    message = "#{method_name} is not an instance method of #{executable_object.class.inspect}"
+    message += "\n candidate_commands = "
+    message += candidate_commands_strings.join("\n")
+    #		message += "\n\n executable_object.class.instance_methods = " + executable_object.class.instance_methods(false).inspect
+  end # method_exception_string
+
+  def arity(method_name)
+    executable_method = executable_method?(method_name)
+    ret = if executable_method.nil?
+            message = "#{method_name} is not an instance method of #{executable_object.class.inspect}"
+            message = candidate_commands_strings.join("\n")
+            #		message += "\n candidate_commands = " + candidate_commands.inspect
+            #		message += "\n\n executable_object.class.instance_methods = " + executable_object.class.instance_methods(false).inspect
+            raise Exception.new(message)
+          else
+            executable_method.arity
+    end # if
+  end # arity
+
+  def default_arguments?(method_name)
+    if arity(method_name) < 0
+      true
+    else
+      false
+    end # if
+    end # default_arguments
+
+  def required_arguments(method_name)
+    method_arity = arity(method_name)
+    if default_arguments?(method_name)
+      -(method_arity + 1)
+    else
+      method_arity
+    end # if
+  end # required_arguments
+  module Examples
+    class EmptyClass
+    end
+  end # Examples
 end # MethodModel
 
 class Example
-def require1(required_arg, default_arg = nil)
-end # require1
-def require2(required_arg, required_arg2)
-end # require2
-def require3(required_arg, required_arg2, required_arg3)
-end # require3
-def all_default(default_arg = nil)
-end # all_default
-module Examples # usually constant objects of the type (easy to understand (perhaps impractical) examples for testing)
-#include DefinitionalConstants
-Method_arity = MethodModel.new(name: :arity, owner: Method, scope: Class)
-Method_require1 = MethodModel.new(name: :require1, owner: Example, scope: Class)
-Method_require2 = MethodModel.new(name: :require2, owner: Example, scope: Class)
-Method_require3 = MethodModel.new(name: :require3, owner: Example, scope: Class)
-Method_all_default = MethodModel.new(name: :all_default, owner: Example, scope: Class)
-end # Examples
+  def require1(required_arg, default_arg = nil)
+  end # require1
 
+  def require2(required_arg, required_arg2)
+  end # require2
+
+  def require3(required_arg, required_arg2, required_arg3)
+  end # require3
+
+  def all_default(default_arg = nil)
+  end # all_default
+  module Examples # usually constant objects of the type (easy to understand (perhaps impractical) examples for testing)
+    # include DefinitionalConstants
+    Method_arity = MethodModel.new(name: :arity, owner: Method, scope: Class)
+    Method_require1 = MethodModel.new(name: :require1, owner: Example, scope: Class)
+    Method_require2 = MethodModel.new(name: :require2, owner: Example, scope: Class)
+    Method_require3 = MethodModel.new(name: :require3, owner: Example, scope: Class)
+    Method_all_default = MethodModel.new(name: :all_default, owner: Example, scope: Class)
+  end # Examples
 end # MethodModel
--- test/unit/method_model_test.rb
+++ test/unit/method_model_test.rb
@@ -7,130 +7,138 @@
 ###########################################################################
 require_relative 'test_environment'
 require_relative '../../app/models/method_model.rb'
-#require_relative '../../app/models/object_memory.rb'
+# require_relative '../../app/models/object_memory.rb'
 class MethodTest < TestCase
-def test_arity
-	assert_equal(-1, Example.method(:all_default).arity)
-	assert_equal(-2, Example.method(:initialize).arity)
-#	assert_equal(-2, No_args.unit_class.method(:initialize).arity)
-#	assert_equal(-2, Example.unit_class.method(:initialize).arity)
-	refute_nil(Example.executable_method?(:argument_types))
-	assert_equal(0, Example.arity(:argument_types), Example.inspect)
-	assert_equal(-1, Example.arity(:executable_object), Example.inspect)
-	assert_equal(1, Example.arity(:executable_method), Example.inspect)
-	assert_equal(1, Example.arity(:arity), Example.inspect)
-	assert_equal(-1, Test_unit_commandline.arity(:error_score?), Test_unit_commandline.to_s)
-end # arity
-def test_default_arguments?
-	executable_object = Test_unit_commandline.executable_object
-	message = 'Example = ' + Example.inspect
-	assert_equal(false, Example.default_arguments?(:argument_types), message)
-	assert_equal(true, Example.default_arguments?(:executable_object), message)
-	assert_equal(false, Example.default_arguments?(:executable_method), message)
-	assert_equal(false, Example.default_arguments?(:arity), message)
-end # default_arguments
-def test_required_arguments
-	executable_object = Test_unit_commandline.executable_object
-	assert_equal(:error_score?, Test_unit_commandline.sub_command)
-	assert_respond_to(executable_object, Test_unit_commandline.sub_command)
-	method = executable_object.method(Test_unit_commandline.sub_command)
-	assert_equal(-1, method.arity)
-	assert_equal(0, method.required_arguments, Test_unit_commandline.to_s)
-end # required_arguments
+  def test_arity
+    assert_equal(-1, Example.method(:all_default).arity)
+    assert_equal(-2, Example.method(:initialize).arity)
+    #	assert_equal(-2, No_args.unit_class.method(:initialize).arity)
+    #	assert_equal(-2, Example.unit_class.method(:initialize).arity)
+    refute_nil(Example.executable_method?(:argument_types))
+    assert_equal(0, Example.arity(:argument_types), Example.inspect)
+    assert_equal(-1, Example.arity(:executable_object), Example.inspect)
+    assert_equal(1, Example.arity(:executable_method), Example.inspect)
+    assert_equal(1, Example.arity(:arity), Example.inspect)
+    assert_equal(-1, Test_unit_commandline.arity(:error_score?), Test_unit_commandline.to_s)
+  end # arity
+
+  def test_default_arguments?
+    executable_object = Test_unit_commandline.executable_object
+    message = 'Example = ' + Example.inspect
+    assert_equal(false, Example.default_arguments?(:argument_types), message)
+    assert_equal(true, Example.default_arguments?(:executable_object), message)
+    assert_equal(false, Example.default_arguments?(:executable_method), message)
+    assert_equal(false, Example.default_arguments?(:arity), message)
+  end # default_arguments
+
+  def test_required_arguments
+    executable_object = Test_unit_commandline.executable_object
+    assert_equal(:error_score?, Test_unit_commandline.sub_command)
+    assert_respond_to(executable_object, Test_unit_commandline.sub_command)
+    method = executable_object.method(Test_unit_commandline.sub_command)
+    assert_equal(-1, method.arity)
+    assert_equal(0, method.required_arguments, Test_unit_commandline.to_s)
+  end # required_arguments
 end # MethodTest
 class MethodModelTest < TestCase
-def assert_method_model_initialized(m,owner,scope)
-	assert_instance_of(Class, owner)
-	assert_respond_to(owner,:new)
-	theMethod=MethodModel.method_query(m, owner)
-	mr=MethodModel.new(theMethod)
-	assert_instance_of(MethodModel,mr)
-	refute_nil(mr)
-	assert_equal(MethodModel, mr.class)
-	assert_instance_of(MethodModel,mr)
+  def assert_method_model_initialized(m, owner, _scope)
+    assert_instance_of(Class, owner)
+    assert_respond_to(owner, :new)
+    theMethod = MethodModel.method_query(m, owner)
+    mr = MethodModel.new(theMethod)
+    assert_instance_of(MethodModel, mr)
+    refute_nil(mr)
+    assert_equal(MethodModel, mr.class)
+    assert_instance_of(MethodModel, mr)
 
-	assert_equal(mr[:name], m.to_s)
-	assert_includes(mr[:scope], [Class, Module])
-	assert_equal(mr[:instance_variable_defined], false)
-	assert_nil(mr[:private])
-	assert_equal(mr[:singleton], false)
-	refute_nil(mr[:owner],"owner is nil for mr=#{mr.inspect}")
-end #
-def test_method_query
-	owner=ActiveRecord::ConnectionAdapters::ColumnDefinition
-	m=:to_sql
-	objects=0
-	ObjectSpace.each_object(owner) do |object| 
-		objects+=1
-		begin
-			theMethod=object.method(m.to_sym)
-		rescue StandardError => exc
-			puts "exc=#{exc}, object=#{object.inspect}"
-		end #begin
-		refute_nil(theMethod)
-		assert_instance_of(Method, theMethod)
-	end #each_object
-	assert_operator(objects, :>, 0)
-	method=MethodModel.method_query(m, owner)
-#	assert_equal(, )
-	refute_nil(method)
-	assert_instance_of(Method, method)
-end #method_query
-def test_initialize
-	owner = MethodModel
-	scope=:instance
-	m=:name
-	explain_assert_respond_to(owner.new,m)
-#	assert_equal(mr[:protected], false)
-	assert_respond_to(owner.new,m)
-	assert_instance_of(Method, owner.new.method(m.to_sym))
-	assert_instance_of(Method, owner.new.method(m.to_sym))
-	assert_method_model_initialized(m, owner, scope)
-	mr=MethodModel.new(m,owner,scope)
-	assert_equal([:init, :theMethod_not_nil, :not_source_location, :rescue_protected, :alphanumeric], mr.init_path)
+    assert_equal(mr[:name], m.to_s)
+    assert_includes(mr[:scope], [Class, Module])
+    assert_equal(mr[:instance_variable_defined], false)
+    assert_nil(mr[:private])
+    assert_equal(mr[:singleton], false)
+    refute_nil(mr[:owner], "owner is nil for mr=#{mr.inspect}")
+  end #
 
-	owner=MethodModel
-	scope=:class
-	m=:inspect
-	assert_method_model_initialized(m,owner,scope)
-	assert_equal_sets(['init_path'],owner.instance_methods(false),"owner=#{owner.inspect}")
-#?	assert_equal_sets(["inspect", "instantiate_observers", "joins", "instance_method_already_implemented?"],owner.matching_class_methods(/ins/,false))
-#new	assert_instance_of(Method,owner.new.method(m.to_sym))
-#new	assert_instance_of(Method,owner.new.method(m.to_sym))
-#?	assert_nil(MethodModel.new(m,owner,scope)[:exception])
+  def test_method_query
+    owner = ActiveRecord::ConnectionAdapters::ColumnDefinition
+    m = :to_sql
+    objects = 0
+    ObjectSpace.each_object(owner) do |object|
+      objects += 1
+      begin
+        theMethod = object.method(m.to_sym)
+      rescue StandardError => exc
+        puts "exc=#{exc}, object=#{object.inspect}"
+      end # begin
+      refute_nil(theMethod)
+      assert_instance_of(Method, theMethod)
+    end # each_object
+    assert_operator(objects, :>, 0)
+    method = MethodModel.method_query(m, owner)
+    #	assert_equal(, )
+    refute_nil(method)
+    assert_instance_of(Method, method)
+  end # method_query
 
-#?	assert_nil(mr[:exception])
-end #new
-def test_constantized
-	assert_equal(['Symbol'],Module.constants.map { |c| c.objectKind}.uniq)
-	assert_includes('String',MethodModel.constantized.map { |c| c.objectKind}.uniq)
-	assert_operator(1000,:>,Module.constants.size)
-	assert_operator(MethodModel.constantized.size,:<,MethodModel.classes_and_modules.size)
-	assert_operator(100,:<,MethodModel.constantized.size)
-#	puts "Module.constants=#{Module.constants.inspect}"
-	method_list=Module.constants.map do |c|
-		if c.objectKind==:class || c.objectKind==:module then
-			new(c)
-		end #if
-	end #map
-	assert_operator(method_list.size,:<,1000)
-	assert_operator(100,:<,method_list.size)
-	assert_includes( "Class",MethodModel.constantized.map { |c| c.objectKind}.uniq)
-	puts "pretty print"
-	#~ pp MethodModel.all
-	#~ refute_nil(new('object_id',Object,:methods))
-end #constantized
-def test_matching_methods
-	testClass=Unit
-	assert_instance_of(Array,testClass.matching_class_methods(//))
-	assert_instance_of(Array,testClass.matching_instance_methods(//))
-end #test
-def test_matching_methods_in_context
-	testClass = Unit
-#error message too long	assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
-#error message too long		assert_equal([testClass.canonicalName,testClass.matching_methods(//)],testClass.matching_methods_in_context(//)[0])
-#error message too long			assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
-end #def
-def test_Examples
-end # Examples
+  def test_initialize
+    owner = MethodModel
+    scope = :instance
+    m = :name
+    explain_assert_respond_to(owner.new, m)
+    #	assert_equal(mr[:protected], false)
+    assert_respond_to(owner.new, m)
+    assert_instance_of(Method, owner.new.method(m.to_sym))
+    assert_instance_of(Method, owner.new.method(m.to_sym))
+    assert_method_model_initialized(m, owner, scope)
+    mr = MethodModel.new(m, owner, scope)
+    assert_equal([:init, :theMethod_not_nil, :not_source_location, :rescue_protected, :alphanumeric], mr.init_path)
+
+    owner = MethodModel
+    scope = :class
+    m = :inspect
+    assert_method_model_initialized(m, owner, scope)
+    assert_equal_sets(['init_path'], owner.instance_methods(false), "owner=#{owner.inspect}")
+    # ?	assert_equal_sets(["inspect", "instantiate_observers", "joins", "instance_method_already_implemented?"],owner.matching_class_methods(/ins/,false))
+    # new	assert_instance_of(Method,owner.new.method(m.to_sym))
+    # new	assert_instance_of(Method,owner.new.method(m.to_sym))
+    # ?	assert_nil(MethodModel.new(m,owner,scope)[:exception])
+
+    # ?	assert_nil(mr[:exception])
+  end # new
+
+  def test_constantized
+    assert_equal(['Symbol'], Module.constants.map(&:objectKind).uniq)
+    assert_includes('String', MethodModel.constantized.map(&:objectKind).uniq)
+    assert_operator(1000, :>, Module.constants.size)
+    assert_operator(MethodModel.constantized.size, :<, MethodModel.classes_and_modules.size)
+    assert_operator(100, :<, MethodModel.constantized.size)
+    #	puts "Module.constants=#{Module.constants.inspect}"
+    method_list = Module.constants.map do |c|
+      if c.objectKind == :class || c.objectKind == :module
+        new(c)
+      end # if
+    end # map
+    assert_operator(method_list.size, :<, 1000)
+    assert_operator(100, :<, method_list.size)
+    assert_includes('Class', MethodModel.constantized.map(&:objectKind).uniq)
+    puts 'pretty print'
+    # ~ pp MethodModel.all
+    # ~ refute_nil(new('object_id',Object,:methods))
+  end # constantized
+
+  def test_matching_methods
+    testClass = Unit
+    assert_instance_of(Array, testClass.matching_class_methods(//))
+    assert_instance_of(Array, testClass.matching_instance_methods(//))
+  end # test
+
+  def test_matching_methods_in_context
+    testClass = Unit
+    # error message too long	assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
+    # error message too long		assert_equal([testClass.canonicalName,testClass.matching_methods(//)],testClass.matching_methods_in_context(//)[0])
+    # error message too long			assert_instance_of(Array,testClass.matching_methods_in_context(//,2))
+  end # def
+
+  def test_Examples
+  end # Examples
 end # MethodModel
