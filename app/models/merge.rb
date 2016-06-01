d2e252c9cc7d6065ec73ef5259185cb22ffa20ea
greg <GregLawson123@gmail.com>
2016-05-31 19:28:43 -0700

    merge
    split off from interactive_bottleneck

---
+++ app/models/merge.rb
@@ -0,0 +1,194 @@
+###########################################################################
+#    Copyright (C) 2013-2016 by Greg Lawson
+#    <GregLawson123@gmail.com>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+require_relative 'unit.rb'
+require_relative 'repository.rb'
+require_relative 'unit_maturity.rb'
+require_relative 'editor.rb'
+class Merge
+  module Constants
+  end # Constants
+  include Constants
+  module ClassMethods
+    include Constants
+  end # ClassMethods
+  extend ClassMethods
+  # Define related (unit) versions
+  # Use as current, lower/upper bound, branch history
+  # parametized by related files, repository, branch_number, executable
+  # record error_score, recent_test, time
+  include Virtus.value_object
+  values do
+    attribute :test_executable, TestExecutable
+    attribute :interactive, Symbol, default: :interactive # non-defaults are primarily for non-interactive testing testing
+    attribute :editor, Editor, default: ->(interactive_bottleneck, _attribute) { Editor.new(interactive_bottleneck.test_executable) }
+    attribute :repository, Repository, default: ->(interactive_bottleneck, _attribute) { interactive_bottleneck.test_executable.repository }
+    attribute :unit_maturity, UnitMaturity, default: ->(interactive_bottleneck, _attribute) { UnitMaturity.new(interactive_bottleneck.test_executable.repository, interactive_bottleneck.test_executable.unit) }
+    #	attribute :branch_index, Fixnum, :default => lambda { |interactive_bottleneck, attribute| Merge.index(interactive_bottleneck.test_executable.repository) }
+  end # values
+  def standardize_position!
+    abort_rebase_and_merge!
+    @repository.git_command('checkout master')
+  end # standardize_position!
+
+  def abort_rebase_and_merge!
+    if File.exist?('.git/rebase-merge/git-rebase-todo')
+      @repository.git_command('rebase --abort')
+    end
+    #	@repository.git_command("stash save").assert_post_conditions
+    if File.exist?('.git/MERGE_HEAD')
+      @repository.git_command('merge --abort')
+    end # if
+  end # abort_rebase_and_merge!
+
+  def state?
+    state = []
+    state << :rebase if File.exist?('.git/rebase-merge/git-rebase-todo')
+    if File.exist?('.git/MERGE_HEAD')
+      state << :merge
+    end # if
+    state << if @repository.something_to_commit?
+               :dirty
+             else
+               :clean
+             end # if
+    state
+  end # state?
+
+  def discard_log_file_merge
+    unmerged_files = @repository.status
+    unmerged_files.each do |conflict|
+      if conflict[:file][-4..-1] == '.log'
+        @repository.git_command('checkout HEAD ' + conflict[:file])
+        puts 'checkout HEAD ' + conflict[:file]
+      end # if
+    end # each
+  end # discard_log_file_merge
+
+  def merge_conflict_recovery(from_branch)
+    # see man git status
+    discard_log_file_merge # each branch's log file status is independant
+    puts '@repository.status = ' + @repository.status.inspect
+    unmerged_files = @repository.status
+    unless unmerged_files.empty?
+      puts 'merge --abort'
+      merge_abort = @repository.git_command('merge --abort')
+      if merge_abort.success?
+        puts 'merge --X ours ' + from_branch.to_s
+        remerge = @repository.git_command('merge --X ours ' + from_branch.to_s)
+      end # if
+      unmerged_files.each do |conflict|
+        puts 'not checkout HEAD ' + conflict[:file]
+        if conflict[:index] == :ignored || conflict[:work_tree] == :ignored
+        # ignore
+        elsif conflict[:description] == 'updated in index'
+        # no merge conflict; test!
+        elsif conflict[:description][0..7] == 'unmerged'
+          test_executable = TestExecutable.new_from_path(conflict[:file])
+          Editor.new(test_executable).edit
+        #				@repository.validate_commit(@repository.current_branch_name?, [conflict[:file]])
+        else
+          raise Exception.new(conflict.inspect)
+          end # if
+      end # each
+      confirm_commit
+    end # if
+  end # merge_conflict_recovery
+
+  # does not return to original branch unlike #safely_visit_branch
+  # does not need a block, since it doesn't switch back
+  # moves all working directory files to new branch
+  def switch_branch(target_branch)
+    push = stash_and_checkout(target_branch)
+  end # switch_branch
+
+  def merge_interactive(source_branch)
+    merge_status = @repository.git_command('merge --no-commit ' + source_branch.to_s)
+  end # merge_interactive
+
+  def stash_and_checkout(target_branch)
+    stash_branch = @repository.current_branch_name?
+    changes_branch = stash_branch #
+    push = @repository.something_to_commit? # remember
+    if push
+      #		status=@grit_repo.status
+      #		puts "status.added=#{status.added.inspect}"
+      #		puts "status.changed=#{status.changed.inspect}"
+      #		puts "status.deleted=#{status.deleted.inspect}"
+      #		puts "@repository.something_to_commit?=#{@repository.something_to_commit?.inspect}"
+      @repository.git_command('stash save --include-untracked')
+      merge_cleanup
+      changes_branch = :stash
+    end # if
+
+    if stash_branch != target_branch
+      confirm_branch_switch(target_branch)
+    end # if
+    push # if switched?
+  end # stash_and_checkout
+
+  def merge(target_branch, source_branch)
+    puts 'merge(' + target_branch.inspect + ', ' + source_branch.inspect + ', ' + @interactive.inspect + ')'
+    safely_visit_branch(target_branch) do |_changes_branch|
+      merge_status = @repository.git_command('merge --no-commit ' + source_branch.to_s)
+      puts 'merge_status= ' + merge_status.inspect
+      if merge_status.output == "Automatic merge went well; stopped before committing as requested\n"
+        puts 'merge OK'
+      else
+        if merge_status.success?
+          puts 'not merge_conflict_recovery' + merge_status.inspect
+        else
+          puts 'merge_conflict_recovery' + merge_status.inspect
+          merge_conflict_recovery(source_branch)
+        end # if
+      end # if
+      confirm_commit
+    end # safely_visit_branch
+  end # merge
+
+  def merge_down(deserving_branch = @repository.current_branch_name?)
+    Branch.merge_range(deserving_branch).each do |i|
+      safely_visit_branch(Branch::Branch_enhancement[i]) do |_changes_branch|
+        puts 'merge(' + Branch::Branch_enhancement[i].to_s + '), ' + Branch::Branch_enhancement[i - 1].to_s + ')' unless $VERBOSE.nil?
+        merge(Branch::Branch_enhancement[i], Branch::Branch_enhancement[i - 1])
+        merge_conflict_recovery(Branch::Branch_enhancement[i - 1])
+        confirm_commit
+      end # safely_visit_branch
+    end # each
+  end # merge_down
+
+  module Assertions
+    module ClassMethods
+      def assert_pre_conditions
+      end # assert_pre_conditions
+
+      def assert_post_conditions
+        #	assert_pathname_exists(TestExecutable, "assert_post_conditions")
+      end # assert_post_conditions
+    end # ClassMethods
+    def assert_pre_conditions
+      #	refute_nil(@test_executable.unit)
+      #	refute_empty(@test_executable.unit.edit_files, "assert_pre_conditions, @test_environmen=#{@test_environmen.inspect}, @test_executable.unit.edit_files=#{@test_executable.unit.edit_files.inspect}")
+      #	assert_kind_of(Grit::Repo, @repository.grit_repo)
+      #	assert_respond_to(@repository.grit_repo, :status)
+      #	assert_respond_to(@repository.grit_repo.status, :changed)
+    end # assert_pre_conditions
+
+    def assert_post_conditions
+    end # assert_post_conditions
+  end # Assertions
+  include Assertions
+  extend Assertions::ClassMethods
+  # TestWorkFlow.assert_pre_conditions
+  include Constants
+  module Examples
+    TestSelf = TestExecutable.new(argument_path: File.expand_path($PROGRAM_NAME))
+    TestMerge = Merge.new(interactive: :interactive, test_executable: TestSelf, editor: Editor::Examples::TestEditor)
+    include Constants
+  end # Examples
+  include Examples
+end # Merge
---
+++ test/unit/merge_test.rb
@@ -0,0 +1,126 @@
+###########################################################################
+#    Copyright (C) 2013-2016 by Greg Lawson
+#    <GregLawson123@gmail.com>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+# require_relative '../unit/test_environment'
+require_relative '../../app/models/test_environment_test_unit.rb'
+require_relative '../../app/models/interactive_bottleneck.rb'
+require_relative '../assertions/repository_assertions.rb'
+require_relative '../assertions/shell_command_assertions.rb'
+require_relative '../../app/models/merge.rb'
+class MergeTest < TestCase
+  # include DefaultTests
+  # include Merge
+  # extend Merge::ClassMethods
+  include Merge::Examples
+  def setup
+    @temp_repo = Repository.create_test_repository
+    refute_equal(Repository::This_code_repository, @temp_repo)
+    @temp_merge = Merge.new(test_executable: TestExecutable.new(argument_path: $PROGRAM_NAME), repository: @temp_repo, interactive: :echo)
+    assert_equal(@temp_repo, @temp_merge.repository)
+    refute_nil(@temp_merge.interactive)
+  end # setup
+
+  def teardown
+    Repository.delete_existing(@temp_repo.path)
+  end # teardown
+
+  def test_initialize
+    refute_empty(TestMerge.test_executable.unit.edit_files, 'TestMerge.test_executable.unit.edit_files= ' + TestMerge.test_executable.unit.inspect)
+    assert_includes(TestMerge.test_executable.unit.edit_files, Pathname.new($PROGRAM_NAME).expand_path, "TestMerge.unit=#{TestMerge.test_executable.unit.inspect}")
+    assert_equal(TestSelf, TestMerge.test_executable, TestMerge.inspect)
+    assert_equal(Repository::This_code_repository, TestMerge.repository, TestMerge.inspect)
+    #	assert_equal(, TestMerge.unit_maturity, TestMerge.inspect)
+    assert_equal(Editor::Examples::TestEditor, TestMerge.editor, TestMerge.inspect)
+
+    refute_nil(Merge.new(interactive: :interactive, test_executable: TestSelf, editor: Editor::Examples::TestEditor).interactive)
+    #	refute_nil(Merge.new(test_executable: TestSelf, editor: Editor::Examples::TestEditor).interactive)
+
+    refute_nil(TestMerge.interactive, TestMerge.inspect)
+    assert_equal(:interactive, TestMerge.interactive, TestMerge.inspect)
+  end # values
+  include Merge::Examples
+  def test_standardize_position!
+    @temp_repo.git_command('rebase --abort').puts
+    @temp_repo.git_command('merge --abort').puts
+    @temp_repo.git_command('stash save') # .assert_post_conditions
+    @temp_repo.git_command('checkout master').puts
+    @temp_merge.standardize_position!
+  end # standardize_position!
+
+  def test_abort_rebase_and_merge!
+  end # abort_rebase_and_merge!
+
+  def test_state?
+    state = TestMerge.state?
+    assert_includes([:clean, :dirty, :merge, :rebase], state[0])
+    assert_equal(1, state.size, state)
+  end # state?
+
+  def test_discard_log_file_merge
+    all_files = Repository::This_code_repository.status
+    all_files.each do |conflict|
+      next unless conflict[:file][-4..-1] == '.log'
+      if conflict[:index] == :ignored || conflict[:work_tree] == :ignored
+        puts conflict[:file] + ' is an ignored log file.'
+      elsif conflict[:index] == :untracked || conflict[:work_tree] == :untracked
+        puts conflict[:file] + ' is an untracked log file.'
+      elsif conflict[:index] == :unmodified && conflict[:work_tree] == :modified
+        puts conflict[:file] + ' is an updated log file.'
+      elsif conflict[:work_tree] == :updated_but_unmerged
+        assert_include(['unmerged, deleted by us'], conflict[:description], conflict.inspect)
+        Repository::This_code_repository.git_command('checkout HEAD ' + conflict[:file])
+        puts 'checkout HEAD ' + conflict[:file]
+      else
+        raise Exception.new(conflict.inspect)
+      end # if
+      # if
+    end # each
+  end # discard_log_file_merge
+
+  def test_merge_conflict_recovery
+  end # merge_conflict_recovery
+
+  def test_merge_interactive
+  end # merge_interactive
+
+  def test_stash_and_checkout
+  end # stash_and_checkout
+
+  def test_merge_cleanup
+    @temp_repo.force_change
+    #    @temp_merge.merge_cleanup
+  end # merge_cleanup
+
+  def test_merge
+    TestMerge.repository.testing_superset_of_passed # .assert_post_conditions
+    TestMerge.repository.edited_superset_of_testing # .assert_post_conditions
+    #	TestMerge.merge(:edited, :testing) # not too long or too dangerous
+  end # merge
+
+  def test_merge_down
+    # (deserving_branch = @repository.current_branch_name?)
+  end # merge_down
+
+  def test_Merge_assert_pre_conditions
+    #    TestMerge.assert_pre_conditions
+  end # assert_pre_conditions
+
+  def test_Merge_assert_post_conditions
+    #    TestMerge.assert_post_conditions
+  end # assert_post_conditions
+
+  def test_local_assert_pre_conditions
+    #    TestMerge.assert_pre_conditions
+  end # assert_pre_conditions
+
+  def test_local_assert_post_conditions
+    #    TestMerge.assert_post_conditions
+  end # assert_post_conditions
+
+  def test_Examples
+  end # Examples
+end # Merge
