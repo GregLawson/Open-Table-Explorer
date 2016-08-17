15cda764dca433a6853423f767ba9f5295624880
greg <GregLawson123@gmail.com>
2016-08-17 16:18:54 -0700

    fixup! ruby_lines_storage

---
+++ app/models/ruby_lines_storage.rb
@@ -0,0 +1,97 @@
+###########################################################################
+#    Copyright (C) 2014-2015 by Greg Lawson
+#    <GregLawson123@gmail.com>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+class Array
+	def ruby_lines_storage(line_length_limit = 50)
+		elements = map do |element|
+			element.ruby_lines_storage
+		end # map
+		ret = ''
+		line_length = 0
+		elements.each do |element|
+			if line_length + element.size + 2 < line_length_limit # if I add it
+				ret += ', '
+			else
+				ret += ",\n"
+				line_length = 0
+			end # if
+			ret += element
+			line_length += element.size + 2 # delimiters
+		end # while
+		'[' + ret[2..-1] + ']' # remove last delimiter
+	end # ruby_lines_storage
+end # Array
+
+class Hash
+	def ruby_lines_storage
+		ret = '{'
+		each_pair do |key, value|
+			ret += key.ruby_lines_storage + ' => ' + value.ruby_lines_storage + ",\n"
+		end # each_pair
+		ret.chomp.chomp + "\n" + "}\n" # remove terminating linefeed and comma
+	end # ruby_lines_storage
+end # Hash
+
+class NilClass
+	def ruby_lines_storage
+		'nil'
+	end # ruby_lines_storage
+end # NilClass
+
+class Fixnum
+	def ruby_lines_storage
+		to_s
+	end # ruby_lines_storage
+end # Fixnum
+
+class String
+	def ruby_lines_storage
+		"'" + to_s + "'" 
+	end # ruby_lines_storage
+end # String
+
+class Symbol
+	def ruby_lines_storage
+		inspect
+#		':' + to_s
+	end # ruby_lines_storage
+end # Symbol
+
+class Date
+	def ruby_lines_storage
+		'Date.new(' + year.to_s + ', ' + month.to_s + ', ' +day.to_s + ', '  +  ')'
+#		strftime('%Y-%m-%d')
+#		':' + to_s
+	end # ruby_lines_storage
+end # Date
+
+class DateTime
+	def ruby_lines_storage
+		'Time.new(' + year.to_s + ', ' + month.to_s + ', ' +day.to_s + ', ' + hour.to_s + ', ' + min.to_s + ', ' + (1000000000 * sec + nsec).to_s + '/1000000000, "' + strftime('%:z') + '").to_datetime'
+#		strftime('%Y-%m-%d')
+#		':' + to_s
+	end # ruby_lines_storage
+end # DateTime
+
+class Time
+	def ruby_lines_storage
+		'Time.new(' + year.to_s + ', ' + month.to_s + ', ' +day.to_s + ', ' + hour.to_s + ', ' + min.to_s + ', Rational(' + (1000000000 * sec + nsec).to_s + ', 1000000000), "' + strftime('%:z') + '")'
+#		strftime('%Y-%m-%d %H:%M:%S.%L')
+#		':' + to_s
+	end # ruby_lines_storage
+end # Time
+
+class Object
+	def ruby_lines_storage
+		ret = '<' + self.class.name.to_s +
+			instance_variables.map do |iv_name|
+				instance_variable_value = instance_variable_get(iv_name).ruby_lines_storage
+				iv_name.to_s + ' => ' + instance_variable_value + ",\n"
+			end.join("\n") # map
+		ret.chomp.chomp + "\n" + ">\n" # remove terminating linefeed and comma
+	end # ruby_lines_storage
+end # Object
---
+++ test/unit/ruby_lines_storage_test.rb
@@ -0,0 +1,169 @@
+###########################################################################
+#    Copyright (C) 2014-2015 by Greg Lawson
+#    <GregLawson123@gmail.com>
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
+#require 'descriptive-statistics'
+require_relative '../../app/models/test_environment_test_unit.rb'
+require_relative '../../app/models/ruby_lines_storage.rb'
+class RubyLinesStorageTest < TestCase
+	module Examples
+		Short_array = [1,2,3]
+		Long_array = Array.new(50, 1)
+		Short_hash = {}
+		Short_nil = nil
+		Short_fixnum = 123
+		Short_string = '123'
+		Long_string = "cat\ndog"
+		Approximate_Time = Time.now
+		Short_Date = Date.today
+		Approximate_DateTime = DateTime.now
+	end # Examples
+	include Examples
+
+	def eval_rescued(example_string)
+		eval(example_string)
+	rescue Exception => exception_raised
+		raise example_string + "\n" + exception_raised.inspect
+	end # eval_example
+	
+	def assert_reversible(object)
+		ruby_lines_storage = object.ruby_lines_storage
+		message = ruby_lines_storage.inspect + ' should eval to ' + object.inspect
+		assert_equal(object, eval_rescued(object.ruby_lines_storage), message)
+	end # assert_reversible
+
+	def assert_lines(object)
+		ruby_lines_storage = object.ruby_lines_storage
+		message = ruby_lines_storage.inspect + ' should have a newline from ' + object.inspect
+		assert_match(/\n/, ruby_lines_storage)
+	end # assert_reversible
+
+	def assert_approximate(numeric, max_error = 0.000000001)
+		ruby_lines_storage = numeric.ruby_lines_storage
+		eval_numeric = eval(ruby_lines_storage)
+		round_off = numeric - eval_numeric
+		message = ruby_lines_storage.inspect + ' should eval to approximately ' + numeric.inspect
+		assert_operator(round_off.abs.to_f, :<=, max_error, message)
+	end # assert_reversible
+
+	def eval_name(name)
+			expression_string = 'Examples::' + name.to_s
+			eval_rescued(expression_string)
+		rescue 
+			raise 'name = ' + name.inspect + ' in expression ' + expression_string + ' should eval.'
+	end # eval_name
+	
+	def test_assert_reversible
+		Examples.constants.each do |name|
+
+			if name.to_s[0,12] == 'Approximate_'
+				assert_approximate(eval_name(name))
+			else
+				assert_reversible(eval_name(name))
+			end # if
+
+		end # each
+	end # assert_reversible
+	
+	def test_assert_lines
+		Examples.constants.each do |name|
+			if name.to_s[0,5] == 'Long_'
+				assert_lines(eval_name(name))
+			end # if
+			assert_reversible(name)
+		end # each
+	end # assert_lines
+	
+	def test_assert_approximate
+		Examples.constants.each do |name|
+			if name.to_s[0,12] == 'Approximate_'
+				assert_approximate(eval_name(name))
+			end # if
+		end # each
+	end # assert_approximate
+	
+	def test_Array_ruby_lines_storage
+		assert_equal('[1, 2, 3]', [1,2,3].ruby_lines_storage)
+		assert_match(/\n/, Long_array.ruby_lines_storage)
+		assert_reversible(Short_array)
+		assert_reversible(Long_array)
+	end # ruby_lines_storage
+
+	def test_Hash_ruby_lines_storage
+#		assert_equal('{}', {}.ruby_lines_storage)
+		assert_reversible(Short_hash)
+	end # ruby_lines_storage
+	
+	def test_NilClass_ruby_lines_storage
+		assert_equal('nil', nil.ruby_lines_storage)
+		assert_reversible(nil)
+	end # ruby_lines_storage
+
+	def test_Fixnum_ruby_lines_storage
+		assert_equal('123', 123.ruby_lines_storage)
+		assert_reversible(123)
+	end # ruby_lines_storage
+
+	def test_String_ruby_lines_storage
+		assert_equal("'cat'", 'cat'.ruby_lines_storage)
+		assert_equal("'cat\ndog'", "cat\ndog".ruby_lines_storage)
+		refute_equal("'cat\ndog'".inspect, "cat\ndog".ruby_lines_storage)
+		assert_reversible(123)
+	end # ruby_lines_storage
+
+	def test_Symbol_ruby_lines_storage
+		assert_equal(':cat', :cat.ruby_lines_storage)
+		assert_reversible(:cat)
+		assert_reversible(:"cat\ndog")
+		assert_equal(':"cat\\ndog"', :"cat\ndog".ruby_lines_storage)
+	end # ruby_lines_storage
+	
+	def test_Date_ruby_lines_storage
+		time = Date.today
+		assert_reversible(time)
+	end # ruby_lines_storage
+	
+	def test_DateTime_ruby_lines_storage
+		samples = (1..120000).map do |i|
+			time = DateTime.now
+		end # map
+		errors = samples.map do |time|
+			((time - eval(time.ruby_lines_storage)) * 1000000000).to_i
+		end # map
+		differences = errors.each_cons(2).map do |pair|
+			pair[0] - pair[1]
+		end.sort.uniq # each_cons
+#		puts 'errors = ' + errors.inspect
+		puts differences.inspect
+#		message = 'mean = ' + samples.mean.to_f.to_s + 'mode = ' + samples.mode.to_f.to_s
+#		puts message
+		message = 'min = ' + errors.min.to_f.to_s + 'max = ' + errors.max.to_f.to_s
+		puts message
+		assert_equal(0, errors.min)
+		assert_equal(11574, errors.max)
+		assert_equal(11574, 2**13, 11574.pack("H*")) # not a simple truncation!
+		assert_approximate(DateTime.now, message)
+	end # ruby_lines_storage
+	
+	def test_Time_ruby_lines_storage
+		time = Time.now
+		seconds = Rational(1000000000 * time.sec + time.nsec, 1000000000)
+		assert_instance_of(Rational, seconds)
+		refute_equal(0, seconds)
+		eval_time = eval(time.ruby_lines_storage)
+		round_off = time - eval_time
+		assert_equal(time, eval_time, time.strftime('%Y-%m-%d %H:%M:%S.%9N %z') + time.ruby_lines_storage + round_off.to_f.to_s)
+		assert_reversible(time)
+	end # ruby_lines_storage
+	
+	def test_Object_ruby_lines_storage
+		assert_equal('[1, 2, 3]', [1,2,3].ruby_lines_storage)
+		assert_equal(['1', '2', '3'], [1,2,3].map {|e| e.ruby_lines_storage})
+		assert_equal([1, 1, 1], [1,2,3].map {|e| e.ruby_lines_storage.size})
+		assert_equal(['1', '2', '3'], [1,2,3].map(&:ruby_lines_storage))
+		assert_equal([1, 1, 1], [1,2,3].map(&:ruby_lines_storage).map(&:size))
+	end # ruby_lines_storage
+end # RubyLinesStorage
