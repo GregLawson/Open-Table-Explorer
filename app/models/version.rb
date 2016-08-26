ab45072b4eed5b8c879062c7733249523eab7c7c
greg <GregLawson123@gmail.com>
2016-08-25 18:44:52 -0700

    fixup! version

--- app/models/version.rb
+++ app/models/version.rb
@@ -11,80 +11,86 @@ require 'fileutils'
 require_relative '../../app/models/parse.rb'
 # see http://semver.org/
 class Version # semantic version and base class for incompatible versioning convertions
-# see http://semver.org/
-module Constants # constant parameters of the type (suggest all CAPS)
-Version_digits = /[1-9]?[0-9]{1,3}/ # 1 to 9999 or 0 to 099 ? counter-examples in the wild?
+  # see http://semver.org/
+  module Constants # constant parameters of the type (suggest all CAPS)
+    Version_digits = /[1-9]?[0-9]{1,3}/ # 1 to 9999 or 0 to 099 ? counter-examples in the wild?
 
-Major_minor_regexp = Version_digits.capture(:major) * '.' * Version_digits.capture(:minor)
-Major_minor_patch_regexp = Major_minor_regexp * '.' * Version_digits.capture(:patch)
-Semantic_version_regexp = Major_minor_patch_regexp * (/[-+.]/ * /[-.a-zA-Z0-9]*/.capture(:pre_release)).group * Regexp::Optional
-end # Constants
-include Constants
-include Virtus.value_object
+    Major_minor_regexp = Version_digits.capture(:major) * '.' * Version_digits.capture(:minor)
+    Major_minor_patch_regexp = Major_minor_regexp * '.' * Version_digits.capture(:patch)
+    Semantic_version_regexp = Major_minor_patch_regexp * (/[-+.]/ * /[-.a-zA-Z0-9]*/.capture(:pre_release)).group * Regexp::Optional
+  end # Constants
+  include Constants
+  include Virtus.value_object
   values do
-  attribute :major, String, :default => '0' # system version
-  attribute :minor, String, :default => '0'
-  attribute :patch, String, :default => '0'
-  attribute :pre_release, String, :default => '0'
-  attribute :regexp, Regexp, :default => Semantic_version_regexp
-  attribute :source_string, String
-end # values
-module ClassMethods
-include Constants
-def new_from_string(string)
-	parse = string.parse(Semantic_version_regexp)
-	Version.new(major: parse[:major], minor: parse[:minor], patch: parse[:patch], pre_release: parse[:pre_release])
-end # new_from_string
-end # ClassMethods
-extend ClassMethods
-#require_relative '../../app/models/assertions.rb'
-module Assertions
-module ClassMethods
-def nested_scope_modules?(context = self)
-	nested_constants = context.class.constants
-	nested_constants.select do |constant|
-		constant.class == Module
-	end # select
-end # nested_scopes
-def assert_nested_scope_submodule(module_symbol, context = self, message='')
-	message+="\nIn assert_nested_scope_submodule for class #{context.name}, "
-	message += "make sure module Constants is nested in #{context.class.name.downcase} #{context.name}"
-	message += " but not in #{context.nested_scope_modules?.inspect}"
-	assert_includes(constants, :Contants, message)
-end # assert_included_submodule
-def assert_included_submodule(module_symbol, context = self, message='')
-	message+="\nIn assert_included_submodule for class #{self.name}, "
-	message += "make sure module Constants is nested in #{self.class.name.downcase} #{self.name}"
-	message += " but not in #{self.nested_scope_modules?.inspect}"
-	assert_includes(included_modules, :Contants, message)
-end # assert_included_submodule
-def asset_nested_and_included(module_symbol, context = self, message='')
-	assert_nested_scope_submodule(module_symbol)
-	assert_included_submodule(module_symbol)
-end # asset_nested_and_included
-def assert_pre_conditions(message='')
-	asset_nested_and_included(:ClassMethods, self)
-	asset_nested_and_included(:Constants, self)
-	asset_nested_and_included(:Assertions, self)
-end #assert_pre_conditions
-def assert_post_conditions(message='')
-	message+="In assert_post_conditions, self=#{inspect}"
-end #assert_post_conditions
-end #ClassMethods
-def assert_pre_conditions(message='')
-end #assert_pre_conditions
-def assert_post_conditions(message='')
-end #assert_post_conditions
-end # Assertions
-include Assertions
-extend Assertions::ClassMethods
-#self.assert_pre_conditions
-module Examples # usually constant objects of the type (easy to understand (perhaps impractical) examples for testing)
-include Version::Constants
-Consecutive_string = '1.2.3'
+    attribute :major, String, default: '0' # system version
+    attribute :minor, String, default: '0'
+    attribute :patch, String, default: '0'
+    attribute :pre_release, String, default: '0'
+    attribute :regexp, Regexp, default: Semantic_version_regexp
+    attribute :source_string, String
+  end # values
+  module ClassMethods
+    include Constants
+    def new_from_string(string)
+      parse = string.parse(Semantic_version_regexp)
+      Version.new(major: parse[:major], minor: parse[:minor], patch: parse[:patch], pre_release: parse[:pre_release])
+    end # new_from_string
+  end # ClassMethods
+  extend ClassMethods
+  # require_relative '../../app/models/assertions.rb'
+  module Assertions
+    module ClassMethods
+      def nested_scope_modules?(context = self)
+        nested_constants = context.class.constants
+        nested_constants.select do |constant|
+          constant.class == Module
+        end # select
+      end # nested_scopes
 
-Sorted_version_names = ['1.9.0', '1.10.0', '1.11.0.']
-First_example_version_name = Sorted_version_names[0]
-First_example_version = Version.new_from_string(First_example_version_name)
-end # Examples
+      def assert_nested_scope_submodule(_module_symbol, context = self, message = '')
+        message += "\nIn assert_nested_scope_submodule for class #{context.name}, "
+        message += "make sure module Constants is nested in #{context.class.name.downcase} #{context.name}"
+        message += " but not in #{context.nested_scope_modules?.inspect}"
+        assert_includes(constants, :Contants, message)
+      end # assert_included_submodule
+
+      def assert_included_submodule(_module_symbol, _context = self, message = '')
+        message += "\nIn assert_included_submodule for class #{name}, "
+        message += "make sure module Constants is nested in #{self.class.name.downcase} #{name}"
+        message += " but not in #{nested_scope_modules?.inspect}"
+        assert_includes(included_modules, :Contants, message)
+      end # assert_included_submodule
+
+      def asset_nested_and_included(module_symbol, _context = self, _message = '')
+        assert_nested_scope_submodule(module_symbol)
+        assert_included_submodule(module_symbol)
+      end # asset_nested_and_included
+
+      def assert_pre_conditions(_message = '')
+        asset_nested_and_included(:ClassMethods, self)
+        asset_nested_and_included(:Constants, self)
+        asset_nested_and_included(:Assertions, self)
+      end # assert_pre_conditions
+
+      def assert_post_conditions(message = '')
+        message += "In assert_post_conditions, self=#{inspect}"
+      end # assert_post_conditions
+    end # ClassMethods
+    def assert_pre_conditions(message = '')
+    end # assert_pre_conditions
+
+    def assert_post_conditions(message = '')
+    end # assert_post_conditions
+  end # Assertions
+  include Assertions
+  extend Assertions::ClassMethods
+  # self.assert_pre_conditions
+  module Examples # usually constant objects of the type (easy to understand (perhaps impractical) examples for testing)
+    include Version::Constants
+    Consecutive_string = '1.2.3'.freeze
+
+    Sorted_version_names = ['1.9.0', '1.10.0', '1.11.0.'].freeze
+    First_example_version_name = Sorted_version_names[0]
+    First_example_version = Version.new_from_string(First_example_version_name)
+  end # Examples
 end # Version
--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -11,65 +11,68 @@ require_relative '../../app/models/shell_command.rb'
 require_relative '../../app/models/version.rb'
 # executed in alphabetical order. Longer names sort later.
 class VersionTest < TestCase
-include RailsishRubyUnit::Executable.model_class?::Examples
-def test_Constants
+  include RailsishRubyUnit::Executable.model_class?::Examples
+  def test_Constants
+    assert_match(Version_digits, Consecutive_string)
+    assert_match(Version_digits.capture(:major) * '.', Consecutive_string)
+    assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.', Consecutive_string)
+    assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.' * Version_digits.capture(:patch), Consecutive_string)
+    assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.' * Version_digits.capture(:patch) * (/[-+.]/ * /[-.a-zA-Z0-9]*/.capture(:pre_release)).group * Regexp::Optional, Consecutive_string)
+    assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.' * Version_digits.capture(:patch) * (/[-+.]/ * /[-.a-zA-Z0-9]*/.capture(:pre_release)).group * Regexp::Optional, Consecutive_string)
+    assert_match(Semantic_version_regexp, Consecutive_string)
+  end # Constants
 
-	assert_match(Version_digits, Consecutive_string)
-	assert_match(Version_digits.capture(:major) * '.' , Consecutive_string)
-	assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.', Consecutive_string)
-	assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.' * Version_digits.capture(:patch), Consecutive_string)
-	assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.' * Version_digits.capture(:patch) * (/[-+.]/ * /[-.a-zA-Z0-9]*/.capture(:pre_release)).group * Regexp::Optional, Consecutive_string)
-	assert_match(Version_digits.capture(:major) * '.' * Version_digits.capture(:minor) * '.' * Version_digits.capture(:patch) * (/[-+.]/ * /[-.a-zA-Z0-9]*/.capture(:pre_release)).group * Regexp::Optional, Consecutive_string)
-	assert_match(Semantic_version_regexp, Consecutive_string)
-end # Constants
-def test_value_object
-	new_version = Version.new
-	assert_equal('0', new_version.major, new_version.inspect)
-	assert_equal('0', new_version.minor, new_version.inspect)
-	assert_equal('0', new_version.patch, new_version.inspect)
-	assert_equal('0', new_version.pre_release, new_version.inspect)
-	new_args = Version.new(major: '1', minor: '2', patch: '3', pre_release: '4')
-	assert_equal('1', new_args.major, new_args.inspect)
-	assert_equal('2', new_args.minor, new_args.inspect)
-	assert_equal('3', new_args.patch, new_args.inspect)
-	assert_equal('4', new_args.pre_release, new_args.inspect)
-end # values
-def test_new_from_string
-	string = '1.2.3'
-	parse = Consecutive_string.parse(Semantic_version_regexp)
-	assert_instance_of(Hash, parse)
-	assert_equal('1', parse[:major], parse.inspect)
-	assert_equal('2', parse[:minor], parse.inspect)
-	assert_equal('3', parse[:patch], parse.inspect)
-	assert_equal(nil, parse[:pre_release], parse.inspect)
-	new_args = Version.new(major: '1', minor: '2', patch: '3', pre_release: '4')
-	assert_equal('1', new_args.major, new_args.inspect)
-	assert_equal('2', new_args.minor, new_args.inspect)
-	assert_equal('3', new_args.patch, new_args.inspect)
-	assert_equal('4', new_args.pre_release, new_args.inspect)
-	version = Version.new(major: parse[:major], minor: parse[:minor], patch: parse[:patch], pre_release: parse[:pre_release])
-	assert_equal('1', version.major, version.inspect)
-	assert_equal('2', version.minor, version.inspect)
-	assert_equal('3', version.patch, version.inspect)
-	assert_equal(nil, version.pre_release, version.inspect)
-	new_from_string = Version.new_from_string(Consecutive_string)
-	assert_equal('1', new_from_string.major, new_from_string.inspect)
-end # new_from_string
-def test_ruby_version
-	executable_suffix = ''
-	ruby_interpreter = Version.new(test_command: 'ruby', options: '--version')
-	parse = ruby_interpreter.attributes
-	assert_instance_of(Hash, parse)
-	assert_operator(parse[:major], :>=, '0')
-	assert_operator(parse[:minor], :>=, '0')
-	assert_operator(parse[:patch], :>=, '0')
-	assert_instance_of(String, parse[:pre_release])
-end # ruby_version
-def test_Version_Examples
-	assert_match(/[1-9]?[0-9]{1,3}/, '1.9.0')
-	assert_match(Version_digits, First_example_version_name)
-	parsed_version_string = First_example_version_name.parse(Semantic_version_regexp)
-	assert_equal({:major=>"1", :minor=>"9", :patch=>"0", :pre_release => nil}, parsed_version_string)
-	first_example_version = Version.new_from_string(First_example_version_name)
-end # Examples
+  def test_value_object
+    new_version = Version.new
+    assert_equal('0', new_version.major, new_version.inspect)
+    assert_equal('0', new_version.minor, new_version.inspect)
+    assert_equal('0', new_version.patch, new_version.inspect)
+    assert_equal('0', new_version.pre_release, new_version.inspect)
+    new_args = Version.new(major: '1', minor: '2', patch: '3', pre_release: '4')
+    assert_equal('1', new_args.major, new_args.inspect)
+    assert_equal('2', new_args.minor, new_args.inspect)
+    assert_equal('3', new_args.patch, new_args.inspect)
+    assert_equal('4', new_args.pre_release, new_args.inspect)
+  end # values
+
+  def test_new_from_string
+    string = '1.2.3'
+    parse = Consecutive_string.parse(Semantic_version_regexp)
+    assert_instance_of(Hash, parse)
+    assert_equal('1', parse[:major], parse.inspect)
+    assert_equal('2', parse[:minor], parse.inspect)
+    assert_equal('3', parse[:patch], parse.inspect)
+    assert_equal(nil, parse[:pre_release], parse.inspect)
+    new_args = Version.new(major: '1', minor: '2', patch: '3', pre_release: '4')
+    assert_equal('1', new_args.major, new_args.inspect)
+    assert_equal('2', new_args.minor, new_args.inspect)
+    assert_equal('3', new_args.patch, new_args.inspect)
+    assert_equal('4', new_args.pre_release, new_args.inspect)
+    version = Version.new(major: parse[:major], minor: parse[:minor], patch: parse[:patch], pre_release: parse[:pre_release])
+    assert_equal('1', version.major, version.inspect)
+    assert_equal('2', version.minor, version.inspect)
+    assert_equal('3', version.patch, version.inspect)
+    assert_equal(nil, version.pre_release, version.inspect)
+    new_from_string = Version.new_from_string(Consecutive_string)
+    assert_equal('1', new_from_string.major, new_from_string.inspect)
+  end # new_from_string
+
+  def test_ruby_version
+    executable_suffix = ''
+    ruby_interpreter = Version.new(test_command: 'ruby', options: '--version')
+    parse = ruby_interpreter.attributes
+    assert_instance_of(Hash, parse)
+    assert_operator(parse[:major], :>=, '0')
+    assert_operator(parse[:minor], :>=, '0')
+    assert_operator(parse[:patch], :>=, '0')
+    assert_instance_of(String, parse[:pre_release])
+  end # ruby_version
+
+  def test_Version_Examples
+    assert_match(/[1-9]?[0-9]{1,3}/, '1.9.0')
+    assert_match(Version_digits, First_example_version_name)
+    parsed_version_string = First_example_version_name.parse(Semantic_version_regexp)
+    assert_equal({ major: '1', minor: '9', patch: '0', pre_release: nil }, parsed_version_string)
+    first_example_version = Version.new_from_string(First_example_version_name)
+  end # Examples
 end # Version
