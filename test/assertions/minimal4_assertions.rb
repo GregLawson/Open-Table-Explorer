1d6c676bb734c7bb8e785c47dcb8d2625632435b
greg <GregLawson123@gmail.com>
2016-09-04 12:30:42 -0700

    fixup! minimal4

--- test/assertions/minimal4_assertions.rb
+++ test/assertions/minimal4_assertions.rb
@@ -1,5 +1,5 @@
 ###########################################################################
-#    Copyright (C) 2013-2014 by Greg Lawson                                      
+#    Copyright (C) 2012-2016 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
@@ -7,28 +7,61 @@
 ###########################################################################
 require_relative '../../test/assertions/ruby_assertions.rb'
 require_relative '../../app/models/minimal4.rb'
-require_relative '../../test/assertions.rb'
 class Minimal4
+require_relative '../../test/assertions.rb'
 module Assertions
-include Test::Unit::Assertions
 module ClassMethods
-include Test::Unit::Assertions
-def assert_pre_conditions(message='')
-	message+="In assert_pre_conditions, self=#{inspect}"
-end #assert_pre_conditions
-def assert_post_conditions(message='')
-	message+="In assert_post_conditions, self=#{inspect}"
-end #assert_post_conditions
-end #ClassMethods
-def assert_pre_conditions(message='')
-end #assert_pre_conditions
-def assert_post_conditions(message='')
-end #assert_post_conditions
+	def nested_scope_modules?
+		nested_constants = self.class.constants
+		message = ''
+		assert_includes(included_modules.map{|m| m.name}, :Assertions, message)
+		assert_equal([:Constants, :Assertions, :ClassMethods], Version.nested_scope_modules?)
+	end # nested_scopes
+	def assert_nested_scope_submodule(module_symbol, context = self, message='')
+		message+="\nIn assert_nested_scope_submodule for class #{context.name}, "
+		message += "make sure module Constants is nested in #{context.class.name.downcase} #{context.name}"
+		message += " but not in #{context.nested_scope_modules?.inspect}"
+		assert_includes(constants, :Contants, message)
+	end # assert_included_submodule
+	def assert_included_submodule(module_symbol, context = self, message='')
+		message+="\nIn assert_included_submodule for class #{self.name}, "
+		message += "make sure module Constants is nested in #{self.class.name.downcase} #{self.name}"
+		message += " but not in #{self.nested_scope_modules?.inspect}"
+		assert_includes(included_modules, :Contants, message)
+	end # assert_included_submodule
+	def asset_nested_and_included(module_symbol, context = self, message='')
+		assert_nested_scope_submodule(module_symbol)
+		assert_included_submodule(module_symbol)
+	end # asset_nested_and_included
+	def assert_pre_conditions(message='')
+		message+="In assert_pre_conditions, self=#{inspect}"
+	#	asset_nested_and_included(:ClassMethods, self)
+	#	asset_nested_and_included(:Constants, self)
+	#	asset_nested_and_included(:Assertions, self)
+		self
+	end #assert_pre_conditions
+
+	def assert_post_conditions(message='')
+		message+="In assert_post_conditions, self=#{inspect}"
+		self
+	end #assert_post_conditions
+	end #ClassMethods
+
+	def assert_pre_conditions(message='')
+		message+="In assert_pre_conditions, self=#{inspect}"
+		self
+	end #assert_pre_conditions
+
+	def assert_post_conditions(message='')
+		message+="In assert_post_conditions, self=#{inspect}"
+		self
+	end #assert_post_conditions
 end #Assertions
 include Assertions
 extend Assertions::ClassMethods
 #self.assert_pre_conditions
-module Examples
-include Constants
+  module Examples # usually constant objects of the type (easy to understand (perhaps impractical) examples for testing)
+    include DefinitionalConstants
+    include ReferenceObjects
 end #Examples
 end #Minimal
