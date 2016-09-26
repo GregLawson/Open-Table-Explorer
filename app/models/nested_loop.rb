e39d2932e774a02ec4a7904d7fa9343c68153edd
greg <GregLawson123@gmail.com>
2016-09-26 11:45:07 -0700

    fixup! nested_loop

--- app/models/nested_loop.rb
+++ app/models/nested_loop.rb
@@ -1,69 +1,81 @@
 ###########################################################################
-#    Copyright (C) 2013-2014 by Greg Lawson                                      
+#    Copyright (C) 2013-2016 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
-#require 'virtus'
-#require_relative '../../app/models/no_db.rb'
+require 'rom' # how differs from rom-sql
+require 'rom-sql' # conflicts with rom-csv and rom-rom
+#require 'rom-relation' # conflicts with rom-csv and rom-rom
+require 'rom-repository' # conflicts with rom-csv and rom-rom
+require 'dry-types'
+require_relative '../../app/models/ruby_interpreter.rb'
+require_relative '../../app/models/branch.rb'
+require_relative '../../app/models/test_executable.rb'
+
+module Types
+	include Dry::Types.module
+end # Types
+
 class NestedLoop
-module Constants
-Loops = [RubyVersion, Unit, Branch, TestRun] # Verbosity
-end # Constants
-include Constants
-module ClassMethods
-def next
-	Loops.map do |dimension_class|
-		if dimension.methods.includes?(:enumerator) then
-			set_instance_variable(index_name, enumerator.next)
-		end # if
-	end # map
-end # next
-end # ClassMethods
-extend ClassMethods
-attr_reader :dimension, :enumerator, :next
-def initialize(dimension)
-	@dimension = dimension
-end # initialize
-def index_name
-			@dimension.name.model_name?
-end # index_name
-def next
+module DefinitionalConstants # constant parameters of the type (suggest all CAPS)
+		Loops = [RubyVersion, Unit, Branch, TestExecutable] # Verbosity
+end # DefinitionalConstants
+include DefinitionalConstants
+	
+  module DefinitionalClassMethods
+	def next
+		Loops.map do |dimension_class|
+			if dimension.methods.includes?(:enumerator) then
+				set_instance_variable(index_name, enumerator.next)
+			end # if
+		end # map
+	end # next
+  end # DefinitionalClassMethods
+  extend DefinitionalClassMethods
+	
+	def initialize(dimension)
+		@dimension = dimension
+	end # initialize
+	def index_name
+				@dimension.name.model_name?
+	end # index_name
+	def next
 		if @dimension.methods.includes?(:enumerator) then
 			@enumerator = @dimension.enumerator
 			@next = @enumerator.next
 		else
 			@enumerator = nil
 			@next = nil
-		end # map
-end # next
-#require_relative '../../app/models/assertions.rb'
-module Assertions
-module ClassMethods
-def assert_pre_conditions(message='')
-#	asset_nested_and_included(:ClassMethods, self)
-#	asset_nested_and_included(:Constants, self)
-#	asset_nested_and_included(:Assertions, self)
-end #assert_pre_conditions
-def assert_post_conditions(message='')
-	message+="In assert_post_conditions, self=#{inspect}"
-	self
-end #assert_post_conditions
-end #ClassMethods
-def assert_pre_conditions(message='')
-	message+="In assert_pre_conditions, self=#{inspect}"
-	self
-end #assert_pre_conditions
-def assert_post_conditions(message='')
-	message+="In assert_post_conditions, self=#{inspect}"
-	self
-end #assert_post_conditions
-end # Assertions
-include Assertions
-extend Assertions::ClassMethods
-#self.assert_pre_conditions
-module Examples
-include Constants
-end # Examples
+		end # if
+	end # next
+	#require_relative '../../app/models/assertions.rb'
+	module Assertions
+	module ClassMethods
+	def assert_pre_conditions(message='')
+	#	asset_nested_and_included(:ClassMethods, self)
+	#	asset_nested_and_included(:Constants, self)
+	#	asset_nested_and_included(:Assertions, self)
+	end #assert_pre_conditions
+	def assert_post_conditions(message='')
+		message+="In assert_post_conditions, self=#{inspect}"
+		self
+	end #assert_post_conditions
+	end #ClassMethods
+	def assert_pre_conditions(message='')
+		message+="In assert_pre_conditions, self=#{inspect}"
+		self
+	end #assert_pre_conditions
+	def assert_post_conditions(message='')
+		message+="In assert_post_conditions, self=#{inspect}"
+		self
+	end #assert_post_conditions
+	end # Assertions
+	include Assertions
+	extend Assertions::ClassMethods
+	#self.assert_pre_conditions
+  module Examples # usually constant objects of the type (easy to understand (perhaps impractical) examples for testing)
+    include DefinitionalConstants
+	end # Examples
 end # NestedLoop
--- test/unit/nested_loop_test.rb
+++ test/unit/nested_loop_test.rb
@@ -1,29 +1,31 @@
 ###########################################################################
-#    Copyright (C) 2014 by Greg Lawson                                      
+#    Copyright (C) 2014-2016 by Greg Lawson                                      
 #    <GregLawson123@gmail.com>                                                             
 #
 # Copyright: See COPYING file that comes with this distribution
 #
 ###########################################################################
 require_relative 'test_environment'
-require_relative '../../app/models/minimal2.rb'
+require_relative '../../app/models/nested_loop.rb'
 class NestedLoopTest < TestCase
 #include DefaultTests
-include RailsishRubyUnit::Executable.model_class?::Examples
-def test_Constants
-	Loops.map do |dimension|
-		if dimension.methods.includes?(:enumerator) then
-			index_name = dimension.name.model_name?
-			set_instance_variable(index_name, enumerator.next
-		end # if
-	end # map
-end # Constants
+	include RailsishRubyUnit::Executable.model_class?::Examples
+  def test_NestedLoop_DefinitionalConstants
+		Loops.map do |dimension|
+			if dimension.methods.include?(:enumerator) then
+				index_name = dimension.name.model_name?
+				set_instance_variable(index_name, enumerator.next)
+			end # if
+		end # map
+  end # DefinitionalConstants
+
 def test_NestedLoop_next
 end # next
-def initialize(dimension)
-end # initialize
+
+
 def index_name
 end # index_name
+
 def test_NestedLoop_next
 end # next
 end # NestedLoop
