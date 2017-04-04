a58c48796621b5327d4b96daf83590f58246a6d6
greg <GregLawson123@gmail.com>
2017-04-04 06:33:31 -0700

    push2github

---
+++ script/push2github.sh
@@ -0,0 +1,6 @@
+git checkout passed+interactive && git pull github passed+interactive && git push github
+git checkout passed && git pull github passed && git push github
+git checkout tested+interactive && git pull github tested+interactive && git push github
+git checkout tested && git pull github tested && git push github
+git checkout edited+interactive && git pull github edited+interactive && git push github
+git checkout edited && git pull github edited && git push github
