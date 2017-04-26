0417c06d5ed0e6a3fbc8ff899d1ac5173efb8e37
greg <GregLawson123@gmail.com>
2017-04-26 09:30:55 -0700

    fixup! partition

--- test/unit/partition_test.rb
+++ test/unit/partition_test.rb
@@ -1,5 +1,5 @@
 ###########################################################################
-#    Copyright (C) 2012-2014 by Greg Lawson
+#    Copyright (C) 2012-2017 by Greg Lawson
 #    <GregLawson123@gmail.com>
 #
 # Copyright: See COPYING file that comes with this distribution
@@ -9,9 +9,10 @@ require_relative 'test_environment'
 require_relative '../../app/models/partition.rb'
 class PartitionTest < TestCase
   # include DefaultTests
-  include RailsishRubyUnit::Executable.model_class?::Examples
+  #  include RailsishRubyUnit::Executable.model_class?::Examples
   def test_initialize
     partition_run = ShellCommands.new('cat /proc/partitions').assert_pre_conditions
     partition_run.output
+    diskstats_run = ShellCommands.new('cat /proc/diskstats').assert_pre_conditions
   end # initialize
 end # Partition
