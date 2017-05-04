fa35b69f3f8f39d1c900414bba23bea3b7a2ddf8
greg <GregLawson123@gmail.com>
2017-05-04 08:30:16 -0700

    fixup! ruby_assertions_test_unit

--- test/unit/ruby_assertions_test_unit_test.rb
+++ test/unit/ruby_assertions_test_unit_test.rb
@@ -7,9 +7,12 @@
 ###########################################################################
 require_relative '../../app/models/test_environment_test_unit.rb'
 require_relative '../assertions/ruby_assertions_test_unit.rb'
-class RubyAssertionsTest < TestCase
+
+#require_relative 'ruby_assertions_common_test.rb'
+
+class RubyAssertionsTestUnitTest < TestCase
 include RubyAssertions
-  def test_missing_file_message
+def test_missing_file_message
     missing_pathname = '/root-kit/bad_stuff/exploit.sh'
     existing_data_file = '~/.gem'
     assert_empty(missing_file_message(existing_data_file))
@@ -32,7 +35,21 @@ include RubyAssertions
     assert_raise(AssertionFailedError) { assert_pathname_exists(bad_pathname) }
   end # assert_pathname_exists
 
+	def test_assert_raises
+		assert_raises(RuntimeError) { fail 'assertion fail class name?' }
+		assert_raises(AssertionFailedError) { refute_equal(1, 1) }
+		assert_raises(AssertionFailedError) { assert_pathname_exists('/catfish') }
+	end # assert_raises
+
 	def test_assert_raise
 		assert_raise(AssertionFailedError) { assert_pathname_exists('/catfish') }
 	end # assert_raise
+
+	def test_refute_raise
+#		refute_raise(AssertionFailedError) { fail 'assertion fail class name?' }
+	end # assert_raise
+
+	def test_refute_raises
+#		refute_raises(AssertionFailedError) { fail 'assertion fail class name?' }
+	end # assert_raises
 end # RubyAssertions
