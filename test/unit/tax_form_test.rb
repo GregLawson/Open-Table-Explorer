eb4fc2168ed83d17af9c73c5120f8a9c517f6c0a
greg <GregLawson123@gmail.com>
2017-05-30 07:42:51 -0700

    fixup! tax_form

--- test/unit/tax_form_test.rb
+++ test/unit/tax_form_test.rb
@@ -11,9 +11,6 @@ require_relative '../../app/models/tax_form.rb'
 require_relative '../../app/models/branch.rb'
 require_relative '../assertions/ruby_assertions_test_unit.rb'
 
-		class TestClass
-		end # TestClass
-
 class FinanceTest < TestCase
   include RubyAssertions
   # include DefaultTests
@@ -30,45 +27,18 @@ class FinanceTest < TestCase
  [{{:clone=>:hash}=>true},
   {{:clone=>:inspect}=>true},
   {{:clone=>:object_id}=>false}]]
-		assert_equal(hidden_clone_answer, 'a'.clone_state)
 		assert_equal(hidden_clone_answer, CA_current_year.clone_state)
-#!		assert_equal(hidden_clone_answer, TestClass.new.freeze.clone_state)
-#!		assert_equal(hidden_clone_answer, TestClass.new.clone_state)
 	end # clone_state
 	
 	def test_assert_clone_state
-		'a'.assert_clone_state
 		CA_current_year.assert_clone_state
-		TestClass.new.freeze.assert_clone_state
-		TestClass.new.assert_clone_state
-		US1040_user.assert_clone_state
 	end # assert_clone_state
 
-	def test_clone_explain
-		assert_equal('', 'a'.clone_explain)
-	end # clone_explain
-	
 	def test_cache
-#		object = TestClass.new.freeze
 		object = CA_current_year
-#!		object = object.clone
 		cache_name = :test
 		assert_raise(RuntimeError) { object.cache(:test)}
-#!		block = 
-		cache_const_name = ('Cached_' + cache_name.to_s).to_sym
-		assert_equal(:Cached_test, cache_const_name)
-		refute(object.class.const_defined?(cache_const_name))
-		ret = 2
-		refute(object.class.const_defined?(cache_const_name))
-		object.class.const_set(cache_const_name, ret)
-		assert(object.class.const_defined?(cache_const_name))
-		assert_equal(2, object.class.const_get(cache_const_name))
-		refute_nil(object.cache(:test){|| 2}, object.inspect)
-		assert_equal(2, object.cache(:test){|| 2})
-		assert_equal(2, object.class.const_get(cache_const_name), object.class.const_get(cache_const_name))
 	end # cache
-
-
 
   def test_Finance_DefinitionalConstants
     refute_empty(OpenTaxSolver_directories, OpenTaxSolver_directories_glob)
