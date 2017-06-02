0ceaebeaaff148e77a91d2f183eaadc9633f18b0
greg <GregLawson123@gmail.com>
2017-06-02 04:05:33 -0700

    fixup! cache

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -15,115 +15,114 @@ require_relative '../../app/models/repository.rb'
 require_relative '../../app/models/parse.rb'
 require 'dry-types'
 module Types
-	include Dry::Types.module
+  include Dry::Types.module
 end # Types
-#! require 'virtus'
+# ! require 'virtus'
 
 module Cache
-	
-	def cache_const_name(cache_name)
-		if self.class == Class
-			('Cached_' + cache_name.to_s).to_sym
-		else
-			('@cached_' + cache_name.to_s).to_sym
-		end # if
-	end # cache_const_name
-	
-	def cached?(cache_name)
-		if self.class == Class
-			self.class.const_defined?(cache_const_name(cache_name))
-		else
-			self.instance_variable_defined?(cache_const_name(cache_name))
-		end # if
-	end # cached?
-	
-	def cache(cache_name = __callee__, &block)
-		unless block_given?
-			raise 'caching ' + cache_name.to_s + 'requires a block.'
-		else
-			if self.class == Class
-				if cached?(cache_name)
-					self.class.const_get(cache_const_name(cache_name))
-				else
-					ret = block.call
-					self.class.const_set(cache_const_name(cache_name), ret)
-				end # if
-			else
-				if cached?(cache_name)
-					self.instance_variable_get(cache_const_name(cache_name))
-				else
-					ret = block.call
-					self.instance_variable_set(cache_const_name(cache_name), ret)
-				end # if
-			end # if 
-		end # unless
-	end # cache
+  def cache_const_name(cache_name)
+    if self.class == Class
+      ('Cached_' + cache_name.to_s).to_sym
+    else
+      ('@cached_' + cache_name.to_s).to_sym
+    end # if
+  end # cache_const_name
 
-	def explain_cache(cache_name)
-		if cached?(cache_name)
-			if self.class == Class
-				cache_name.inspect + ' is cached as constant ' + self.class.name + '::' + cache_const_name(cache_name).to_s + ' = ' + self.class.const_get(cache_const_name(cache_name)).inspect
-			else
-				cache_name.inspect + ' is cached as instance variable '  + cache_const_name(cache_name).inspect + ' = ' + instance_variable_get(cache_const_name(cache_name)).inspect
-			end # if
-		else
-			cache_name.inspect + ' is not cached in ' + self.class.constants.inspect
-			if self.class == Class
-				cache_name.inspect + ' is not cached as constant ' + self.class.name + '::' + cache_const_name(cache_name).to_s + ' in ' + self.class.constants.inspect
-			else
-				cache_name.inspect + ' is not cached as instance variable '  + cache_const_name(cache_name).inspect + ' in ' + instance_variables.inspect
-			end # if
-		end # if
-	end # explain_cache
-		
-	def clear_cache(cache_name)
-		self.class.const_set(cache_const_name(cache_name), nil)
-	end # clear_cache
-	
-	def assert_cached(cache_name)
-		assert(cached?(cache_name), explain_cache(cache_name))
-	end # assert_cached
-	
-	def refute_cached(cache_name)
-		refute(cached?(cache_name), explain_cache(cache_name))
-	end # refute_cached
+  def cached?(cache_name)
+    if self.class == Class
+      self.class.const_defined?(cache_const_name(cache_name))
+    else
+      instance_variable_defined?(cache_const_name(cache_name))
+    end # if
+  end # cached?
+
+  def cache(cache_name = __callee__)
+    if block_given?
+      if self.class == Class
+        if cached?(cache_name)
+          self.class.const_get(cache_const_name(cache_name))
+        else
+          ret = yield
+          self.class.const_set(cache_const_name(cache_name), ret)
+        end # if
+      else
+        if cached?(cache_name)
+          instance_variable_get(cache_const_name(cache_name))
+        else
+          ret = yield
+          instance_variable_set(cache_const_name(cache_name), ret)
+        end # if
+      end # if
+    else
+      raise 'caching ' + cache_name.to_s + 'requires a block.'
+    end # unless
+  end # cache
+
+  def explain_cache(cache_name)
+    if cached?(cache_name)
+      if self.class == Class
+        cache_name.inspect + ' is cached as constant ' + self.class.name + '::' + cache_const_name(cache_name).to_s + ' = ' + self.class.const_get(cache_const_name(cache_name)).inspect
+      else
+        cache_name.inspect + ' is cached as instance variable ' + cache_const_name(cache_name).inspect + ' = ' + instance_variable_get(cache_const_name(cache_name)).inspect
+      end # if
+    else
+      cache_name.inspect + ' is not cached in ' + self.class.constants.inspect
+      if self.class == Class
+        cache_name.inspect + ' is not cached as constant ' + self.class.name + '::' + cache_const_name(cache_name).to_s + ' in ' + self.class.constants.inspect
+      else
+        cache_name.inspect + ' is not cached as instance variable ' + cache_const_name(cache_name).inspect + ' in ' + instance_variables.inspect
+      end # if
+    end # if
+  end # explain_cache
+
+  def clear_cache(cache_name)
+    self.class.const_set(cache_const_name(cache_name), nil)
+  end # clear_cache
+
+  def assert_cached(cache_name)
+    assert(cached?(cache_name), explain_cache(cache_name))
+  end # assert_cached
+
+  def refute_cached(cache_name)
+    refute(cached?(cache_name), explain_cache(cache_name) + "\n" + inspect)
+  end # refute_cached
 end # Cache
 
 class Object
-	def clone_state
-#		ret = {}
-		[ :dup, :clone ].map do |copy_method|
-			mutable_object = send(copy_method)
-			[ :hash , :inspect, :object_id].map do |attribute|
-				{ {copy_method => attribute} => send(attribute) == mutable_object.send(attribute) }
-			end # each
-		end # each
-	end # clone_state
-	
-	def assert_clone_state
-		dup_object = dup
-		clone_object = clone
-		# dup copies taint
-		# clone copies internal stste, dup creates new object
-		unless frozen? # test unit modifies object
-			assert_equal(tainted?, clone_object.tainted?, clone_state.inspect)
-			assert_equal(tainted?, dup_object.tainted?, clone_state.inspect)
-			assert_equal(frozen?, dup_object.frozen?, clone_state.inspect)
-			refute_equal(object_id, dup_object.object_id, dup_object.inspect)
-			refute_equal(object_id, clone_object.object_id, clone_state.inspect)
-#!			assert_equal(self, clone_object, clone_object.clone_explain)
-#!			assert_equal(self, dup_object, dup_object.clone_explain)
-		end # unless
-	end # assert_clone_state
+  def clone_state
+    #		ret = {}
+    [:dup, :clone].map do |copy_method|
+      mutable_object = send(copy_method)
+      [:hash, :inspect, :object_id].map do |attribute|
+        { { copy_method => attribute } => send(attribute) == mutable_object.send(attribute) }
+      end # each
+    end # each
+  end # clone_state
 
-	def clone_explain
-		ret = []
-		dup_object = dup
-		clone_object = clone
-		ret << 'clone hash not equal' if hash != clone_object.hash
-		ret << 'dup hash not equal' if hash != dup_object.hash
-		ret << 'dup inspect not equal' if inspect != dup_object.inspect
-		ret << 'clone inspect not equal' if inspect != clone_object.inspect
-		ret.join(', ')
-	end # clone_explain
+  def assert_clone_state
+    dup_object = dup
+    clone_object = clone
+    # dup copies taint
+    # clone copies internal stste, dup creates new object
+    unless frozen? # test unit modifies object
+      assert_equal(tainted?, clone_object.tainted?, clone_state.inspect)
+      assert_equal(tainted?, dup_object.tainted?, clone_state.inspect)
+      assert_equal(frozen?, dup_object.frozen?, clone_state.inspect)
+      refute_equal(object_id, dup_object.object_id, dup_object.inspect)
+      refute_equal(object_id, clone_object.object_id, clone_state.inspect)
+      # !			assert_equal(self, clone_object, clone_object.clone_explain)
+      # !			assert_equal(self, dup_object, dup_object.clone_explain)
+    end # unless
+  end # assert_clone_state
+
+  def clone_explain
+    ret = []
+    dup_object = dup
+    clone_object = clone
+    ret << 'clone hash not equal' if hash != clone_object.hash
+    ret << 'dup hash not equal' if hash != dup_object.hash
+    ret << 'dup inspect not equal' if inspect != dup_object.inspect
+    ret << 'clone inspect not equal' if inspect != clone_object.inspect
+    ret.join(', ')
+  end # clone_explain
 end # Object
--- test/unit/cache_test.rb
+++ test/unit/cache_test.rb
@@ -10,107 +10,105 @@ require_relative '../../app/models/test_environment_test_unit.rb'
 require_relative '../../app/models/cache.rb'
 require_relative '../assertions/ruby_assertions_test_unit.rb'
 
-		class TestClass
-			include Cache
-			extend Cache
-			def make_instance_variables
-				@a = 1
-				instance_variable_set(:@cached_test, 2)
-				self
-			end # make_instance_variables
-		end # TestClass
+class TestClass
+  include Cache
+  extend Cache
+  def make_instance_variables
+    @a = 1
+    instance_variable_set(:@cached_test, 2)
+    self
+  end # make_instance_variables
+    end # TestClass
 
 class CacheTest < TestCase
   include RubyAssertions
 
-	
-	def test_cache_const_name
-		assert_equal([:@cached_test, :@a], TestClass.new.make_instance_variables.instance_variables)
-	end # cache_const_name
-	
-	def test_cached?
-		object = TestClass.new
-		cache_name = :test
-		refute(object.cached?(cache_name), object.explain_cache(cache_name))
-		assert_equal(2, object.cache(cache_name){|| 2})
-		assert(object.cached?(cache_name), object.explain_cache(cache_name))
-	end # cached?
-	
-	def test_cache
-		object = TestClass.new
-#		object = CA_current_year
-#!		object = object.clone
-		cache_name = :test
-		assert_raise(RuntimeError) { object.cache(:test)}
-#!		block = 
-		cache_const_name = ('Cached_' + cache_name.to_s).to_sym
-		assert_equal(:Cached_test, cache_const_name)
-		refute(object.class.const_defined?(cache_const_name))
-		ret = 2
-		refute(object.class.const_defined?(cache_const_name))
-		object.class.const_set(cache_const_name, ret)
-		assert(object.class.const_defined?(cache_const_name))
-		assert_equal(2, object.class.const_get(cache_const_name))
-		refute_nil(object.cache(:test){|| 2}, object.inspect)
-		assert_equal(2, object.cache(:test){|| 2})
-		assert_equal(2, object.cache(:test){|| 3})
-		assert_equal(2, object.cache{|| 2})
-		assert_equal(2, object.class.const_get(cache_const_name), object.class.const_get(cache_const_name))
-	end # cache
+  def test_cache_const_name
+    assert_equal([:@cached_test, :@a], TestClass.new.make_instance_variables.instance_variables)
+  end # cache_const_name
 
-	def test_explain_cache
-		object = TestClass.new
-		cache_name = :test
-		assert_equal(':test is not cached as instance variable :@cached_test in []', object.explain_cache(cache_name))
-		assert_equal(2, object.cache(cache_name){|| 2})
-		assert_equal(':test is cached as instance variable :@cached_test = 2', object.explain_cache(cache_name))
-		assert_equal(':junk is not cached as instance variable :@cached_junk in [:@cached_test]', object.explain_cache(:junk))
+  def test_cached?
+    object = TestClass.new
+    cache_name = :test
+    refute(object.cached?(cache_name), object.explain_cache(cache_name))
+    assert_equal(2, object.cache(cache_name) { || 2 })
+    assert(object.cached?(cache_name), object.explain_cache(cache_name))
+  end # cached?
 
-		object = TestClass
-		assert_equal(':test is not cached as constant Class::Cached_test in [:DelegationError, :RUBY_RESERVED_WORDS, :Concerning]', object.explain_cache(cache_name))
-	end # explain_cache
-		
-	def test_clear_cache
-	end # clear_cache
-	
-	def test_assert_cached
-		object = TestClass.new
-		cache_name = :test
-		assert_equal(2, object.cache(cache_name){|| 2})
-		object.assert_cached(cache_name)
-	end # assert_cached
-	
-	def test_refute_cached
-		object = TestClass.new
-		cache_name = :test
-		object.refute_cached(cache_name)
-	end # refute_cached
-	
+  def test_cache
+    object = TestClass.new
+    #		object = CA_current_year
+    # !		object = object.clone
+    cache_name = :test
+    assert_raise(RuntimeError) { object.cache(:test) }
+    # !		block =
+    cache_const_name = ('Cached_' + cache_name.to_s).to_sym
+    assert_equal(:Cached_test, cache_const_name)
+    refute(object.class.const_defined?(cache_const_name))
+    ret = 2
+    refute(object.class.const_defined?(cache_const_name))
+    object.class.const_set(cache_const_name, ret)
+    assert(object.class.const_defined?(cache_const_name))
+    assert_equal(2, object.class.const_get(cache_const_name))
+    refute_nil(object.cache(:test) { || 2 }, object.inspect)
+    assert_equal(2, object.cache(:test) { || 2 })
+    assert_equal(2, object.cache(:test) { || 3 })
+    assert_equal(2, object.cache { || 2 })
+    assert_equal(2, object.class.const_get(cache_const_name), object.class.const_get(cache_const_name))
+  end # cache
+
+  def test_explain_cache
+    object = TestClass.new
+    cache_name = :test
+    assert_equal(':test is not cached as instance variable :@cached_test in []', object.explain_cache(cache_name))
+    assert_equal(2, object.cache(cache_name) { || 2 })
+    assert_equal(':test is cached as instance variable :@cached_test = 2', object.explain_cache(cache_name))
+    assert_equal(':junk is not cached as instance variable :@cached_junk in [:@cached_test]', object.explain_cache(:junk))
+
+    object = TestClass
+    assert_equal(':test is not cached as constant Class::Cached_test in [:DelegationError, :RUBY_RESERVED_WORDS, :Concerning]', object.explain_cache(cache_name))
+  end # explain_cache
+
+  def test_clear_cache
+  end # clear_cache
+
+  def test_assert_cached
+    object = TestClass.new
+    cache_name = :test
+    assert_equal(2, object.cache(cache_name) { || 2 })
+    object.assert_cached(cache_name)
+  end # assert_cached
+
+  def test_refute_cached
+    object = TestClass.new
+    cache_name = :test
+    object.refute_cached(cache_name)
+  end # refute_cached
 end # Cache
 
 class CloneTest < TestCase
-	def test_clone_state
-		hidden_clone_answer = [[{{:dup=>:hash}=>true},
-  {{:dup=>:inspect}=>true},
-  {{:dup=>:object_id}=>false}],
- [{{:clone=>:hash}=>true},
-  {{:clone=>:inspect}=>true},
-  {{:clone=>:object_id}=>false}]]
-		assert_equal(hidden_clone_answer, 'a'.clone_state)
-#		assert_equal(hidden_clone_answer, CA_current_year.clone_state)
-#!		assert_equal(hidden_clone_answer, TestClass.new.freeze.clone_state)
-#!		assert_equal(hidden_clone_answer, TestClass.new.clone_state)
-	end # clone_state
-	
-	def test_assert_clone_state
-		'a'.assert_clone_state
-#		CA_current_year.assert_clone_state
-		TestClass.new.freeze.assert_clone_state
-		TestClass.new.assert_clone_state
-#		US1040_user.assert_clone_state
-	end # assert_clone_state
+  def test_clone_state
+    hidden_clone_answer = [[{ { dup: :hash } => true },
+                            { { dup: :inspect } => true },
+                            { { dup: :object_id } => false }],
+                           [{ { clone: :hash } => true },
+                            { { clone: :inspect } => true },
+                            { { clone: :object_id } => false }]]
+    assert_equal(hidden_clone_answer, 'a'.clone_state)
+    #		assert_equal(hidden_clone_answer, CA_current_year.clone_state)
+    # !		assert_equal(hidden_clone_answer, TestClass.new.freeze.clone_state)
+    # !		assert_equal(hidden_clone_answer, TestClass.new.clone_state)
+  end # clone_state
 
-	def test_clone_explain
-		assert_equal('', 'a'.clone_explain)
-	end # clone_explain
+  def test_assert_clone_state
+    'a'.assert_clone_state
+    #		CA_current_year.assert_clone_state
+    TestClass.new.freeze.assert_clone_state
+    TestClass.new.assert_clone_state
+    #		US1040_user.assert_clone_state
+  end # assert_clone_state
+
+  def test_clone_explain
+    assert_equal('', 'a'.clone_explain)
+  end # clone_explain
 end # Clone
