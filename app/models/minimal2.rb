cd6017d584ea74f4f6e67bef947b811c05692095
greg <GregLawson123@gmail.com>
2017-06-09 09:02:03 -0700

    fixup! minimal2

--- app/models/minimal2.rb
+++ app/models/minimal2.rb
@@ -7,11 +7,11 @@
 ###########################################################################
 require 'rom' # how differs from rom-sql
 require 'rom-sql' # conflicts with rom-csv and rom-rom
-#require 'rom-relation' # conflicts with rom-csv and rom-rom
+# require 'rom-relation' # conflicts with rom-csv and rom-rom
 require 'rom-repository' # conflicts with rom-csv and rom-rom
 require 'dry-types'
 module Types
-	include Dry::Types.module
+  include Dry::Types.module
 end # Types
 
 class Minimal2 < Dry::Types::Value
@@ -19,76 +19,75 @@ class Minimal2 < Dry::Types::Value
   end # DefinitionalClassMethods
   extend DefinitionalClassMethods
 
-	module DefinitionalConstants # constant parameters in definition of the type (suggest all CAPS)
-	end # DefinitionalConstants
-	include DefinitionalConstants
-	
+  module DefinitionalConstants # constant parameters in definition of the type (suggest all CAPS)
+  end # DefinitionalConstants
+  include DefinitionalConstants
+
   module DefinitionalClassMethods # if reference DefinitionalConstants
     include DefinitionalConstants
   end # DefinitionalClassMethods
   extend DefinitionalClassMethods
 
-#    attribute :name, Types::Strict::Symbol | Types::Strict::String
-#		attribute :data_regexp, Types::Coercible::String
-#		attribute :ruby_conversion, Types::Strict::String.optional
+  #    attribute :name, Types::Strict::Symbol | Types::Strict::String
+  #		attribute :data_regexp, Types::Coercible::String
+  #		attribute :ruby_conversion, Types::Strict::String.optional
 
   module Constructors # such as alternative new methods
     include DefinitionalConstants
   end # Constructors
   extend Constructors
-	
+
   module ReferenceObjects # example constant objects of the type (e.g. default_objects)
     include DefinitionalConstants
-		Minimal_object = Minimal2.new
+    Minimal_object = Minimal2.new
   end # ReferenceObjects
   include ReferenceObjects
-	
-require_relative '../../app/models/assertions.rb'
 
-	module Assertions
+  require_relative '../../app/models/assertions.rb'
+
+  module Assertions
     module ClassMethods
+      def assert_nested_scope_submodule(module_symbol, context = self, message = '')
+        message += "\nIn assert_nested_scope_submodule for class #{context.name}, "
+        message += "make sure module Constants is nested in #{context.class.name.downcase} #{context.name}"
+        message += " but not in #{context.nested_scope_module_names}"
+        assert_includes(nested_scope_module_names, module_symbol)
+      end # assert_included_submodule
 
-			def assert_nested_scope_submodule(module_symbol, context = self, message='')
-				message+="\nIn assert_nested_scope_submodule for class #{context.name}, "
-				message += "make sure module Constants is nested in #{context.class.name.downcase} #{context.name}"
-				message += " but not in #{context.nested_scope_module_names}"
-				assert_includes(nested_scope_module_names, module_symbol)
-			end # assert_included_submodule
-			
-			def assert_included_submodule(module_symbol, context = self, message='')
-				message+="\nIn assert_included_submodule for class #{self.name}, "
-				message += "make sure module Constants is nested in #{self.class.name.downcase} #{self.name}"
-				message += " but not in #{self.nested_scope_module_names}"
-				assert_includes(included_modules.map(&:module_name), module_symbol)
-			end # assert_included_submodule
-			
-			def asset_nested_and_included(module_symbol, context = self, message='')
-				assert_nested_scope_submodule(module_symbol)
-				assert_included_submodule(module_symbol)
-			end # asset_nested_and_included
-			
-			def assert_pre_conditions(message='')
-				message+="In assert_pre_conditions, self=#{inspect}"
-			#	asset_nested_and_included(:ClassMethods, self)
-			#	asset_nested_and_included(:Constants, self)
-			#	asset_nested_and_included(:Assertions, self)
-				self
-			end #assert_pre_conditions
+      def assert_included_submodule(module_symbol, _context = self, message = '')
+        message += "\nIn assert_included_submodule for class #{name}, "
+        message += "make sure module Constants is nested in #{self.class.name.downcase} #{name}"
+        message += " but not in #{nested_scope_module_names}"
+        assert_includes(included_modules.map(&:module_name), module_symbol)
+      end # assert_included_submodule
 
-			def assert_post_conditions(message='')
-				message+="In assert_post_conditions, self=#{inspect}"
-				self
-			end #assert_post_conditions
-		end #ClassMethods
+      def assert_nested_and_included(module_symbol, _context = self, _message = '')
+        assert_nested_scope_submodule(module_symbol)
+        assert_included_submodule(module_symbol)
+      end # assert_nested_and_included
+
+      def assert_pre_conditions(message = '')
+        message += "In assert_pre_conditions, self=#{inspect}"
+        #	assert_nested_and_included(:ClassMethods, self)
+        #	assert_nested_and_included(:Constants, self)
+        #	assert_nested_and_included(:Assertions, self)
+        self
+      end # assert_pre_conditions
+
+      def assert_post_conditions(message = '')
+        message += "In assert_post_conditions, self=#{inspect}"
+        self
+      end # assert_post_conditions
+     end # ClassMethods
 
     def assert_pre_conditions(message = '')
       message += "In assert_pre_conditions, self=#{inspect}"
-				self # return for command chaining
+      self # return for command chaining
     end # assert_pre_conditions
 
     def assert_post_conditions(message = '')
       message += "In assert_post_conditions, self=#{inspect}"
-				self # return for command chaining
+      self # return for command chaining
     end # assert_post_conditions
   end # Assertions
   include Assertions
--- test/unit/minimal2_test.rb
+++ test/unit/minimal2_test.rb
@@ -7,130 +7,131 @@
 ###########################################################################
 require_relative 'test_environment'
 require_relative '../../app/models/minimal2.rb'
+require_relative '../../app/models/unit.rb'
 class Minimal2Test < TestCase
-    include Minimal2::ReferenceObjects
-  # include DefaultTests
-  #  include RailsishRubyUnit::Executable.model_class?::Examples
+  include RailsishRubyUnit::Executable.model_class?::DefinitionalConstants
+  include RailsishRubyUnit::Executable.model_class?::ReferenceObjects
   module Examples
   end #  Examples
   include Examples
+
+  def setup
+    # !    @temp_repo = Repository.create_test_repository
+  end # setup
+
+  def teardown
+    # !    Repository.delete_even_nonxisting(@temp_repo.path)
+    #    assert_empty(Dir[Cleanup_failed_test_paths], Cleanup_failed_test_paths)
+  end # teardown
 
   # rubocop:disable Style/MethodName
   def test_Minimal_DefinitionalConstants
   end # DefinitionalConstants
 
+  def test_included_module_names
+    this_class = RailsishRubyUnit::Executable.model_class?
+    # !		assert_includes(this_class.included_module_names, (this_class.name + '::DefinitionalClassMethods').to_sym)
+    assert_includes(this_class.included_module_names, (this_class.name + '::DefinitionalConstants').to_sym)
+    # !		assert_includes(this_class.included_module_names, (this_class.name + '::Constructors').to_sym)
+    assert_includes(this_class.included_module_names, (this_class.name + '::ReferenceObjects').to_sym)
+    assert_includes(this_class.included_module_names, (this_class.name + '::Assertions').to_sym)
+  end # included_module_names
 
+  def test_nested_scope_modules
+    this_class = RailsishRubyUnit::Executable.model_class?
+    assert_includes(this_class.constants, :DefinitionalClassMethods)
+    assert_includes(this_class.constants, :DefinitionalConstants)
+    assert_includes(this_class.constants, :Constructors)
+    assert_includes(this_class.constants, :ReferenceObjects)
+    assert_includes(this_class.constants, :Assertions)
+    nested_constants = this_class.constants.map do |m|
+      trial_eval = eval(this_class.name.to_s + '::' + m.to_s)
+      if trial_eval.is_a?(Module)
+        trial_eval
+      end # if
+    end.compact # map
 
+    assert_includes(nested_constants, this_class::DefinitionalClassMethods)
+    assert_includes(nested_constants, this_class::DefinitionalConstants)
+    assert_includes(nested_constants, this_class::Constructors)
+    assert_includes(nested_constants, this_class::ReferenceObjects)
+    assert_includes(nested_constants, this_class::Assertions)
 
+    assert_includes(this_class.nested_scope_modules, this_class::DefinitionalClassMethods)
+    assert_includes(this_class.nested_scope_modules, this_class::DefinitionalConstants)
+    assert_includes(this_class.nested_scope_modules, this_class::Constructors)
+    assert_includes(this_class.nested_scope_modules, this_class::ReferenceObjects)
+    assert_includes(this_class.nested_scope_modules, this_class::Assertions)
+    assert_equal(this_class::ClassInterface, Dry::Types::Struct::ClassInterface)
+    # !		refute_includes(this_class.nested_scope_modules, Dry::Types::Struct::ClassInterface)
+  end # nested_scope_modules
 
+  def test_nested_scope_module_names
+    this_class = RailsishRubyUnit::Executable.model_class?
+    assert_includes(this_class.nested_scope_module_names, (this_class.name + '::DefinitionalClassMethods').to_sym)
+    assert_includes(this_class.nested_scope_module_names, (this_class.name + '::DefinitionalConstants').to_sym)
+    assert_includes(this_class.nested_scope_module_names, (this_class.name + '::Constructors').to_sym)
+    assert_includes(this_class.nested_scope_module_names, (this_class.name + '::ReferenceObjects').to_sym)
+    assert_includes(this_class.nested_scope_module_names, (this_class.name + '::Assertions').to_sym)
+    assert_includes(this_class.constants, :Minimal_object)
+  end # nested_scope_module_names
 
+  def test_assert_nested_scope_submodule
+    this_class = RailsishRubyUnit::Executable.model_class?
+    this_class.assert_nested_scope_submodule((this_class.name + '::DefinitionalClassMethods').to_sym)
+    this_class.assert_nested_scope_submodule((this_class.name + '::DefinitionalConstants').to_sym)
+    this_class.assert_nested_scope_submodule((this_class.name + '::Constructors').to_sym)
+    this_class.assert_nested_scope_submodule((this_class.name + '::ReferenceObjects').to_sym)
+    this_class.assert_nested_scope_submodule((this_class.name + '::Assertions').to_sym)
+    end # assert_included_submodule
 
-	def test_included_module_names
-		this_class = Minimal2
-#!		assert_includes(this_class.included_module_names, (this_class.name + '::DefinitionalClassMethods').to_sym)
-		assert_includes(this_class.included_module_names, (this_class.name + '::DefinitionalConstants').to_sym)
-#!		assert_includes(this_class.included_module_names, (this_class.name + '::Constructors').to_sym)
-		assert_includes(this_class.included_module_names, (this_class.name + '::ReferenceObjects').to_sym)
-		assert_includes(this_class.included_module_names, (this_class.name + '::Assertions').to_sym)
-	end # included_module_names
-	
-	def test_nested_scope_modules
-		this_class = Minimal2
-		assert_includes(this_class.constants, :DefinitionalClassMethods)
-		assert_includes(this_class.constants, :DefinitionalConstants)
-		assert_includes(this_class.constants, :Constructors)
-		assert_includes(this_class.constants, :ReferenceObjects)
-		assert_includes(this_class.constants, :Assertions)
-				nested_constants = this_class.constants.map do |m|
-					trial_eval = eval(this_class.name.to_s + '::' + m.to_s)
-					if trial_eval.kind_of?(Module)
-						trial_eval
-					else
-						nil
-					end # if
-				end.compact # map
+  def test_assert_included_submodule
+    this_class = RailsishRubyUnit::Executable.model_class?
+    # !class				this_class.assert_included_submodule((this_class.name + '::DefinitionalClassMethods').to_sym)
+    this_class.assert_included_submodule((this_class.name + '::DefinitionalConstants').to_sym)
+    # !class				this_class.assert_included_submodule((this_class.name + '::Constructors').to_sym)
+    this_class.assert_included_submodule((this_class.name + '::ReferenceObjects').to_sym)
+    this_class.assert_included_submodule((this_class.name + '::Assertions').to_sym)
+    end # assert_included_submodule
 
-		assert_includes(nested_constants, this_class::DefinitionalClassMethods)
-		assert_includes(nested_constants, this_class::DefinitionalConstants)
-		assert_includes(nested_constants, this_class::Constructors)
-		assert_includes(nested_constants, this_class::ReferenceObjects)
-		assert_includes(nested_constants, this_class::Assertions)
+  def test_assert_nested_and_included
+    this_class = RailsishRubyUnit::Executable.model_class?
+    # !class				this_class.assert_nested_and_included((this_class.name + '::DefinitionalClassMethods').to_sym)
+    this_class.assert_nested_and_included((this_class.name + '::DefinitionalConstants').to_sym)
+    # !class				this_class.assert_nested_and_included((this_class.name + '::Constructors').to_sym)
+    this_class.assert_nested_and_included((this_class.name + '::ReferenceObjects').to_sym)
+    this_class.assert_nested_and_included((this_class.name + '::Assertions').to_sym)
+    end # assert_nested_and_included
 
-		assert_includes(this_class.nested_scope_modules, this_class::DefinitionalClassMethods)
-		assert_includes(this_class.nested_scope_modules, this_class::DefinitionalConstants)
-		assert_includes(this_class.nested_scope_modules, this_class::Constructors)
-		assert_includes(this_class.nested_scope_modules, this_class::ReferenceObjects)
-		assert_includes(this_class.nested_scope_modules, this_class::Assertions)
-		assert_equal(this_class::ClassInterface, Dry::Types::Struct::ClassInterface)
-#!		refute_includes(this_class.nested_scope_modules, Dry::Types::Struct::ClassInterface)
-	end # nested_scope_modules
-			
-	def test_nested_scope_module_names
-		this_class = Minimal2
-		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::DefinitionalClassMethods').to_sym)
-		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::DefinitionalConstants').to_sym)
-		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::Constructors').to_sym)
-		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::ReferenceObjects').to_sym)
-		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::Assertions').to_sym)
-		assert_includes(this_class.constants, :Minimal_object)
-	end # nested_scope_module_names
-			
-			def test_assert_nested_scope_submodule
-				this_class = Minimal2
-				this_class.assert_nested_scope_submodule((this_class.name + '::DefinitionalClassMethods').to_sym)
-				this_class.assert_nested_scope_submodule((this_class.name + '::DefinitionalConstants').to_sym)
-				this_class.assert_nested_scope_submodule((this_class.name + '::Constructors').to_sym)
-				this_class.assert_nested_scope_submodule((this_class.name + '::ReferenceObjects').to_sym)
-				this_class.assert_nested_scope_submodule((this_class.name + '::Assertions').to_sym)
-			end # assert_included_submodule
-			
-			def test_assert_included_submodule
-				this_class = Minimal2
-#!class				this_class.assert_included_submodule((this_class.name + '::DefinitionalClassMethods').to_sym)
-				this_class.assert_included_submodule((this_class.name + '::DefinitionalConstants').to_sym)
-#!class				this_class.assert_included_submodule((this_class.name + '::Constructors').to_sym)
-				this_class.assert_included_submodule((this_class.name + '::ReferenceObjects').to_sym)
-				this_class.assert_included_submodule((this_class.name + '::Assertions').to_sym)
-			end # assert_included_submodule
-			
-			def test_asset_nested_and_included
-				this_class = Minimal2
-#!class				this_class.asset_nested_and_included((this_class.name + '::DefinitionalClassMethods').to_sym)
-				this_class.asset_nested_and_included((this_class.name + '::DefinitionalConstants').to_sym)
-#!class				this_class.asset_nested_and_included((this_class.name + '::Constructors').to_sym)
-				this_class.asset_nested_and_included((this_class.name + '::ReferenceObjects').to_sym)
-				this_class.asset_nested_and_included((this_class.name + '::Assertions').to_sym)
-			end # asset_nested_and_included
-			
   def test_Minimal_assert_pre_conditions
-		this_class = Minimal2
-		this_class.assert_pre_conditions
-		message = ''
-		my_style_modules = [this_class::Assertions, this_class::ReferenceObjects, this_class::DefinitionalConstants]
-		my_style_module_names = my_style_modules.map{|m| m.name.to_sym}
-		assert_includes(my_style_module_names, (this_class.name + '::ReferenceObjects').to_sym, message)
-		assert_includes(my_style_module_names, (this_class.name + '::DefinitionalConstants').to_sym, message)
-		assert_includes(my_style_module_names, (this_class.name + '::Assertions').to_sym, message)
+    this_class = RailsishRubyUnit::Executable.model_class?
+    this_class.assert_pre_conditions
+    message = ''
+    my_style_modules = [this_class::Assertions, this_class::ReferenceObjects, this_class::DefinitionalConstants]
+    my_style_module_names = my_style_modules.map { |m| m.name.to_sym }
+    assert_includes(my_style_module_names, (this_class.name + '::ReferenceObjects').to_sym, message)
+    assert_includes(my_style_module_names, (this_class.name + '::DefinitionalConstants').to_sym, message)
+    assert_includes(my_style_module_names, (this_class.name + '::Assertions').to_sym, message)
 
-		super_class = this_class.superclass
-		superclass_modules = super_class.included_modules
-		superclass_module_names = super_class.included_modules.map(&:module_name)
-		message = ''
-		assert_includes(super_class.included_modules.map(&:module_name), :'Dry::Equalizer::Methods', message)
-		assert_includes(super_class.included_modules.map(&:module_name), :'JSON::Ext::Generator::GeneratorMethods::Object', message)
-#! ruby 2.4		assert_includes(Module.used_modules.map(&:module_name), :'JSON::Ext::Generator::GeneratorMethods::Object', message)
+    super_class = this_class.superclass
+    superclass_modules = super_class.included_modules
+    superclass_module_names = super_class.included_modules.map(&:module_name)
+    message = ''
+    assert_includes(super_class.included_modules.map(&:module_name), :'Dry::Equalizer::Methods', message)
+    assert_includes(super_class.included_modules.map(&:module_name), :'JSON::Ext::Generator::GeneratorMethods::Object', message)
+    # ! ruby 2.4		assert_includes(Module.used_modules.map(&:module_name), :'JSON::Ext::Generator::GeneratorMethods::Object', message)
   end # assert_pre_conditions
 
   def test_Minimal_assert_post_conditions
-		Minimal2.assert_pre_conditions
+    RailsishRubyUnit::Executable.model_class?.assert_pre_conditions
   end # assert_post_conditions
 
   def test_assert_pre_conditions
-		Minimal_object.assert_pre_conditions
+    Minimal_object.assert_pre_conditions
   end # assert_pre_conditions
 
   def test_assert_post_conditions
-		Minimal_object.assert_post_conditions
+    Minimal_object.assert_post_conditions
   end # assert_post_conditions
 
   def test_Minimal_Examples
