db89f46d9baac97d32f1661da019b4c43f7c516f
greg <GregLawson123@gmail.com>
2017-06-10 02:55:03 -0700

    fixup! cache

--- test/unit/cache_test.rb
+++ test/unit/cache_test.rb
@@ -8,7 +8,9 @@
 require_relative '../../app/models/test_environment_test_unit.rb'
 # require_relative 'test_environment'
 require_relative '../../app/models/cache.rb'
+require_relative '../../app/models/unit.rb'
 require_relative '../assertions/ruby_assertions_test_unit.rb'
+require 'benchmark'
 
 class TestClass
   include Cache
@@ -78,6 +80,21 @@ class CacheClassTest < TestCase
   def test_clear_cache
   end # clear_cache
 
+	def test_timing
+
+		Unit.refute_cached(:all_basenames)
+		cache_miss = Benchmark.measure { Unit.all_basenames }
+		message = 'cache_miss = ' + cache_miss.inspect
+		Unit.assert_cached(:all_basenames, message)
+		cache_hit = Benchmark.measure { Unit.all_basenames }
+		message += ', cache_hit = ' + cache_hit.inspect
+		assert_operator(cache_miss.real, :>, Benchmark.measure { Unit::Cached_all_basenames }.real, message)
+		assert_operator(cache_miss.real, :>, cache_hit.real, message)
+		message += "\n" + 'all_basenames = ' + Benchmark.measure { Unit.all_basenames }.inspect
+		message += "\n" + 'all = ' + Benchmark.measure { Unit.all }.inspect
+		puts message
+	end # 
+	
   def test_assert_cached
     TestClass.refute_cached(:cache, TestClass.constants.inspect)
     TestClass.make_class_cache
@@ -153,6 +170,102 @@ class CacheTest < TestCase
     cache_name = :test
     object.refute_cached(cache_name)
   end # refute_cached
+
+	def test_included_module_names
+		this_class = TestClass
+#		assert_includes(this_class.included_module_names, (this_class.name + '::ClassMethods').to_sym)
+#		assert_includes(this_class.included_module_names, (this_class.name + '::DefinitionalConstants').to_sym)
+#		assert_includes(this_class.included_module_names, (this_class.name + '::Constructors').to_sym)
+#		assert_includes(this_class.included_module_names, (this_class.name + '::ReferenceObjects').to_sym)
+#!		assert_includes(this_class.included_module_names, (this_class.name + '::Assertions').to_sym)
+	end # included_module_names
+	
+	def test_nested_scope_modules
+		this_class = TestClass
+		assert_includes(this_class.constants, :ClassMethods)
+#!		assert_includes(this_class.constants, :DefinitionalConstants)
+#!		assert_includes(this_class.constants, :Constructors)
+#!		assert_includes(this_class.constants, :ReferenceObjects)
+		assert_includes(this_class.constants, :Assertions)
+				nested_constants = this_class.constants.map do |m|
+					trial_eval = eval(this_class.name.to_s + '::' + m.to_s)
+					if trial_eval.kind_of?(Module)
+						trial_eval
+					else
+						nil
+					end # if
+				end.compact # map
+
+		assert_includes(nested_constants, this_class::ClassMethods)
+#!		assert_includes(nested_constants, this_class::DefinitionalConstants)
+#!		assert_includes(nested_constants, this_class::Constructors)
+#!		assert_includes(nested_constants, this_class::ReferenceObjects)
+		assert_includes(nested_constants, this_class::Assertions)
+
+		assert_includes(this_class.nested_scope_modules, this_class::ClassMethods)
+#!		assert_includes(this_class.nested_scope_modules, this_class::DefinitionalConstants)
+#!		assert_includes(this_class.nested_scope_modules, this_class::Constructors)
+#!		assert_includes(this_class.nested_scope_modules, this_class::ReferenceObjects)
+		assert_includes(this_class.nested_scope_modules, this_class::Assertions)
+#!		assert_equal(this_class::ClassInterface, Dry::Types::Struct::ClassInterface)
+#!		refute_includes(this_class.nested_scope_modules, Dry::Types::Struct::ClassInterface)
+	end # nested_scope_modules
+			
+	def test_nested_scope_module_names
+		this_class = TestClass
+#!		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::ClassMethods').to_sym)
+#!		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::DefinitionalConstants').to_sym)
+#!		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::Constructors').to_sym)
+#!		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::ReferenceObjects').to_sym)
+#!		assert_includes(this_class.nested_scope_module_names, (this_class.name + '::Assertions').to_sym)
+#!		assert_includes(this_class.constants, :Minimal_object)
+	end # nested_scope_module_names
+			
+			def test_assert_nested_scope_submodule
+				this_class = TestClass
+#!				this_class.assert_nested_scope_submodule((this_class.name + '::ClassMethods').to_sym)
+#				this_class.assert_nested_scope_submodule((this_class.name + '::DefinitionalConstants').to_sym)
+#				this_class.assert_nested_scope_submodule((this_class.name + '::Constructors').to_sym)
+#				this_class.assert_nested_scope_submodule((this_class.name + '::ReferenceObjects').to_sym)
+#!				this_class.assert_nested_scope_submodule((this_class.name + '::Assertions').to_sym)
+			end # assert_included_submodule
+			
+			def test_assert_included_submodule
+				this_class = TestClass
+#!class				this_class.assert_included_submodule((this_class.name + '::ClassMethods').to_sym)
+#				this_class.assert_included_submodule((this_class.name + '::DefinitionalConstants').to_sym)
+#!class				this_class.assert_included_submodule((this_class.name + '::Constructors').to_sym)
+#				this_class.assert_included_submodule((this_class.name + '::ReferenceObjects').to_sym)
+#!				this_class.assert_included_submodule((this_class.name + '::Assertions').to_sym)
+			end # assert_included_submodule
+			
+			def test_assert_nested_and_included
+				this_class = TestClass
+#!class				this_class.assert_nested_and_included((this_class.name + '::ClassMethods').to_sym)
+#				this_class.assert_nested_and_included((this_class.name + '::DefinitionalConstants').to_sym)
+#!class				this_class.assert_nested_and_included((this_class.name + '::Constructors').to_sym)
+#				this_class.assert_nested_and_included((this_class.name + '::ReferenceObjects').to_sym)
+#!				this_class.assert_nested_and_included((this_class.name + '::Assertions').to_sym)
+			end # assert_nested_and_included
+			
+  def test_Minimal_assert_pre_conditions
+		this_class = TestClass
+#		this_class.assert_pre_conditions
+		message = ''
+#		my_style_modules = [this_class::Assertions, this_class::ReferenceObjects, this_class::DefinitionalConstants]
+#!		my_style_module_names = my_style_modules.map{|m| m.name.to_sym}
+#		assert_includes(my_style_module_names, (this_class.name + '::ReferenceObjects').to_sym, message)
+#		assert_includes(my_style_module_names, (this_class.name + '::DefinitionalConstants').to_sym, message)
+#!		assert_includes(my_style_module_names, (this_class.name + '::Assertions').to_sym, message)
+
+		super_class = this_class.superclass
+		superclass_modules = super_class.included_modules
+		superclass_module_names = super_class.included_modules.map(&:module_name)
+		message = ''
+#		assert_includes(super_class.included_modules.map(&:module_name), :'Dry::Equalizer::Methods', message)
+		assert_includes(super_class.included_modules.map(&:module_name), :'JSON::Ext::Generator::GeneratorMethods::Object', message)
+#! ruby 2.4		assert_includes(Module.used_modules.map(&:module_name), :'JSON::Ext::Generator::GeneratorMethods::Object', message)
+  end # assert_pre_conditions
 end # Cache
 
 class CloneTest < TestCase
